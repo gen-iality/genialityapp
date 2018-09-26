7c9617b17d9d5c8a610c9981dfd7e152f25416cb
Alejandro <alejomg27@gmail.com>
2018-09-26 08:51:10 -0500

    Fix warning log

--- src/components/modal/addUser.jsx
+++ src/components/modal/addUser.jsx
@@ -148,9 +148,9 @@ class AddUser extends Component {
                 <div className="modal-background"/>
                 <div className="modal-card">
                     <header className="modal-card-head">
-                        <p className="modal-card-title">
+                        <div className="modal-card-title">
                             <div className="icon-header" dangerouslySetInnerHTML={{ __html: icon }}/>
-                        </p>
+                        </div>
                         <button className="delete" aria-label="close" onClick={this.props.handleModal}/>
                     </header>
                     <section className="modal-card-body">
--- src/components/modal/importUser.jsx
+++ src/components/modal/importUser.jsx
@@ -107,9 +107,9 @@ class ImportUsers extends Component {
                 <div className="modal-background"/>
                 <div className="modal-card">
                     <header className="modal-card-head">
-                        <p className="modal-card-title">
+                        <div className="modal-card-title">
                             <div className="icon-header" dangerouslySetInnerHTML={{ __html: icon }}/>
-                        </p>
+                        </div>
                         <button className="delete" aria-label="close" onClick={this.closeModal}/>
                     </header>
                     <section className="modal-card-body">
