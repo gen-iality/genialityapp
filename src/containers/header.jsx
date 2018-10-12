9d6522ff4ba223e9a1e5f6d160082b4ee75b8b8e
Alejandro <alejomg27@gmail.com>
2018-10-12 15:44:28 -0500

    Header showed on mobile

--- src/containers/header.jsx
+++ src/containers/header.jsx
@@ -57,6 +57,12 @@ class Header extends Component {
         window.location.replace(`${AuthUrl}/logout`);
     };
 
+    openMenu = () => {
+        this.setState((prevState) => {
+            return {open:!prevState.open}
+        });
+    };
+
     render() {
         const { timeout } = this.state;
         const icon = '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"\n' +
