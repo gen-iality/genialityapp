2d7fd4409d7372e556a65cef2b0886d58c790834
Alejandro <alejomg27@gmail.com>
2018-10-15 10:19:03 -0500

    Init with form user. TODO: Define inputs

--- src/components/profiles/userEdit.jsx
+++ src/components/profiles/userEdit.jsx
@@ -4,6 +4,8 @@ import {CategoriesApi, EventsApi} from "../../helpers/request";
 import Loading from "../loaders/loading";
 import EventCard from "../shared/eventCard";
 import LogOut from "../shared/logOut";
+import ImageInput from "../shared/imageInput";
+import {TiArrowLoopOutline} from "react-icons/ti";
 
 class UserEditProfile extends Component {
     constructor(props) {
@@ -11,6 +13,7 @@ class UserEditProfile extends Component {
         this.state = {
             selectedOption: [],
             events: [],
+            user: {},
             loading: true
         };
     }
@@ -41,15 +44,53 @@ class UserEditProfile extends Component {
         }
     }
 
+    handleChange = (e) => {
+        const {name, value} = e.target;
+        this.setState({org:{...this.state.org,[name]:value}},this.valid)
+    };
+
     render() {
-        const { loading, timeout, events } = this.state;
+        const { loading, timeout, events, user } = this.state;
         return (
             <section className="section">
                 {
                     loading ? <Loading/> :
                         <div className="container org-profile">
                             <div className="columns">
-                                Perfil Usuario
+                                <div className="column is-3">
+                                    <ImageInput picture={user.picture} imageFile={this.state.imageFile}
+                                                divClass={'circle-img'}
+                                                content={<div style={{backgroundImage: `url(${user.picture})`}}
+                                                              className="avatar-img"/>}
+                                                classDrop={'change-img is-size-2'}
+                                                contentDrop={<TiArrowLoopOutline className="has-text-white"/>}
+                                                contentZone={<figure className="image is-128x128">
+                                                    <img className="is-rounded"
+                                                         src="https://bulma.io/images/placeholders/128x128.png"/>
+                                                </figure>} style={{}}
+                                                changeImg={this.changeImg}/>
+                                    <div className="field">
+                                        <label className="label">Nombre</label>
+                                        <div className="control">
+                                            <input className="input" name={"name"} type="text"
+                                                   placeholder="Text input" value={user.name}
+                                                   onChange={this.handleChange}
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="field">
+                                        <label className="label">Correo</label>
+                                        <div className="control">
+                                            <input className="input" name={"email"} type="email"
+                                                   placeholder="Text input" value={user.email}
+                                                   onChange={this.handleChange}
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="column is-9 user-data">
+                                    <h1 className="title has-text-primary">Datos</h1>
+                                </div>
                             </div>
                             <div>
                                 <h2>Eventos:</h2>
