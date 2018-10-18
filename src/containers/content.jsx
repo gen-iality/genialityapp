ccddcc03c01ebee2350726e7d7c56ecc975f0afd
Alejandro <alejomg27@gmail.com>
2018-10-18 08:59:11 -0500

    Trying to exacting path

--- src/containers/content.jsx
+++ src/containers/content.jsx
@@ -17,7 +17,7 @@ class ContentContainer extends Component {
             <main className="main">
                 <Route exact path="/" component={ Home } />
                 <Route exact path="/page/:id" component={ HomeProfile } />
-                <Route path="/landing/:event" component={ Landing }/>
+                <Route exact path="/landing/:event" component={ Landing }/>
                 <PrivateRoute path="/my_events" component={ Events }/>
                 <PrivateRoute path="/event/:event" component={ Event }/>
                 <PrivateRoute path="/profile/:id" component={ MyProfile }/>
