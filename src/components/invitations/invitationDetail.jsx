48448121599765f14a23e1a12b271872f06881a9
Alejandro <alejomg27@gmail.com>
2018-10-22 10:07:53 -0500

    Refactor template

--- src/components/invitations/invitationDetail.jsx
+++ src/components/invitations/invitationDetail.jsx
@@ -22,32 +22,30 @@ class InvitationDetail extends Component {
             <EmailPrev event={this.props.event} item={item}/>
         ];
         return (
-            <section className="modal-card-body">
-                <div className="content has-text-centered">
-                    <nav className="breadcrumb" aria-label="breadcrumbs">
-                        <ul>
-                            <li onClick={this.close}>
-                                <a>
-                                    <span className="icon is-small">
-                                        <i className="far fa-arrow-alt-circle-left"/>
-                                    </span>
-                                </a>
-                            </li>
-                            <li className={`${this.state.step === 0 ? "is-active" : ""}`}
-                                onClick={(e)=>{this.setState({step:0})}}>
-                                <a>Users</a>
-                            </li>
-                            <li className={`${this.state.step === 1 ? "is-active" : ""}`}
-                                onClick={(e)=>{this.setState({step:1})}}>
-                                <a>Email</a>
-                            </li>
-                        </ul>
-                    </nav>
-                    {
-                        layout[this.state.step]
-                    }
-                </div>
-            </section>
+            <React.Fragment>
+                <nav className="breadcrumb" aria-label="breadcrumbs">
+                    <ul>
+                        <li onClick={this.close}>
+                            <a>
+                                <span className="icon is-small">
+                                    <i className="far fa-arrow-alt-circle-left"/>
+                                </span>
+                            </a>
+                        </li>
+                        <li className={`${this.state.step === 0 ? "is-active" : ""}`}
+                            onClick={(e)=>{this.setState({step:0})}}>
+                            <a>Users</a>
+                        </li>
+                        <li className={`${this.state.step === 1 ? "is-active" : ""}`}
+                            onClick={(e)=>{this.setState({step:1})}}>
+                            <a>Email</a>
+                        </li>
+                    </ul>
+                </nav>
+                {
+                    layout[this.state.step]
+                }
+            </React.Fragment>
         );
     }
 }
