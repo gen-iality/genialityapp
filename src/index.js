553435db0344c2a67a12f9e34fbc91b8973fde2b
Alejandro <alejomg27@gmail.com>
2018-11-14 13:35:33 -0500

    Save the day (?)

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom';
 import  './styles/main.css';
 import App from './App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 //index.html loading old files because of service worker after build
 //https://github.com/facebook/create-react-app/issues/2715
-unregister();
 import {addLocaleData, IntlProvider,} from 'react-intl';
 //Import local files from intl
 import locale_en from 'react-intl/locale-data/en';
 import locale_es from 'react-intl/locale-data/es';
 import localeData from "./helpers/locale.json";
 
+unregister();
 addLocaleData([...locale_en, ...locale_es]);
 // Define user's language.
 const language = (navigator.languages && navigator.languages[0]) || navigator.language;
