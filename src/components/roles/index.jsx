2da08df1d2e3bfa08fcc199c7d16aea94dac98de
Alejandro <alejomg27@gmail.com>
2018-12-11 15:07:30 -0500

    Added modal logic and binding form

--- src/components/roles/index.jsx
+++ src/components/roles/index.jsx
@@ -3,24 +3,50 @@ import SearchComponent from "../shared/searchTable";
 import Loading from "../loaders/loading";
 import Pagination from "../shared/pagination";
 import ErrorServe from "../modal/serverError";
-import {firestore} from "../../helpers/firebase";
+import {icon} from "../../helpers/constants";
+import connect from "react-redux/es/connect/connect";
 
 class AdminRol extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            user:       {name:'',email:'',rol:''},
             users:      [],
             userReq:    [],
             pageOfItems:[],
             total:      0,
             checkIn:    0,
             estados:    {DRAFT:0,BOOKED:0,RESERVED:0,INVITED:0},
-            extraFields:[]
+            extraFields:[],
+            message:    {},
+            modal:      false
         };
     }
 
+    componentDidMount(){
+        console.log('PROPS ',this.props);
+    }
+
+    handleModal = () => {
+        const html = document.querySelector("html");
+        this.setState((prevState)=>{
+            !prevState.modal ? html.classList.add('is-clipped') : html.classList.remove('is-clipped');
+            return {modal:!prevState.modal}})
+    };
+
+    onChange = (e) => {
+        const {value,name} = e.target;
+        this.setState({user:{...this.state.user,[name]:value}}, this.validForm);
+    };
+
+    handleSubmit = () => {
+        const {user} = this.state;
+        console.log(user);
+    };
+
     render() {
-        const {timeout, userReq, users, total, checkIn, extraFields, estados} = this.state;
+        const {timeout, userReq, users, total, extraFields, estados, modal, user} = this.state;
+        const {rolstate:{roles}} = this.props;
         return (
             <React.Fragment>
                 <div className="checkin">
@@ -34,43 +60,14 @@ class AdminRol extends Component {
                         </div>
                         <div className="column">
                             <div className="columns is-mobile is-multiline is-centered">
-                                {
-                                    userReq.length>0 && (
-                                        <div className="column is-narrow has-text-centered">
-                                            <button className="button" onClick={this.exportFile}>
-                                                <span className="icon">
-                                                    <i className="fas fa-download"/>
-                                                </span>
-                                                <span>Exportar</span>
-                                            </button>
-                                        </div>
-                                    )
-                                }
                                 <div className="column is-narrow has-text-centered">
-                                    <button className="button is-inverted" onClick={this.checkModal}>Leer Código QR</button>
-                                </div>
-                                <div className="column is-narrow has-text-centered">
-                                    <button className="button is-primary" onClick={this.addUser}>Agregar Usuario +</button>
+                                    <button className="button is-primary" onClick={this.handleModal}>Agregar Usuario +</button>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className="checkin-tags-wrapper">
+                    {/*<div className="checkin-tags-wrapper">
                         <div className="columns is-mobile is-multiline checkin-tags">
-                            <div className="column is-narrow">
-                                <div className="tags is-centered">
-                                    <span className="tag is-primary">{checkIn}</span>
-                                    <span className="tag is-white">Check In</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="columns is-mobile is-multiline checkin-tags">
-                            <div className="column is-narrow">
-                                <div className="tags is-centered">
-                                    <span className="tag is-light">{total}</span>
-                                    <span className="tag is-white">Total</span>
-                                </div>
-                            </div>
                             {
                                 Object.keys(estados).map(item=>{
                                     return <div className="column is-narrow" key={item}>
@@ -118,11 +115,62 @@ class AdminRol extends Component {
                                             />
                                         </React.Fragment>
                                     }
-                                </div>}
+                                </div>
+                            }
                         </div>
-                    </div>
-                    <div className="checkin-warning">
-                        <p className="is-size-7 has-text-right has-text-centered-mobile">Se muestran los primeros 50 usuarios, para verlos todos porfavor descargar el excel o realizar una búsqueda.</p>
+                    </div>*/}
+                    <p>Usuarios</p>
+                </div>
+                <div className={`modal modal-add-user ${modal ? "is-active" : ""}`}>
+                    <div className="modal-background"/>
+                    <div className="modal-card">
+                        <header className="modal-card-head">
+                            <div className="modal-card-title">
+                                <div className="icon-header" dangerouslySetInnerHTML={{ __html: icon }}/>
+                            </div>
+                            <button className="delete" aria-label="close" onClick={this.handleModal}/>
+                        </header>
+                        <section className="modal-card-body">
+                            <div className="field">
+                                <label className={`label has-text-grey-light is-capitalized required`}>Nombre</label>
+                                <div className="control">
+                                    <input className="input" type='text' name='name' value={user.name} onChange={this.onChange}/>
+                                </div>
+                            </div>
+                            <div className="field">
+                                <label className={`label has-text-grey-light is-capitalized required`}>Correo</label>
+                                <div className="control">
+                                    <input className="input" type='text' name='email' value={user.email} onChange={this.onChange}/>
+                                </div>
+                            </div>
+                            <div className="field">
+                                <label className="label">Rol</label>
+                                <div className="control">
+                                    <div className="select">
+                                        <select value={user.rol} onChange={this.onChange} name={'rol'}>
+                                            <option value={''}>Seleccione...</option>
+                                            {
+                                                roles.map((item,key)=>{
+                                                    return <option key={key} value={item.value}>{item.label}</option>
+                                                })
+                                            }
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                        <footer className="modal-card-foot">
+                            {
+                                this.state.create?<div>Creando...</div>:
+                                    <div className="modal-buttons">
+                                        <button className="button is-primary" onClick={this.handleSubmit} disabled={this.state.valid}>{this.state.edit?'Guardar':'Crear'}</button>
+                                        <button className="button" onClick={this.handleModal}>Cancel</button>
+                                    </div>
+                            }
+                            <div className={"msg"}>
+                                <p className={`help ${this.state.message.class}`}>{this.state.message.content}</p>
+                            </div>
+                        </footer>
                     </div>
                 </div>
                 {timeout&&(<ErrorServe/>)}
@@ -131,4 +179,10 @@ class AdminRol extends Component {
     }
 }
 
-export default AdminRol;
+const mapStateToProps = state => ({
+    rolstate: state.rolstate.items,
+    loading: state.rolstate.loading,
+    error: state.rolstate.error
+});
+
+export default connect(mapStateToProps)(AdminRol);
