1cfac97f1b575fd908cc76c3f74ec996efcf5985
Alejandro Gómez <man_alejo@hotmail.com>
2019-01-08 18:34:08 -0500

    Added function to organizations event module

--- src/components/organization/events.jsx
+++ src/components/organization/events.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import EventCard from "../shared/eventCard";
 import {Link} from "react-router-dom";
 import {OrganizationApi} from "../../helpers/request";
+import LoadingEvent from "../loaders/loadevent";
 
 class OrgEvents extends Component {
     constructor(props) {
@@ -27,23 +28,23 @@ class OrgEvents extends Component {
                         <small className="is-italic has-text-grey-light has-text-weight-300">Tus</small><br/>
                         <span className="has-text-grey-dark is-size-3">Eventos</span>
                     </h2>
-                    <div className="columns home is-multiline is-mobile">
-                        {
-                            events.map((event,key)=>{
-                                return  <EventCard event={event} key={event._id} action={''} size={'column is-one-third'} right={
-                                    <div className="edit">
-                                        <Link className="button-edit has-text-grey-light" to={`/event/${event._id}`}>
-                                                                    <span className="icon is-medium">
-                                                                        <i className="fas fa-lg fa-pencil-alt"/>
-                                                                    </span>
-                                            <span className="is-size-7 is-italic">Editar</span>
-                                        </Link>
-                                    </div>
+                    {
+                        loading ? <LoadingEvent/>:
+                            <div className="columns home is-multiline is-mobile">
+                                {
+                                    events.map((event,key)=>{
+                                        return  <EventCard event={event} key={event._id} action={''} size={'column is-one-third'} right={<div className="edit">
+                                            <Link className="button-edit has-text-grey-light" to={`/event/${event._id}`}>
+                                                                <span className="icon is-medium">
+                                                                    <i className="fas fa-lg fa-pencil-alt"/>
+                                                                </span>
+                                                <span className="is-size-7 is-italic">Editar</span>
+                                            </Link>
+                                        </div>}/>
+                                    })
                                 }
-                                />
-                            })
-                        }
-                    </div>
+                            </div>
+                    }
                 </div>
             </div>
         );
