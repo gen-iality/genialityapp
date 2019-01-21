9d2c137e5460c65c2634617c59873c0adfbe5cdd
david rodriguez <david.rodriguez@mocionsoft.com>
2019-01-21 15:50:36 -0500

    se agrego el archivo de configuracion

--- src/components/shared/crud/containers/index.jsx
+++ src/components/shared/crud/containers/index.jsx
@@ -1,14 +1,19 @@
 import React, {Component} from 'react';
+import configCrud from '../../../../configComponents/config-crud'
 
 
 class ContainerCrud extends Component {
  
-
+    constructor(props){
+        super(props)
+        console.log('holajlkdsjfk', configCrud.speakers.name)
+    }
     render() {
         return(
             <React.Fragment>
                 <div>
                     hola insecto
+               
                 </div>
                </React.Fragment>
         )
---
+++ src/configComponents/config-crud.js
@@ -0,0 +1,22 @@
+const configCrud = {
+
+    speakers: {
+        //Configuracion de las urls para realizar las peticiones para realiazar las peticiones 
+        urls: {
+            get: 'youo',
+            post: 'jdlj',
+            update: 'kfjdj',
+            delete: 'delete'
+        },
+        ListCrud: {
+
+        },
+        UpdateAndCreate: {
+
+        }
+    }
+
+
+
+}
+export default configCrud;
