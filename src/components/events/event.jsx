d51476f6e17759a5c24d4fc48c34cf806a4a33ea
Andres Pedraza <andres.pedraza@mocionsoft.com>
2019-01-22 10:41:32 -0500

    finally showing modal for create speakers

--- src/components/events/event.jsx
+++ src/components/events/event.jsx
@@ -235,11 +235,11 @@ class Event extends Component {
                                             }
                                             {
                                                 permissions.items.includes(rolPermissions.admin_staff._id) &&
-                                                    <Route path={`${match.url}/crud/speakers/`} render={(props) =><ContainerCrud idModel='speakers'/>}/>
+                                                    <Route path={`${match.url}/crud/speakers/`} render={(props) => <ContainerCrud idModel="speakers" eventId={this.state.event} />}/>
                                             }
                                             {
                                                 permissions.items.includes(rolPermissions.admin_staff._id) &&
-                                                    <Route path={`${match.url}/crud/programme`} render={() => <ContainerCrud idModel="programme"/>}/>
+                                                    <Route path={`${match.url}/crud/programme`} render={() => <ContainerCrud idModel="programme" eventId={this.state.event} />}/>
                                             }
                                             <Route path={`${match.url}/dashboard`} render={()=><DashboardEvent eventId={this.state.event._id} />}/>
                                             <Route path={`${match.url}/orders`} render={()=><OrdersEvent eventId={this.state.event._id}/>}/>
--- src/components/shared/crud/containers/index.jsx
+++ src/components/shared/crud/containers/index.jsx
@@ -1,17 +1,44 @@
 import React, {Component} from 'react';
+import ModalCrud from "../../../shared/crud/containers/modalCrud";
 
 
 class ContainerCrud extends Component {
     constructor(props){
         super(props);
+        this.state = {
+            show: false,
+            addUser: false,
+            modal: false,
+            fields: []
+        };
     }
 
+    componentDidMount() {
+        // console.log("here all info", this.props.eventId);
+    }
+    
+
+    showModal = () => {
+        this.setState(prevState => {
+            return {modal: true, show: true}
+        });
+    };
+
+    hideModal = () => {
+        this.setState({ show: false });
+    };
+
     render() {
-        return(
-            <div>
-                {this.props.idModel}
+        return (
+            <div>{
+                this.state.show ? 
+                (<ModalCrud hideModal={this.hideModal} modal={this.state.modal} info={this.props.eventId.user_properties}/>) : ("")
+            }
+                <div className="column is-narrow has-text-centered">
+                    <button className="button is-primary" onClick={this.showModal}>Agregar Conferencista +</button>
+                </div>
             </div>
-        )
+        );
     }
 }
 
