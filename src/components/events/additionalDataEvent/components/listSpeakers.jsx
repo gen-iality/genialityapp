71eacf88168ce6dcf8ade9fa3d6eb4d2a536f194
david rodriguez <david.rodriguez@mocionsoft.com>
2019-01-24 14:44:09 -0500

    se agregaron estilos para mostrar los speakers

--- src/components/events/additionalDataEvent/components/listSpeakers.jsx
+++ src/components/events/additionalDataEvent/components/listSpeakers.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import '../styles.css'
 class ListSpeakers extends Component{
 
     constructor(props){
@@ -14,6 +14,8 @@ class ListSpeakers extends Component{
 
 
             <React.Fragment>
+                <br/>   <br/>
+                <p class="has-text-grey-dark is-size-2  titulo cabecera">SPEAKERS</p>
                 <div className="columns is-multiline">
                     {/* <div className="column">
                         First column
@@ -26,35 +28,32 @@ class ListSpeakers extends Component{
                     </div> */}
                     {this.props.speakers.map((item,key)=>{
                         return (
-                            <div className="column is-one-quarter" >
-                    <div className="card">
-                    <div className="card-image">
-                        <figure className="image is-4by3">
-                        <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
-                        </figure>
-                    </div>
-                    <div className="card-content">
-                        <div className="media">
-                        <div className="media-left">
-                            <figure className="image is-48x48">
-                            <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
-                            </figure>
-                        </div>
-                        <div className="media-content">
-                            <p className="title is-4">John Smith</p>
-                            <p className="subtitle is-6">@johnsmith</p>
-                        </div>
-                        </div>
-
-                        <div className="content">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-                        <a href="#">#css</a> <a href="#">#responsive</a>
-                        <br/>
-                        <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-                        </div>
-                        </div>
-                        </div> 
+                    
+                    <div className="column is-one-quarter" key={key} >
+                            <div className="card">
+                                <div className="card-image">
+                                    <figure className="image is-4by3">
+                                    <img src={(item.picture) ? item.picture :'https://bulma.io/images/placeholders/1280x960.png'} alt="Placeholder image" />
+                                    </figure>
+                                </div>
+                                <div className="card-content">
+                                    <div className="media">                         
+                                        <div className="media-content">
+                                            {/* <p className="title is-4">{item.name}</p> */}
+                                            <p class="has-text-grey-dark is-size-3  titulo">{item.name}</p>
+                                            <a class="verMas"><span>+ </span><br/> Ver Más</a>
+                                            {/* <h2 className="data-title has-text-left">
+                                                <small className="is-italic has-text-grey-light has-text-weight-300">Encuentra la</small><br/>
+                                                <span className="has-text-grey-dark is-size-3">Ubicación</span>
+                                            </h2> */}
+                                            {/* <p className="subtitle is-6">@johnsmith</p> */}
+                                        </div>
+                                    </div>
+                                    <div className="content">
+                                        
+                                    </div>
+                                </div>
+                             </div> 
                     </div>
                         )
                     })}
@@ -71,32 +70,4 @@ class ListSpeakers extends Component{
 export default ListSpeakers;
 
 
-                {/* 
-                <div className="card">
-                <div className="card-image">
-                    <figure className="image is-4by3">
-                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
-                    </figure>
-                </div>
-                <div className="card-content">
-                    <div className="media">
-                    <div className="media-left">
-                        <figure className="image is-48x48">
-                        <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
-                        </figure>
-                    </div>
-                    <div className="media-content">
-                        <p className="title is-4">John Smith</p>
-                        <p className="subtitle is-6">@johnsmith</p>
-                    </div>
-                    </div>
-
-                    <div className="content">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-                    <a href="#">#css</a> <a href="#">#responsive</a>
-                    <br/>
-                    <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-                    </div>
-                </div>
-                </div> */}
+              
