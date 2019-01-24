c523bf07e697537fa751e64fcd7e6bc66b85187d
Andres Pedraza <andres.pedraza@mocionsoft.com>
2019-01-24 10:07:54 -0500

    creating modal for speakers

---
+++ src/components/events/additionalDataEvent/components/modalSpeakers.jsx
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import {icon} from "../../../../helpers/constants";
+
+class ModalSpeaker extends Component{
+    constructor(props){
+        super(props)
+        this.state = {};
+    }
+
+    componentDidMount() {
+      
+    }
+
+    
+    render(){
+        console.log('here info speaker', this.props.infoSpeaker);
+        return(
+            <React.Fragment>
+                <div className={`modal modal-add-user ${this.props.modal ? "is-active" : ""}`}>
+                        <div className="modal-background"/>
+                        <div className="modal-card">
+                            <header className="modal-card-head">
+                                <div className="modal-card-title">
+                                    <div className="icon-header" dangerouslySetInnerHTML={{ __html: icon }}/>
+                                </div>
+                                <button className="delete" aria-label="close" onClick={this.props.hideModal}/>
+                            </header>
+                            <section className="modal-card-body">
+                             <p>Here speaker</p>
+                            </section>
+                            {
+                                <footer className="modal-card-foot">
+                                {
+                                    this.props.infoSpeaker
+                                }
+                                </footer>
+                            }
+                        </div>
+                    </div>
+                    {/* <div className={`modal modal-add-user ${this.props.modal ? "is-active" : ""}`}>
+                        
+                        <div className="column is-narrow has-text-centered">
+                            <button className="button is-primary" onClick={this.props.hideModal}>Cerrar</button>
+                        </div>
+                    </div> */}
+            </React.Fragment>
+        )
+    }
+}
+
+export default ModalSpeaker
