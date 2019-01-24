2a796e4cf8dfd62a63cc306addf861e680ea2caf
Andres Pedraza <andres.pedraza@mocionsoft.com>
2019-01-24 10:07:49 -0500

    creating modal for speakers

--- src/components/events/additionalDataEvent/containers/index.jsx
+++ src/components/events/additionalDataEvent/containers/index.jsx
@@ -1,15 +1,29 @@
 import React,{Component} from 'react';
 import Slider from "react-slick";
+import ModalSpeaker from '../components/modalSpeakers';
+
 // Muestra la información adicional de el evento como speakers, agenta, boleteria etc...
 class AdditonalDataEvent extends Component{
     constructor(props){
         super(props)
+
+        this.state = {
+            show: false,
+            modal: false
+        }
     }
 
+    showModal = () => {
+        this.setState(prevState => {
+            return {modal: true, show: true}
+        });
+    };
+
+    hideModal = () => {
+        this.setState({ show: false });
+    };
+
     render() {
-
-     
-
         const settings = {
           dots: true,
           infinite: true,
@@ -17,14 +31,18 @@ class AdditonalDataEvent extends Component{
           slidesToShow: 1,
           slidesToScroll: 1
         };
-        console.log(this.props)
         return (
-
-
-            <React.Fragment>
-               
-                 
-            </React.Fragment>
+            <div>
+            {
+                this.state.show ? (<ModalSpeaker hideModal={this.hideModal} modal={this.state.modal} infoSpeaker={this.props.eventInfo.speaker}/>) 
+                : 
+                (null)
+            }
+                <div className="column is-narrow has-text-centered">
+                    <button className="button is-primary" onClick={this.showModal}>Show modal {this.props.buttonName} +</button>
+                </div>
+            </div>
+            
         );
     }
 }
