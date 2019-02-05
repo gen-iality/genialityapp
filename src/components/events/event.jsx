fe21b1f58d9899b53df97b398c9799b7153f8db0
Alejandro Gómez <man_alejo@hotmail.com>
2019-02-05 11:58:06 -0500

    Ticketes → Tickets

--- src/components/events/event.jsx
+++ src/components/events/event.jsx
@@ -154,7 +154,7 @@ class Event extends Component {
                                             {
                                                 permissions.items.includes(rolPermissions.admin_ticket._id) &&
                                                     <p className="menu-label has-text-centered-mobile">
-                                                        <NavLink className="item" onClick={this.handleClick} activeClassName={"active"} to={`${match.url}/ticket`}>Ticketes</NavLink>
+                                                        <NavLink className="item" onClick={this.handleClick} activeClassName={"active"} to={`${match.url}/ticket`}>Tickets</NavLink>
                                                     </p>
                                             }
                                             <p className="menu-label has-text-centered-mobile">
