ac86fc3c49b460234d6e3a6ce1d1e6af4a588f1c
Alejandro Gómez <alejomg27@gmail.com>
2019-08-30 11:18:02 -0500

    Change the way to handle editor and init with preview

--- src/components/certificados/certificado.jsx
+++ src/components/certificados/certificado.jsx
@@ -1,5 +1,8 @@
 import React, {Component} from "react";
 import {withRouter} from "react-router-dom";
+import MediumEditor from 'medium-editor';
+import * as html2canvas from "html2canvas";
+import * as jsPDF from 'jspdf';
 import Dropzone from "react-dropzone";
 import {toast} from "react-toastify";
 
@@ -7,10 +10,11 @@ class Certificado extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            showEditor:true,
-            imageFile:"https://even3.blob.core.windows.net/certificados/Certificado-02.jpg"
+            imageFile:"https://even3.blob.core.windows.net/certificados/Certificado-02.jpg",
+            imageData:{}
         };
         this.elementEdit = null;
+        this.editor = {};
     }
 
     componentDidMount() {
@@ -23,7 +27,21 @@ class Certificado extends Component {
 
     handleImage = (files) => {
         const file = files[0];
-        if(file) this.setState({imageFile: URL.createObjectURL(file)});
+        if(file) {
+            this.setState({imageFile: URL.createObjectURL(file)});
+            const self = this;
+            const i = new Image();
+            i.onload = () => {
+                let reader = new FileReader()
+                reader.readAsDataURL(file)
+                reader.onload = () => {
+                    const imageData = {data:reader.result,type:file.type.split('/')[1]};
+                    self.setState({imageData:imageData})
+                }
+            };
+
+            i.src = file.preview
+        }
         else toast.error("Solo se permiten imágenes. Intentalo de nuevo");
     };
 
@@ -32,24 +50,36 @@ class Certificado extends Component {
     handleClickOutside = (e) => {
         if ( ( this.contenedor && this.contenedor.contains(e.target) ) ) {
             this.elementEdit = e.target;
-            this.setState({showEditor:true});
+            this.editor = new MediumEditor(this.elementEdit,{
+                placeholder: false,
+                buttonLabels:true,
+                toolbar:{
+                    buttons:['bold','italic','underline']
+                }});
         }else {
-            if( e.target.classList.contains("icon") || e.target.classList.contains("editor-element") || e.target.classList.contains("fas") ){
-                this.setState({showEditor:true});
-            }else{
-                this.elementEdit = null;
-                this.setState({showEditor:false});
-            }
+            this.elementEdit = null;
         }
     };
 
-    editClick = (action) => {
-      this.setState({showEditor:true});
-      this.elementEdit.style.marginLeft = "30px"
+    saveCert = (e) => {
+        console.log(this.contenedor.innerHTML);
     };
 
+    previewCert = (e) => {
+        const {imageData} = this.state;
+        html2canvas(document.querySelector("#contenedorCert"), {logging:true,useCORS:true})
+            .then((canvas) => {
+                const contentData = canvas.toDataURL('image/png');
+                const pdf = new jsPDF({
+                    orientation: 'landscape'
+                });
+                if(imageData.data) pdf.addImage(imageData.data, imageData.type.toUpperCase(), 0, 0);
+                pdf.addImage(contentData, 'PNG', 0, 0);
+                pdf.save("download.pdf");
+            });
+    }
+
     render() {
-        console.log(this.elementEdit);
         return (
             <div className="editor-certificado">
                 <nav className="level">
@@ -65,70 +95,33 @@ class Certificado extends Component {
                             <button className="button is-info">Tags disponibles</button>
                         </div>
                         <div className="level-item">
-                            <Dropzone onDrop={this.handleImage} accept=".png,.jpeg" className="zone">
+                            <Dropzone onDrop={this.handleImage} accept="image/*" className="zone">
                                 <button className="button is-link">Imagen de Fondo</button>
                             </Dropzone>
                         </div>
                         <div className="level-item">
-                            <button className="button is-primary">Preview</button>
+                            <button className="button is-primary" onClick={this.previewCert}>Preview</button>
                         </div>
                         <div className="level-item">
-                            <button className="button">Cancelar</button>
+                            <button className="button" onClick={this.props.listTab}>Cancelar</button>
                         </div>
                         <div className="level-item">
-                            <button className="button is-success">Guardar</button>
+                            <button className="button is-success" onClick={this.saveCert}>Guardar</button>
                         </div>
                     </div>
                 </nav>
-                {this.state.showEditor&&<div className="editor">
-                    <div className="edit-option">
-                        <div className="edit-element">Font Family</div>
-                    </div>
-                    <div className="edit-option">
-                        <div className="edit-element">Font Sizes</div>
-                    </div>
-                    <div className="edit-option">
-                        <div className="edit-element">
-                            <span className="icon"><i className="fas fa-tint"/></span></div>
-                        <div className="edit-element">
-                            <span className="icon"><i className="fas fa-paint-roller"/></span></div>
-                    </div>
-                    <div className="edit-option">
-                        <div className="edit-element">
-                            <span className="icon"><i className="fas fa-bold"/></span></div>
-                        <div className="edit-element">
-                            <span className="icon"><i className="fas fa-italic"/></span></div>
-                        <div className="edit-element">
-                            <span className="icon"><i className="fas fa-underline"/></span></div>
-                    </div>
-                    <div className="edit-option">
-                        <div className="edit-element">
-                            <span className="icon"><i className="fas fa-align-right"/></span></div>
-                        <div className="edit-element">
-                            <span className="icon"><i className="fas fa-align-center"/></span></div>
-                        <div className="edit-element">
-                            <span className="icon"><i className="fas fa-align-left"/></span></div>
-                        <div className="edit-element">
-                            <span className="icon"><i className="fas fa-align-justify"/></span></div>
-                    </div>
-                    <div className="edit-option">
-                        <div className="edit-element" onClick={e=>{this.editClick('indent')}}>
-                            <span className="icon"><i className="fas fa-indent"/></span></div>
-                        <div className="edit-element">
-                            <span className="icon"><i className="fas fa-outdent"/></span></div>
-                    </div>
-                </div>}
                 <div className="contenedor">
-                    <img src={this.state.imageFile} alt="background-image" className="bg"/>
-                    <div className="texto-certificado" contentEditable={true} ref={this.setContenedorRef}>
+                    <div className="texto-certificado" id={"contenedorCert"} contentEditable={true} ref={this.setContenedorRef}>
+                        <img src={this.state.imageFile} alt="background-image" className="bg"/>
                         <p>&nbsp;</p>
                         <p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
-                        <p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>Certificamos
-                        que <strong>participante.nome}</strong>, participou com êxito do
-                        evento evento.titulo} realizado em evento.data.realizacao}, na cidade de evento.cidade},
-                        contabilizando carga horária total de evento.carga.horaria} horas.</p><p>&nbsp;</p>
-                        <p>&nbsp;</p><p>evento.cidade},
-                        informe a data aqui.</p>
+                        <p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
+                        <p>Certificamos que <strong>[participante.nome]</strong>, participou com êxito do
+                        evento [evento.titulo] realizado em [evento.data.realizacao], na cidade de [evento.cidade],
+                        contabilizando carga horária total de [evento.carga.horaria] horas.</p>
+                        <p>&nbsp;</p>
+                        <p>&nbsp;</p>
+                        <p>[evento.cidade], informe a data aqui.</p>
                     </div>
                 </div>
             </div>
