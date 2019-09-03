446b6f790a901099b4a7c6aee4ea539423c8fc81
Alejandro Gómez <alejomg27@gmail.com>
2019-09-03 07:56:50 -0500

    Modal and logic to show tags allowed

--- src/components/certificados/certificado.jsx
+++ src/components/certificados/certificado.jsx
@@ -1,6 +1,5 @@
 import React, {Component} from "react";
 import {withRouter} from "react-router-dom";
-import MediumEditor from 'medium-editor';
 import * as html2canvas from "html2canvas";
 import * as jsPDF from 'jspdf';
 import Dropzone from "react-dropzone";
@@ -10,6 +9,16 @@ class Certificado extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            tags:{
+                'event.name' : 'Nombre del Evento',
+                'event.start' : 'Fecha Inicio del Evento',
+                'event.end' : 'Fecha Fin del Evento',
+                'event.venue' : 'Lugar del Evento',
+                'event.address' : 'Dirección del Evento',
+                'user.names' : 'Nombre(s) de asistente',
+                'user.email' : 'Correo de asistente',
+            },
+            openTAG:false,
             imageFile:"https://even3.blob.core.windows.net/certificados/Certificado-02.jpg",
             imageData:{}
         };
@@ -18,11 +27,11 @@ class Certificado extends Component {
     }
 
     componentDidMount() {
-        document.addEventListener('mousedown', this.handleClickOutside);
-    }
-
-    componentWillUnmount() {
-        document.removeEventListener('mousedown', this.handleClickOutside);
+        const {user_properties} = this.props.event;
+        let fields = user_properties.filter(item=>item.name!=="names"&&item.name!=="email");
+        const list = Object.assign({},this.state.tags);
+        fields.map(field=> list[`user.${field.name}`] = field.label );
+        this.setState({tags:list})
     }
 
     handleImage = (files) => {
@@ -47,20 +56,6 @@ class Certificado extends Component {
 
     setContenedorRef = (node) => { this.contenedor = node };
 
-    handleClickOutside = (e) => {
-        if ( ( this.contenedor && this.contenedor.contains(e.target) ) ) {
-            this.elementEdit = e.target;
-            this.editor = new MediumEditor(this.elementEdit,{
-                placeholder: false,
-                buttonLabels:true,
-                toolbar:{
-                    buttons:['bold','italic','underline']
-                }});
-        }else {
-            this.elementEdit = null;
-        }
-    };
-
     saveCert = (e) => {
         console.log(this.contenedor.innerHTML);
     };
@@ -81,50 +76,77 @@ class Certificado extends Component {
 
     render() {
         return (
-            <div className="editor-certificado">
-                <nav className="level">
-                    <div className="level-left">
-                        <div className="level-item">
-                            <p className="subtitle is-5">
-                                <strong>{this.props.data.name}</strong>
-                            </p>
+            <React.Fragment>
+                <div className="editor-certificado">
+                    <nav className="level">
+                        <div className="level-left">
+                            <div className="level-item">
+                                <p className="subtitle is-5">
+                                    <strong>{this.props.data.name}</strong>
+                                </p>
+                            </div>
                         </div>
-                    </div>
-                    <div className="level-right">
-                        <div className="level-item">
-                            <button className="button is-info">Tags disponibles</button>
+                        <div className="level-right">
+                            <div className="level-item">
+                                <button className="button is-info" onClick={e=>{this.setState({openTAG:true})}}>Tags disponibles</button>
+                            </div>
+                            <div className="level-item">
+                                <Dropzone onDrop={this.handleImage} accept="image/*" className="zone">
+                                    <button className="button is-link">Imagen de Fondo</button>
+                                </Dropzone>
+                            </div>
+                            <div className="level-item">
+                                <button className="button is-primary" onClick={this.previewCert}>Preview</button>
+                            </div>
+                            <div className="level-item">
+                                <button className="button" onClick={this.props.listTab}>Cancelar</button>
+                            </div>
+                            <div className="level-item">
+                                <button className="button is-success" onClick={this.saveCert}>Guardar</button>
+                            </div>
                         </div>
-                        <div className="level-item">
-                            <Dropzone onDrop={this.handleImage} accept="image/*" className="zone">
-                                <button className="button is-link">Imagen de Fondo</button>
-                            </Dropzone>
+                    </nav>
+                    <div className="contenedor">
+                        <div className="texto-certificado" id={"contenedorCert"} contentEditable={true} ref={this.setContenedorRef}>
+                            <img src={this.state.imageFile} alt="background-image" className="bg"/>
+                            <p>&nbsp;</p>
+                            <p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
+                            <p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
+                            <p>Certificamos que <strong>[user.names]</strong>, participo con êxito de
+                                evento [event.name] realizado del [event.start] al [event.end].</p>
+                            <p>&nbsp;</p>
+                            <p>&nbsp;</p>
+                            <p>[event.venue], información.</p>
                         </div>
-                        <div className="level-item">
-                            <button className="button is-primary" onClick={this.previewCert}>Preview</button>
-                        </div>
-                        <div className="level-item">
-                            <button className="button" onClick={this.props.listTab}>Cancelar</button>
-                        </div>
-                        <div className="level-item">
-                            <button className="button is-success" onClick={this.saveCert}>Guardar</button>
-                        </div>
-                    </div>
-                </nav>
-                <div className="contenedor">
-                    <div className="texto-certificado" id={"contenedorCert"} contentEditable={true} ref={this.setContenedorRef}>
-                        <img src={this.state.imageFile} alt="background-image" className="bg"/>
-                        <p>&nbsp;</p>
-                        <p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
-                        <p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
-                        <p>Certificamos que <strong>[participante.nome]</strong>, participou com êxito do
-                        evento [evento.titulo] realizado em [evento.data.realizacao], na cidade de [evento.cidade],
-                        contabilizando carga horária total de [evento.carga.horaria] horas.</p>
-                        <p>&nbsp;</p>
-                        <p>&nbsp;</p>
-                        <p>[evento.cidade], informe a data aqui.</p>
                     </div>
                 </div>
-            </div>
+                {
+                    this.state.openTAG &&
+                        <div className={`modal ${this.state.openTAG ? "is-active" : ""}`}>
+                            <div className="modal-background"/>
+                            <div className="modal-card">
+                                <header className="modal-card-head">
+                                    <p className="modal-card-title">Etiquetas Disponibles</p>
+                                    <button className="delete is-large" aria-label="close" onClick={e=>{this.setState({openTAG:false})}}/>
+                                </header>
+                                <section className="modal-card-body tags-certificado">
+                                    <p>Use etiquetas para ingresar información referente al evento o los asistentes</p>
+                                    <dl>
+                                        {Object.keys(this.state.tags).map( key => {
+                                            return <React.Fragment key={key}>
+                                                <dt> <code>{key}</code> </dt>
+                                                <dd> {this.state.tags[key]} </dd>
+                                            </React.Fragment>
+                                        } )}
+                                    </dl>
+                                </section>
+                                <footer className="modal-card-foot">
+                                    <button className="button is-text" onClick={e=>{this.setState({openTAG:false})}}>Cerrar</button>
+                                </footer>
+                            </div>
+                        </div>
+                }
+            </React.Fragment>
         )
     }
 }
