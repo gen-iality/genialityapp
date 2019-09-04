cd98e7ec84b471867147dd84ab94312aacfacfb6
Brayan Triana <brayan.triana@mocionsoft.com>
2019-09-04 03:11:05 +0000

    formTicket.jsx edited online with Bitbucket adding a condition if stagings havent been configured

--- src/components/tickets/formTicket.jsx
+++ src/components/tickets/formTicket.jsx
@@ -348,7 +348,9 @@ function ListadoTiquetes({...props}) {
         <React.Fragment>
             <div className='columns content-tabs'>
                 {
-                    stages.map(stage=>{
+                    
+                    (
+                        (stage)?<div></div>:stages.map(stage=>{
                         return <div className={`column box has-text-weight-bold tab stage ${active===stage.stage_id?'is-active':''} ${"ended"===stage.status?'is-disabled':''}`}
                                     key={stage.stage_id} onClick={event => selectStage(stage)}>
                             <p>{stage.title}</p>
@@ -368,6 +370,7 @@ function ListadoTiquetes({...props}) {
                             </div>
                         </div>
                     })
+                    )
                 }
             </div>
             {
