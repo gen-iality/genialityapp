c5fcf0e06d79d772b5f2dc36d0aa5a7175ce54fd
Alejandro Gómez <alejomg27@gmail.com>
2019-09-05 07:08:11 -0500

    Keep going landing certs

--- src/components/certificados/cerLanding.jsx
+++ src/components/certificados/cerLanding.jsx
@@ -1,25 +1,44 @@
 import React, {Component} from "react";
 import {firestore} from "../../helpers/firebase";
+import {CertsApi} from "../../helpers/request";
 
 class CertificadoLanding extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            tags:[
+                {tag:'event.name', label:'Nombre del Evento', value:'name'},
+                {tag:'event.start', label:'Fecha Inicio del Evento', value:'datetime_from'},
+                {tag:'event.end', label:'Fecha Fin del Evento', value:'datetime_to'},
+                {tag:'event.venue', label:'Lugar del Evento', value:'venue'},
+                {tag:'event.address', label:'Dirección del Evento', value:'location.FormattedAddress'},
+                {tag: 'user.names', label: 'Nombre(s) de asistente', value: 'names'},
+                {tag: 'user.email', label: 'Correo de asistente', value: 'email'},
+            ],
             disabled:true,
             toSearch:"",
             dataUser:{},
             message:false
         };
-        this.usersRef = ''
+        this.usersRef = '';
+        this.generateCert = this.generateCert.bind(this)
     }
 
     componentDidMount() {
-        this.usersRef = firestore.collection(`${this.props.eventId}_event_attendees`);
+        const {user_properties} = this.props.event;
+        let fields = user_properties.filter(item=>item.name!=="names"&&item.name!=="email");
+        const list = [...this.state.tags];
+        fields.map(field=> list.push({
+            tag:`user.${field.name}`,
+            value:field.name,
+            label:field.label}));
+        this.usersRef = firestore.collection(`${this.props.event._id}_event_attendees`);
+        this.setState({tags:list});
     }
 
     onChange = (e) => {
         const {value} = e.target;
-        this.setState({toSearch:value,disabled:!(value.length>4),message:false,dataUser:{}})
+        this.setState({toSearch:value,disabled:!(value.length>0),message:false,dataUser:{}})
     };
 
     searchCert = () => {
@@ -40,6 +59,10 @@ class CertificadoLanding extends Component {
             });
     };
 
+    async generateCert() {
+        const certs = await CertsApi.byEvent(this.props.event._id);
+    };
+
     render() {
         return (
             <section>
@@ -56,7 +79,7 @@ class CertificadoLanding extends Component {
                 {this.state.message && <p>{this.state.message}</p>}
                 {this.state.dataUser.properties && <div className="box">
                     <h3>Certificado encontrado</h3>
-                    <button className="is-primary">Descargar</button>
+                    <button className="button is-primary" onClick={this.generateCert}>Descargar</button>
                 </div>}
             </section>
         )
