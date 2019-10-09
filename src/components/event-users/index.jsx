257cb950082c5aaa78db30f878efc7645c1c533a
Alejandro Mora Gómez <alejomg27@gmail.com>
2019-10-09 22:33:25 +0000

    index.jsx edited online with Bitbucket

--- src/components/event-users/index.jsx
+++ src/components/event-users/index.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import {firestore} from "../../helpers/firebase";
 import QrReader from "react-qr-reader";
 import { FaCamera} from "react-icons/fa";
