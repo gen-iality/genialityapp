a27de391e01ac4af1b3d1cc1f536cd92781bf57a
Alejandro Gómez <alejomg27@gmail.com>
2019-10-29 21:15:11 -0500

    Set submodules to handle types and categories

--- src/components/agenda/index.jsx
+++ src/components/agenda/index.jsx
@@ -2,6 +2,8 @@ import React, {Fragment} from 'react';
 import {Route, Switch, withRouter} from "react-router-dom";
 import Agenda from "./agenda";
 import AgendaEdit from "./edit";
+import AgendaTypes from "./types";
+import AgendaCat from "./categories";
 
 function AgendaRoutes({...props}){
     const {event, match} = props;
@@ -10,6 +12,8 @@ function AgendaRoutes({...props}){
             <Switch>
                 <Route exact path={`${match.url}/`} render={()=><Agenda event={event} matchUrl={match.url}/>}/>
                 <Route exact path={`${match.url}/actividad`} render={()=><AgendaEdit event={event} matchUrl={match.url}/>}/>
+                <Route exact path={`${match.url}/tipos`} render={()=><AgendaTypes event={event} matchUrl={match.url}/>}/>
+                <Route exact path={`${match.url}/categorias`} render={()=><AgendaCat event={event} matchUrl={match.url}/>}/>
             </Switch>
         </Fragment>
     );
---
+++ src/components/agenda/types.jsx
@@ -0,0 +1,128 @@
+import React, {Component} from "react";
+import {Link} from "react-router-dom";
+import {FaChevronLeft} from "react-icons/fa";
+import EventContent from "../events/shared/content";
+import EvenTable from "../events/shared/table";
+import Loading from "../loaders/loading";
+import { TypesAgendaApi } from "../../helpers/request";
+
+class AgendaTypes extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading:true,
+            id:"",
+            name:"",
+            list:[]
+        };
+        this.eventID = ""
+    }
+
+    async componentDidMount() {
+        this.eventID = this.props.event._id;
+        const list = await TypesAgendaApi.byEvent(this.eventID);
+        this.setState({list,loading:false})
+    }
+
+    editType = (type) => this.setState({id:type._id,name:type.name});
+
+    onChange = (e) => {
+        this.setState({name:e.target.value})
+    };
+
+    newType = () => {
+        if(!this.state.list.find(({value})=>value === "new"))
+            this.setState(state => ({list: state.list.concat({label: '', value: 'new'}), id: 'new'}));
+    };
+
+    removeNewType = () => this.setState(state => ({list:state.list.filter(({value}) => value !== "new"),id:"",name:""}));
+
+    async saveRole() {
+        try{
+            if(this.state.id !== 'new') {
+                await TypesAgendaApi.editOne({name: this.state.name}, this.state.id, this.eventID);
+                this.setState(state => {
+                    const types = state.list.map(item => {
+                        if (item._id === state.id) {
+                            item.name = state.name;
+                            return item;
+                        } else return item;
+                    });
+                    return {list: types, id: "", name: ""};
+                });
+            }else{
+                const newRole = await TypesAgendaApi.create(this.eventID, {name: this.state.name});
+                this.setState(state => {
+                    const types = state.list.map(item => {
+                        if (item.value === state.id) {
+                            item.label = newRole.name;
+                            item.item = newRole;
+                            item.value = newRole._id;
+                            return item;
+                        } else return item;
+                    });
+                    return {list: types, id: "", name: ""};
+                });
+            }
+        }catch (e) {
+            console.log(e);
+        }
+    };
+
+    async removeType(deleteID) {
+        this.setState({isLoading:'Cargando....'});
+        try {
+            await TypesAgendaApi.deleteOne(deleteID);
+            this.setState(state => ({list:state.list.filter(({value}) => value !== deleteID),id:"",name:""}))
+        }
+        catch (error) {
+            console.log(error);
+        }
+    };
+
+    render() {
+        const {matchUrl} = this.props;
+        const {loading,list,id,name} = this.state;
+        return (
+            <EventContent title={<span><Link to={matchUrl}><FaChevronLeft/></Link>Tipos de Actividad</span>}
+                          addAction={this.newType} addTitle={"Nuevo tipo"}>
+                {loading ? <Loading/> :
+                    <EvenTable head={["Nombre", ""]}>
+                        {list.map(type => {
+                            return <tr key={type.value}>
+                                <td>
+                                    {
+                                        id === type.value ?
+                                            <input type="text" value={name} autoFocus onChange={this.onChange}/>:
+                                            <p>{type.label}</p>
+                                    }</td>
+                                <td>
+                                    {
+                                        id === type.value ?
+                                            <button>
+                                            <span className="icon has-text-grey"
+                                                  onClick={(e)=>{this.saveRole(type)}}><i className="fas fa-save"/></span>
+                                            </button>:
+                                            <button>
+                                            <span className="icon has-text-grey"
+                                                  onClick={(e)=>this.editType(type.item)}><i className="fas fa-edit"/></span>
+                                            </button>
+                                    }
+                                    {
+                                        type.value === 'new' ?
+                                            <button><span className='icon has-text-grey'
+                                                          onClick={this.removeNewType}><i className='fas fa-times'/></span></button>:
+                                            <button><span className='icon has-text-grey'
+                                                          onClick={(e)=>{this.removeType(type.value)}}><i className='far fa-trash-alt'/></span></button>
+                                    }
+                                </td>
+                            </tr>
+                        })}
+                    </EvenTable>
+                }
+            </EventContent>
+        );
+    }
+}
+
+export default AgendaTypes
