c1c3d5108abe4eaeabe60ab7e774835f2cc39a5c
Alejandro Gómez <alejomg27@gmail.com>
2019-10-30 10:58:57 -0500

    One file for types and categories. Set color field for cats

--- src/components/agenda/index.jsx
+++ src/components/agenda/index.jsx
@@ -2,8 +2,7 @@ import React, {Fragment} from 'react';
 import {Route, Switch, withRouter} from "react-router-dom";
 import Agenda from "./agenda";
 import AgendaEdit from "./edit";
-import AgendaTypes from "./types";
-import AgendaCat from "./categories";
+import AgendaTypeCat from "./typecat";
 
 function AgendaRoutes({...props}){
     const {event, match} = props;
@@ -12,8 +11,8 @@ function AgendaRoutes({...props}){
             <Switch>
                 <Route exact path={`${match.url}/`} render={()=><Agenda event={event} matchUrl={match.url}/>}/>
                 <Route exact path={`${match.url}/actividad`} render={()=><AgendaEdit event={event} matchUrl={match.url}/>}/>
-                <Route exact path={`${match.url}/tipos`} render={()=><AgendaTypes event={event} matchUrl={match.url}/>}/>
-                <Route exact path={`${match.url}/categorias`} render={()=><AgendaCat event={event} matchUrl={match.url}/>}/>
+                <Route exact path={`${match.url}/tipos`} render={()=><AgendaTypeCat event={event} matchUrl={match.url}/>}/>
+                <Route exact path={`${match.url}/categorias`} render={()=><AgendaTypeCat event={event} matchUrl={match.url}/>}/>
             </Switch>
         </Fragment>
     );
