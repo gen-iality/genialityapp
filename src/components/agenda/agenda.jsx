2bb56939434c8f9615dd9f505ee23a6045ab7e2b
Alejandro Gómez <alejomg27@gmail.com>
2019-10-29 21:12:23 -0500

    Fetch agenda by event and display it

--- src/components/agenda/agenda.jsx
+++ src/components/agenda/agenda.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import {Link, Redirect} from "react-router-dom";
 import EventContent from "../events/shared/content";
+import {AgendaApi} from "../../helpers/request";
 
 class Agenda extends Component {
     constructor(props) {
@@ -11,12 +12,15 @@ class Agenda extends Component {
         }
     }
 
-    async componentDidMount() {}
+    async componentDidMount() {
+        const {data} = await AgendaApi.byEvent(this.props.event._id);
+        this.setState({list:data})
+    }
 
     redirect = () => this.setState({redirect:true});
 
     render() {
-        if(this.state.redirect) return <Redirect to={`${this.props.matchUrl}/actividad`}/>;
+        if(this.state.redirect) return <Redirect to={{pathname:`${this.props.matchUrl}/actividad`,state:{new:true}}}/>;
         const {list} = this.state;
         return (
             <EventContent title={"Programación"} addAction={list.length>0?this.redirect:false} addTitle={"Nueva actividad"}>
@@ -26,7 +30,14 @@ class Agenda extends Component {
                             <p>Agregue una actividad para la programación</p>
                             <Link to={{pathname:`${this.props.matchUrl}/actividad`,state:{new:true}}}>Nueva actividad</Link>
                         </div>:
-                        <p>Listado</p>
+                        <div>
+                            {
+                                list.map(i=><div key={i._id}>
+                                    <p>{i.name}</p>
+                                    <Link to={{pathname:`${this.props.matchUrl}/actividad`,state:{edit:i._id}}}>Editar</Link>
+                                </div>)
+                            }
+                        </div>
                 }
             </EventContent>
         )
