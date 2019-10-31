4f4546bd3914e5c5853709b2c822597ef11eec68
Alejandro Gómez <alejomg27@gmail.com>
2019-10-31 08:50:02 -0500

    Print agenda "pretty" and added actions for edit and remove

--- src/components/agenda/agenda.jsx
+++ src/components/agenda/agenda.jsx
@@ -2,43 +2,112 @@ import React, {Component} from "react";
 import {Link, Redirect} from "react-router-dom";
 import EventContent from "../events/shared/content";
 import {AgendaApi} from "../../helpers/request";
+import Moment from "moment";
+import EvenTable from "../events/shared/table";
+import {handleRequestError, sweetAlert} from "../../helpers/utils";
 
 class Agenda extends Component {
     constructor(props) {
         super(props);
         this.state = {
             list:[],
+            days:[],
+            day:"",
+            filtered:[],
             redirect:false
         }
     }
 
-    async componentDidMount() {
-        const {data} = await AgendaApi.byEvent(this.props.event._id);
-        this.setState({list:data})
+    componentDidMount() {
+        const { event } = this.props;
+        let days = [];
+        const init = Moment(event.date_start);
+        const end = Moment(event.date_end);
+        const diff = end.diff(init, 'days');
+        for(let i = 0; i < diff+1; i++){
+            days.push(Moment(init).add(i,'d'))
+        }
+        this.setState({days},this.fetchAgenda);
     }
+
+    fetchAgenda = async() => {
+        const {data} = await AgendaApi.byEvent(this.props.event._id);
+        const filtered = this.filterByDay(this.state.days[0],data);
+        this.setState({list:data,day:this.state.days[0],filtered})
+    };
+
+    filterByDay = (day,agenda) => {
+        //First filter activities by day. Use include to see if has the same day
+        //Sort the filtered list by hour start, use moment format HHmm to get a number and used it to sort
+        return agenda.filter(a => a.datetime_start.includes(day.format("YYYY-MM-DD")))
+            .sort((a, b) => Moment(a.datetime_start, "YYYY-MM-DD HH:mm").format("HHmm") - Moment(b.datetime_start, "YYYY-MM-DD HH:mm").format("HHmm"))
+    };
+
+    selectDay = (day) => {
+        const filtered = this.filterByDay(day,this.state.list);
+        this.setState({filtered,day})
+
+    };
+
+    remove = (info) => {
+        sweetAlert.twoButton(`Está seguro de borrar a ${info.name}`, "warning", true, "Borrar", async (result)=>{
+            try{
+                if(result.value){
+                    sweetAlert.showLoading("Espera (:", "Borrando...");
+                    await AgendaApi.deleteOne(info._id, this.props.eventID);
+                    this.fetchAgenda();
+                    sweetAlert.hideLoading();
+                    sweetAlert.showSuccess("Actividad borrada")
+                }
+            }catch (e) {
+                sweetAlert.showError(handleRequestError(e))
+            }
+        })
+    };
 
     redirect = () => this.setState({redirect:true});
 
     render() {
         if(this.state.redirect) return <Redirect to={{pathname:`${this.props.matchUrl}/actividad`,state:{new:true}}}/>;
-        const {list} = this.state;
+        const {days,day,filtered} = this.state;
         return (
-            <EventContent title={"Programación"} addAction={list.length>0?this.redirect:false} addTitle={"Nueva actividad"}>
-                {
-                    list.length <= 0 ?
-                        <div className="empty">
-                            <p>Agregue una actividad para la programación</p>
-                            <Link to={{pathname:`${this.props.matchUrl}/actividad`,state:{new:true}}}>Nueva actividad</Link>
-                        </div>:
-                        <div>
-                            {
-                                list.map(i=><div key={i._id}>
-                                    <p>{i.name}</p>
-                                    <Link to={{pathname:`${this.props.matchUrl}/actividad`,state:{edit:i._id}}}>Editar</Link>
-                                </div>)
-                            }
-                        </div>
-                }
+            <EventContent title={"Programación"} classes={"agenda-list"} addAction={this.redirect} addTitle={"Nueva actividad"}>
+                <nav className="level">
+                    <div className="level-left">
+                        {
+                            days.map((date,key)=> <div onClick={()=>this.selectDay(date)} key={key} className={`level-item date ${date===day?"active":""}`}>
+                                    <p className="subtitle is-5">
+                                        <strong>{date.format("MMM DD")}</strong>
+                                    </p>
+                                </div>
+                            )
+                        }
+                    </div>
+                    <div className="level-right">
+
+                    </div>
+                </nav>
+                <EvenTable head={["Hora", "Actividad", "Espacio", "Conferencista", ""]}>
+                    {filtered.map(agenda=><tr key={agenda._id}>
+                        <td>{Moment(agenda.datetime_start,"YYYY-MM-DD HH:mm").format("HH:mm")} - {Moment(agenda.datetime_end,"YYYY-MM-DD HH:mm").format("HH:mm")}</td>
+                        <td>
+                            <p>{agenda.name}</p>
+                            {agenda.activity_categories&&<div className="tags is-small">{agenda.activity_categories.map(cat=><span style={{background:cat.color,color:cat.color?"white":""}} className="tag">{cat.name}</span>)}</div>}
+                            {agenda.type&&<strong>{agenda.type.name}</strong>}
+                        </td>
+                        <td>{agenda.space?agenda.space.name:""}</td>
+                        <td>{agenda.hosts.map(({name})=><p>{name}</p>)}</td>
+                        <td>
+                            <Link to={{pathname:`${this.props.matchUrl}/actividad`,state:{edit:agenda._id}}}>
+                                <button><span className="icon has-text-grey"><i className="fas fa-edit"/></span></button>
+                            </Link>
+                            <button>
+                                <span className="icon has-text-grey"
+                                      onClick={(e)=>{this.remove(agenda)}}><i className="far fa-trash-alt"/></span>
+                            </button>
+                        </td>
+                    </tr>)}
+                </EvenTable>
             </EventContent>
         )
     }
--- src/components/events/shared/content.jsx
+++ src/components/events/shared/content.jsx
@@ -2,15 +2,19 @@ import React from "react";
 
 export default function EventContent({...props}) {
     return (
-        <div className="event-datos">
+        <div className={`event-datos ${props.classes}`}>
             <h2 className="title-section">{props.title}</h2>
             {props.description&&<p>{props.description}</p>}
             {
                 props.addAction &&
-                <button className="button add is-pulled-right" onClick={props.addAction}>
-                    <span className="icon"><i className="fas fa-plus-circle"/></span>
-                    <span>{props.addTitle}</span>
-                </button>
+                <div className="columns is-mobile">
+                    <div className="column is-3 is-offset-9">
+                        <button className="button add" onClick={props.addAction}>
+                            <span className="icon"><i className="fas fa-plus-circle"/></span>
+                            <span>{props.addTitle}</span>
+                        </button>
+                    </div>
+                </div>
             }
             {props.children}
         </div>
