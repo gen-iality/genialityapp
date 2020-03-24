d773186437f4b7f64800cd5b07fdc91343e74b25
Esteban Sanchez <esteban.sanchez@mocionsoft.com>
2020-03-24 14:10:43 -0500

    added service and button to subscribe in activity

--- src/components/events/agendaLanding.jsx
+++ src/components/events/agendaLanding.jsx
@@ -1,185 +1,298 @@
 import React, { Component } from "react";
 import EventContent from "../events/shared/content";
 import Moment from "moment";
+import * as Cookie from "js-cookie";
 import EvenTable from "../events/shared/table";
 import SearchComponent from "../shared/searchTable";
-import { AgendaApi, SpacesApi, Actions } from "../../helpers/request";
+import API, {
+  AgendaApi,
+  SpacesApi,
+  Actions,
+  Activity
+} from "../../helpers/request";
 import { Link, Redirect } from "react-router-dom";
 import ReactQuill from "react-quill";
 import { toolbarEditor } from "../../helpers/constants";
 
 class Agenda extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            list: [],
-            listDay: [],
-            days: [],
-            day: "",
-            space: "",
-            spaces: [],
-            nameSpace: "",
-            filtered: [],
-            toShow: [],
-            redirect: false,
-            disabled: false
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [],
+      listDay: [],
+      days: [],
+      day: "",
+      space: "",
+      spaces: [],
+      nameSpace: "",
+      filtered: [],
+      toShow: [],
+      redirect: false,
+      disabled: false
+    };
+    this.returnList = this.returnList.bind(this);
+  }
+
+  async componentDidMount() {
+    //Se carga esta funcion para cargar los datos
+    this.fetchAgenda();
+
+    // Se obtiene informacion del usuario actual
+    this.getCurrentUser();
+
+    const { event } = this.props;
+    let days = [];
+    const init = Moment(event.date_start);
+    const end = Moment(event.date_end);
+    const diff = end.diff(init, "days");
+    //Se hace un for para sacar los días desde el inicio hasta el fin, inclusivos
+    for (let i = 0; i < diff + 1; i++) {
+      days.push(Moment(init).add(i, "d"));
+    }
+    this.setState({ days, day: days[0] }, this.fetchAgenda);
+  }
+
+  // Funcion para consultar la informacion del actual usuario
+  getCurrentUser = async () => {
+    let evius_token = Cookie.get("evius_token");
+
+    if (!evius_token) {
+      this.setState({ user: false });
+    } else {
+      try {
+        const resp = await API.get(
+          `/auth/currentUser?evius_token=${Cookie.get("evius_token")}`
+        );
+        if (resp.status === 200) {
+          const data = resp.data;
+          // Solo se desea obtener el id del usuario
+          this.setState({ uid: data._id });
         }
-        this.returnList = this.returnList.bind(this)
+      } catch (error) {
+        const { status } = error.response;
+        console.log("STATUS", status, status === 401);
+      }
     }
+  };
 
-    async componentDidMount() {
-        //Se carga esta funcion para cargar los datos 
-        this.fetchAgenda()
+  fetchAgenda = async () => {
+    // Se consulta a la api de agenda
+    const { data } = await AgendaApi.byEvent(this.props.eventId);
 
-        const { event } = this.props;
-        let days = [];
-        const init = Moment(event.date_start);
-        const end = Moment(event.date_end);
-        const diff = end.diff(init, 'days');
-        //Se hace un for para sacar los días desde el inicio hasta el fin, inclusivos
-        for (let i = 0; i < diff + 1; i++) {
-            days.push(Moment(init).add(i, 'd'))
-        }
-        this.setState({ days, day: days[0] }, this.fetchAgenda);
-    }
+    //se consulta la api de espacios para
+    let space = await SpacesApi.byEvent(this.props.event._id);
 
-    fetchAgenda = async () => {
-        // Se consulta a la api de agenda
-        const { data } = await AgendaApi.byEvent(this.props.eventId);
+    //Después de traer la info se filtra por el primer día por defecto y se mandan los espacios al estado
+    const filtered = this.filterByDay(this.state.days[0], data);
+    this.setState({ list: data, filtered, toShow: filtered, spaces: space });
+  };
 
-        //se consulta la api de espacios para 
-        let space = await SpacesApi.byEvent(this.props.event._id);
+  returnList() {
+    //con la lista previamente cargada en el estado se retorna a la constante toShow Para mostrar la lista completa
+    this.setState({ toShow: this.state.listDay, nameSpace: "inicio" });
+  }
 
-        //Después de traer la info se filtra por el primer día por defecto y se mandan los espacios al estado
-        const filtered = this.filterByDay(this.state.days[0], data);
-        this.setState({ list: data, filtered, toShow: filtered, spaces: space })
-    };
+  filterByDay = (day, agenda) => {
+    //Se trae el filtro de dia para poder filtar por fecha y mostrar los datos
+    const list = agenda
+      .filter(a => a.datetime_start.includes(day.format("YYYY-MM-DD")))
+      .sort(
+        (a, b) =>
+          Moment(a.datetime_start, "YYYY-MM-DD HH:mm").format("HHmm") -
+          Moment(b.datetime_start, "YYYY-MM-DD HH:mm").format("HHmm")
+      );
+    this.setState({ listDay: list });
 
-    returnList() {
-        //con la lista previamente cargada en el estado se retorna a la constante toShow Para mostrar la lista completa
-        this.setState({ toShow: this.state.listDay, nameSpace: "inicio" })
-    }
+    //Se mapea la lista para poder retornar los datos ya filtrados
+    list.map(item => {
+      item.restriction =
+        item.access_restriction_type === "EXCLUSIVE"
+          ? "Exclusiva para: "
+          : item.access_restriction_type === "SUGGESTED"
+          ? "Sugerida para: "
+          : "Abierta";
+      item.roles = item.access_restriction_roles.map(({ name }) => name);
+      return item;
+    });
+    return list;
+  };
 
-    filterByDay = (day, agenda) => {
-        //Se trae el filtro de dia para poder filtar por fecha y mostrar los datos
-        const list = agenda.filter(a => a.datetime_start.includes(day.format("YYYY-MM-DD")))
-            .sort((a, b) => Moment(a.datetime_start, "YYYY-MM-DD HH:mm").format("HHmm") - Moment(b.datetime_start, "YYYY-MM-DD HH:mm").format("HHmm"));
-        this.setState({ listDay: list })
+  //Fn para manejar cuando se selecciona un dia, ejecuta el filtrado
+  selectDay = day => {
+    const filtered = this.filterByDay(day, this.state.list);
+    this.setState({ filtered, toShow: filtered, day });
+  };
 
-        //Se mapea la lista para poder retornar los datos ya filtrados
-        list.map(item => {
-            item.restriction = item.access_restriction_type === "EXCLUSIVE" ? "Exclusiva para: " : item.access_restriction_type === "SUGGESTED" ? "Sugerida para: " : "Abierta";
-            item.roles = item.access_restriction_roles.map(({ name }) => name);
-            return item
-        })
-        return list
-    };
+  //Funcion para ejecutar el filtro por espacio y mandar el espacio a filtrar
+  selectSpace(space) {
+    const filtered = this.filterBySpace(space, this.state.list);
+    this.setState({ filtered, toShow: filtered, space });
+  }
 
-    //Fn para manejar cuando se selecciona un dia, ejecuta el filtrado
-    selectDay = (day) => {
-        const filtered = this.filterByDay(day, this.state.list);
-        this.setState({ filtered, toShow: filtered, day })
+  //Funcion que realiza el filtro por espacio, teniendo en cuenta el dia
+  filterBySpace = (space, dates) => {
+    //Se filta la lista anterior para esta vez filtrar por espacio
+    const list = this.state.listDay.filter(a => a.space.name === space);
 
-    };
+    this.setState({ nameSpace: space });
 
-    //Funcion para ejecutar el filtro por espacio y mandar el espacio a filtrar
-    selectSpace(space) {
-        const filtered = this.filterBySpace(space, this.state.list);
-        this.setState({ filtered, toShow: filtered, space })
-    }
+    //Se mapea la lista para poder retornar la lista filtrada por espacio
+    list.map(item => {
+      item.restriction =
+        item.access_restriction_type === "EXCLUSIVE"
+          ? "Exclusiva para: "
+          : item.access_restriction_type === "SUGGESTED"
+          ? "Sugerida para: "
+          : "Abierta";
+      item.roles = item.access_restriction_roles.map(({ name }) => name);
+      return item;
+    });
+    return list;
+  };
 
-    //Funcion que realiza el filtro por espacio, teniendo en cuenta el dia
-    filterBySpace = (space, dates) => {
+  //Fn para el resultado de la búsqueda
+  searchResult = data => this.setState({ toShow: !data ? [] : data });
 
-        //Se filta la lista anterior para esta vez filtrar por espacio
-        const list = this.state.listDay.filter(a => a.space.name === space);
+  registerInActivity = activityKey => {
+    const { eventId } = this.props;
+    let { uid } = this.state;
+    Activity.Register(eventId, uid, activityKey)
+      .then(result => {
+        console.log("result:", result);
+      })
+      .catch(err => {
+        console.log("err:", err);
+      });
+  };
 
-        this.setState({ nameSpace: space })
+  redirect = () => this.setState({ redirect: true });
 
-        //Se mapea la lista para poder retornar la lista filtrada por espacio
-        list.map(item => {
-            item.restriction = item.access_restriction_type === "EXCLUSIVE" ? "Exclusiva para: " : item.access_restriction_type === "SUGGESTED" ? "Sugerida para: " : "Abierta";
-            item.roles = item.access_restriction_roles.map(({ name }) => name);
-            return item
-        })
-        return list
-    };
-
-    //Fn para el resultado de la búsqueda
-    searchResult = (data) => this.setState({ toShow: !data ? [] : data });
-
-    redirect = () => this.setState({ redirect: true });
-
-    render() {
-        const { days, day, nameSpace, spaces, toShow } = this.state;
-        return (
-            <div>
-                <div className="columns is-desktop ">
-                    <div style={{ float: "left" }}>
-                        <button style={{ marginTop: "3%", marginBottom: "3%" }} className={`${nameSpace === "inicio" ? "button is-danger" : "button is-primary"}`} onClick={this.returnList}>Inicio</button>
-                        {
-                            spaces.map((space, key) => <div onClick={() => this.selectSpace(space.name, space.datetime_start, space.datetime_start)} key={key}>
-                                <button disabled={false} style={{ marginTop: "3%", marginBottom: "3%" }} className={`${nameSpace === space.name ? "button is-danger" : "button is-primary"}`}>{space.name}</button>
-                            </div>
-                            )
-                        }
-                    </div>
-                    <div className="column is-offset-1">
-                        <div className="container-day_calendar tabs is-centered is-fullwidth is-boxed is-medium">
-                            {
-                                days.map((date, key) => (
-                                    <li onClick={() => this.selectDay(date)} key={key} className="is-active" className="is-active">
-                                        <a  className={`${date === day ? " select-day" : " unselect-day"}`}>
-                                            <span className="level-item date">{date.format("MMM DD")}</span>
-                                        </a>
-                                    </li>
-                                    )
-                                )
-                            }
-                        </div>
-                        {toShow.map((agenda, key) =>
-                            <div key={key} className="container_agenda-information">
-                                <div className="card agenda_information">
-                                    <header className="card-header">
-                                        <p className="card-header-title">
-                                            {agenda.name}
-                                        </p>
-                                    </header>
-                                    <div className="card-content">
-                                        <div className="content">
-                                            <div>
-                                                <strong>{agenda.datetime_start} - {agenda.datetime_end}</strong>
-                                            </div>
-                                            <div style={{ marginTop: "3%", marginBottom: "3%" }}>
-                                                <div dangerouslySetInnerHTML={{ __html: agenda.description }}></div>
-                                                <div style={{marginTop:"4%"}}>
-                                                    <strong>Lugar</strong><br/>
-                                                    {agenda.space.name}
-                                                </div>
-
-                                            </div>
-                                            {agenda.hosts.map((speaker, key) =>
-                                                <div key={key}>
-                                                    <br style={{ marginTop: "5%" }} />
-                                                    <strong>Conferencista</strong>
-                                                    <p>{speaker.name}</p>
-                                                </div>
-                                            )}
-                                        </div>
-
-                                    </div>
-                                </div>
-                                <footer className="card-footer">
-                                    {agenda.activity_categories.map((cat, key) => <span key={key} style={{ background: cat.color, color: cat.color ? "white" : "" }} className="tag">{cat.name}</span>)}
-                                </footer>
-                            </div>
-                        )
-                        }
-                    </div>
-                </div>
+  render() {
+    const { days, day, nameSpace, spaces, toShow } = this.state;
+    return (
+      <div>
+        <div className="columns is-desktop ">
+          <div style={{ float: "left" }}>
+            <button
+              style={{ marginTop: "3%", marginBottom: "3%" }}
+              className={`${
+                nameSpace === "inicio"
+                  ? "button is-danger"
+                  : "button is-primary"
+              }`}
+              onClick={this.returnList}
+            >
+              Inicio
+            </button>
+            {spaces.map((space, key) => (
+              <div
+                onClick={() =>
+                  this.selectSpace(
+                    space.name,
+                    space.datetime_start,
+                    space.datetime_start
+                  )
+                }
+                key={key}
+              >
+                <button
+                  disabled={false}
+                  style={{ marginTop: "3%", marginBottom: "3%" }}
+                  className={`${
+                    nameSpace === space.name
+                      ? "button is-danger"
+                      : "button is-primary"
+                  }`}
+                >
+                  {space.name}
+                </button>
+              </div>
+            ))}
+          </div>
+          <div className="column is-offset-1">
+            <div className="container-day_calendar tabs is-centered is-fullwidth is-boxed is-medium">
+              {days.map((date, key) => (
+                <li
+                  onClick={() => this.selectDay(date)}
+                  key={key}
+                  className="is-active"
+                  className="is-active"
+                >
+                  <a
+                    className={`${
+                      date === day ? " select-day" : " unselect-day"
+                    }`}
+                  >
+                    <span className="level-item date">
+                      {date.format("MMM DD")}
+                    </span>
+                  </a>
+                </li>
+              ))}
             </div>
-        )
-    }
+            {toShow.map((agenda, key) => (
+              <div key={key} className="container_agenda-information">
+                <div className="card agenda_information">
+                  <header className="card-header">
+                    <p className="card-header-title">{agenda.name}</p>
+                  </header>
+                  <div className="card-content">
+                    <div className="content">
+                      <div>
+                        <strong>
+                          {agenda.datetime_start} - {agenda.datetime_end}
+                        </strong>
+                      </div>
+                      <div style={{ marginTop: "3%", marginBottom: "3%" }}>
+                        <div
+                          dangerouslySetInnerHTML={{
+                            __html: agenda.description
+                          }}
+                        ></div>
+                        <div style={{ marginTop: "4%" }}>
+                          <strong>Lugar</strong>
+                          <br />
+                          {agenda.space.name}
+                        </div>
+                      </div>
+                      {agenda.hosts.map((speaker, key) => (
+                        <div key={key}>
+                          <br style={{ marginTop: "5%" }} />
+                          <strong>Conferencista</strong>
+                          <p>{speaker.name}</p>
+                        </div>
+                      ))}
+                    </div>
+                    <div>
+                      <button
+                        onClick={() => this.registerInActivity(agenda._id)}
+                      >
+                        Inscribirme
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <footer className="card-footer">
+                  {agenda.activity_categories.map((cat, key) => (
+                    <span
+                      key={key}
+                      style={{
+                        background: cat.color,
+                        color: cat.color ? "white" : ""
+                      }}
+                      className="tag"
+                    >
+                      {cat.name}
+                    </span>
+                  ))}
+                </footer>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Agenda
+export default Agenda;
