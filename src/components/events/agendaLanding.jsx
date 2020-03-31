728132ef27619ed9a11b4cf32992c26f5edb95c8
Laura Prieto <laura.prieto@mocionsoft.com>
2020-03-31 08:41:46 -0500

    hidden video in agenda

--- src/components/events/agendaLanding.jsx
+++ src/components/events/agendaLanding.jsx
@@ -273,7 +273,7 @@ class Agenda extends Component {
                     <span class="card-header-title has-text-left">
                     {agenda.name}
                     </span>
-                  <ReactPlayer style={{maxWidth:"100%"}} url='https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8' controls playing />
+                  {/* <ReactPlayer style={{maxWidth:"100%"}} url='https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8' controls playing /> */}
                   </div>
             
     
