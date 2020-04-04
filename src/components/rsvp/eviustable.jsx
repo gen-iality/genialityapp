e4bf439bb55854db33ba187756658e9d0aee6528
JKLopez <juan.lopez@mocionsoft.com>
2020-04-03 19:36:08 -0500

    desarrollo parcial invitaciones usuarios

---
+++ src/components/rsvp/eviustable.jsx
@@ -0,0 +1,119 @@
+import React, {Fragment, useState, useEffect} from 'react';
+import {
+    useTable,useBlockLayout,
+    useGroupBy,
+    useFilters,
+    useSortBy,
+    useExpanded,
+    usePagination
+  } from 'react-table'
+  import { FixedSizeList } from 'react-window'
+
+
+export default function EviusTable({ columns, data }) {
+// Use the useTable Hook to send the columns and data to build the table
+const {
+    getTableProps, // table props from react-table
+    getTableBodyProps, // table body props from react-table
+    headerGroups, // headerGroups if your table have groupings
+    rows, // rows for the table based on the data passed
+    prepareRow, 
+    totalColumnsWidth  // Prepare the row (this function need to called for each row before getting the row props)
+  } = useTable({
+    columns,
+    data
+  });
+
+
+  return (
+    <table >
+    <thead className="ant-table-thead">
+      {headerGroups.map(headerGroup => (
+        <tr {...headerGroup.getHeaderGroupProps()}>
+          {headerGroup.headers.map(column => (
+            <th className="ant-table-header-column">{column.render('Header')}</th>
+          ))}
+        </tr>
+      ))}
+    </thead>
+    <tbody className="ant-table-tbody">
+      {rows.map(row => {
+        prepareRow(row)
+        return (
+          <tr className="ant-table-row nt-table-row-level-0">
+            {row.cells.map(cell => {
+              return <td>{cell.render('Cell')}</td>
+            })}
+          </tr>
+        )
+      })}
+    </tbody>
+  </table>
+  );
+
+
+    }
+
+
+
+/*  Inicio de metodo para cargar millones de datos trabajo en proceso
+
+  const RenderRow = React.useCallback(
+    ({ index, style }) => {
+      const row = rows[index]
+      prepareRow(row)
+      return (
+        <div
+          {...row.getRowProps({
+            style,
+          })}
+          className="tr"
+        >
+          {row.cells.map(cell => {
+            return (
+              <div {...cell.getCellProps()} className="td">
+                {cell.render('Cell')}
+              </div>
+            )
+          })}
+        </div>
+      )
+    },
+    [prepareRow, rows]
+  )
+
+
+
+  return (
+<div>
+    <div {...getTableProps()} className="table">
+    <div>
+      {headerGroups.map(headerGroup => (
+        <div {...headerGroup.getHeaderGroupProps()} className="tr">
+          {headerGroup.headers.map(column => (
+            <div {...column.getHeaderProps()} className="th">
+              {column.render('Header')}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+
+    <div {...getTableBodyProps()}>
+      <FixedSizeList
+        height={400}
+        itemCount={rows.length}
+        itemSize={50}
+        width={totalColumnsWidth*2}
+      >
+        {RenderRow}
+      </FixedSizeList>
+    </div>
+  </div>
+
+  </div>
+  
+  )
+
+
+*/
--- src/components/rsvp/index.jsx
+++ src/components/rsvp/index.jsx
@@ -1,109 +1,69 @@
-import React, {Fragment, useState, useEffect} from 'react';
-import {Route, Switch, withRouter} from "react-router-dom";
+import React, { Fragment, useState, useEffect } from "react";
+import { Route, Switch, withRouter } from "react-router-dom";
 import UsersRsvp from "./users";
 import { EventsApi, UsersApi } from "../../helpers/request";
 import ImportUsers from "../import-users/importUser";
-import {
-    useTable,
-    useGroupBy,
-    useFilters,
-    useSortBy,
-    useExpanded,
-    usePagination
-  } from 'react-table'
+import EviusTable from "./eviustable";
 
+function Tabla(props) {
+  const [guests, setGuests] = useState([]);
 
-  function Tabla(props){
-    let data = [
-        {
-          col1: 'Hello',
-          col2: 'World',
-        },
-        {
-          col1: 'react-table',
-          col2: 'rocks',
-        },
-        {
-          col1: 'whatever',
-          col2: 'you want',
-        },
-      ]      
-    const [guests, setGuests] = useState(data); 
+  const columns = [
+    {
+      Header: "Email",
+      accessor: "email" // accessor is the "key" in the data
+    },
+    {
+      Header: "Names",
+      accessor: "names"
+    }
+  ];
 
+  useEffect(() => {
+    console.log("mounted", props);
 
-    
-      const columns = [
-          {
-            Header: 'Column 1',
-            accessor: 'col1', // accessor is the "key" in the data
-          },
-          {
-            Header: 'Column 2',
-            accessor: 'col2',
-          },
-        ]
-    
-    useEffect(() => {
-        console.log('mounted',props)
-        
-        async function fetchData(props){
-            const guestsResult = await UsersApi.getAll(props.event._id, "?pageSize=10");
-            //setGuests(guestsResult.data);
-            console.log("resultado",props, guestsResult);
-        }
-        fetchData(props);
-    }, []);
+    async function fetchData(props) {
+      const guestsResult = await UsersApi.getAll(
+        props.event._id,
+        "?pageSize=1000"
+      );
+      setGuests(guestsResult.data);
+      console.log("resultado", props, guestsResult);
+    }
+    fetchData(props);
+  }, []);
 
-let {getTableProps,getTableBodyProps,headerGroups,rows,prepareRow} = useTable({ columns, data });
-
-
-return (
-
-
-
-    <table {...getTableProps()}>
-       <thead>
-         {headerGroups.map(headerGroup => (
-           <tr {...headerGroup.getHeaderGroupProps()}>
-             {headerGroup.headers.map(column => (
-               <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-             ))}
-           </tr>
-         ))}
-       </thead>
-       <tbody {...getTableBodyProps()}>
-         {rows.map(row => {
-           prepareRow(row)
-           return (
-             <tr {...row.getRowProps()}>
-               {row.cells.map(cell => {
-                 return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-               })}
-             </tr>
-           )
-         })}
-       </tbody>
-     </table>
-   
- )
-
-  
+  return <EviusTable columns={columns} data={guests} />;
 }
 
+function ListaInvitados({ ...props }) {
+  const { eventId, event, match } = props;
+  return (
+    <Fragment>
+      {/* <Tabla {...props} /> */}
 
-function ListaInvitados({...props}) {
-    const {eventId, event, match} = props;
-    return (
-        <Fragment>
-            <Tabla {...props}/>
-            <Switch>
-               {/* 
-                <Route exact path={`${match.url}/`} render={()=><UsersRsvp event={event} eventID={eventId} matchUrl={match.url}/>}/>
-                */}
-                <Route exact path={`${match.url}/importar-excel`} render={()=><ImportUsers extraFields={event.user_properties} eventId={eventId} matchUrl={match.url}/>}/>
-            </Switch>
-        </Fragment>
-    );
+      <Switch>
+        <Route
+          exact
+          path={`${match.url}/`}
+          render={() => (
+            <UsersRsvp event={event} eventID={eventId} matchUrl={match.url} />
+          )}
+        />
+        <Route
+          exact
+          path={`${match.url}/importar-excel`}
+          render={() => (
+            <ImportUsers
+              extraFields={event.user_properties}
+              eventId={eventId}
+              matchUrl={match.url}
+            />
+          )}
+        />
+      </Switch>
+    </Fragment>
+  );
 }
 
 export default withRouter(ListaInvitados);
