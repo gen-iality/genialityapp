a2745bd94e7c14cf0283c0594b9537d70de9fabd
Esteban Sanchez <esteban.sanchez@mocionsoft.com>
2020-04-06 11:51:43 -0500

    added section created questions

--- src/components/trivia/index.jsx
+++ src/components/trivia/index.jsx
@@ -1,18 +1,33 @@
-import React, {Fragment} from 'react';
-import {Route, Switch, withRouter} from "react-router-dom";
+import React, { Fragment } from "react";
+import { Route, Switch, withRouter } from "react-router-dom";
 import Trivia from "./trivia";
 import TriviaEdit from "./edit";
+import FormQuestions from "./questions";
 
-function TriviaRoutes({...props}){
-    const {event, match} = props;
-    return (
-        <Fragment>
-            <Switch>
-                <Route exact path={`${match.url}/`} render={()=><Trivia event={event} matchUrl={match.url}/>}/>
-                <Route exact path={`${match.url}/encuesta`} render={()=><TriviaEdit event={event} matchUrl={match.url}/>}/>
-            </Switch>
-        </Fragment>
-    );
+function TriviaRoutes({ ...props }) {
+  const { event, match } = props;
+  return (
+    <Fragment>
+      <Switch>
+        <Route
+          exact
+          path={`${match.url}/`}
+          render={() => <Trivia event={event} matchUrl={match.url} />}
+        />
+        <Route
+          exact
+          path={`${match.url}/encuesta`}
+          render={() => <TriviaEdit event={event} matchUrl={match.url} />}
+        />
+        <Route
+          exact
+          //   path={`${match.url}/encuesta/:id/preguntas`}
+          path={`${match.url}/encuesta/preguntas`}
+          render={() => <FormQuestions event={event} matchUrl={match.url} />}
+        />
+      </Switch>
+    </Fragment>
+  );
 }
 
 export default withRouter(TriviaRoutes);
