c304a6fa5a12797dfa1cabb21af4781caae6a0a9
Brayan Triana <brayan.triana@mocionsoft.com>
2020-04-08 20:06:13 -0500

    Menú de landing arreglado, vista de faqs editado

--- src/components/events/landing.jsx
+++ src/components/events/landing.jsx
@@ -611,22 +611,7 @@ class Landing extends Component {
                         <span>
                           Preguntas Frecuentes
                         </span>
-                      </Menu.Item>
-
-
-                      <Menu.Item 
-                        key="9"
-                        onClick={e => {
-                          this.showSection("faqs");
-                        }}
-                        >
-                          <QuestionCircleOutlined />
-                          <span>
-                            Preguntas Frecuentes
-                          </span>
-                        </Menu.Item>
-
-                      
+                      </Menu.Item>        
                   </Menu>
 
                   
@@ -795,8 +780,6 @@ class Landing extends Component {
                             Preguntas Frecuentes
                           </span>
                         </Menu.Item>
-
-                        faqs
                       </Menu>
 
                     </Drawer>
