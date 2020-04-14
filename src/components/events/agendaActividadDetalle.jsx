f82782aea53c92f646ba5fb6bdc5c97b2f117c37
Esteban Sanchez <esteban.sanchez@mocionsoft.com>
2020-04-14 10:46:37 -0500

    removed import list duplicated

--- src/components/events/agendaActividadDetalle.jsx
+++ src/components/events/agendaActividadDetalle.jsx
@@ -11,7 +11,7 @@ import { addLoginInformation, showMenu } from "../../redux/user/actions";
 import { NavLink, Link, withRouter } from "react-router-dom";
 import { List, Avatar } from "antd";
 import SurveyComponent from "./surveys/surveyComponent";
-import { PageHeader, Alert, Row, Col, Tag, List, Button, Drawer } from "antd";
+import { PageHeader, Alert, Row, Col, Tag, Button, Drawer } from "antd";
 import AttendeeNotAllowedCheck from "./shared/attendeeNotAllowedCheck";
 
 let agendaActividadDetalle = props => {
