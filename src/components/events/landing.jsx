40ea795d60a1f1d39eb4044df67c30aa9f8d3bd6
JKLopez <juan.lopez@mocionsoft.com>
2020-05-14 10:36:43 -0500

    refactoring componente mapa

--- src/components/events/landing.jsx
+++ src/components/events/landing.jsx
@@ -228,8 +228,26 @@ class Landing extends Component {
                   )}
               </div>
             </Card>
+
+
+
+
           </div>
-          <MapComponent event={event} toggleConference={this.toggleConference} namesUser={this.state.namesUser} />
+
+          <div className="column container-map">
+            {/*           <AttendeeNotAllowedCheck
+              event={event}
+              usuarioRegistrado={this.state.eventUser}
+            />
+
+            <VirtualConference
+              event={event}
+              toggleConference={this.toggleConference}
+              currentUser={this.state.currentUser}
+ />*/}
+
+            <MapComponent event={event} toggleConference={this.toggleConference} namesUser={this.state.namesUser} />
+          </div>
         </div>
       ),
     };
@@ -459,6 +477,7 @@ class Landing extends Component {
                   }}
                 />
               </div>
+
               <Dialog
                 modal={modalTicket}
                 title={"Atención!!"}
@@ -484,58 +503,58 @@ const MapComponent = (props) => {
   const { event, toggleConference, namesUser } = props;
   { console.log(props.event) }
   return (
-    <div className="column container-map">
-      <div>
-        {
-          (console.log(event),
-            event.type_event === "onlineEvent" ? (
-              <></>
-            ) : (
-                <div>
-                  <Card>
-                    <div className="map-head">
-                      <h2 className="is-size-5 has-text-left">
-                        <b>Encuentra la ubicación</b>
-                      </h2>
-                      <div className="lugar item columns">
-                        <div className="column is-12 container-icon hours has-text-left">
-                          <span className="icon is-small">
-                            <i className="far fa-clock" />
-                          </span>
-                          <span className="subt is-size-6 has-text-left">
-                            {""} Desde {Moment(event.hour_start).format("HH:mm")}
-                          </span>
-                          <span className="subt is-size-6 has-text-left"> a {Moment(event.hour_end).format("HH:mm")}</span>
-                        </div>
-                      </div>
-                      <div className="lugar item columns">
-                        <div className="column is-12 container-icon has-text-left">
-                          <span className="icon is-small">
-                            <i className="fas fa-map-marker-alt" />
-                          </span>
-                          <span className="has-text-left">
-                            {""} {event.venue} {event.location.FormattedAddress}
-                          </span>
-                        </div>
+
+    <div>
+      {
+        (console.log(event),
+          event.type_event === "onlineEvent" ? (
+            <></>
+          ) : (
+              <div>
+                <Card>
+                  <div className="map-head">
+                    <h2 className="is-size-5 has-text-left">
+                      <b>Encuentra la ubicación</b>
+                    </h2>
+                    <div className="lugar item columns">
+                      <div className="column is-12 container-icon hours has-text-left">
+                        <span className="icon is-small">
+                          <i className="far fa-clock" />
+                        </span>
+                        <span className="subt is-size-6 has-text-left">
+                          {""} Desde {Moment(event.hour_start).format("HH:mm")}
+                        </span>
+                        <span className="subt is-size-6 has-text-left"> a {Moment(event.hour_end).format("HH:mm")}</span>
                       </div>
                     </div>
-                  </Card>
-                  <div style={{ height: "400px", width: "100%" }}>
-                    <GoogleMapReact
-                      bootstrapURLKeys={{ key: EVIUS_GOOGLE_MAPS_KEY }}
-                      defaultCenter={{
-                        lat: event.location.Latitude,
-                        lng: event.location.Longitude,
-                      }}
-                      defaultZoom={11}>
-                      <AnyReactComponent lat={event.location.Latitude} lng={event.location.Longitude} text="My Marker" />
-                    </GoogleMapReact>
+                    <div className="lugar item columns">
+                      <div className="column is-12 container-icon has-text-left">
+                        <span className="icon is-small">
+                          <i className="fas fa-map-marker-alt" />
+                        </span>
+                        <span className="has-text-left">
+                          {""} {event.venue} {event.location.FormattedAddress}
+                        </span>
+                      </div>
+                    </div>
                   </div>
+                </Card>
+                <div style={{ height: "400px", width: "100%" }}>
+                  <GoogleMapReact
+                    bootstrapURLKeys={{ key: EVIUS_GOOGLE_MAPS_KEY }}
+                    defaultCenter={{
+                      lat: event.location.Latitude,
+                      lng: event.location.Longitude,
+                    }}
+                    defaultZoom={11}>
+                    <AnyReactComponent lat={event.location.Latitude} lng={event.location.Longitude} text="My Marker" />
+                  </GoogleMapReact>
                 </div>
-              ))
-        }
-      </div>
+              </div>
+            ))
+      }
     </div>
+
   );
 };
 
