ec36fe5baffe1b55baee2621de116050d8d633c1
Laura Prieto <laura.prieto@mocionsoft.com>
2020-05-14 11:43:33 -0500

    adding component for survey Notification

---
+++ src/components/events/surveyNotification.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react'
+import { Button, notification, Drawer } from 'antd';
+
+
+let surveyNotification = () => {
+
+    let [drawerVisible, setDrawerVisible] = useState(false)
+
+    // función que abre el drawer
+    function clearStates(visible) {
+        setDrawerVisible(visible)
+    }
+
+    // función que cierra el notificación 
+    const close = () => {
+        console.log(
+            'Encuesta',
+        );
+    };
+
+
+    function surveyVisible(key, visible) {
+        notification.close(key)
+        clearStates(visible)
+    }
+
+    // Esta funcion construye la notificación. 
+
+    const openNotification = () => {
+        const key = `open${Date.now()}`;
+        const btn = (
+            <Button type="primary" size="small" onClick={() => surveyVisible(key, true)}>
+                Contestar
+            </Button>
+
+        );
+        notification.open({
+            message: 'Encuesta en vivo',
+            description:
+                'Se encuentra activa la encuesta',
+            btn,
+            key,
+            duration: 0,
+            onClose: close,
+        });
+    };
+
+
+    return (
+        <>
+            <Button type="primary" onClick={openNotification}>
+                Open Notification
+            </Button>
+
+            <Drawer
+                title="Basic Drawer"
+                placement="right"
+                closable={true}
+                width={"auto"}
+                onClose={() => clearStates()}
+                visible={drawerVisible}
+            >
+            </Drawer>
+        </>
+    );
+}
+
+
+export default surveyNotification;
+
