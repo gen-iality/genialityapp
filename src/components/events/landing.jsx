51c5ec629dc10e2c1e1cc77a8cbc6652e7ec2ac1
JKLopez <juan.lopez@mocionsoft.com>
2020-05-15 18:40:56 -0500

    registro

--- src/components/events/landing.jsx
+++ src/components/events/landing.jsx
@@ -169,8 +169,10 @@ class Landing extends Component {
 
     this.loadDynamicEventStyles(id);
 
-
-    let eventUser = await EventsApi.getEventUser(user._id, event._id);
+    let eventUser = null;
+    if (event && user) {
+      let eventUser = await EventsApi.getEventUser(user._id, event._id);
+    }
 
 
 
