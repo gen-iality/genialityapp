15aad14080c29c673423bf2807e4358dc4b09b79
Esteban Sanchez <esteban.sanchez@mocionsoft.com>
2020-05-26 12:09:07 -0500

    created anonymous survey list component

--- src/components/events/surveys/surveyList.jsx
+++ src/components/events/surveys/surveyList.jsx
@@ -2,30 +2,88 @@ import React, { useState, useEffect, Fragment } from "react";
 
 import { List, Button, Card, Col, Tag, Spin, Result, Empty } from "antd";
 import { MehOutlined } from "@ant-design/icons";
+
+const AnonymousList = ({ anonymousSurveyList, showSurvey, usuarioRegistrado }) => {
+  return (
+    <Fragment>
+      {anonymousSurveyList && anonymousSurveyList.length == 0 && (
+        <Result icon={<MehOutlined />} title="Aún no se han publicado encuestas" />
+      )}
+      {console.log("anonymousSurveyList:", anonymousSurveyList)}
+      {anonymousSurveyList && anonymousSurveyList.length > 0 && (
+        <List
+          dataSource={anonymousSurveyList}
+          renderItem={(survey) =>
+            survey.open == "true" ? (
+              <Fragment>
+                {survey.allow_anonymous_answers === "true" && !usuarioRegistrado && (
+                  <List.Item key={survey._id}>
+                    <List.Item.Meta title={survey.survey} style={{ textAlign: "left" }} />
+                    {survey.userHasVoted && (
+                      <div>
+                        <Tag color="success">Respondida</Tag>
+                      </div>
+                    )}
+                    <div>
+                      <Button
+                        type={!survey.userHasVoted ? "primary" : ""}
+                        className={`${
+                          !survey.userHasVoted
+                            ? "animate__animated  animate__pulse animate__slower animate__infinite"
+                            : ""
+                        }`}
+                        onClick={() => showSurvey(survey)}
+                        loading={survey.userHasVoted == undefined}>
+                        {!survey.userHasVoted ? "Ir a la Encuesta" : " Ver Resultados"}
+                      </Button>
+                    </div>
+                  </List.Item>
+                )}
+              </Fragment>
+            ) : (
+              <Fragment>
+                {survey.allow_anonymous_answers === "true" && !usuarioRegistrado && (
+                  <List.Item key={survey._id} actions={[]}>
+                    <List.Item.Meta title={survey.survey} style={{ textAlign: "left" }} />
+                    <div>
+                      <Tag color="red">Cerrada</Tag>
+                    </div>
+                    <Button onClick={() => showSurvey(survey)}>Ver Resultados</Button>
+                  </List.Item>
+                )}
+              </Fragment>
+            )
+          }
+        />
+      )}
+    </Fragment>
+  );
+};
+
 export default ({ jsonData, showSurvey, usuarioRegistrado }) => {
   const [surveyList, setSurveyList] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [anonymousSurveyList, setAnonymousSurveyList] = useState([]);
 
   useEffect(() => {
     setSurveyList(jsonData);
-    if (jsonData) setLoading(false);
+    setAnonymousSurveyList(jsonData.filter((item) => item.allow_anonymous_answers === "true"));
     console.log(usuarioRegistrado);
   }, [jsonData]);
 
   return (
     <Col xs={24} sm={22} md={18} lg={18} xl={22} style={{ margin: "0 auto" }}>
       <Card>
-        {surveyList && surveyList.length == 0 && (
-          <Result icon={<MehOutlined />} title="Aún no se han publicado encuestas" />
-        )}
+        {usuarioRegistrado ? (
+          <Fragment>
+            {surveyList && surveyList.length == 0 && (
+              <Result icon={<MehOutlined />} title="Aún no se han publicado encuestas" />
+            )}
 
-        {surveyList && surveyList.length > 0 && (
-          <List
-            dataSource={surveyList}
-            renderItem={(survey) =>
-              survey.open == "true" ? (
-                <Fragment>
-                  {(usuarioRegistrado || (survey.allow_anonymous_answers === "true" && !usuarioRegistrado)) && (
+            {surveyList && surveyList.length > 0 && (
+              <List
+                dataSource={surveyList}
+                renderItem={(survey) =>
+                  survey.open == "true" ? (
                     <List.Item key={survey._id}>
                       <List.Item.Meta title={survey.survey} style={{ textAlign: "left" }} />
                       {survey.userHasVoted && (
@@ -47,11 +105,7 @@ export default ({ jsonData, showSurvey, usuarioRegistrado }) => {
                         </Button>
                       </div>
                     </List.Item>
-                  )}
-                </Fragment>
-              ) : (
-                <Fragment>
-                  {(usuarioRegistrado || (survey.allow_anonymous_answers === "true" && !usuarioRegistrado)) && (
+                  ) : (
                     <List.Item key={survey._id} actions={[]}>
                       <List.Item.Meta title={survey.survey} style={{ textAlign: "left" }} />
                       <div>
@@ -59,10 +113,16 @@ export default ({ jsonData, showSurvey, usuarioRegistrado }) => {
                       </div>
                       <Button onClick={() => showSurvey(survey)}>Ver Resultados</Button>
                     </List.Item>
-                  )}
-                </Fragment>
-              )
-            }
+                  )
+                }
+              />
+            )}
+          </Fragment>
+        ) : (
+          <AnonymousList
+            anonymousSurveyList={anonymousSurveyList}
+            showSurvey={showSurvey}
+            usuarioRegistrado={usuarioRegistrado}
           />
         )}
       </Card>
