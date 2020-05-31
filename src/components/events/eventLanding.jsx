bd97fc3601dac5148175230d80b64c19c72ef7f5
JKLopez <juan.lopez@mocionsoft.com>
2020-05-31 14:38:08 -0500

    quitando extrana condición de 11 caracteres para mostrar el titulo de evento en el landing

--- src/components/events/eventLanding.jsx
+++ src/components/events/eventLanding.jsx
@@ -16,37 +16,31 @@ class eventLanding extends Component {
         const { event } = this.props
         return (
             <div className="description-container column is-8">
-                {event.description.length > 11 ?
-                    <div>
-                        <Card className="event-description" bodyStyle={{ padding: "25px 5px" }} bordered={true}>
-                            <h1 className="is-size-4-desktop has-text-weight-semibold">{event.name}</h1>
+                <Card className="event-description" bodyStyle={{ padding: "25px 5px" }} bordered={true}>
+                    <h1 className="is-size-4-desktop has-text-weight-semibold">{event.name}</h1>
 
-                            {event.video && (
-                                <div className="column is-centered mediaplayer">
-                                    <ReactPlayer
-                                        width={"100%"}
-                                        style={{
-                                            display: "block",
-                                            margin: "0 auto",
-                                        }}
-                                        //
-                                        url={event.video}
-                                        //url="https://firebasestorage.googleapis.com/v0/b/eviusauth.appspot.com/o/eviuswebassets%2FLa%20asamblea%20de%20copropietarios_%20una%20pesadilla%20para%20muchos.mp4?alt=media&token=b622ad2a-2d7d-4816-a53a-7f743d6ebb5f"
-                                        controls
-                                    />
-                                </div>
-                            )}
+                    {event.video && (
+                        <div className="column is-centered mediaplayer">
+                            <ReactPlayer
+                                width={"100%"}
+                                style={{
+                                    display: "block",
+                                    margin: "0 auto",
+                                }}
+                                url={event.video}
+                                //url="https://firebasestorage.googleapis.com/v0/b/eviusauth.appspot.com/o/eviuswebassets%2FLa%20asamblea%20de%20copropietarios_%20una%20pesadilla%20para%20muchos.mp4?alt=media&token=b622ad2a-2d7d-4816-a53a-7f743d6ebb5f"
+                                controls
+                            />
+                        </div>
+                    )}
 
-                            <div>
-                                {typeof event.description === "string" ? (
-                                    <ReactQuill value={event.description} modules={{ toolbar: false }} readOnly={true} theme="bubble" />
-                                ) : (
-                                        "json"
-                                    )}
-                            </div>
-                        </Card>
-                    </div> :
-                    <div></div>}
+                    {(event.description && typeof event.description === "string") && (
+                        <div>
+                            <ReactQuill value={event.description} modules={{ toolbar: false }} readOnly={true} theme="bubble" />
+                        </div>
+                    )}
+
+                </Card>
             </div>
         )
     }
