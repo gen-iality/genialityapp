586cc2b58f0f650f2745b9d413e83be972709471
Brayan Triana <brayan.triana@mocionsoft.com>
2020-06-12 15:55:05 -0500

    vista de documentsList re-configurada, muestra de vista diferente, con base a activity_id

--- src/components/documents/documentsList.jsx
+++ src/components/documents/documentsList.jsx
@@ -3,10 +3,9 @@ import React, { Component } from "react";
 //custom
 import { getFiles } from "./services";
 import { ApiGoogleDocuments } from "../../helpers/constants";
-import { List, Col, Row, Card, Button, Table, Tag, Space } from 'antd';
-import { AgendaApi, DocumentsApi } from "../../helpers/request"
-
-import { LikeOutlined, EyeOutlined } from '@ant-design/icons';
+import { List, Col, Row, Card, Button, Table } from 'antd';
+import { LikeOutlined, EyeOutlined, DownloadOutlined } from '@ant-design/icons';
+import { AgendaApi } from "../../helpers/request"
 
 const columns = [
     {
@@ -21,14 +20,11 @@ const columns = [
     },
     {
         render: (item) => (
-            <Space size="middle">
-                <a target="_blank" href={item.file} download>
-                    <IconText
-                        text="Observar"
-                        icon={EyeOutlined}
-
-                    /></a>
-            </Space>
+            <a target="_blank" href={item.file} download>
+                <IconText
+                    text="Descargar"
+                    icon={DownloadOutlined}
+                /></a>
         ),
     },
 ];
@@ -51,36 +47,80 @@ class documentsList extends Component {
 
     constructor(props) {
         super(props);
+        console.log("INNERPROPS", props);
         this.state = {
             data: props.data || [],
             documentDates: []
         };
-        this.getDatesFromDocumentList = this.getDatesFromDocumentList.bind(this)
     }
+
     componentDidMount() {
         this.getDatesFromDocumentList()
     }
 
     async getDatesFromDocumentList() {
         const { data } = this.state
+        console.log(data)
         const documentDates = []
         for (let i = 0; i < data.length; i++) {
-            const agenda = await AgendaApi.getOne(data[i].activity_id)
-            documentDates.push({
-                activity: agenda.name,
-                document: data[i].title,
-                file: data[i].file
-            })
+            if (data[i].activity_id) {
+                const agenda = await AgendaApi.getOne(data[i].activity_id, data[i].event_id)
+                documentDates.push({
+                    activity: agenda.name,
+                    document: data[i].title ? data[i].title : data[i].name,
+                    file: data[i].file
+                })
+                this.setState({ documentDates })
+            }
         }
-        this.setState({ documentDates })
     }
 
     render() {
-        const { documentDates } = this.state;
+        const { documentDates, data } = this.state;
 
         return (
             <Card style={{ textAlign: "left" }}>
-                <Table columns={columns} dataSource={documentDates} />
+                {
+                    data[0].activity_id ?
+                        <div>
+                            <Table dataSource={documentDates} columns={columns} />
+                        </div>
+                        :
+                        <div>
+                            <List itemLayout="horizontal"
+                                //Se traen los datos del state 
+                                dataSource={data}
+                                //se mapean los datos del array data
+                                renderItem={item => (
+                                    <List.Item key={item._id}
+                                        //boton de descarga
+                                        actions={[
+                                            <a target="_blank" href={item.file} download>
+                                                <IconText
+                                                    text="Descargar"
+                                                    icon={DownloadOutlined}
+
+                                                /></a>,
+                                            // <a
+                                            //     href={ApiGoogleDocuments + encodeURIComponent(item.file)}
+                                            //     target="_blank"
+                                            //     style={{ wordBreak: "break-word" }}
+                                            // >
+                                            //     <IconText
+                                            //         text="Previsualizar"
+                                            //         icon={EyeOutlined}
+                                            //     />
+                                            // </a>
+                                        ]}
+                                    >
+                                        <div>
+                                            <List.Item.Meta title={item.title ? item.title : item.name} />
+                                        </div>
+                                    </List.Item>
+                                )}
+                            />
+                        </div>
+                }
             </Card>
         )
     }
