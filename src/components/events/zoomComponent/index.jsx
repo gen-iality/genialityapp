f2da1751e1bd1d75bcff5ec5b3edc44f1d29c129
Brayan Triana <brayan.triana@mocionsoft.com>
2020-07-07 12:07:56 -0500

    validacion por medio de id para maggie

--- src/components/events/zoomComponent/index.jsx
+++ src/components/events/zoomComponent/index.jsx
@@ -138,34 +138,40 @@ export default class ZoomComponent extends Component {
             <SurveyComponent event={event} activity={activity} availableSurveysBar={true} />
           }
 
+          {
+            event._id === "5efd0295c9d8313f9a671d53" ?
+              // VIMEO LIVESTREAMING only chat not interactive
+              <div style={{ "padding": "39.3% 0 0 0", "width": "100%", "position": "relative" }}>
+                <iframe
+                  src="https://vimeo.com/event/156201/embed/ea3234a09b"
+                  frameborder="0"
+                  allow="autoplay; fullscreen; camera *;microphone *"
+                  allowFullScreen
+                  allowUserMedia
+                  style={{ "position": "absolute", "top": 0, "left": 0, "width": "70%", "height": "100%" }}
+                ></iframe>
+
+                <iframe
+                  src="https://vimeo.com/event/156201/chat/ea3234a09b"
+                  style={{ "position": "absolute", "top": 0, "right": 0, "width": "30%", "height": "100%" }}
+                  frameborder=""
+                ></iframe>
+              </div>
+              :
+              // ZOOOM IFRAME
+              <iframe
+                src={url_conference + meeting_id + `&userName=${displayName}` + `&email=${email}`}
+                allow="camera *;microphone *"
+                allowusermedia
+                className="iframe-zoom nuevo">
+                <p>Your browser does not support iframes.</p>
+              </iframe>
+          }
 
 
-          {/* ZOOOM IFRAME */}
-          {/* <iframe
-            src={url_conference + meeting_id + `&userName=${displayName}` + `&email=${email}`}
-            allow="camera *;microphone *"
-            allowusermedia
-            className="iframe-zoom nuevo">
-            <p>Your browser does not support iframes.</p>
-          </iframe> */}
 
-          {/** VIMEO LIVESTREAMING only chat not interactive */}
-          <div style={{ "padding": "39.3% 0 0 0", "width": "100%", "position": "relative" }}>
-            <iframe
-              src="https://vimeo.com/event/156201/embed/ea3234a09b"
-              frameborder="0"
-              allow="autoplay; fullscreen; camera *;microphone *"
-              allowFullScreen
-              allowUserMedia
-              style={{ "position": "absolute", "top": 0, "left": 0, "width": "70%", "height": "100%" }}
-            ></iframe>
 
-            <iframe
-              src="https://vimeo.com/event/156201/chat/ea3234a09b"
-              style={{ "position": "absolute", "top": 0, "right": 0, "width": "30%", "height": "100%" }}
-              frameborder=""
-            ></iframe>
-          </div>
+
         </Fullscreen>
       </div>
     );
