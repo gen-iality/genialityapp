060ef3db8efccf6f7de442faad7e00835d5399be
Brayan Triana <brayan.triana@mocionsoft.com>
2020-07-07 18:12:26 -0500

    pagina export para networking creada pdte por funcionalidad con datos reales

---
+++ src/components/networking/report.jsx
@@ -0,0 +1,172 @@
+import React, { Component, Fragment } from "react";
+import { Row, Col, Button, Table, Input, Space } from 'antd';
+import Highlighter from 'react-highlight-words';
+import { SearchOutlined } from '@ant-design/icons';
+
+import * as Cookie from "js-cookie";
+import API, { EventsApi, RolAttApi, EventFieldsApi } from "../../helpers/request";
+import { getCurrentUser, getCurrentEventUser, userRequest } from "./services";
+
+
+const style = { background: '#0092ff', padding: '8px 0' };
+const data = [
+    {
+        key: '1',
+        name: 'John Brown',
+        age: 32,
+        address: 'New York No. 1 Lake Park',
+    },
+    {
+        key: '2',
+        name: 'Joe Black',
+        age: 42,
+        address: 'London No. 1 Lake Park',
+    },
+    {
+        key: '3',
+        name: 'Jim Green',
+        age: 32,
+        address: 'Sidney No. 1 Lake Park',
+    },
+    {
+        key: '4',
+        name: 'Jim Red',
+        age: 32,
+        address: 'London No. 2 Lake Park',
+    },
+];
+
+class Report extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchText: '',
+            searchedColumn: '',
+            changeItem: false,
+        }
+    }
+
+    async componentDidMount() {
+        this.loadData();
+    }
+
+    loadData = async () => {
+        let { changeItem } = this.state;
+        const { event } = this.props;
+
+        // Servicio que trae la lista de asistentes excluyendo el usuario logeado
+        let eventUserList = await userRequest.getEventUserList(event._id, Cookie.get("evius_token"));
+        let asistantData = await EventFieldsApi.getAll(event._id)
+        // console.log("eventUserList:", eventUserList);
+        this.setState((prevState) => {
+            return {
+                userReq: eventUserList,
+                users: eventUserList,
+                changeItem,
+                loading: false,
+                clearSearch: !prevState.clearSearch,
+                asistantData
+            };
+        });
+    };
+
+    getColumnSearchProps = dataIndex => ({
+        filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+            <div style={{ padding: 8 }}>
+                <Input
+                    ref={node => {
+                        this.searchInput = node;
+                    }}
+                    placeholder={`Search ${dataIndex}`}
+                    value={selectedKeys[0]}
+                    onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+                    onPressEnter={() => this.handleSearch(selectedKeys, confirm, dataIndex)}
+                    style={{ width: 188, marginBottom: 8, display: 'block' }}
+                />
+                <Space>
+                    <Button
+                        type="primary"
+                        onClick={() => this.handleSearch(selectedKeys, confirm, dataIndex)}
+                        icon={<SearchOutlined />}
+                        size="small"
+                        style={{ width: 90 }}
+                    >
+                        Search
+              </Button>
+                    <Button onClick={() => this.handleReset(clearFilters)} size="small" style={{ width: 90 }}>
+                        Reset
+              </Button>
+                </Space>
+            </div>
+        ),
+        filterIcon: filtered => <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />,
+        onFilter: (value, record) =>
+            record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+        onFilterDropdownVisibleChange: visible => {
+            if (visible) {
+                setTimeout(() => this.searchInput.select());
+            }
+        },
+        render: text =>
+            this.state.searchedColumn === dataIndex ? (
+                <Highlighter
+                    highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+                    searchWords={[this.state.searchText]}
+                    autoEscape
+                    textToHighlight={text.toString()}
+                />
+            ) : (
+                    text
+                ),
+    });
+
+    handleSearch = (selectedKeys, confirm, dataIndex) => {
+        confirm();
+        this.setState({
+            searchText: selectedKeys[0],
+            searchedColumn: dataIndex,
+        });
+    };
+
+    handleReset = clearFilters => {
+        clearFilters();
+        this.setState({ searchText: '' });
+    };
+
+    render() {
+        const columns = [
+            {
+                title: 'Name',
+                dataIndex: 'name',
+                key: 'name',
+                width: '30%',
+                ...this.getColumnSearchProps('name'),
+            },
+            {
+                title: 'Age',
+                dataIndex: 'age',
+                key: 'age',
+                width: '20%',
+                ...this.getColumnSearchProps('age'),
+            },
+            {
+                title: 'Address',
+                dataIndex: 'address',
+                key: 'address',
+                ...this.getColumnSearchProps('address'),
+            },
+        ];
+        return (
+            <Fragment>
+                <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+                    <Col className="gutter-row" span={6}>
+                        <Button>Exportar</Button>
+                    </Col>
+                </Row>
+                <Table columns={columns} dataSource={data} style={{ marginTop: "5%" }} />
+            </Fragment>
+        )
+    }
+}
+
+export default Report
