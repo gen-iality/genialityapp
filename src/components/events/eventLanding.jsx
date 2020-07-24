87bcc73f5d143b2e6ec6fb0a9d5338fc4574e09f
Brayan Triana <brayan.triana@mocionsoft.com>
2020-07-23 22:33:31 -0500

    estilos agregados, en recomendacion con Diana

--- src/components/events/eventLanding.jsx
+++ src/components/events/eventLanding.jsx
@@ -4,6 +4,7 @@ import { Card } from "antd";
 import ReactQuill from "react-quill";
 import ReactPlayer from "react-player";
 import { Row, Col, Button } from 'antd';
+import NetworkingForm from "../networking";
 
 class eventLanding extends Component {
     constructor(props) {
@@ -16,9 +17,12 @@ class eventLanding extends Component {
     render() {
         const { event } = this.props
         return (
-            <div className="description-container column is-12" style={{ width: "66.66667%" }}>
+            <div className="description-container column is-12" style={{ width: "80.66667%" }}>
                 <Card className="event-description" bodyStyle={{ padding: "25px 5px" }} bordered={true}>
-                    <h1 className="is-size-4-desktop has-text-weight-semibold">{event.name}</h1>
+                    {
+                        event._id === "5f0622f01ce76d5550058c32" ? <></> : <h1 className="is-size-4-desktop has-text-weight-semibold">{event.name}</h1>
+                    }
+
 
                     {event.video && (
                         <div className="column is-centered mediaplayer">
@@ -39,18 +43,18 @@ class eventLanding extends Component {
                         event._id === "5f0622f01ce76d5550058c32" ? (
                             <div>
                                 <div className="containerfenalco">
-                                    <Row gutter={[6, 16]}>
-                                        <Col xs={16} sm={16} md={6} lg={6} xl={6}>
+                                    <Row gutter={[8, 16]}>
+                                        <Col xs={16} sm={16} md={6} lg={8} xl={8}>
                                             <div className="imagen">
                                                 <img src="https://firebasestorage.googleapis.com/v0/b/eviusauth.appspot.com/o/Btn-A3.png?alt=media&token=3ff840dc-d9a6-4ea1-9e9c-a623cb796ef5" />
                                             </div>
                                         </Col>
-                                        <Col xs={16} sm={16} md={6} lg={6} xl={6}>
+                                        <Col xs={16} sm={16} md={6} lg={8} xl={8}>
                                             <div className="imagen">
                                                 <img src="https://firebasestorage.googleapis.com/v0/b/eviusauth.appspot.com/o/Btn-B3.png?alt=media&token=d9a64548-1fed-43d8-9adf-3aaee0e719f5" />
                                             </div>
                                         </Col>
-                                        <Col xs={16} sm={16} md={6} lg={6} xl={6}>
+                                        <Col xs={16} sm={16} md={6} lg={8} xl={8}>
                                             <img src="https://firebasestorage.googleapis.com/v0/b/eviusauth.appspot.com/o/Btn-C3.png?alt=media&token=615fb718-af55-478f-b444-d8486edfc24a" />
                                         </Col>
                                     </Row>
