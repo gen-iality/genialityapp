5bf8d8c14d977f72bc4a966fd2fd49be9f937d11
Diana <dianamilu15@gmail.com>
2020-07-27 19:37:23 -0500

    animacion en la imagen

---
+++ src/components/shared/Animate_Img/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { } from 'antd';
+import './Index.css';
+
+class AnimateImg extends React.Component {
+
+    render() {
+        return (
+            <>
+                <div className="imagen">
+                </div>
+            </>
+        );
+    }
+}
+
+export default AnimateImg;
