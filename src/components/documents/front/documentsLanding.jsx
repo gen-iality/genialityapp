40daad3d1ca35e2161d34d945e0c192eb10d6c9c
Andres Rosero <andres@andres.sh>
2020-07-31 08:42:07 -0500

    loader mientras se cargan los documentos

--- src/components/documents/front/documentsLanding.jsx
+++ src/components/documents/front/documentsLanding.jsx
@@ -1,3 +1,4 @@
+import { notification } from 'antd'
 import React, { Component } from "react";
 
 //custom
@@ -5,6 +6,7 @@ import { getFiles } from "../services";
 import { ApiGoogleDocuments } from "../../../helpers/constants";
 import { List, Col, Row, Card, Button, Result } from 'antd';
 import { LikeOutlined, DownloadOutlined } from '@ant-design/icons';
+import Loading from '../../loaders/loading'
 
 import DocumentsList from "../documentsList";
 
@@ -28,7 +30,7 @@ class documentsDetail extends Component {
         super(props);
         this.state = {
             documents: [],
-            loading: false,
+            loading: true,
             category_id: null,
             previewImage: "",
             data: []
@@ -37,22 +39,37 @@ class documentsDetail extends Component {
 
     async componentDidMount() {
         let { documents } = this.state;
-
-        //console.log(this.props)
         let data = []
-        documents = await getFiles(this.props.eventId);
 
-        //Se itera para poder pasar un array al componente List de ant
-        for (const document in documents) {
-            console.log(documents[document]);
+        try {
+          documents = await getFiles(this.props.eventId);
+
+          //Se itera para poder pasar un array al componente List de ant
+          for (const document in documents) {
             data.push(documents[document])
+          }
+
+          this.setState({ data }, this.removeLoader)
+        } catch (error) {
+          console.error(error)
+
+          notification.error({
+            message: 'Error',
+            description: 'Ha ocurrido un error obteniendo los documentos'
+          })
         }
-        console.log("data", data)
-        this.setState({ data })
+    }
+
+    removeLoader = () => {
+      this.setState({ loading: false })
     }
 
     render() {
-        const { data } = this.state;
+        const { data, loading } = this.state;
+
+        if (loading) {
+          return <Loading />
+        }
 
         return (
             <>
