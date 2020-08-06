fc537d94786742a3d57ffcf08a9034d984d879e5
Diana <dianamilu15@gmail.com>
2020-08-06 16:57:28 -0500

    cambios de estilos en el boton de entrar al evento y el boton de reproducir video

--- src/components/shared/Animate_Img/index.jsx
+++ src/components/shared/Animate_Img/index.jsx
@@ -53,7 +53,7 @@ class AnimateImg extends React.Component {
                             autoplay ? (
                                 <PauseOutlined twoToneColor="#1cdcb7" style={{position:"absolute", top:270, marginLeft:8, backgroundColor:"#f5f5f500" }} onClick={this.autoplayVideo}/>                                
                             ):(
-                                <PlayCircleTwoTone twoToneColor="#1cdcb7" style={{position:"absolute", top:270, marginLeft:8, backgroundColor:"#f5f5f500" }} onClick={this.autoplayVideo}/> 
+                                <PlayCircleTwoTone className="icono-play"twoToneColor="#1cdcb7" style={{position:"absolute", top:270, marginLeft:8, backgroundColor:"#f5f5f500" }} onClick={this.autoplayVideo}/> 
                             )
                            
                         }
