2def5b3ce890e22f91b1163d28f1e3b5f9c06f01
Diana <dianamilu15@gmail.com>
2020-08-14 12:26:57 -0500

    ajustes de estilos en landing

--- src/components/events/eventLanding.jsx
+++ src/components/events/eventLanding.jsx
@@ -50,61 +50,56 @@ class eventLanding extends Component {
         const { eventUserId, users } = this.state
 
         return (
-            <Row >
-                <div style={{ marginRight: 12, marginBottom: 12 }}>
-                    <Col sm={24} md={24} lg={24} xl={24}>
-                        <Card className="event-description" bodyStyle={{ padding: "25px 5px" }} bordered={true}>
-                            {
-                                event._id === "5f0622f01ce76d5550058c32" ? <></> : <h1 className="is-size-4-desktop has-text-weight-semibold">{event.name}</h1>
-                            }
-                            {event.video && (
-                                <div className="column is-centered mediaplayer">
-                                    <ReactPlayer
-                                        width={"100%"}
-                                        style={{
-                                            display: "block",
-                                            margin: "0 auto",
-                                        }}
-                                        url={event.video}
-                                        //url="https://firebasestorage.googleapis.com/v0/b/eviusauth.appspot.com/o/eviuswebassets%2FLa%20asamblea%20de%20copropietarios_%20una%20pesadilla%20para%20muchos.mp4?alt=media&token=b622ad2a-2d7d-4816-a53a-7f743d6ebb5f"
-                                        controls
-                                    />
-                                </div>
-                            )}
+            <div style={{ marginRight: 12, marginBottom: 12 }}>
+                <Card className="event-description" bodyStyle={{ padding: "25px 5px" }} bordered={true}>
+                    {
+                        event._id === "5f0622f01ce76d5550058c32" ? <></> : <h1 className="is-size-4-desktop has-text-weight-semibold">{event.name}</h1>
+                    }
+                    {event.video && (
+                        <div className="column is-centered mediaplayer">
+                            <ReactPlayer
+                                width={"100%"}
+                                style={{
+                                   display: "block",
+                                   margin: "0 auto",
+                                }}
+                                url={event.video}
+                                 //url="https://firebasestorage.googleapis.com/v0/b/eviusauth.appspot.com/o/eviuswebassets%2FLa%20asamblea%20de%20copropietarios_%20una%20pesadilla%20para%20muchos.mp4?alt=media&token=b622ad2a-2d7d-4816-a53a-7f743d6ebb5f"
+                                  controls
+                                />
+                        </div>
+                    )}
                         
-                            {
-                                event._id === "5f0622f01ce76d5550058c32" ? (
+                    {
+                        event._id === "5f0622f01ce76d5550058c32" ? (
+                            <div>
+                                <div className="containerfenalco">
+                                    <Row gutter={[8, 16]}>
+                                        <Col xs={16} sm={12} md={12} lg={12} xl={8}>
+                                            <div className="imagen">
+                                                <img onClick={() => this.onChangePage("interviews")} style={{cursor:'pointer'}} src="https://firebasestorage.googleapis.com/v0/b/eviusauth.appspot.com/o/Btn-A3.png?alt=media&token=3ff840dc-d9a6-4ea1-9e9c-a623cb796ef5" />
+                                            </div>
+                                        </Col>
+                                        <Col xs={16} sm={12} md={12} lg={12} xl={8}>
+                                            <div className="imagen">
+                                                <img onClick={() => this.onChangePage("networking")} style={{cursor:'pointer'}} src="https://firebasestorage.googleapis.com/v0/b/eviusauth.appspot.com/o/Btn-B3.png?alt=media&token=d9a64548-1fed-43d8-9adf-3aaee0e719f5" />
+                                            </div>
+                                        </Col>
+                                        <Col xs={16} sm={12} md={12} lg={12} xl={8}>
+                                            <img onClick={() => this.onChangePage("agenda")} style={{cursor:'pointer'}} src="https://firebasestorage.googleapis.com/v0/b/eviusauth.appspot.com/o/Btn-C3.png?alt=media&token=615fb718-af55-478f-b444-d8486edfc24a" />
+                                        </Col>
+                                    </Row>
+                                </div>
+                            </div>
+                            ) : (event.description && typeof event.description === "string") && (
                                     <div>
-                                        <div className="containerfenalco">
-                                            <Row gutter={[8, 16]}>
-                                                <Col xs={16} sm={12} md={12} lg={12} xl={8}>
-                                                    <div className="imagen">
-                                                        <img onClick={() => this.onChangePage("interviews")} style={{cursor:'pointer'}} src="https://firebasestorage.googleapis.com/v0/b/eviusauth.appspot.com/o/Btn-A3.png?alt=media&token=3ff840dc-d9a6-4ea1-9e9c-a623cb796ef5" />
-                                                    </div>
-                                                </Col>
-                                                <Col xs={16} sm={12} md={12} lg={12} xl={8}>
-                                                    <div className="imagen">
-                                                        <img onClick={() => this.onChangePage("networking")} style={{cursor:'pointer'}} src="https://firebasestorage.googleapis.com/v0/b/eviusauth.appspot.com/o/Btn-B3.png?alt=media&token=d9a64548-1fed-43d8-9adf-3aaee0e719f5" />
-                                                    </div>
-                                                </Col>
-                                                <Col xs={16} sm={12} md={12} lg={12} xl={8}>
-                                                    <img onClick={() => this.onChangePage("agenda")} style={{cursor:'pointer'}} src="https://firebasestorage.googleapis.com/v0/b/eviusauth.appspot.com/o/Btn-C3.png?alt=media&token=615fb718-af55-478f-b444-d8486edfc24a" />
-                                                </Col>
-                                            </Row>
-                                        </div>
+                                        <ReactQuill value={event.description} modules={{ toolbar: false }} readOnly={true} theme="bubble" />
                                     </div>
-                                ) :
-                                    (event.description && typeof event.description === "string") && (
-                                        <div>
-                                            <ReactQuill value={event.description} modules={{ toolbar: false }} readOnly={true} theme="bubble" />
-                                        </div>
-                                    )
-                            }
-                        </Card>
-                    </Col>
-                </div>
-            </Row>
+                                )
+                     }
+                </Card>
 
+            </div>
         )
     }
 }
--- src/components/events/landing.jsx
+++ src/components/events/landing.jsx
@@ -249,42 +249,44 @@ class Landing extends Component {
       interviews: <MyAgendaIndepend event={event} />,
       evento: (
 
-        <div className="columns is-centered" style={{ miHeight: "600px" }}>
-          {(this.state.event && this.state.event._id != "5f0b95ca34c8116f9b21ebd6") &&
-            <EventLanding event={event} toggleConference={this.toggleConference} showSection={this.showSection} />
-          }
-          {(this.state.event && this.state.event._id == "5f0b95ca34c8116f9b21ebd6") &&
-            <div className="the-lobby-video-column">
-              <div className="the-lobby-video">
-                <div className="the-lobby-video-wrap-holder">
-                  <div className="the-lobby-video-holder">
-                    <img src="/lobby/TIRA_PANTALLA.png" alt="" />
+        <Row justify="center" >
+          <Col sm={24} md={16} lg={18} xl={18}>
+            {(this.state.event && this.state.event._id != "5f0b95ca34c8116f9b21ebd6") &&
+              <EventLanding event={event} toggleConference={this.toggleConference} showSection={this.showSection} />
+            }
+            {(this.state.event && this.state.event._id == "5f0b95ca34c8116f9b21ebd6") &&
+              <div className="the-lobby-video-column">
+                <div className="the-lobby-video">
+                  <div className="the-lobby-video-wrap-holder">
+                    <div className="the-lobby-video-holder">
+                      <img src="/lobby/TIRA_PANTALLA.png" alt="" />
+                    </div>
+                    <div className="the-lobby-video-holder">
+                      <img src="/lobby/TIRA_PANTALLA.png" alt="" />
+                    </div>
                   </div>
-                  <div className="the-lobby-video-holder">
-                    <img src="/lobby/TIRA_PANTALLA.png" alt="" />
-                  </div>
-                </div>
-                <div className="the-lobby-video-wrap">
-                  <div className="the-lobby-video-container">
-                    <ReactPlayer
+                  <div className="the-lobby-video-wrap">
+                    <div className="the-lobby-video-container">
+                      <ReactPlayer
 
-                      url={"https://firebasestorage.googleapis.com/v0/b/eviusauth.appspot.com/o/WhatsApp%20Video%202020-07-26%20at%2018.57.30.mp4?alt=media&token=d304d8b9-530d-4972-9a00-373bd19b0158"}
-                      //url="https://firebasestorage.googleapis.com/v0/b/eviusauth.appspot.com/o/eviuswebassets%2FLa%20asamblea%20de%20copropietarios_%20una%20pesadilla%20para%20muchos.mp4?alt=media&token=b622ad2a-2d7d-4816-a53a-7f743d6ebb5f"
-                      controls
-                    />
+                        url={"https://firebasestorage.googleapis.com/v0/b/eviusauth.appspot.com/o/WhatsApp%20Video%202020-07-26%20at%2018.57.30.mp4?alt=media&token=d304d8b9-530d-4972-9a00-373bd19b0158"}
+                        //url="https://firebasestorage.googleapis.com/v0/b/eviusauth.appspot.com/o/eviuswebassets%2FLa%20asamblea%20de%20copropietarios_%20una%20pesadilla%20para%20muchos.mp4?alt=media&token=b622ad2a-2d7d-4816-a53a-7f743d6ebb5f"
+                        controls
+                      />
+                    </div>
                   </div>
                 </div>
+                {
+                  // Todo: Poner link a listado de empresas
+                }
+                {/*
+              <Button onClick={this.showSection('companies')} className="the-lobby-exhibitors-btn">
+                <img src="/lobby/BOTON_STANDS.png" alt=""/>
+              </Button>
+              */}
               </div>
-              {
-                // Todo: Poner link a listado de empresas
-              }
-              {/*
-            <Button onClick={this.showSection('companies')} className="the-lobby-exhibitors-btn">
-              <img src="/lobby/BOTON_STANDS.png" alt=""/>
-            </Button>
-            */}
-            </div>
-          }
+            }
+          </Col>
           <Col sm={24} md={8} lg={6} xl={6}>
             <VirtualConference
               event={this.state.event}
@@ -294,7 +296,7 @@ class Landing extends Component {
             />
             <MapComponent event={event} />
           </Col>
-        </div>
+        </Row>
       ),
     };
     //default section is firstone
