433ea071ff9d667dc8585ab8d78bf7b9b73e75aa
JKLopez <juan.lopez@mocionsoft.com>
2020-09-22 15:54:51 -0500

    trabajo parcial aceptación rápida de reuniones

---
+++ src/components/networking/appointmentAccept.jsx
@@ -0,0 +1,53 @@
+// import { Avatar, Button, Card, Col, Divider, notification, Row, Spin } from "antd"
+// import moment from "moment"
+// import { find, map, pathOr, propEq } from "ramda"
+// import { isNonEmptyArray } from "ramda-adjunct"
+// import React, { useEffect, useState } from 'react'
+// import { acceptOrRejectAgenda} from "./services";
+import React from 'react'
+// const { Meta } = Card;
+
+// const requestStatusText = {
+//   rejected: 'rechazada',
+//   accepted: 'aceptada'
+// }
+
+
+ //props
+  const AppointmentAccept = (  ) => {
+    // const {event_id, meeting_id, eventUser_id} = props.match.params
+    // let data = {};
+    // let newStatus =  "accepted";
+    //changeAgendaStatus(event_id, eventUser_id, data,newStatus)
+    return (<h1> Trabajo en progreso. para la aceptación de reuniones de forma rápida</h1>);
+  }
+
+
+// let changeAgendaStatus = function( newStatus ){ 
+
+//       acceptOrRejectAgenda( eventId, currentEventUserId, data, newStatus )
+//         .then( () => setRequestResponse( newStatus ) )
+//         .catch( ( error ) => {
+//           if ( !error ) {
+//             notification.error( {
+//               message: 'Solicitud no encontrada',
+//               description: 'La solicitud no existe o no esta en estado pendiente'
+//             } )
+//           } else if ( error === 'HOURS_NOT_AVAILABLE' ) {
+//             notification.error( {
+//               message: 'Horario agendado',
+//               description: 'Ya tienes agendada esta hora'
+//             } )
+//           } else {
+//             notification.error( {
+//               message: 'Error',
+//               description: 'Error al actualizar la solicitud'
+//             } )
+//           }
+//         } )
+//         .finally( () => setFetching( false ) )
+    
+  
+// }
+
+export default AppointmentAccept
