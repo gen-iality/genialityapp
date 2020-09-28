b235d70e3470dc81973e87f9edc77b564e74d4f5
Ivan Dario Sanchez Jimenez <ivan.sanchez@mocionsoft.com>
2020-09-28 16:25:53 -0500

    create component events/Partners

---
+++ src/components/events/Partners/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Button, Row, Col, Avatar, Card, Spin } from 'antd';
+import useGetEventCompanies from '../../empresas/customHooks/useGetEventCompanies';
+
+export default function Partners({ eventId }) {
+  const [companies, loadingCompanies] = useGetEventCompanies(eventId);
+
+  return (
+    <Card title='Partners'>
+      {companies.map((company, index) => {
+        return (
+          <>
+            {loadingCompanies && <Spin />}
+            <Row key={index} gutter={[16, 16]} justify='center' align='middle'>
+              <Col xs={24} sm={24} md={12} lg={6} xl={6} xxl={6}>
+                <Avatar src={company.list_image} size={100}></Avatar>
+              </Col>
+              <Col
+                xs={24}
+                sm={24}
+                md={12}
+                lg={12}
+                xl={12}
+                xxl={12}
+                style={{ textAlign: 'left', fontSize: '12px', borderLeft: '5px solid', padding: '18px' }}>
+                <h1 style={{ fontSize: '22px', fontWeight: '300' }}>{company.name}</h1>
+                <div dangerouslySetInnerHTML={{ __html: company.short_description }} />
+              </Col>
+              <Col xs={24} sm={24} md={24} lg={4} xl={4} xxl={4} span={4}>
+                <Button type='primary'>Conocer Más</Button>
+              </Col>
+            </Row>
+          </>
+        );
+      })}
+    </Card>
+  );
+}
