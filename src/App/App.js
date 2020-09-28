7b5186010252779152fd1667aaa3082b8db213ac
Ivan Dario Sanchez Jimenez <ivan.sanchez@mocionsoft.com>
2020-09-28 14:51:31 -0500

    cleaning warnings

--- src/App/App.js
+++ src/App/App.js
@@ -1,19 +1,19 @@
-import React, { Component } from "react";
-import { Provider } from "react-redux";
-import * as Cookie from "js-cookie";
-import { parseUrl } from "../helpers/constants";
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import * as Cookie from 'js-cookie';
+import { parseUrl } from '../helpers/constants';
 import './App.less';
-import { userContext } from "../containers/userContext";
-import privateInstance, { Actions } from "../helpers/request";
-import store from "../redux/store";
-import MainRouter from "../containers/router";
-import "bulma-spacing/css/bulma-spacing.min.css";
+import { userContext } from '../containers/userContext';
+import privateInstance, { Actions } from '../helpers/request';
+import store from '../redux/store';
+import MainRouter from '../containers/router';
+import 'bulma-spacing/css/bulma-spacing.min.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      user: { name: "albert" },
+      user: { name: 'albert' },
     };
   }
   /* TOKENS
@@ -26,14 +26,12 @@ class App extends Component {
     let dataUrl = parseUrl(document.URL);
     if (dataUrl && dataUrl.token) {
       if (dataUrl.token) {
-        Cookie.set("evius_token", dataUrl.token);
+        Cookie.set('evius_token', dataUrl.token);
         privateInstance.defaults.params = {};
-        privateInstance.defaults.params["evius_token"] = dataUrl.token;
+        privateInstance.defaults.params['evius_token'] = dataUrl.token;
       }
       if (dataUrl.refresh_token) {
-        Actions.put("/api/me/storeRefreshToken", { refresh_token: dataUrl.refresh_token }).then((resp) => {
-          console.log(resp);
-        });
+        Actions.put('/api/me/storeRefreshToken', { refresh_token: dataUrl.refresh_token });
       }
     }
   }
