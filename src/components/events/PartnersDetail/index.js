25453eec3966590fb8f9eab0fe9acf15a6ff8865
Brayan Triana <brayan.triana@mocionsoft.com>
2020-09-30 07:37:24 -0500

    reactPlayer confgurado para muestra de video principal

--- src/components/events/PartnersDetail/index.js
+++ src/components/events/PartnersDetail/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Row, Col, Button } from 'antd';
 import { LeftOutlined } from '@ant-design/icons';
+import ReactPlayer from 'react-player';
+// https://youtu.be/d6ssSYyld4o
 
 export default function PartnerDetail({ company, handleClosePartnerDetail }) {
   return (
@@ -16,8 +18,24 @@ export default function PartnerDetail({ company, handleClosePartnerDetail }) {
         <h1 style={{ fontWeight: '300', fontSize: '30px', textAlign: 'left' }}>{company.name}</h1>
       </div>
 
+      {company.list_image && (
+        <Row justify='center'>
+          <img src={company.list_image} alt='img-logo-parner' style={{ width: '100%' }} />
+        </Row>
+      )}
       <Row justify='center'>
-        <img src={company.list_image} alt='img-logo-parner' style={{ width: '100%' }} />
+        {company.video_url && (
+          <ReactPlayer
+            width={'100%'}
+            style={{
+              display: 'block',
+              margin: '0 auto',
+            }}
+            url={company.video_url}
+            //url="https://firebasestorage.googleapis.com/v0/b/eviusauth.appspot.com/o/eviuswebassets%2FLa%20asamblea%20de%20copropietarios_%20una%20pesadilla%20para%20muchos.mp4?alt=media&token=b622ad2a-2d7d-4816-a53a-7f743d6ebb5f"
+            controls
+          />
+        )}
       </Row>
       <Row justify='start'>
         <div
