a2c3a0cf3711a91df49ae7a8c3fdbd08f8cbe1ab
JKLopez <juan.lopez@mocionsoft.com>
2020-10-15 13:29:55 -0500

    ajustando el listado de encuestas para tener en cuenta los usuarios anónimos

--- src/components/events/surveys/surveyList.jsx
+++ src/components/events/surveys/surveyList.jsx
@@ -9,143 +9,71 @@ const headStyle = {
   color: "#000",
 };
 
-const AnonymousList = ({ anonymousSurveyList, showSurvey }) => {
-  return (
-    <Fragment>
-      {anonymousSurveyList && anonymousSurveyList.length === 0 && (
-        <Result icon={<MehOutlined />} title="Aún no se han publicado encuestas" />
-      )}
-      {/* {console.log("anonymousSurveyList:", anonymousSurveyList)} */}
-      {anonymousSurveyList && anonymousSurveyList.length > 0 && (
-        <List
-          dataSource={anonymousSurveyList}
-          renderItem={(survey) =>
-            survey.open === "true" ? (
-              <List.Item key={survey._id}>
-                <List.Item.Meta title={survey.survey} style={{ textAlign: "left" }} />
-                {survey.userHasVoted && (
-                  <div>
-                    <Tag color="success">Respondida</Tag>
-                  </div>
-                )}
-                <div>
-                  <Button
-                    type={!survey.userHasVoted ? "primary" : ""}
-                    className={`${!survey.userHasVoted ? "animate__animated  animate__pulse animate__slower animate__infinite" : ""
-                      }`}
-                    onClick={() => showSurvey(survey)}
-                    loading={survey.userHasVoted === undefined}>
-                    {/* {console.log("iteracion de voto", survey)} */}
-                    {survey.userHasVoted ? "Ir a la Encuesta" : " Ver Resultados"}
-                  </Button>
-                </div>
-              </List.Item>
-            ) : (
-                <List.Item key={survey._id} actions={[]}>
-                  <List.Item.Meta title={survey.survey} style={{ textAlign: "left" }} />
-                  <div>
-                    <Tag color="red">Cerrada</Tag>
-                  </div>
-                  <Button onClick={() => showSurvey(survey)}>Ver Resultados</Button>
-                </List.Item>
-              )
-          }
-        />
-      )}
-    </Fragment>
-  );
-};
-
-
 //Componente que renderiza el listado de encuestas publicadas para la actividad
 
-const SurveyList = ({ jsonData, showSurvey, usuarioRegistrado, surveyLabel }) => {
-  const [surveyList, setSurveyList] = useState([]);
-  const [anonymousSurveyList, setAnonymousSurveyList] = useState([]);
-  const [isASpeaker, setIsASpeaker] = useState(false);
+const SurveyList = ( { jsonData, showSurvey, usuarioRegistrado, surveyLabel } ) => {
+  const [ surveyList, setSurveyList ] = useState( [] );
+  //const [ isASpeaker, setIsASpeaker ] = useState( false );
 
-  useEffect(() => {
-    setSurveyList(jsonData);
-    setAnonymousSurveyList(jsonData.filter((item) => item.allow_anonymous_answers === "true"));
-  }, [jsonData]);
-
-  useEffect(() => {
-    let isASpeaker = false
-    if (usuarioRegistrado.rol) {
-      if (usuarioRegistrado.rol === 'Speaker') {
-        isASpeaker = true
-      }
+  useEffect( () => {
+    let surveyList = jsonData;
+    //Los usuarios anónimos solo ven las encuestas que permiten respuestas anónimas
+    if ( !usuarioRegistrado ) {
+      surveyList = jsonData.filter( ( item ) => { return item.allow_anonymous_answers !== "false" } )
     }
-    setIsASpeaker(isASpeaker)
-  }, [usuarioRegistrado])
 
-  const pluralToSingular = (char, t1, t2) => {
-    if (t1 !== undefined) return `${t1}${t2}`;
+    setSurveyList( surveyList );
+
+  }, [ jsonData, usuarioRegistrado ] );
+
+  // useEffect( () => {
+  //   let isASpeaker = false
+  //   if ( usuarioRegistrado.rol ) {
+  //     if ( usuarioRegistrado.rol === 'Speaker' ) {
+  //       isASpeaker = true
+  //     }
+  //   }
+  //   setIsASpeaker( isASpeaker )
+  // }, [ usuarioRegistrado ] )
+
+  const pluralToSingular = ( char, t1, t2 ) => {
+    if ( t1 !== undefined ) return `${ t1 }${ t2 }`;
     return "";
   };
 
   return (
-    <Card title={`Lista de ${surveyLabel.name}`} className="survyCard" headStyle={headStyle}>
+    <Card title={ `Lista de ${ surveyLabel.name }` } className="survyCard" headStyle={ headStyle }>
+      <Fragment>
+        { surveyList && surveyList.length === 0 && (
+          <Result icon={ <MehOutlined /> } title="Aún no se han publicado encuestas" />
+        ) }
 
-      {usuarioRegistrado ? (
-        <Fragment>
-          {surveyList && surveyList.length === 0 && (
-            <Result icon={<MehOutlined />} title="Aún no se han publicado encuestas" />
-          )}
-
-          {surveyList && surveyList.length > 0 && (
-            <List
-              dataSource={surveyList}
-              renderItem={(survey) =>
-                survey.open === "true" ? (
-                  <List.Item key={survey._id}>
-                    <List.Item.Meta title={survey.survey} style={{ textAlign: "left" }} />
-                    {survey.userHasVoted && (
-                      <div>
-                        <Tag color="success">Respondida</Tag>
-                      </div>
-                    )}
-                    <div>
-                      <Button
-                        ghost
-                        type={!survey.userHasVoted ? "primary" : ""}
-                        className={`${!survey.userHasVoted
-                          ? "animate__animated  animate__pulse animate__slower animate__infinite"
-                          : ""
-                          }`}
-                        onClick={() => showSurvey(survey)}
-                        loading={survey.userHasVoted === undefined}>
-                        {(!survey.userHasVoted && usuarioRegistrado && usuarioRegistrado.rol && usuarioRegistrado.rol.name !== "Speaker")
-                          ? `Ir a ${surveyLabel.name
-                            ? surveyLabel.name.replace(/([^aeiou]{2})?(e)?s\b/gi, pluralToSingular)
-                            : "Encuesta"
-                          }`
-                          : " Ver Resultados"}
-                      </Button>
-                    </div>
-                  </List.Item>
-                ) : (
-                    <List.Item key={survey._id} actions={[]}>
-                      <List.Item.Meta title={survey.survey} style={{ textAlign: "left" }} />
-                      <div>
-                        <Tag color="red">Cerrada</Tag>
-                      </div>
-                      <Button onClick={() => showSurvey(survey)}>Ver Resultados</Button>
-                    </List.Item>
-                  )
-              }
-            />
-          )}
-        </Fragment>
-
-      ) : (
-          <AnonymousList
-            anonymousSurveyList={anonymousSurveyList}
-            showSurvey={showSurvey}
-            usuarioRegistrado={usuarioRegistrado}
+        { surveyList && surveyList.length > 0 && (
+          <List
+            dataSource={ surveyList }
+            renderItem={ ( survey ) =>
+              <List.Item key={ survey._id }>
+                <List.Item.Meta title={ survey.survey } style={ { textAlign: "left" } } />
+                { survey.userHasVoted && ( <div><Tag color="success">Respondida</Tag></div> ) }
+                { survey.open && ( <div> {survey.open == "true" ? <Tag color="green">Abierta</Tag> : <Tag color="red">Cerrada</Tag> }</div> ) }
+                <div>
+                  <Button
+                    ghost
+                    type={ !survey.userHasVoted ? "primary" : "" }
+                    className={ `${ !survey.userHasVoted ? "animate__animated  animate__pulse animate__slower animate__infinite" : "" }` }
+                    onClick={ () => showSurvey( survey ) }
+                    loading={ survey.userHasVoted === undefined }>
+                    { ( !survey.userHasVoted && survey.open == "true" )
+                      ? `Ir a ${ surveyLabel.name ? surveyLabel.name.replace( /([^aeiou]{2})?(e)?s\b/gi, pluralToSingular ) : "Encuesta"
+                      }`
+                      : " Ver Resultados" }
+                  </Button>
+                </div>
+              </List.Item>
+            }
           />
-        )}
-
+        ) }
+      </Fragment>
     </Card>
   );
 };
