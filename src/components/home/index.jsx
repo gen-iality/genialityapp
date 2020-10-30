027e791dc48eddde62bf3a9b006e29f89e0e7b44
Ivan D. Sanchez <cosmosoft.soluciones@gmail.com>
2020-10-30 16:09:56 -0500

    avance eviusquickfill

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from "react-redux";
 import Moment from "moment"
@@ -16,11 +16,11 @@ class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            loading:true,
-            typeEvent:"next",
-            events:[],
-            tabEvt:false,
-            tabEvtType:false,
+            loading: true,
+            typeEvent: "next",
+            events: [],
+            tabEvt: false,
+            tabEvtType: false,
             tabEvtCat: false,
             loadingState: false,
             timeout: false,
@@ -30,119 +30,118 @@ class Home extends Component {
         this.fetchEvent = this.fetchEvent.bind(this);
     }
 
-    componentDidMount() {
-        this.fetchEvent("next")
+    async componentDidMount() {
+        await this.fetchEvent("next")
     }
 
     async fetchEvent(type) {
-        try{
-            this.setState({loading:true,typeEvent:type});
-            const resp = type === 'next' ?  await EventsApi.getPublic('?pageSize=30') : await EventsApi.getOldEvents('?pageSize=60');
+        try {
+            this.setState({ loading: true, typeEvent: type });
+            const resp = type === 'next' ? await EventsApi.getPublic('?pageSize=30') : await EventsApi.getOldEvents('?pageSize=60');
             const events = resp.data.filter(item => item.organizer);
-            this.setState({events,loading:false,current_page:resp.meta.current_page,total:resp.meta.total});
+            this.setState({ events, loading: false, current_page: resp.meta.current_page, total: resp.meta.total });
         }
         catch (error) {
             if (error.response) {
-                const {status} = error.response;
-                if(status === 401) this.setState({timeout:true,loader:false});
-                else this.setState({serverError:true,loader:false})
+                const { status } = error.response;
+                if (status === 401) this.setState({ timeout: true, loader: false });
+                else this.setState({ serverError: true, loader: false })
             } else {
-                if(error.request) console.log(error.request);
-                this.setState({serverError:true,loader:false})
+                if (error.request) console.error(error.request);
+                this.setState({ serverError: true, loader: false })
             }
         }
     }
 
-    componentWillReceiveProps(nextProps){
+    UNSAFE_componentWillReceiveProps(nextProps) {
         const search = nextProps.location.search;
         const params = new URLSearchParams(search);
         const type = params.get('type');
         const category = params.get('category');
         let query = '?';
         let queryFilter = [];
-        if(type){
-            queryFilter.push({"id":"event_type_id","value":type,"comparator":"like"})
+        if (type) {
+            queryFilter.push({ "id": "event_type_id", "value": type, "comparator": "like" })
         }
-        if(category){
-            queryFilter.push({"id":"category_ids","value":category,"comparator":"like"})
+        if (category) {
+            queryFilter.push({ "id": "category_ids", "value": category, "comparator": "like" })
         }
         queryFilter = JSON.stringify(queryFilter);
-        query = query+`filtered=${queryFilter}`;
-        this.setState({loading:true});
+        query = query + `filtered=${queryFilter}`;
+        this.setState({ loading: true });
         API.get(`/api/events${query}`)
-            .then(({data})=>{
-            console.log(data);
-            const events = data.data.filter(item => item.organizer);
-            this.setState({events,loading:false,type,category});
-        })
+            .then(({ data }) => {
+                const events = data.data.filter(item => item.organizer);
+                this.setState({ events, loading: false, type, category });
+            })
             .catch(error => {
                 if (error.response) {
 
-                    const {status,data:{message}} = error.response;
-                    
-                    if(status === 401) this.setState({timeout:true,loader:false});
-                    else this.setState({serverError:true,loader:false,errorData:{status,message}})
+                    const { status, data: { message } } = error.response;
+
+                    if (status === 401) this.setState({ timeout: true, loader: false });
+                    else this.setState({ serverError: true, loader: false, errorData: { status, message } })
                 } else {
-                    let errorData = {message:error.message};
-                
+                    let errorData = { message: error.message };
+
                     errorData.status = 520;
-                    this.setState({serverError:true,loader:false,errorData})
+                    this.setState({ serverError: true, loader: false, errorData })
                 }
-             
+
             });
     }
 
     render() {
-        const {timeout, typeEvent, serverError, errorData, events, loading} = this.state;
+        const { timeout, typeEvent, serverError, errorData, events, loading } = this.state;
         return (
             <React.Fragment>
                 <h2 className="is-size-2 bold-text">Eventos</h2>
                 <section className="home">
                     <div className="tabs">
                         <ul>
-                            <li onClick={e=>this.fetchEvent('next')} className={typeEvent==="next"?"is-active":""}><a href="#proximos">Próximos</a></li>
-                            <li onClick={e=>this.fetchEvent('prev')} className={typeEvent==="prev"?"is-active":""}><a href="#pasados">Pasados</a></li>
+                            <li onClick={e => this.fetchEvent('next')} className={typeEvent === "next" ? "is-active" : ""}><a href="#proximos">Próximos</a></li>
+                            <li onClick={e => this.fetchEvent('prev')} className={typeEvent === "prev" ? "is-active" : ""}><a href="#pasados">Pasados</a></li>
                         </ul>
                     </div>
                     <div className="dynamic-content">
                         {
-                            loading ? <LoadingEvent/> :
+                            loading ? <LoadingEvent /> :
                                 <div className="columns home is-multiline">
                                     {
-                                        events.length<=0 ? <p className="sin-evento">No hay eventos próximos</p> :
-                                        events.map((event,key)=>{
-                                            return <EventCard key={event._id} event={event}
-                                                              action={{name:'Ver',url:`landing/${event._id}`}}
-                                                              size={'column is-one-third'}
-                                                              right={<div className="actions">
-                                                                  <p className="is-size-7">
-                                                                                <span className="icon is-small has-text-grey">
-                                                                                    <i className="fas fa-share"/>
-                                                                                </span>
-                                                                      <span>Compartir</span>
-                                                                  </p>
-                                                                  <p className="is-size-7">
-                                                                                <span className="icon is-small has-text-grey">
-                                                                                    <i className="fas fa-check"/>
-                                                                                </span>
-                                                                      <span>Asistiré</span>
-                                                                  </p>
-                                                                  <p className="is-size-7">
-                                                                                <span className="icon is-small has-text-grey">
-                                                                                    <i className="fas fa-heart"/>
-                                                                                </span>
-                                                                      <span>Me interesa</span>
-                                                                  </p>
-                                                              </div>}
-                                            />
-                                        })
+                                        events.length <= 0 ? <p className="sin-evento">No hay eventos próximos</p> :
+                                            events.map((event, key) => {
+                                                return <EventCard key={event._id} event={event}
+                                                    action={{ name: 'Ver', url: `landing/${event._id}` }}
+                                                    size={'column is-one-third'}
+                                                    right={<div className="actions">
+                                                        <p className="is-size-7">
+                                                            <span className="icon is-small has-text-grey">
+                                                                <i className="fas fa-share" />
+                                                            </span>
+                                                            <span>Compartir</span>
+                                                        </p>
+                                                        <p className="is-size-7">
+                                                            <span className="icon is-small has-text-grey">
+                                                                <i className="fas fa-check" />
+                                                            </span>
+                                                            <span>Asistiré</span>
+                                                        </p>
+                                                        <p className="is-size-7">
+                                                            <span className="icon is-small has-text-grey">
+                                                                <i className="fas fa-heart" />
+                                                            </span>
+                                                            <span>Me interesa</span>
+                                                        </p>
+                                                    </div>}
+                                                />
+                                            })
                                     }
                                 </div>
                         }
                     </div>
                 </section>
-                {timeout&&(<LogOut/>)}
-                {serverError&&(<ErrorServe errorData={errorData}/>)}
+                {timeout && (<LogOut />)}
+                {serverError && (<ErrorServe errorData={errorData} />)}
             </React.Fragment>
         );
     }
--- src/components/shared/eviusReactQuill.jsx
+++ src/components/shared/eviusReactQuill.jsx
@@ -16,94 +16,92 @@ Quill.register('modules/imageDropAndPaste', QuillImageDropAndPaste)
  */
 
 function EviusReactQuill(props) {
-	const reactQuilllRef = useRef(null)
+  const reactQuilllRef = useRef(null)
 
-	let quillModules = {
-		...toolbarEditor,
-		imageDropAndPaste: {
-			// add an custom image handler
-			handler: (imageDataUrl, type, imageData) => { imageHandler(imageDataUrl, type, imageData, reactQuilllRef) }
-		}
-	}
-	// useEffect(() => {
-	// 	console.log('React Quill', props)
-	// }, [])
-
-
-	/* si restringimos el setupToolBarImageUploadInput para que solo se ejecute una vez cuando reactQuilllRef este disponible
-	extranamente deja de funcionar no se porque lo dejamos que se ejecute en cada render y funciona la maravilla
-	*/
-	useEffect(() => {
-		setupToolBarImageUploadInput(reactQuilllRef)
-	});
+  let quillModules = {
+    ...toolbarEditor,
+    imageDropAndPaste: {
+      // add an custom image handler
+      handler: (imageDataUrl, type, imageData) => { imageHandler(imageDataUrl, type, imageData, reactQuilllRef) }
+    }
+  }
+  // useEffect(() => {
+  // 	console.log('React Quill', props)
+  // }, [])
 
 
-	return (
-		<ReactQuill
-			ref={reactQuilllRef}
-			{...props}
-			modules={quillModules} />
-	)
+  /* si restringimos el setupToolBarImageUploadInput para que solo se ejecute una vez cuando reactQuilllRef este disponible
+  extranamente deja de funcionar no se porque lo dejamos que se ejecute en cada render y funciona la maravilla
+  */
+  useEffect(() => {
+    setupToolBarImageUploadInput(reactQuilllRef)
+  }, []);
+
+  return (
+    <ReactQuill
+      ref={reactQuilllRef}
+      modules={quillModules} />
+  )
 }
 
 let imageHandler = async (imageDataUrl, type, imageData, reactQuilllRef) => {
-	let editor = reactQuilllRef.current.getEditor();
-	//no se cómo definir el nombre de la imagen
-	var filename = 'default.png'
-	var blob = imageData.toBlob()
-	var file = imageData.toFile(filename)
+  let editor = reactQuilllRef.current.getEditor();
+  //no se cómo definir el nombre de la imagen
+  var filename = 'default.png'
+  var blob = imageData.toBlob()
+  var file = imageData.toFile(filename)
 
-	// generate a form data
-	var formData = new FormData()
+  // generate a form data
+  var formData = new FormData()
 
-	// append blob data
-	formData.append('filename', filename)
-	formData.append('file', blob)
+  // append blob data
+  formData.append('filename', filename)
+  formData.append('file', blob)
 
-	// or just append the file
-	formData.append('file', file)
+  // or just append the file
+  formData.append('file', file)
 
-	const url = '/api/files/upload'
-	// upload image to your server
+  const url = '/api/files/upload'
+  // upload image to your server
 
-	let image = await Actions.post(url, formData);
+  let image = await Actions.post(url, formData);
 
-	let index = (editor.getSelection() || {}).index;
-	editor.insertEmbed(index, 'image', image, 'user')
+  let index = (editor.getSelection() || {}).index;
+  editor.insertEmbed(index, 'image', image, 'user')
 }
 
 let setupToolBarImageUploadInput = (reactQuilllRef) => {
-	var ImageData = QuillImageDropAndPaste.ImageData
-	let editor = reactQuilllRef.current.getEditor();
+  var ImageData = QuillImageDropAndPaste.ImageData
+  let editor = reactQuilllRef.current.getEditor();
 
-	editor.getModule('toolbar').addHandler('image', function (clicked) {
-		if (clicked) {
+  editor.getModule('toolbar').addHandler('image', function (clicked) {
+    if (clicked) {
 
-			var fileInput = this.container.querySelector('input.ql-image[type=file]')
-			if (fileInput == null) {
-				fileInput = document.createElement('input')
-				fileInput.setAttribute('type', 'file')
-				fileInput.setAttribute('accept', 'image/png, image/gif, image/jpeg, image/bmp, image/x-icon')
-				fileInput.classList.add('ql-image')
-				fileInput.addEventListener('change', function (e) {
-					var files = e.target.files, file
-					if (files.length > 0) {
-						file = files[0]
-						var type = file.type
-						var reader = new FileReader()
-						reader.onload = (e) => {
-							// handle the inserted image
-							var dataUrl = e.target.result
-							imageHandler(dataUrl, type, new ImageData(dataUrl, type), reactQuilllRef)
-							fileInput.value = ''
-						}
-						reader.readAsDataURL(file)
-					}
-				})
-			}
-			fileInput.click()
-		}
-	})
+      var fileInput = this.container.querySelector('input.ql-image[type=file]')
+      if (fileInput == null) {
+        fileInput = document.createElement('input')
+        fileInput.setAttribute('type', 'file')
+        fileInput.setAttribute('accept', 'image/png, image/gif, image/jpeg, image/bmp, image/x-icon')
+        fileInput.classList.add('ql-image')
+        fileInput.addEventListener('change', function (e) {
+          var files = e.target.files, file
+          if (files.length > 0) {
+            file = files[0]
+            var type = file.type
+            var reader = new FileReader()
+            reader.onload = (e) => {
+              // handle the inserted image
+              var dataUrl = e.target.result
+              imageHandler(dataUrl, type, new ImageData(dataUrl, type), reactQuilllRef)
+              fileInput.value = ''
+            }
+            reader.readAsDataURL(file)
+          }
+        })
+      }
+      fileInput.click()
+    }
+  })
 }
 
 export default EviusReactQuill;
