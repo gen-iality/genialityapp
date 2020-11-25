ed67a7618797bcf6814b1eaeeb507e10f3a130c8
Ivan D. Sanchez <cosmosoft.soluciones@gmail.com>
2020-11-24 21:08:41 -0500

    speaker group by category

--- src/components/events/speakers.jsx
+++ src/components/events/speakers.jsx
@@ -29,7 +29,7 @@ class Speakers extends Component {
     //Se hace la consulta a la api de speakers
     let speakers = await SpeakersApi.byEvent(eventId);
     //Se envia al estado para acceder desde ahí a los datos
-    this.setState({ speakers });
+
     //Se comprueban los datos desde el estado
     // console.log(this.state.speakers)
 
@@ -70,7 +70,7 @@ class Speakers extends Component {
     });
   }
 
-  modal(id, image, name, profession, description) {
+  modal(id, image, name, profession, description, category) {
     //Se llama esta funcion para cargar la consulta de actividades por conferencista
     this.activitySpeakers(id);
     // Se envian los datos al estado para mostrarlos en el modal, Esto para hacer el modal dinamico
@@ -80,6 +80,7 @@ class Speakers extends Component {
         nombre: name,
         cargo: profession,
         descripcion: description,
+        category,
       },
 
       modalVisible: true,
@@ -91,9 +92,7 @@ class Speakers extends Component {
   }
 
   render() {
-    const { event } = this.props;
     const {
-      speakers,
       infoSpeaker,
       activityesBySpeaker,
       speakerCategories,
@@ -101,218 +100,218 @@ class Speakers extends Component {
       speakersWithoutCategory,
     } = this.state;
     return (
-      <div className='container-calendar-section'>
-        <div className='columns is-centered'>
-          <div className='container-calendar is-three-fifths'>
-            {speakerCategories.length && (
-              <>
-                {speakerCategories.map((category, index) => {
-                  return (
-                    <div
-                      className='container_agenda-information'
-                      key={`category-${index}`}
-                      style={{ marginBottom: '30px' }}>
-                      <h2 style={{ backgroundColor: '#FFFFFF', fontSize: '18px', fontWeight: '900' }}>
-                        {category.name}
-                      </h2>
-
-                      {speakersWithCategory.length && (
-                        <>
-                          {speakersWithCategory[category.order].length && (
-                            <>
-                              {speakersWithCategory[category.order].map((speaker, key) => (
-                                <div key={key}>
-                                  <Card
-                                    onClick={() =>
-                                      this.modal(
-                                        speaker._id,
-                                        speaker.image,
-                                        speaker.name,
-                                        speaker.profession,
-                                        speaker.description
-                                      )
-                                    }
-                                    hoverable
-                                    style={{ paddingTop: '30px' }}
-                                    cover={
-                                      speaker.image ? (
-                                        <Avatar
-                                          style={{ display: 'block', margin: '0 auto' }}
-                                          size={210}
-                                          src={speaker.image}
-                                        />
-                                      ) : (
-                                        <Avatar
-                                          style={{ display: 'block', margin: '0 auto' }}
-                                          size={210}
-                                          icon={<UserOutlined />}
-                                        />
-                                      )
-                                    }
-                                    actions={[
-                                      <Button
-                                        type='primary'
-                                        className='modal-button'
-                                        onClick={() =>
-                                          this.modal(
-                                            speaker._id,
-                                            speaker.image,
-                                            speaker.name,
-                                            speaker.profession,
-                                            speaker.description
-                                          )
-                                        }
-                                        key={key}
-                                        data-target='#myModal'
-                                        aria-haspopup='true'>
-                                        Ver más...
-                                      </Button>,
-                                    ]}>
-                                    <Meta
-                                      title={[
-                                        <div>
-                                          <span>{speaker.name}</span>
-                                        </div>,
-                                      ]}
-                                      description={[
-                                        <div style={{ minHeight: '100px' }}>
-                                          <p>{speaker.profession}</p>
-                                        </div>,
-                                      ]}
-                                    />
-                                  </Card>
-                                </div>
-                              ))}
-                            </>
-                          )}
-                        </>
-                      )}
-                    </div>
-                  );
-                })}
-              </>
-            )}
-            {/* Mapeo de datos para mostrar los Speakers */}
-            <div className='calendar-speakers'>
-              {speakersWithoutCategory.length &&
-                speakersWithoutCategory.map((speaker, key) => (
-                  <div key={key}>
-                    <Card
-                      onClick={() =>
-                        this.modal(speaker._id, speaker.image, speaker.name, speaker.profession, speaker.description)
-                      }
-                      hoverable
-                      style={{ paddingTop: '30px' }}
-                      cover={
-                        speaker.image ? (
-                          <Avatar style={{ display: 'block', margin: '0 auto' }} size={210} src={speaker.image} />
-                        ) : (
-                          <Avatar style={{ display: 'block', margin: '0 auto' }} size={210} icon={<UserOutlined />} />
-                        )
-                      }
-                      actions={[
-                        <Button
-                          type='primary'
-                          className='modal-button'
-                          onClick={() =>
-                            this.modal(
-                              speaker._id,
-                              speaker.image,
-                              speaker.name,
-                              speaker.profession,
-                              speaker.description
-                            )
-                          }
-                          key={key}
-                          data-target='#myModal'
-                          aria-haspopup='true'>
-                          Ver más...
-                        </Button>,
-                      ]}>
-                      <Meta
-                        title={[
-                          <div>
-                            <span>{speaker.name}</span>
-                          </div>,
-                        ]}
-                        description={[
-                          <div style={{ minHeight: '100px' }}>
-                            <p>{speaker.profession}</p>
-                          </div>,
-                        ]}
-                      />
-                    </Card>
+      <>
+        {speakerCategories.length && (
+          <>
+            {speakerCategories.map((category) => {
+              return (
+                <>
+                  <div
+                    style={{
+                      width: '90%',
+                      margin: '30px auto',
+                      padding: '5px',
+                      borderRadius: '5px',
+                      backgroundColor: '#FFFFFF',
+                    }}>
+                    <span style={{ fontSize: '18px', fontWeight: '700' }}>{category.name}</span>
                   </div>
-                ))}
-            </div>
 
-            {/* Modal de Speakers para mostrar la información del conferencista junto con sus actividades */}
-
-            <Modal
-              title='Conferencista'
-              centered
-              width={1000}
-              visible={this.state.modalVisible}
-              onCancel={() => this.setModalVisible(false)}
-              footer={[
-                <Button key='cerrar' type='primary' onClick={() => this.setModalVisible(false)}>
-                  Cerrar
-                </Button>,
-              ]}>
-              <Row>
-                {/* Imagen del conferencista */}
-
-                <Col flex='1 1 auto'>
-                  {infoSpeaker.imagen ? (
-                    <Avatar style={{ display: 'block', margin: '0 auto' }} size={210} src={infoSpeaker.imagen} />
-                  ) : (
-                    <Avatar style={{ display: 'block', margin: '0 auto' }} size={210} icon={<UserOutlined />} />
-                  )}
-                </Col>
-
-                {/* Descripción del conferencista */}
-                <Col flex='1 1 600px'>
-                  <span>
-                    <b>{infoSpeaker.nombre}</b>
-                  </span>
-                  <p>
-                    <span>
-                      <b>{infoSpeaker.cargo}</b>
-                    </span>
-                    <br />
-                    <br />
-                    <div style={{ width: '90%' }} dangerouslySetInnerHTML={{ __html: infoSpeaker.descripcion }} />
-                  </p>
-                </Col>
-              </Row>
-              {infoSpeaker.description_activity === 'true' && (
-                <div>
-                  {/* Contenedor para descripcion 
-                            Se mapea tambien las actividades por Speaker 
-                        */}
-                  {activityesBySpeaker.map((activities, key) => (
-                    <div key={key}>
-                      <Card style={{ padding: '24px 40px', top: '50px', marginBottom: '30px' }}>
-                        <div>
-                          <p>
-                            <b>
-                              {Moment(activities.datetime_start).format('lll')} -{' '}
-                              {Moment(activities.datetime_end).format('lll')}
-                            </b>
-                          </p>
-                          <p>{activities.name}</p>
-                          <br />
-                          <div dangerouslySetInnerHTML={{ __html: activities.description }} />
+                  {speakersWithCategory.length && (
+                    <>
+                      {speakersWithCategory[category.order].length && (
+                        <div className='container-calendar-speaker calendar-speakers'>
+                          <div className='calendar-speakers'>
+                            {speakersWithCategory[category.order].map((speaker, key) => (
+                              <div key={key}>
+                                <Card
+                                  onClick={() =>
+                                    this.modal(
+                                      speaker._id,
+                                      speaker.image,
+                                      speaker.name,
+                                      speaker.profession,
+                                      speaker.description,
+                                      speaker.category
+                                    )
+                                  }
+                                  hoverable
+                                  style={{ paddingTop: '30px' }}
+                                  cover={
+                                    speaker.image ? (
+                                      <Avatar
+                                        style={{ display: 'block', margin: '0 auto' }}
+                                        size={210}
+                                        src={speaker.image}
+                                      />
+                                    ) : (
+                                      <Avatar
+                                        style={{ display: 'block', margin: '0 auto' }}
+                                        size={210}
+                                        icon={<UserOutlined />}
+                                      />
+                                    )
+                                  }
+                                  actions={[
+                                    <Button
+                                      type='primary'
+                                      className='modal-button'
+                                      onClick={() =>
+                                        this.modal(
+                                          speaker._id,
+                                          speaker.image,
+                                          speaker.name,
+                                          speaker.profession,
+                                          speaker.description
+                                        )
+                                      }
+                                      key={key}
+                                      data-target='#myModal'
+                                      aria-haspopup='true'>
+                                      Ver más...
+                                    </Button>,
+                                  ]}>
+                                  <Meta
+                                    title={[
+                                      <div key={'speaker-name' + key}>
+                                        <span>{speaker.name}</span>
+                                      </div>,
+                                    ]}
+                                    description={[
+                                      <div style={{ minHeight: '100px' }} key={'speaker-description' + key}>
+                                        <p>{speaker.profession}</p>
+                                      </div>,
+                                    ]}
+                                  />
+                                </Card>
+                              </div>
+                            ))}
+                          </div>
                         </div>
-                      </Card>
-                    </div>
-                  ))}
+                      )}
+                    </>
+                  )}
+                </>
+              );
+            })}
+          </>
+        )}
+        {/* Mapeo de datos para mostrar los Speakers */}
+        <div className='container-calendar-speaker calendar-speakers' style={{ marginTop: '50px' }}>
+          {/* Mapeo de datos para mostrar los Speakers */}
+          <div className='calendar-speakers'>
+            {speakersWithoutCategory.length &&
+              speakersWithoutCategory.map((speaker, key) => (
+                <div key={key}>
+                  <Card
+                    onClick={() =>
+                      this.modal(speaker._id, speaker.image, speaker.name, speaker.profession, speaker.description)
+                    }
+                    hoverable
+                    style={{ paddingTop: '30px' }}
+                    cover={
+                      speaker.image ? (
+                        <Avatar style={{ display: 'block', margin: '0 auto' }} size={210} src={speaker.image} />
+                      ) : (
+                        <Avatar style={{ display: 'block', margin: '0 auto' }} size={210} icon={<UserOutlined />} />
+                      )
+                    }
+                    actions={[
+                      <Button
+                        type='primary'
+                        className='modal-button'
+                        onClick={() =>
+                          this.modal(speaker._id, speaker.image, speaker.name, speaker.profession, speaker.description)
+                        }
+                        key={key}
+                        data-target='#myModal'
+                        aria-haspopup='true'>
+                        Ver más...
+                      </Button>,
+                    ]}>
+                    <Meta
+                      title={[
+                        <div key={'speaker-name' + key}>
+                          <span>{speaker.name}</span>
+                        </div>,
+                      ]}
+                      description={[
+                        <div key={'speaker-description' + key} style={{ minHeight: '100px' }}>
+                          <p>{speaker.profession}</p>
+                        </div>,
+                      ]}
+                    />
+                  </Card>
                 </div>
-              )}
-            </Modal>
+              ))}
           </div>
         </div>
-      </div>
+
+        {/* Modal de Speakers para mostrar la información del conferencista junto con sus actividades */}
+
+        <Modal
+          title={infoSpeaker.category ? infoSpeaker.category : 'Conferencista'}
+          centered
+          width={1000}
+          visible={this.state.modalVisible}
+          onCancel={() => this.setModalVisible(false)}
+          footer={[
+            <Button key='cerrar' type='primary' onClick={() => this.setModalVisible(false)}>
+              Cerrar
+            </Button>,
+          ]}>
+          <Row>
+            {/* Imagen del conferencista */}
+
+            <Col flex='1 1 auto'>
+              {infoSpeaker.imagen ? (
+                <Avatar style={{ display: 'block', margin: '0 auto' }} size={210} src={infoSpeaker.imagen} />
+              ) : (
+                <Avatar style={{ display: 'block', margin: '0 auto' }} size={210} icon={<UserOutlined />} />
+              )}
+            </Col>
+
+            {/* Descripción del conferencista */}
+            <Col flex='1 1 600px'>
+              <span>
+                <b>{infoSpeaker.nombre}</b>
+              </span>
+              <p>
+                <span>
+                  <b>{infoSpeaker.cargo}</b>
+                </span>
+                <br />
+                <br />
+                <div style={{ width: '90%' }} dangerouslySetInnerHTML={{ __html: infoSpeaker.descripcion }} />
+              </p>
+            </Col>
+          </Row>
+          {infoSpeaker.description_activity === 'true' && (
+            <div>
+              {/* Contenedor para descripcion 
+                            Se mapea tambien las actividades por Speaker 
+                        */}
+              {activityesBySpeaker.map((activities, key) => (
+                <div key={key}>
+                  <Card style={{ padding: '24px 40px', top: '50px', marginBottom: '30px' }}>
+                    <div>
+                      <p>
+                        <b>
+                          {Moment(activities.datetime_start).format('lll')} -{' '}
+                          {Moment(activities.datetime_end).format('lll')}
+                        </b>
+                      </p>
+                      <p>{activities.name}</p>
+                      <br />
+                      <div dangerouslySetInnerHTML={{ __html: activities.description }} />
+                    </div>
+                  </Card>
+                </div>
+              ))}
+            </div>
+          )}
+        </Modal>
+      </>
     );
   }
 }
