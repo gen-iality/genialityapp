05f15f6c953f10de3146630d2054b8ef157267d5
JKLopez <juan.lopez@mocionsoft.com>
2020-11-30 12:09:31 -0500

    agregando otro juego el de las motos para las pruebas

--- src/components/events/zoomComponent/listadoJuegos.jsx
+++ src/components/events/zoomComponent/listadoJuegos.jsx
@@ -4,6 +4,7 @@ export default function ListadoJuegos(props) {
   return (
     <ul>
       <li onClick={() => props.changeContentDisplayed('game')}>Juego 1</li>
+      <li onClick={() => props.changeContentDisplayed('game2')}>Juego 2</li>
       <li onClick={() => props.changeContentDisplayed('conference')}>Conferencia</li>
     </ul>
   );
