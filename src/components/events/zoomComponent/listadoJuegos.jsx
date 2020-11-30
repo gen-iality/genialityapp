6327b2a77f362ec60157d318797def41ef097ef3
Ivan D. Sanchez <cosmosoft.soluciones@gmail.com>
2020-11-30 12:39:13 -0500

    consulta de ranking

--- src/components/events/zoomComponent/listadoJuegos.jsx
+++ src/components/events/zoomComponent/listadoJuegos.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { firestore } from '../../../helpers/firebase';
 
 export default function ListadoJuegos(props) {
+  const [ranking, setRanking] = useState([]);
+
+  useEffect(() => {
+    let gameId = '0biWfCwWbUGhbZmfhkvu';
+    firestore.collection('juegos/' + gameId + '/puntajes/').onSnapshot(function(querySnapshot) {
+      var puntajes = [];
+      querySnapshot.forEach(function(doc) {
+        puntajes.push(doc.data());
+      });
+      setRanking(puntajes);
+    });
+  }, []);
+
   return (
-    <ul>
-      <li onClick={() => props.changeContentDisplayed('game')}>Juego 1</li>
-      <li onClick={() => props.changeContentDisplayed('conference')}>Conferencia</li>
-    </ul>
+    <>
+      <ul>
+        <li onClick={() => props.changeContentDisplayed('game')}>Juego 1</li>
+        <li onClick={() => props.changeContentDisplayed('conference')}>Conferencia</li>
+      </ul>
+      <h2>Ranking</h2>
+      <ul>
+        {ranking.map((item, key) => (
+          <li key={'item' + key}>{item.puntaje}</li>
+        ))}
+      </ul>
+    </>
   );
 }
