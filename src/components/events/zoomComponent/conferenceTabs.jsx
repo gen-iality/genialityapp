bdf8bca9f0bdfdc672226f7883d3c1f7b710bd7f
diana silva <diana.silva@mocionsoft.com>
2020-12-02 16:06:38 -0500

    tab name

--- src/components/events/zoomComponent/conferenceTabs.jsx
+++ src/components/events/zoomComponent/conferenceTabs.jsx
@@ -33,12 +33,12 @@ export default function ConferenceTabsComponent(props) {
         <div className='zoom-collapsed_tabs'>
           <Tabs defaultActiveKey={props.activeTab} activeKey={props.activeTab} onChange={callback}>
             {chat && (
-              <TabPane tab={<CommentOutlined style={{ fontSize: '26px' }} />} key='chat'>
+              <TabPane tab={<><CommentOutlined style={{fontSize:'26px', textAlign:'center'}}/><p style={{marginBottom:'0px'}}>Chat</p></>} key='chat'>
                 <LiveChat {...props} />
               </TabPane>
             )}
             {surveys && (
-              <TabPane tab={<PieChartOutlined style={{ fontSize: '26px' }} />} key='surveys'>
+              <TabPane tab={<><PieChartOutlined style={{fontSize:'26px', textAlign:'center'}}/><p style={{marginBottom:'0px'}}>Encuestas</p></>} key='surveys'>
                 <Row justify='space-between'>
                   <Col span={4}>
                     <ArrowLeftOutlined onClick={() => props.changeContentDisplayed('conference')} />
@@ -67,12 +67,12 @@ export default function ConferenceTabsComponent(props) {
             )}
 
             {attendees && (
-              <TabPane tab={<TeamOutlined style={{ fontSize: '26px' }} />} key='attendees'>
+              <TabPane tab={<><TeamOutlined style={{fontSize:'26px', textAlign:'center'}}/><p style={{marginBottom:'0px'}}>Asistentes</p></>} key='attendees'>
                 Asistentes
               </TabPane>
             )}
             {games && (
-              <TabPane tab={<BuildOutlined style={{ fontSize: '26px' }} />} key='games'>
+              <TabPane tab={<><BuildOutlined style={{fontSize:'26px', textAlign:'center'}}/><p style={{marginBottom:'0px'}}>Juegos</p></>} key='games'>
                 <ListadoJuegos {...props} />
               </TabPane>
             )}
