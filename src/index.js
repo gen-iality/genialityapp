b8d5b557c2001ce7078284419e9f9879aebb2891
JKLopez <juan.lopez@mocionsoft.com>
2020-12-18 10:12:37 -0500

    updating react-intl to version 5 from 2

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import  './styles/main.scss';
+import './styles/main.scss';
 import App from './App/App';
 
 //import registerServiceWorker from './registerServiceWorker';
-import { unregister as unregisterServiceWorker } from './registerServiceWorker'
+import { unregister as unregisterServiceWorker } from './registerServiceWorker';
 //index.html loading old files because of service worker after build
 //https://github.com/facebook/create-react-app/issues/2715
 //https://gist.github.com/kirillshevch/a7d778a6aaa788149ae86a9b313cb0ad
-import {addLocaleData, IntlProvider,} from 'react-intl';
+import { IntlProvider } from 'react-intl';
 //Import local files from intl
-import locale_en from 'react-intl/locale-data/en';
-import locale_es from 'react-intl/locale-data/es';
-import localeData from "./helpers/locale.json";
+//import locale_en from 'react-intl/locale-data/en';
+//import locale_es from 'react-intl/locale-data/es';
+import localeData from './helpers/locale.json';
 
-addLocaleData([...locale_en, ...locale_es]);
 // Define user's language.
 const language = (navigator.languages && navigator.languages[0]) || navigator.language;
 // Split locales with a region code
@@ -23,9 +22,11 @@ const languageWithoutRegionCode = language.toLowerCase().split(/[_-]+/)[0];
 const messages = localeData[languageWithoutRegionCode] || localeData[language] || localeData.en;
 
 ReactDOM.render(
-    <IntlProvider locale={language} messages={messages}>
-        <App/>
-    </IntlProvider>, document.getElementById('root'));
+  <IntlProvider locale={language} messages={messages} defaultLocale='es'>
+    <App />
+  </IntlProvider>,
+  document.getElementById('root')
+);
 ////index.html loading old files because of service worker after build
 //registerServiceWorker();
 unregisterServiceWorker();
