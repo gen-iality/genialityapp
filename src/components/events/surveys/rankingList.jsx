95fb0ce88cb42e7f069a947a995eb006cdace332
Ivan D. Sanchez <cosmosoft.soluciones@gmail.com>
2021-02-25 15:45:59 -0500

    integracion ranking

---
+++ src/components/events/surveys/rankingList.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import { Row, Col, Avatar } from 'antd';
+
+export default function RankingList({ data }) {
+  function formatName(name) {
+    const result = decodeURIComponent(name);
+    return result;
+  }
+
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    setList(data);
+  }, [data]);
+
+  return (
+    <div style={{ marginTop: 16 }}>
+      <h3 style={{ fontSize: '14px', fontWeight: '700' }}>Ranking de jugadores</h3>
+      <div className='container-ranking' style={{ marginTop: 16 }}>
+        {list.length > 0 ? (
+          list.map((item, key) => (
+            <div className='card-games-ranking' key={'item' + key}>
+              <Row justify='space-between' align='middle'>
+                <Col span={6}>
+                  <Avatar size={35}>
+                    {item.name && item.name.charAt(0).toUpperCase()}
+                    {item.name && item.name.substring(item.name.indexOf(' ') + 1, item.name.indexOf(' ') + 2)}
+                  </Avatar>
+                </Col>
+                <Col span={12}>
+                  <h3>{formatName(item.name)}</h3>
+                </Col>
+                <Col span={6}>
+                  <h4>{item.score} Pts</h4>
+                </Col>
+              </Row>
+            </div>
+          ))
+        ) : (
+          <div className='card-games-ranking'>
+            <Row>
+              <Col>No hay resultados</Col>
+            </Row>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
