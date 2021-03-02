aceb7fdcb58636ef0f8127582b8037d27e465ec0
Diana <diana.silva@mocionsoft.com>
2021-03-02 15:18:23 -0500

    estilos de la altura del chat

--- src/components/events/landing.jsx
+++ src/components/events/landing.jsx
@@ -871,7 +871,7 @@ class Landing extends Component {
                             style={this.state.visibleChat == true ? { display: 'none' } : {}}></Button>
                         </div>
                         <Drawer
-                          height={353}
+                          height={450}
                           placement={this.state.placementBottom}
                           closable={true}
                           onClose={this.onClose}
