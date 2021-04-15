5a9021d20d4d7798e4656a22c0e3594b1bb83a1c
Ivan D. Sanchez <cosmosoft.soluciones@gmail.com>
2021-04-15 13:25:11 -0500

    Reestructuracion metricas de comunicaciones enviadas

--- src/components/messages/list.jsx
+++ src/components/messages/list.jsx
@@ -1,204 +1,93 @@
-import React, { Component } from "react";
-import { FormattedDate, FormattedTime } from "react-intl";
-import { Link, withRouter } from "react-router-dom";
-import { FaEye } from "react-icons/fa";
-import API from "../../helpers/request";
-import Table from "../shared/table";
+/* eslint-disable react/display-name */
+import React, { useState, useEffect } from 'react';
+import { Table } from 'antd';
+import { Link, withRouter } from 'react-router-dom';
+import { FaEye } from 'react-icons/fa';
+import API from '../../helpers/request';
 
-class InvitationsList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      invitations: [],
-      pageOfItems: [],
-      loading: true,
-      pages: null,
-      pageSize: 25,
-      columns: columns,
-      sorted: [],
-    };
-    this.fetchData = this.fetchData.bind(this);
-  }
+function InvitationsList(props) {
+  const { match, eventId } = props;
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  componentDidMount() {
-    const { match } = this.props;
-    let pos = this.state.columns
-      .map((e) => {
-        return e.id;
-      })
-      .indexOf("edit");
+  useEffect(() => {
+    (async function() {
+      setLoading(true);
+      const data = await fetchData();
+      setData(data);
+      setLoading(false);
+    })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    let editaction = {
-      Header: "Detalle",
-      id: "edit",
-      accessor: (d) => d,
-      Cell: (props) => (
+  const columns = [
+    {
+      title: '',
+      key: 'action',
+      render: (item) => (
         <Link
           to={{
             pathname: `${match.url}/detail`,
-            state: { item: props.value, users: props.value.message_users },
-          }}
-        >
+            state: { item: item, users: item.message_users },
+          }}>
           <FaEye />
         </Link>
       ),
-      width: 50,
-    };
-    if (pos < 0) {
-      columns.unshift(editaction);
-    } else {
-      columns[pos] = editaction;
-    }
-
-    this.setState({ columns: [...columns], loading: false });
-
-    this.fetchData(this.state);
-  }
-
-  componentWillUnmount() {
-    this.setState({ invitations: [] });
-  }
-
-  //Table
-  fetchData(state, instance) {
-    this.setState({ loading: true });
-    this.requestData(
-      this.state.invitations,
-      this.props.eventId,
-      state.pageSize,
-      state.page,
-      state.sorted
-    ).then((res) => {
-      const page = Math.ceil(res.total / res.perPage);
-      this.setState({
-        invitations: res.rows,
-        pages: page,
-        loading: false,
-      });
-    });
-  }
-
-  requestData = (invitations, eventId, pageSize, page, sorted) => {
+    },
+    {
+      title: 'Asunto',
+      dataIndex: 'subject',
+      key: 'subject',
+      render: (text) => <span>{text}</span>,
+    },
+    {
+      title: '# Correos',
+      dataIndex: 'number_of_recipients',
+      key: 'recipients',
+      render: (text) => <div style={{ textAlign: 'center' }}>{text}</div>,
+    },
+    {
+      title: 'Entregados',
+      dataIndex: 'total_delivered',
+      key: 'sent',
+      render: (text) => <div style={{ textAlign: 'center' }}>{text}</div>,
+    },
+    {
+      title: 'Rebotados',
+      dataIndex: 'total_bounced',
+      key: 'bounced',
+      render: (text) => <div style={{ textAlign: 'center' }}>{text}</div>,
+    },
+    {
+      title: 'Abiertos',
+      dataIndex: 'total_opened',
+      key: 'opened',
+      render: (text) => <div style={{ textAlign: 'center' }}>{text}</div>,
+    },
+    {
+      title: 'Clicked',
+      dataIndex: 'total_clicked',
+      key: 'clicked',
+      render: (text) => <div style={{ textAlign: 'center' }}>{text}</div>,
+    },
+  ];
+  function fetchData() {
     return new Promise((resolve, reject) => {
-      let filteredData = invitations;
-      let query = "?";
-      if (sorted.length) {
-        let querySort = [];
-        sorted.map((sort) => {
-          return querySort.push({
-            id: sort.id,
-            order: sort.desc ? "desc" : "asc",
-          });
-        });
-        querySort = JSON.stringify(querySort);
-        query = query + `&orderBy=${querySort}`;
-      }
-
-      API.get(
-        `/api/events/${eventId}/messages${query}&page=${page + 1
-        }&pageSize=${pageSize}`
-      )
+      API.get(`/api/events/${eventId}/messages`)
         .then(({ data }) => {
-          filteredData = data;
-          console.log(data);
-          let res = {
-            rows: filteredData.data,
-            total: filteredData.meta.total,
-            perPage: filteredData.meta.per_page,
-          };
-          resolve(res);
+          resolve(data.data);
         })
         .catch((e) => {
-          console.log(e);
-          this.setState({ timeout: true });
+          reject(e);
         });
     });
-  };
-
-  goToDetail = (row) => {
-    console.log("GOTODETAIL", row);
-  };
-
-  render() {
-    const { invitations, pageSize, pages, loading } = this.state;
-    return (
-      <React.Fragment>
-        {
-          <Table
-            onRowClick={this.goToDetail}
-            columns={this.state.columns}
-            manual
-            data={invitations}
-            loading={loading}
-            onFetchData={this.fetchData}
-            showPaginationTop={true}
-            showPaginationBottom={false}
-            pages={pages}
-            defaultPageSize={pageSize}
-            className="-highlight"
-          />
-        }
-      </React.Fragment>
-    );
   }
-}
 
-let columns = [
-  {
-    Header: "Subject",
-    id: "subject",
-    accessor: (d) => d.subject,
-    sortable: false,
-    Cell: (props) => props.value,
-  },
-  {
-    Header: "Fecha",
-    id: "created_at",
-    accessor: (d) => d.created_at,
-    Cell: (props) => (
-      <span>
-        <FormattedDate value={props.value} />{" "}
-        <FormattedTime value={props.value} />
-      </span>
-    ),
-    width: 180,
-  },
-  {
-    Header: "Total",
-    id: "number_of_recipients",
-    accessor: (d) => d.number_of_recipients,
-    width: 90,
-  },
-  {
-    Header: "Request",
-    id: "request",
-    accessor: (d) => d.request,
-    width: 90,
-  },
-  {
-    Header: "Delivered",
-    id: "delivered",
-    accessor: (d) => d.delivered,
-    width: 90,
-  },
-  {
-    Header: "Unique",
-    id: "unique_opened",
-    accessor: (d) => d.unique_opened,
-    width: 90,
-  },
-  {
-    Header: "Opened",
-    id: "opened",
-    accessor: (d) => d.opened,
-    width: 90,
-  },
-  {
-    Header: "Click",
-    id: "click",
-    accessor: (d) => d.click,
-    width: 90,
-  },
-];
+  return (
+    <>
+      <Table loading={loading} columns={columns} dataSource={data} />
+    </>
+  );
+}
 
 export default withRouter(InvitationsList);
--- src/components/messages/messageUser.jsx
+++ src/components/messages/messageUser.jsx
@@ -1,46 +1,43 @@
-import React, {Component} from 'react';
-import Pagination from "../shared/pagination";
+import React, { Component } from 'react';
+import Pagination from '../shared/pagination';
 
 class MessageUser extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {pageOfItems: []}
-    }
+  constructor(props) {
+    super(props);
+    this.state = { pageOfItems: [] };
+  }
 
-    onChangePage = (pageOfItems) => {
-        this.setState({ pageOfItems: pageOfItems });
-    };
+  onChangePage = (pageOfItems) => {
+    this.setState({ pageOfItems: pageOfItems });
+  };
 
-    render() {
-        return (
-            <React.Fragment>
-                <table className="table is-fullwidth">
-                    <thead>
-                    <tr>
-                        <th>Email</th>
-                        <th>Status</th>
-                        <th>Email Status</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {
-                        this.state.pageOfItems.map((item,key)=>{
-                            return <tr key={key}>
-                                <td>{item.email}</td>
-                                <td>{item.status}</td>
-                                <td>{item.status_message}</td>
-                            </tr>
-                        })
-                    }
-                    </tbody>
-                </table>
-                <Pagination
-                    items={this.props.users}
-                    onChangePage={this.onChangePage}
-                />
-            </React.Fragment>
-        );
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <table className='table is-fullwidth'>
+          <thead>
+            <tr>
+              <th>Email</th>
+              <th>Status</th>
+              <th>Email Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.pageOfItems.map((item, key) => {
+              return (
+                <tr key={key}>
+                  <td>{item.email}</td>
+                  <td>{item.status}</td>
+                  <td>{item.status_message}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        <Pagination items={this.props.users} onChangePage={this.onChangePage} />
+      </React.Fragment>
+    );
+  }
 }
 
 export default MessageUser;
