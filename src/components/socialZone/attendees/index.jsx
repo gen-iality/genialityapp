d6e14c24b606d57a18ea7af31716d9811be46a2d
Marlon Carrillo Barcenas <marloncb2011@hotmail.com>
2021-04-15 10:32:37 -0500

    se comento el metodo onClick que estaba en los nombres de los asistentes de la zona social

--- src/components/socialZone/attendees/index.jsx
+++ src/components/socialZone/attendees/index.jsx
@@ -148,7 +148,17 @@ const AttendeList = function(props) {
                     style={{ padding: '0px !important' }}
                     placement='leftTop'
                     content={<PopoverInfoUser item={item} props={props} />}>
-                    <a key='list-loadmore-edit'>{item.names}</a>
+                    <a
+                      key='list-loadmore-edit'
+                      /* onClick={() => {
+                        var user = props.currentUser;
+                        console.log(props.currentUser);
+                        console.log(item);
+                        props.createNewOneToOneChat(user.uid, user.names, item.iduser, item.names);
+                      }} */
+                    >
+                      {item.names}
+                    </a>
                   </Popover>
                 ) : null
               }
