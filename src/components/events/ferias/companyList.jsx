af19b05f451c75790bc7760f5460fd9cd46ec541
Jaime Daniel <jaime.barros@growdata.com.co>
2021-07-14 17:47:10 -0500

    fix ferias redirect

--- src/components/events/ferias/companyList.jsx
+++ src/components/events/ferias/companyList.jsx
@@ -5,16 +5,16 @@ import { useHistory } from 'react-router';
 
 function Companylist(props) {
   const history=useHistory()
-  const [disableRedirect,setDisableRedirect]=useState(true)  
+ 
     return(
-      <div className='company-list' onClick={()=>{ setDisableRedirect(false);!disableRedirect && history.push(`/landing/${props.eventId}/ferias/${props.companyId}/detailsCompany`)} }>
+      <div className='company-list' >
         <Row className='container' gutter={[10,10]} >
-               <Col xs={24} sm={24} md={24} lg={6} xl={6} className='col'>            
+               <Col xs={24} sm={24} md={24} lg={6} xl={6} className='col' onClick={()=>{ history.push(`/landing/${props.eventId}/ferias/${props.companyId}/detailsCompany`)}  }>            
                  <div  className='img-contact' > 
                    <img className='img' src={props.img} />
                  </div>
                </Col>
-               <Col xs={24} sm={24} md={24} lg={9} xl={9}>
+               <Col xs={24} sm={24} md={24} lg={9} xl={9} onClick={()=>{ history.push(`/landing/${props.eventId}/ferias/${props.companyId}/detailsCompany`)}  }>
                  <div className='info-contact'>
                    <span className='name'>{props.name}</span>
                    <span className='position'>{props.position} </span>
@@ -25,7 +25,7 @@ function Companylist(props) {
                  <div className='redes-contact'>
                   { props.tel && <span className='tel'> <PhoneOutlined className='icono'/> {props.tel} </span>}
                   {props.email && <span className='email'> <MailOutlined className='icono'/> {props.email} </span>}
-                  {props.pagweb &&<span className='web'> <GlobalOutlined  className='icono'/><a rel="noreferrer" onClick={()=>{setDisableRedirect(true);window.open(`${props.pagweb}`, '_blank')}} target="_blank"> {props.pagweb}</a> </span>}
+                  {props.pagweb &&<span className='web'> <GlobalOutlined  className='icono'/><a rel="noreferrer" onClick={()=>{window.open(`${props.pagweb}`, '_blank')}} target="_blank"> {props.pagweb}</a> </span>}
                  </div>
                  <Button type='default' className='boton' size='large'>
                     ver mas detalles
