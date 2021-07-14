b07a7e3c5ea551c26037d0c0628fa2cf90982c67
Marlon Carrillo <marlon.carrillo@mocionsoft.com>
2021-07-14 16:06:00 -0500

    formato en fechas de metricas para las graficas

--- src/components/dashboard/serviceAnalytics.js
+++ src/components/dashboard/serviceAnalytics.js
@@ -1,7 +1,7 @@
-import { EventsApi } from "../../helpers/request";
-import moment from "moment";
+import { EventsApi } from '../../helpers/request';
+import moment from 'moment';
 //METRICAS QUE SE PUEDEN OBTENER
- /*let startDate = '7daysAgo'
+/*let startDate = '7daysAgo'
     let endDate = 'today'
     let metrics = [
       'ga:users', //The total number of users for the requested time period.
@@ -33,185 +33,184 @@ export const totalsMetricasMail = async (eventId) => {
 };
 
 export const totalsMetricasMailDetails = async (eventId, idBell) => {
-    return new Promise((resolve, reject) => {
-      fetch(`http://18.211.124.171/api/events/${eventId}/message/${idBell}/messageUser`)
-        .then((response) => response.json())
-        .then(({ data }) => {
-          resolve(data);
-        })
-        .catch((e) => {
-          reject(e);
-        });
-    });
-  };
-  
-  export const totalsMetricasEventsDetails = async (eventId) => {
-    const metrics = await EventsApi.metrics(eventId)  
-    return  metrics;
-  };
-  
-  export const totalsMetricasActivityDetails = async (eventId) => {
-    const metrics = await EventsApi.metricsByActivity(eventId)  
-    return  metrics;
-  };
-  
-  export const metricasRegisterByDate = async (eventId) => { 
-    const metrics = await EventsApi.metricsRegisterBydate(eventId,"created_at");  
-    console.log(metrics)  
-    return  metrics;
-  };
-  
-  export const metricasCheckedByDate = async (eventId) => { 
-    const metrics = await EventsApi.metricsRegisterBydate(eventId,"checkedin_at");  
-    console.log(metrics)  
-    return  metrics;
-  };
+  return new Promise((resolve, reject) => {
+    fetch(`http://18.211.124.171/api/events/${eventId}/message/${idBell}/messageUser`)
+      .then((response) => response.json())
+      .then(({ data }) => {
+        resolve(data);
+      })
+      .catch((e) => {
+        reject(e);
+      });
+  });
+};
+
+export const totalsMetricasEventsDetails = async (eventId) => {
+  const metrics = await EventsApi.metrics(eventId);
+  return metrics;
+};
+
+export const totalsMetricasActivityDetails = async (eventId) => {
+  const metrics = await EventsApi.metricsByActivity(eventId);
+  return metrics;
+};
+
+export const metricasRegisterByDate = async (eventId) => {
+  const metrics = await EventsApi.metricsRegisterBydate(eventId, 'created_at');
+  console.log(metrics);
+  return metrics;
+};
+
+export const metricasCheckedByDate = async (eventId) => {
+  const metrics = await EventsApi.metricsRegisterBydate(eventId, 'checkedin_at');
+  console.log(metrics);
+  return metrics;
+};
 
 //Esta funcion realiza la consulta de los datos a la API de analytics
 export const queryReportGnal = async (eventID) => {
-    const devEvius='http://apiprueba.evius.co/api/googleanalytics';
-    let fechaActual=moment().format("YYYY-MM-DD")
-    const data={
-      startDate: "2019-01-01",
-      endDate: fechaActual,     
-      filtersExpression: `ga:pagePath=@/landing/${eventID};ga:pagePath!@token`,
-      metrics: "ga:pageviews, ga:users, ga:sessions, ga:sessionDuration, ga:avgTimeOnPage",
-      dimensions: "ga:pagePath",
-      fieldName: "ga:pagePath",
-      sortOrder: "ASCENDING"
-    }  
+  const devEvius = 'http://apiprueba.evius.co/api/googleanalytics';
+  let fechaActual = moment().format('YYYY-MM-DD');
+  const data = {
+    startDate: '2019-01-01',
+    endDate: fechaActual,
+    filtersExpression: `ga:pagePath=@/landing/${eventID};ga:pagePath!@token`,
+    metrics: 'ga:pageviews, ga:users, ga:sessions, ga:sessionDuration, ga:avgTimeOnPage',
+    dimensions: 'ga:pagePath',
+    fieldName: 'ga:pagePath',
+    sortOrder: 'ASCENDING',
+  };
 
-    let resp=await fetch(devEvius, {
-      headers: {
-        'content-type': 'application/json',
-        "Accept": "application/json",
-      },
-      body: JSON.stringify(data),
-      method: 'POST'
-    })
-    let respjson= await resp.json()
-    const dataEvents = respjson.rows;
-    const totalMetrics = respjson.totalsForAllResults;      
-    let metrics = [];
-    dataEvents.map((data, i) => {
-      let objeto = {
-        view: dataEvents[i][0],
-        metrics: Array.from(dataEvents[i].slice(1,dataEvents[i].length)),
-      };       
-      metrics.push(objeto);
-    });
-    let totalAvg=parseFloat(totalMetrics["ga:avgTimeOnPage"]);   
-   return {metrics,totalAvg,totalMetrics};       
+  let resp = await fetch(devEvius, {
+    headers: {
+      'content-type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify(data),
+    method: 'POST',
+  });
+  let respjson = await resp.json();
+  const dataEvents = respjson.rows;
+  const totalMetrics = respjson.totalsForAllResults;
+  let metrics = [];
+  dataEvents.map((data, i) => {
+    let objeto = {
+      view: dataEvents[i][0],
+      metrics: Array.from(dataEvents[i].slice(1, dataEvents[i].length)),
+    };
+    metrics.push(objeto);
+  });
+  let totalAvg = parseFloat(totalMetrics['ga:avgTimeOnPage']);
+  return { metrics, totalAvg, totalMetrics };
 };
 
 //Esta funcion trae datos por fecha
 export const queryReportGnalByMoth = async (eventID) => {
-  const devEvius='http://apiprueba.evius.co/api/googleanalytics';  
-  let fechaActual=moment().format("YYYY-MM-DD")  
-  const data={
-    startDate: "2019-01-01",
-    endDate: fechaActual,     
+  const devEvius = 'http://apiprueba.evius.co/api/googleanalytics';
+  let fechaActual = moment().format('YYYY-MM-DD');
+  const data = {
+    startDate: '2019-01-01',
+    endDate: fechaActual,
     filtersExpression: `ga:pagePath=@/landing/${eventID};ga:pagePath!@token`,
-    metrics: "ga:pageviews, ga:avgTimeOnPage, ga:pageviewsPerSession",
-    dimensions: "ga:date",
-    fieldName: "ga:date",
-    sortOrder: "ASCENDING"
-  }   
-  let resp=await fetch(devEvius, {
-      headers: {
-        'content-type': 'application/json',
-        "Accept": "application/json",
-      },
-      body: JSON.stringify(data),
-      method: 'POST'
-    })
-    let respjson= await resp.json()
-    let datos = respjson.rows;        
-        let totalMetrics = [];
-        datos.map((dat) => {
-          let metric = {
-            month: dat[0],
-            view: dat[1],
-            time: (dat[2]/60).toFixed(2),
-          };
-          totalMetrics.push(metric);
-        });
-    return totalMetrics;
-  }
-
-   //FUNCION QUE PERMITE CREAR OBJETO PARA ASIGNAR A LA GRAFICA
-   export const setDataGraphic=(labels, values, name) =>{
-    let data = {
-      labels: labels,
-      datasets: [
-        {
-          label: name,
-          data: values,
-          fill: false,
-          backgroundColor: backgroud,
-          borderColor: lineBackground,
-        },
-      ],
-    };
-    return data;
-  }
-    //Función que permite obtener metricas por vistas de actividad
-   export const obtenerMetricasByView = (view,metricsGnal) => {
-      let metrics = metricsGnal.filter((m) => m.view == view)[0];
-      return metrics;
-    };
-
-      //Función que permite exportar los reportes formato excel
-  export const exportDataReport = (datos,type) => {    
-
-    let data = [];
-    if (datos.length > 0) {
-      if (type == 'register') {
-        data = datos.map((item) => {
-          return { fecha: item.date, cantidadregistros: item.quantity };
-        });
-      }
-      if (type == 'views') {
-        data = datos.map((item) => {
-          return { fecha: moment(item.month).format('YYYY-MM-DD'), 'cantidad de visitas': item.view };
-        });
-      }
-
-      if (type == 'time') {
-        data = datos.map((item) => {
-          return {
-            fecha: moment(item.month).format('YYYY-MM-DD'),
-            'tiempoPromedio(min)': parseFloat(item.time).toFixed(2),
-          };
-        });
-      }
-
-      for (let i = 0; data.length > i; i++) {
-        if (Array.isArray(data[i].response)) {
-          data[i].response = data[i].response.toString();
-        }
-      }
-      return data;
-    } else {
-      return null
-    }
+    metrics: 'ga:pageviews, ga:avgTimeOnPage, ga:pageviewsPerSession',
+    dimensions: 'ga:date',
+    fieldName: 'ga:date',
+    sortOrder: 'ASCENDING',
   };
+  let resp = await fetch(devEvius, {
+    headers: {
+      'content-type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify(data),
+    method: 'POST',
+  });
+  let respjson = await resp.json();
+  let datos = respjson.rows;
+  let totalMetrics = [];
+  datos.map((dat) => {
+    let metric = {
+      month: moment(dat[0]).format('YYYY-MM-DD'),
+      view: dat[1],
+      time: (dat[2] / 60).toFixed(2),
+    };
+    totalMetrics.push(metric);
+  });
+  return totalMetrics;
+};
 
-   //Función que permite obtener las métricas por cada actividad
-  export const updateMetricasActivity = (data,eventId,metricsGActivity) => {
-    if (data.length > 0) {     
-      let metricsActivity = [];
-      data.map((activity) => {
-        let metricsView = obtenerMetricasByView('/landing/' + eventId + '/activity/' + activity.name,metricsGActivity);
-        let metricaActivity = {
-          name: activity.name,
-          view: metricsView ? metricsView.metrics[0] : 0,
-          prints: 0,
-          time: metricsView ? (metricsView.metrics[4] / 60).toFixed(2) + ' min' : '0 min',
-        };
-        
-        metricsActivity.push(metricaActivity);
+//FUNCION QUE PERMITE CREAR OBJETO PARA ASIGNAR A LA GRAFICA
+export const setDataGraphic = (labels, values, name) => {
+  let data = {
+    labels: labels,
+    datasets: [
+      {
+        label: name,
+        data: values,
+        fill: false,
+        backgroundColor: backgroud,
+        borderColor: lineBackground,
+      },
+    ],
+  };
+  return data;
+};
+//Función que permite obtener metricas por vistas de actividad
+export const obtenerMetricasByView = (view, metricsGnal) => {
+  let metrics = metricsGnal.filter((m) => m.view == view)[0];
+  return metrics;
+};
+
+//Función que permite exportar los reportes formato excel
+export const exportDataReport = (datos, type) => {
+  let data = [];
+  if (datos.length > 0) {
+    if (type == 'register') {
+      data = datos.map((item) => {
+        return { fecha: item.date, cantidadregistros: item.quantity };
       });
-      return metricsActivity;
     }
+    if (type == 'views') {
+      data = datos.map((item) => {
+        return { fecha: moment(item.month).format('YYYY-MM-DD'), 'cantidad de visitas': item.view };
+      });
+    }
+
+    if (type == 'time') {
+      data = datos.map((item) => {
+        return {
+          fecha: moment(item.month).format('YYYY-MM-DD'),
+          'tiempoPromedio(min)': parseFloat(item.time).toFixed(2),
+        };
+      });
+    }
+
+    for (let i = 0; data.length > i; i++) {
+      if (Array.isArray(data[i].response)) {
+        data[i].response = data[i].response.toString();
+      }
+    }
+    return data;
+  } else {
+    return null;
   }
+};
+
+//Función que permite obtener las métricas por cada actividad
+export const updateMetricasActivity = (data, eventId, metricsGActivity) => {
+  if (data.length > 0) {
+    let metricsActivity = [];
+    data.map((activity) => {
+      let metricsView = obtenerMetricasByView('/landing/' + eventId + '/activity/' + activity.name, metricsGActivity);
+      let metricaActivity = {
+        name: activity.name,
+        view: metricsView ? metricsView.metrics[0] : 0,
+        prints: 0,
+        time: metricsView ? (metricsView.metrics[4] / 60).toFixed(2) + ' min' : '0 min',
+      };
+
+      metricsActivity.push(metricaActivity);
+    });
+    return metricsActivity;
+  }
+};
