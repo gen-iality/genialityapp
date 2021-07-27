d781c9fe1bcf107979ba21575982d162028d7ff2
Diana <diana.silva@mocionsoft.com>
2021-07-27 14:20:36 -0500

    stand

--- src/components/events/ferias/FeriasList.jsx
+++ src/components/events/ferias/FeriasList.jsx
@@ -1,4 +1,4 @@
-import { Empty, Spin } from 'antd';
+import { Empty, Spin, Col, Row} from 'antd';
 import React from 'react';
 import Companylist from './companyList';
 import { useEffect } from 'react';
@@ -6,6 +6,7 @@ import useGetEventCompanies from '../../empresas/customHooks/useGetEventCompanie
 import { useState } from 'react';
 import { connect } from 'react-redux';
 import { setVirtualConference } from '../../../redux/virtualconference/actions';
+import FeriaStand from './FeriasStand'
 import {setTopBanner} from '../../../redux/topBanner/actions';
 import { firestore } from '../../../helpers/firebase';
 
@@ -64,10 +65,38 @@ const FeriasList = ({ event_id,setVirtualConference,setTopBanner }) => {
             )
         )}
        {/*PARA OTRO TIPO DE VISUALIZACION */} 
+       {companiesEvent.length > 0 && config.visualization==='stand' && (
+         <div className='mini-banner'
+         style={{ borderRadius:'12px', width:'60%', display:'block', margin:'auto', marginBottom:'25px'}}>
+        <img
+          src={'http://via.placeholder.com/1500x540/50D3C9/FFFFFF?text=Banner%20empresa'}
+          style={{
+            width: '100%',
+            border:'15px solid white', borderRadius:'15px'
+            // height:'50vh'
+          }}
+        />  
+      </div>
+       )}
+       <Row>
       {companiesEvent.length > 0 && config.visualization==='stand' &&
         companiesEvent.map(
           (company, index) =>
-            company.visible && ( <div>otra visualizaciòn</div>))}
+            company.visible && (
+            
+             <Col key={index}span={8} >
+            <FeriaStand
+              image={
+                company.list_image === ''
+                  ? 'https://via.placeholder.com/200/50D3C9/FFFFFF?text=Logo' // imagen por defecto si no encuentra una imagen guardada
+                  : company.list_image
+              }
+              eventId={event_id}
+              name={company.name}
+            />
+            </Col> 
+           
+            ))} </Row>
       {companiesEvent.length == 0 && <Empty />}
     </div>
   );
