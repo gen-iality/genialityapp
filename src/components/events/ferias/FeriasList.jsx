9e826da659213ce15cf46e361f080a11291ac130
Diana <diana.silva@mocionsoft.com>
2021-07-27 18:12:57 -0500

    Validar campo de comprar producto

--- src/components/events/ferias/FeriasList.jsx
+++ src/components/events/ferias/FeriasList.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Companylist from './companyList';
 import { useEffect } from 'react';
 import useGetEventCompanies from '../../empresas/customHooks/useGetEventCompanies';
+import MiniBanner from './MiniBanner';
 import { useState } from 'react';
 import { connect } from 'react-redux';
 import { setVirtualConference } from '../../../redux/virtualconference/actions';
@@ -41,7 +42,9 @@ const FeriasList = ({ event_id,setVirtualConference,setTopBanner }) => {
   return (
     <div>
       {loadingCompanies && <Spin size='small' />}
-
+      {companiesEvent.length > 0 && config.visualization==='list' && (
+      <MiniBanner/>
+       )}
       {companiesEvent.length > 0 && config.visualization==='list' &&
         companiesEvent.map(
           (company, index) =>
@@ -65,38 +68,26 @@ const FeriasList = ({ event_id,setVirtualConference,setTopBanner }) => {
             )
         )}
        {/*PARA OTRO TIPO DE VISUALIZACION */} 
-       {companiesEvent.length > 0 && config.visualization==='stand' && (
-         <div className='mini-banner'
-         style={{ borderRadius:'12px', width:'60%', display:'block', margin:'auto', marginBottom:'25px'}}>
-        <img
-          src={'http://via.placeholder.com/1500x540/50D3C9/FFFFFF?text=Banner%20empresa'}
-          style={{
-            width: '100%',
-            border:'15px solid white', borderRadius:'15px'
-            // height:'50vh'
-          }}
-        />  
-      </div>
-       )}
+       
        <Row>
-      {companiesEvent.length > 0 && config.visualization==='stand' &&
-        companiesEvent.map(
-          (company, index) =>
-            company.visible && (
+        {companiesEvent.length > 0 && config.visualization==='stand' &&
+          companiesEvent.map(
+            (company, index) =>
+              company.visible && (
+              <Col key={index}span={8} >
+              <FeriaStand
+                image={
+                  company.list_image === ''
+                    ? 'https://via.placeholder.com/200/50D3C9/FFFFFF?text=Logo' // imagen por defecto si no encuentra una imagen guardada
+                    : company.list_image
+                }
+                eventId={event_id}
+                name={company.name}
+              />
+              </Col> 
             
-             <Col key={index}span={8} >
-            <FeriaStand
-              image={
-                company.list_image === ''
-                  ? 'https://via.placeholder.com/200/50D3C9/FFFFFF?text=Logo' // imagen por defecto si no encuentra una imagen guardada
-                  : company.list_image
-              }
-              eventId={event_id}
-              name={company.name}
-            />
-            </Col> 
-           
-            ))} </Row>
+              ))} 
+        </Row>
       {companiesEvent.length == 0 && <Empty />}
     </div>
   );
