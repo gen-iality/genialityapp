43a7d97c6039a0050766e1623756ae38aa1877c5
Marlon Carrillo <marlon.carrillo@mocionsoft.com>
2021-08-05 10:46:00 -0500

    validacion de elipsis en la descripcion corta del listado de ferias

--- src/components/events/ferias/companyList.jsx
+++ src/components/events/ferias/companyList.jsx
@@ -5,9 +5,20 @@ import { useHistory } from 'react-router';
 import { Link } from 'react-router-dom';
 
 function Companylist(props) {
-  const { Text } = Typography;
+  const { Text, Paragraph } = Typography;
   const history = useHistory();
-  console.log(props)
+  console.log(props);
+
+  function stripHtml(html) {
+    // Crea un nuevo elemento div
+    var temporalDivElement = document.createElement('div');
+    // Establecer el contenido HTML con el div
+    temporalDivElement.innerHTML = html;
+    // Recuperar la propiedad de texto del elemento
+    return temporalDivElement.textContent || temporalDivElement.innerText || '';
+  }
+
+  const description = stripHtml(props.description);
 
   function showModal(info) {
     Modal.info({
@@ -44,79 +55,91 @@ function Companylist(props) {
   return (
     <div className='company-list'>
       <Badge.Ribbon text={props.text} color={props.colorStand}>
-      <Card bodyStyle={{padding:'12px'}} style={{ cursor: 'pointer', 
-      border:`1px solid ${props.colorStand}`, 
-      paddingBottom:'12px', 
-      paddingTop:'12px', 
-      margin:'auto',
-      borderRadius:'5px'}}>
-      <Row className='container' gutter={[10, 10]}  >
-        <Col
-          xs={24}
-          sm={24}
-          md={24}
-          lg={6}
-          xl={6}
-          className='col'
-          onClick={() => {
-            history.push(`/landing/${props.eventId}/ferias/${props.companyId}/detailsCompany`);
+        <Card
+          bodyStyle={{ padding: '12px' }}
+          style={{
+            cursor: 'pointer',
+            border: `1px solid ${props.colorStand}`,
+            paddingBottom: '12px',
+            paddingTop: '12px',
+            margin: 'auto',
+            borderRadius: '5px',
           }}>
-          <div className='img-contact'>
-            <img className='img' src={props.img} />
-          </div>
-        </Col>
-        <Col
-          xs={24}
-          sm={24}
-          md={24}
-          lg={9}
-          xl={9}>
-          <div className='info-contact'>
-            <span className='name'><Tag color={props.colorStand} style={{height:'12px'}}/>{props.name} </span>
-            <span className='position'>{props.position} </span>
-            <span className='description'>
-              <div dangerouslySetInnerHTML={{ __html: props.description }} />
-            </span>
-            <span style={{marginTop:15}}><Link to={`/landing/${props.eventId}/ferias/${props.companyId}/detailsCompany`}>Ver más</Link></span>
-          </div>
-        </Col>
-        {props.tel || props.email || props.pagweb ? (
-          <Col xs={24} sm={24} md={24} lg={9} xl={9}>
-            <div className='redes-contact'>
-              {props.tel && (
-                <span className='tel'>
-                  <PhoneOutlined className='icono' /> {props.tel}
+          <Row className='container' gutter={[10, 10]}>
+            <Col
+              xs={24}
+              sm={24}
+              md={24}
+              lg={6}
+              xl={6}
+              className='col'
+              onClick={() => {
+                history.push(`/landing/${props.eventId}/ferias/${props.companyId}/detailsCompany`);
+              }}>
+              <div className='img-contact'>
+                <img className='img' src={props.img} />
+              </div>
+            </Col>
+            <Col xs={24} sm={24} md={24} lg={9} xl={9}>
+              <div className='info-contact'>
+                <span className='name'>
+                  <Tag color={props.colorStand} style={{ height: '12px' }} />
+                  {props.name}{' '}
                 </span>
-              )}
-              {props.email && (
-                <span className='email'>
-                  <MailOutlined className='icono' /> {props.email}
+                <span className='position'>{props.position} </span>
+                <Paragraph
+                  ellipsis={{
+                    rows: 3, // Determina la cantidad de filas que se muestran antes de cortar el texto.
+                    expandable: false,
+                  }}
+                  className='description'>
+                  {description}
+                </Paragraph>
+                <span style={{ marginTop: 15 }}>
+                  <Link to={`/landing/${props.eventId}/ferias/${props.companyId}/detailsCompany`}>
+                    <Button type='primary'>Visitar stand</Button>
+                  </Link>
                 </span>
-              )}
-              {props.pagweb && (
-                <span className='web'>
-                  <GlobalOutlined className='icono' />
-                  <a
-                    rel='noreferrer'
-                    onClick={() => {
-                      window.open(`${props.pagweb}`, '_blank');
-                    }}
-                    target='_blank'>
-                    {props.pagweb}
-                  </a>
-                </span>
-              )}
-            </div>
-            <div style={{ textAlign: 'center' }}>
-              <Button type='default' className='boton' size='large' onClick={() => showModal(props)}>
-                Información de contacto
-              </Button>
-            </div>
-          </Col>
-        ) : (
-          ''
-        )}
-      </Row></Card>
+              </div>
+            </Col>
+            {props.tel || props.email || props.pagweb ? (
+              <Col xs={24} sm={24} md={24} lg={9} xl={9}>
+                <div className='redes-contact'>
+                  {props.tel && (
+                    <span className='tel'>
+                      <PhoneOutlined className='icono' /> {props.tel}
+                    </span>
+                  )}
+                  {props.email && (
+                    <span className='email'>
+                      <MailOutlined className='icono' /> {props.email}
+                    </span>
+                  )}
+                  {props.pagweb && (
+                    <span className='web'>
+                      <GlobalOutlined className='icono' />
+                      <a
+                        rel='noreferrer'
+                        onClick={() => {
+                          window.open(`${props.pagweb}`, '_blank');
+                        }}
+                        target='_blank'>
+                        {props.pagweb}
+                      </a>
+                    </span>
+                  )}
+                </div>
+                <div style={{ textAlign: 'center' }}>
+                  <Button type='default' className='boton' size='large' onClick={() => showModal(props)}>
+                    Información de contacto
+                  </Button>
+                </div>
+              </Col>
+            ) : (
+              ''
+            )}
+          </Row>
+        </Card>
       </Badge.Ribbon>
     </div>
   );
