a44c341885405b6aecaed7c49c570a71d6474e71
Jaime Daniel <jaime.barros@growdata.com.co>
2021-08-11 14:39:19 -0500

    fix partners

--- src/components/events/PartnersList/index.js
+++ src/components/events/PartnersList/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { Button, Row, Col, Avatar, Card } from 'antd';
 
 function PartnerItemList({ company, handleOpenPartnerDetail, companyId }) {
+
   return (
-    <Card style={{ marginBottom: '12px' }}>
+    (company.visible || company.visible=='true') && <Card style={{ marginBottom: '12px' }}>
       <Row gutter={[16, 16]} justify='center' align='middle'>
         <Col xs={24} sm={24} md={12} lg={6} xl={6} xxl={6}>
           <Avatar src={company.list_image} size={100}></Avatar>
