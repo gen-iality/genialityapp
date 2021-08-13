453810a4621d3348599edd5b134518b8d6056741
Edwin Ferney Villa T <edwin.villa@mocionsoft.com>
2021-08-12 20:48:21 -0500

    ♻️ REFACTOR : Se agrega boton Crear producto con antd asi como el titulo de la seccion, se quita componente EventContent

--- src/components/products/product.jsx
+++ src/components/products/product.jsx
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
-import { Table, Tooltip, Space, Button, Image, Modal, message, Typography } from 'antd';
-import { EditOutlined, DeleteOutlined, ExclamationCircleOutlined } from '@ant-design/icons';
+import { Table, Tooltip, Space, Button, Image, Modal, message, Typography, Row } from 'antd';
+import { EditOutlined, DeleteOutlined, ExclamationCircleOutlined, PlusCircleOutlined } from '@ant-design/icons';
 import { EventsApi } from '../../helpers/request';
 import Loading from '../loaders/loading';
-import EventContent from '../events/shared/content';
-import { handleRequestError, sweetAlert } from '../../helpers/utils';
-import axios from 'axios/index';
-import { toast } from 'react-toastify';
-import { FormattedMessage } from 'react-intl';
 import { withRouter } from 'react-router';
 import htmlParser from 'html-react-parser';
-import { reject } from 'ramda';
 
 const { Column } = Table;
 const { confirm } = Modal;
-const { Paragraph } = Typography;
+const { Title, Paragraph } = Typography;
 
 class Product extends Component {
    constructor(props) {
@@ -73,88 +67,87 @@ class Product extends Component {
 
    render() {
       return (
-         <React.Fragment>
-            <div className='column is-12 is-desktop'>
-               <EventContent
-                  title='Producto'
-                  closeAction={this.goBack}
-                  description={'Agregue o edite los productos que se muestran en la aplicación'}
-                  addAction={this.newProduct}
-                  addTitle={'Nuevo producto'}>
-                  {this.state.loading ? (
-                     <Loading />
-                  ) : (
-                     <Table
-                        size='small'
-                        bordered
-                        dataSource={this.state.list}
-                        pagination={{ pageSize: 6, position: ['bottomCenter'] }}
-                        scroll={{ x: 1300 }}>
-                        <Column key='_id' title='Nombre' dataIndex='name' align='center' />
-                        <Column
-                           key='_id'
-                           title='Descripción'
-                           align='center'
-                           render={(data, index) => (
-                              <Paragraph
-                                 ellipsis={{
-                                    rows: 3,
-                                    expandable: true,
-                                    symbol: <span style={{ color: '#2D7FD6', fontSize: '14px' }}>Ver mas</span>,
-                                 }}>
-                                 {htmlParser(data.description)}
-                              </Paragraph>
-                           )}
-                        />
+         <div>
+            <Title level={4}>{'Crear producto'}</Title>
 
-                        <Column key='_id' title='Valor' dataIndex='price' align='center' />
+            <Row justify='end' style={{ marginBottom: '10px' }}>
+               <Button onClick={this.newProduct} type='primary' icon={<PlusCircleOutlined />}>
+                  {'Crear producto'}
+               </Button>
+            </Row>
 
-                        <Column
-                           key='_id'
-                           title='Imagenes del producto'
-                           align='center'
-                           render={(data, index) => (
-                              <Space key={index} size='small'>
-                                 <Tooltip key={index} placement='topLeft' title='Vista previa'>
-                                    {data.image.map((images, index) => {
-                                       return <Image key={index} width={100} src={images} />;
-                                    })}
-                                 </Tooltip>
-                              </Space>
-                           )}
-                        />
+            {this.state.loading ? (
+               <Loading />
+            ) : (
+               <Table
+                  size='small'
+                  bordered
+                  dataSource={this.state.list}
+                  pagination={{ pageSize: 6, position: ['bottomCenter'] }}
+                  scroll={{ x: 1300 }}>
+                  <Column key='_id' title='Nombre' dataIndex='name' align='center' />
+                  <Column
+                     key='_id'
+                     title='Descripción'
+                     align='center'
+                     render={(data, index) => (
+                        <Paragraph
+                           ellipsis={{
+                              rows: 3,
+                              expandable: true,
+                              symbol: <span style={{ color: '#2D7FD6', fontSize: '14px' }}>Ver mas</span>,
+                           }}>
+                           {htmlParser(data.description)}
+                        </Paragraph>
+                     )}
+                  />
 
-                        <Column
-                           title='Herramientas'
-                           key='_id'
-                           align='center'
-                           fixed='right'
-                           render={(data, index) => (
-                              <Space key={index} size='large'>
-                                 <Tooltip key={index} placement='topLeft' title='Editar'>
-                                    <Button
-                                       key={index}
-                                       onClick={() => this.editProduct(data)}
-                                       type='primary'
-                                       icon={<EditOutlined style={{ fontSize: 25 }} />}
-                                    />
-                                 </Tooltip>
-                                 <Tooltip key={index} placement='topLeft' title='Eliminar'>
-                                    <Button
-                                       key={index}
-                                       onClick={() => this.removeProduct(data)}
-                                       type='danger'
-                                       icon={<DeleteOutlined style={{ fontSize: 25 }} />}
-                                    />
-                                 </Tooltip>
-                              </Space>
-                           )}
-                        />
-                     </Table>
-                  )}
-               </EventContent>
-            </div>
-         </React.Fragment>
+                  <Column key='_id' title='Valor' dataIndex='price' align='center' />
+
+                  <Column
+                     key='_id'
+                     title='Imagenes del producto'
+                     align='center'
+                     render={(data, index) => (
+                        <Space key={index} size='small'>
+                           <Tooltip key={index} placement='topLeft' title='Vista previa'>
+                              {data.image.map((images, index) => {
+                                 return <Image key={index} width={100} src={images} />;
+                              })}
+                           </Tooltip>
+                        </Space>
+                     )}
+                  />
+
+                  <Column
+                     title='Herramientas'
+                     key='_id'
+                     align='center'
+                     fixed='right'
+                     render={(data, index) => (
+                        <Space key={index} size='large'>
+                           <Tooltip key={index} placement='topLeft' title='Editar'>
+                              <Button
+                                 key={index}
+                                 onClick={() => this.editProduct(data)}
+                                 type='primary'
+                                 icon={<EditOutlined style={{ fontSize: 25 }} />}
+                              />
+                           </Tooltip>
+                           <Tooltip key={index} placement='topLeft' title='Eliminar'>
+                              <Button
+                                 key={index}
+                                 onClick={() => this.removeProduct(data)}
+                                 type='danger'
+                                 icon={<DeleteOutlined style={{ fontSize: 25 }} />}
+                              />
+                           </Tooltip>
+                        </Space>
+                     )}
+                  />
+               </Table>
+            )}
+         </div>
       );
    }
 }
