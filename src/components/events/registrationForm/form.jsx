ea032e0888fcdde1d0002240834fb5a2ad8a1768
Jaime Daniel <jaime.barros@growdata.com.co>
2021-08-17 11:17:46 -0500

    flujo register pay and free

--- src/components/events/registrationForm/form.jsx
+++ src/components/events/registrationForm/form.jsx
@@ -27,7 +27,8 @@ import ImgCrop from 'antd-img-crop';
 import { saveImageStorage } from '../../../helpers/helperSaveImage';
 import { areaCode } from '../../../helpers/constants';
 import TypeRegister from '../../tickets/typeRegister';
-
+import  {ButtonPayment, PayForm}  from './payRegister';
+ 
 // import InputFile from "./inputFile"
 const { Option } = Select;
 const { Panel } = Collapse;
@@ -603,7 +604,7 @@ export default ({
 
   return (
     <>
-      {console.log('PROPS==>', eventId)}
+       {console.log("EVENTUSER==>",eventUser)}
       <Col xs={24} sm={22} md={18} lg={18} xl={18} style={center}>
         {!submittedForm ? (
           <Card
@@ -617,6 +618,7 @@ export default ({
             {eventId && eventId == '60cb7c70a9e4de51ac7945a2' && !eventUser && (
               <TypeRegister typeRegister={typeRegister} setTypeRegister={setTypeRegister} />
             )}
+            {eventUser !== undefined && eventUser.rol_id=='60e8a7e74f9fb74ccd00dc22' && <ButtonPayment />}
             <Form
               form={form}
               layout='vertical'
@@ -706,59 +708,7 @@ export default ({
             </Result>
           </Card>
         ) : (
-          <Card>
-            {eventId && eventId == '60cb7c70a9e4de51ac7945a2' && (
-              <Result
-              icon={<ShopOutlined />}
-                title='Su registro ha sido exitoso, click al siguiente enlace para realizar la donación'
-                extra={
-                  <div style={{ width: '100%', textAlign:'center' }}>
-                    {' '}
-                    <form
-                      method='post'
-                      action='https://gateway.payulatam.com/ppp-web-gateway/pb.zul'
-                      acceptCharset='UTF-8'>
-                       {' '}
-                      <input
-                        style={{textAlign:'center', width:'100%',height:'46px',objectFit:'contain'}}
-                        type='image'
-                        border='0'
-                        alt=''
-                        src='http://www.payulatam.com/img-secure-2015/boton_pagar_mediano.png'
-                        onClick='this.form.urlOrigen.value = window.location.href;'
-                      />
-                       {' '}
-                      <input
-                        name='buttonId'
-                        type='hidden'
-                        value='EoWD0oG4B5RBZsfk85w2xpcGXj4HaXMobwad57IAKn8gvwD6jUhHuw=='
-                      />
-                        <input name='merchantId' type='hidden' value='519160' />
-                        <input name='accountId' type='hidden' value='520706' />
-                        <input name='description' type='hidden' value='entrada puja' />
-                        <input name='referenceCode' type='hidden' value='entrada puja' />
-                        <input name='amount' type='hidden' value='50000.00' />
-                        <input name='tax' type='hidden' value='0.00' />
-                        <input name='taxReturnBase' type='hidden' value='0.00' />
-                        <input name='shipmentValue' value='0.00' type='hidden' />
-                        <input name='currency' type='hidden' value='COP' />
-                        <input name='lng' type='hidden' value='es' />
-                        <input name='approvedResponseUrl' type='hidden' value='https://evius.co' />
-                        <input name='declinedResponseUrl' type='hidden' value='https://evius.co' />
-                        <input name='pendingResponseUrl' type='hidden' value='https://evius.co' />
-                        <input name='sourceUrl' id='urlOrigen' value='' type='hidden' />
-                        <input name='buttonType' value='SIMPLE' type='hidden' /> {' '}
-                      <input
-                        name='signature'
-                        value='369539171cf3b776a309aca17fc609287bd94401cafd00337db6d208c1b5da0f'
-                        type='hidden'
-                      />
-                    </form>{' '}
-                  </div>
-                }
-              />
-            )}
-          </Card>
+         <PayForm eventId={eventId} />
         )}
       </Col>
     </>
