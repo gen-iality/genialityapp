9e5f5c10f18dc7a735fc774644e1fb1d90f032da
Jaime Daniel <jaime.barros@growdata.com.co>
2021-08-18 15:30:58 -0500

    redirect footer

--- src/components/events/Landing/EviusFooter.jsx
+++ src/components/events/Landing/EviusFooter.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { UseEventContext } from '../../../Context/eventContext';
+import { useHistory } from 'react-router-dom';
 const EviusFooter = () => {
   let cEventContext = UseEventContext();
+  let history = useHistory();
   return (
     <>
       {cEventContext.value.styles && cEventContext.value.styles.banner_footer && (
-        <div style={{ textAlign: 'center' }}>
+        <div onClick={()=>cEventContext.value._id=='60cb7c70a9e4de51ac7945a2'? history.push(`/landing/${cEventContext.value._id}/tickets`):null} style={{ textAlign: 'center' }}>
           <img alt='image-dialog' src={cEventContext.value.styles.banner_footer} />
         </div>
       )}
