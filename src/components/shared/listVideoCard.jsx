393cb0d472dcb602f4e0a53c61c9c4767cb325da
mdmontero <marioupc3@gmail.com>
2021-08-20 12:45:34 -0500

    fix show section videos

--- src/components/shared/listVideoCard.jsx
+++ src/components/shared/listVideoCard.jsx
@@ -2,12 +2,29 @@ import { Card, Space } from 'antd';
 import React, { Fragment, useContext } from 'react';
 import VideoCard from './videoCard';
 import { HelperContext } from '../../Context/HelperContext';
+import { useState } from 'react';
 const ListVideoCard = () => {
   let { activitiesEvent } = useContext(HelperContext);
+  const [existActivity, setexistActivity] = useState(0);
+  function ExistvideoInActivity() {
+    activitiesEvent &&
+      activitiesEvent.map((activity) => {
+        console.log('video', activity.video);
+        if (activity.video != undefined || activity.video != null) {
+          {
+            setexistActivity(1);
+          }
+        }
+      });
+  }
+  React.useEffect(() => {
+    ExistvideoInActivity();
+    console.log('existactivity', existActivity);
+  }, [activitiesEvent]);
 
   return (
     <>
-      {activitiesEvent && (
+      {existActivity == 1 && (
         <Fragment style={{ width: '100%' }}>
           <Card headStyle={{ border: 'none' }} title='Videos grabados'>
             <Space size='large' style={{ width: '100%', overflowX: 'auto', padding: '10px', margin: '10px' }}>
