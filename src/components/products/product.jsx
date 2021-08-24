36bf15bdd6c2b5de61af6feee25b0a89acc71cff
Jaime Daniel <jaime.barros@growdata.com.co>
2021-08-24 11:29:42 -0500

    order galeria

--- src/components/products/product.jsx
+++ src/components/products/product.jsx
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
-import { Table, Tooltip, Space, Button, Image, Modal, message, Typography, Row } from 'antd';
-import { EditOutlined, DeleteOutlined, ExclamationCircleOutlined, PlusCircleOutlined } from '@ant-design/icons';
+import { Table, Tooltip, Space, Button, Image, Modal, message, Typography, Row, Spin } from 'antd';
+import { EditOutlined, DeleteOutlined, ExclamationCircleOutlined, PlusCircleOutlined, DragOutlined } from '@ant-design/icons';
+import { sortableContainer, sortableElement, sortableHandle } from 'react-sortable-hoc';
+import arrayMove from 'array-move';
 import { EventsApi } from '../../helpers/request';
 import Loading from '../loaders/loading';
 import { withRouter } from 'react-router';
+import { product } from 'ramda';
 
 const { Column } = Table;
 const { confirm } = Modal;
 const { Title, Paragraph } = Typography;
+const  DragHandle = sortableHandle(() => <DragOutlined  style={{ cursor: 'grab', color: '#999' }} />);
+const SortableItem = sortableElement(props => <tr {...props} />);
+const SortableContainer = sortableContainer(props => <tbody {...props} />)
+
 
 class Product extends Component {
    constructor(props) {
@@ -15,8 +22,37 @@ class Product extends Component {
       this.state = {
          list: [],
          loading: true,
+         loadingPosition:false         
       };
    }
+   //DRAG TABLE
+   DraggableContainer = props => (
+      <SortableContainer
+        useDragHandle
+        disableAutoscroll
+        helperClass="row-dragging"
+        onSortEnd={this.onSortEnd}
+        {...props}
+      />
+    );
+    DraggableBodyRow = ({ className, style, ...restProps }) => {
+      const { list } = this.state;
+      // function findIndex base on Table rowKey props and should always be a right array index
+      const index = list.findIndex(x => x.index === restProps['data-row-key']);
+      return <SortableItem index={index} {...restProps} />;
+    };
+
+    onSortEnd = ({ oldIndex, newIndex }) => {
+      const { list } = this.state;
+      list[newIndex].index=oldIndex;
+      list[oldIndex].index=newIndex;
+      if (oldIndex !== newIndex) {
+        const newData = arrayMove([].concat(list), oldIndex, newIndex).filter(el => !!el);
+        console.log('Sorted items: ', newData);
+        this.setState({list: newData });
+      }
+    };
+  
 
    componentDidMount() {
       this.fetchItem();
@@ -24,8 +60,38 @@ class Product extends Component {
 
    fetchItem = async () => {
       const data = await EventsApi.getProducts(this.props.eventId);
-      this.setState({ list: data.data, loading: false });
+      console.log("DATA==>",data.data)
+      let listproduct=[];
+      if(data.data){
+        listproduct= data.data.map((product,index)=>{
+           return {...product,index:product.position?product.position:index}
+         })
+         listproduct=listproduct.sort((a,b)=>a.index-b.index)
+         this.setState({ list: listproduct, loading: false });         
+      }    
    };
+
+   savePosition=async()=>{
+      //alert("A GUARDAR")     
+      const loadingSave = message.open({
+         key: 'loading',
+         type: 'loading',
+         content: <> Por favor espere miestras se guarda la configuración..</>,
+       });
+    //console.log("NEW LIST==>",this.state.list)
+    if(this.state.list){
+      await Promise.all(this.state.list.map(async(product,index)=>{
+          let productChange={...product,position:product.index}
+          await EventsApi.editProduct(productChange,this.props.eventId,product._id)
+         console.log("ACTUALIZADO..",index)
+       }))
+    }
+    message.destroy(loadingSave.key);
+    message.open({
+      type: 'success',
+      content: <> Configuración guardada correctamente!</>,
+    });
+   }
 
    editProduct = (cert) => {
       this.props.history.push(`/event/${this.props.eventId}/product/addproduct/${cert._id}`);
@@ -73,8 +139,11 @@ class Product extends Component {
                <Button onClick={this.newProduct} type='primary' style={{marginRight:'20px'}} icon={<PlusCircleOutlined />}>
                   {'Crear producto'}
                </Button>
-               <Button onClick={this.configuration} type='primary' icon={<PlusCircleOutlined />}>
+               <Button onClick={this.configuration} type='primary' style={{marginRight:'20px'}} icon={<PlusCircleOutlined />}>
                   {'Configuración'}
+               </Button>
+               <Button onClick={this.savePosition} type='primary'  icon={<PlusCircleOutlined />}>
+                  {'Guardar posición'}
                </Button>
             </Row>
 
@@ -85,8 +154,26 @@ class Product extends Component {
                   size='small'
                   rowKey='index'
                   dataSource={this.state.list}
-                  pagination={{ pageSize: 6, position: ['bottomCenter'] }}
-                  scroll={{ x: 1300 }}>
+                  pagination={false } //pageSize: 6, position: ['bottomCenter'] }}
+                  scroll={{ x: 1300 }}
+                  components={{
+                     body: {
+                       wrapper: this.DraggableContainer,
+                       row: this.DraggableBodyRow,
+                     }}}
+                  >
+               <Column
+                  title=''
+                  dataIndex='move'
+                  width='50px'
+                  render={ () => <DragHandle />}
+                  />
+                  <Column
+                  title='Posición'
+                  dataIndex='index'
+                  width='100px'
+                  render={ (data,index) => <div>{data}</div>}
+                  />
                   <Column
                      key='_id'
                      title='Nombre'
@@ -142,9 +229,8 @@ class Product extends Component {
                            </Tooltip>
                         </Space>
                      )}
-                  />
-               </Table>
-            )}
+                  />                
+               </Table>)}
          </div>
       );
    }
