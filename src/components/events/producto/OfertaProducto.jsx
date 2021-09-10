ba0ebcb2ab20db4fd41aaa3f1d920e207c04502b
Jaime Daniel <jaimedaniel.bm91@gmail.com>
2021-09-10 14:35:05 -0500

    fix messsage obra

--- src/components/events/producto/OfertaProducto.jsx
+++ src/components/events/producto/OfertaProducto.jsx
@@ -261,7 +261,7 @@ const OfertaProduct = ({ product, eventId, cEventUser, cUser, hability,messageF,
           )}
            {!permission() && cEventUser.value===null &&(
             <Row>
-              <Alert type='warning' message='Aún no te has registrado en el evento.' />
+              <Alert type='warning' message='Aún no te has registrado al evento.' />
             </Row>
           )}
         </Row>
