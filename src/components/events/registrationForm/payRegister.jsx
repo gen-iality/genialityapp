b060d3ef2ad345d0a120127b29db7a88a41345e2
Jaime Daniel <jaimedaniel.bm91@gmail.com>
2021-09-14 13:49:34 -0500

    price products and currency

--- src/components/events/registrationForm/payRegister.jsx
+++ src/components/events/registrationForm/payRegister.jsx
@@ -129,7 +129,7 @@ export default withContext(PayForm);
             <input name="algorithmSignature" value="SHA256" type="hidden"/>
           </form>*/}     
       
-        <form
+       {/* <form
         method='post'
         target={'_blank'}
         action='https://gateway.payulatam.com/ppp-web-gateway/pb.zul'
@@ -169,7 +169,7 @@ export default withContext(PayForm);
           value='369539171cf3b776a309aca17fc609287bd94401cafd00337db6d208c1b5da0f'
           type='hidden' 
         />
-      </form>
+      </form>*/}
       </>
     );
 }
