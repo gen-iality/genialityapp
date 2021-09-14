c2c6b07e1ceccb46f3ca679d690be73ca0abf5b8
mdmontero <marioupc3@gmail.com>
2021-09-14 11:57:53 -0500

    test for fixtures

--- cypress/integration/register.spec.js
+++ cypress/integration/register.spec.js
@@ -2,21 +2,15 @@ const urlToTest = '/landing/5fa9a9431cd0d1074b1e9242/evento';
 
 describe('Evius Initial', () => {
   beforeEach(() => {
-    cy.clearLocalStorage();
     cy.visit(`${urlToTest}`);
   });
 
   it('User can register', () => {
-    let fakeuser = {
-      email: 'fakeuser@gmail.com',
-      name: 'fakeuser',
-      phone: '57 4567849',
-      address: 'my crazy direction',
-      interested: 'Vender',
-      economy_activity: 'whatever economy',
-      description: 'whatever description',
-      termsService: true,
-    };
-    cy.registerUser(fakeuser);
+    cy.fixture('fakeuser.json').then((userfake) => {
+      cy.registerUser(userfake).then(() => {
+        cy.contains('Registro Exitoso')
+      });
+    })
+    
   });
 });
