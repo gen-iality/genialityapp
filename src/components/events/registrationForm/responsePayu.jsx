5512b884a017d63df648ab47fa2c613c6ff42d18
Marlon Carrillo <marlon.carrillo@mocionsoft.com>
2021-09-14 17:13:35 -0500

    estilos del modal de resultado del pago

--- src/components/events/registrationForm/responsePayu.jsx
+++ src/components/events/registrationForm/responsePayu.jsx
@@ -1,53 +1,98 @@
+import { CheckCircleFilled, CloseCircleFilled, InfoCircleFilled } from '@ant-design/icons';
 import { Button, Card, Modal, Result, Row } from 'antd';
 import React, { useEffect, useState } from 'react';
 import withContext from '../../../Context/withContext';
 
+const ResponsePayu = (props) => {
+  const [referenceCode, setReferenceCode] = useState();
+  const [response, setResponse] = useState();
+  const [visible, setVisible] = useState(true);
+  useEffect(() => {
+    const parameters = window.location.search;
+    const urlParams = new URLSearchParams(parameters);
+    //Accedemos a los valores
+    var reference = urlParams.get('referenceCode');
+    var lapResponseCode = urlParams.get('lapResponseCode');
+    var polTransactionState = urlParams.get('polTransactionState');
+    var polResponseCode = urlParams.get('polResponseCode');
+    var lapTransactionState = urlParams.get('lapTransactionState');
+    var message = urlParams.get('message');
+    setResponse({
+      reference,
+      lapResponseCode,
+      polTransactionState,
+      polResponseCode,
+      lapTransactionState,
+      message,
+    });
 
-const ResponsePayu=(props)=>{
-    const [referenceCode,setReferenceCode]=useState()
-    const [response,setResponse]=useState()
-    const [visible,setVisible]=useState(true)
-    useEffect(()=>{
-      const parameters = window.location.search;
-      const urlParams = new URLSearchParams(parameters);
-      //Accedemos a los valores
-      var reference= urlParams.get('referenceCode');
-      var lapResponseCode= urlParams.get('lapResponseCode');
-      var polTransactionState= urlParams.get('polTransactionState');
-      var polResponseCode= urlParams.get('polResponseCode');
-      var lapTransactionState= urlParams.get('lapTransactionState');
-      var message= urlParams.get('message');
-      setResponse({
-        reference,
-        lapResponseCode,
-        polTransactionState,
-        polResponseCode,
-        lapTransactionState,
-        message
-      })
-      
-      //lapResponseCode,polTransactionState,polResponseCode,lapTransactionState=DECLINED&message=DECLINED
+    //lapResponseCode,polTransactionState,polResponseCode,lapTransactionState=DECLINED&message=DECLINED
 
-      if(reference) setReferenceCode(reference)
-      console.log("parameteres==>",referenceCode,urlParams,props.cEvent.value._id)
-
-    },[])
-    return(
-         <Modal closable={false} visible={visible} footer={null}>
-           <Result 
-           title={
-            response && (response?.lapTransactionState=="DECLINED" || response?.lapTransactionState=="ERROR" || response?.lapTransactionState=="EXPIRED"  )?<div>Transacción declinada</div>:
-            response?.lapTransactionState=="APPROVED"?<div>Transacción Aceptada</div>:
-             response?.lapTransactionState=="PENDING"?<div>Transacción Pending</div>:<div></div>
-           }
-           extra={<Button onClick={()=>{setVisible(false);
-            window.location.href=`${window.location.origin}/landing/${props.cEvent.value._id}/evento`;
-          }}>Aceptar</Button>}
-           />          
-           </Modal>
-     
-
-    );
-}
+    if (reference) setReferenceCode(reference);
+    console.log('parameteres==>', referenceCode, urlParams, props.cEvent.value._id);
+  }, []);
+  return (
+    <Modal
+      bodyStyle={{ textAlign: 'center', borderTop: `10px solid ${response &&
+        (response?.lapTransactionState == 'DECLINED' ||
+          response?.lapTransactionState == 'ERROR' ||
+          response?.lapTransactionState == 'EXPIRED') ? (
+         '#FF3830'
+        ) : response?.lapTransactionState == 'APPROVED' ? (
+          '#52C41A'
+        ) : response?.lapTransactionState == 'PENDING' ? (
+          '#518BFB'
+        ) : (
+          ''
+        )}` }}
+      closable={false}
+      visible={visible}
+      footer={null}>
+      <Result
+        icon={
+          response &&
+          (response?.lapTransactionState == 'DECLINED' ||
+            response?.lapTransactionState == 'ERROR' ||
+            response?.lapTransactionState == 'EXPIRED') ? (
+            <CloseCircleFilled style={{color:'#FF3830'}} />
+          ) : response?.lapTransactionState == 'APPROVED' ? (
+            <CheckCircleFilled style={{color:'#52C41A'}} />
+          ) : response?.lapTransactionState == 'PENDING' ? (
+            <InfoCircleFilled style={{color:'#518BFB'}} />
+          ) : (
+            null
+          )
+        }
+        title={
+          response &&
+          (response?.lapTransactionState == 'DECLINED' ||
+            response?.lapTransactionState == 'ERROR' ||
+            response?.lapTransactionState == 'EXPIRED') ? (
+            <div>Transacción Fallida</div>
+          ) : response?.lapTransactionState == 'APPROVED' ? (
+            <div>Transacción Exitosa</div>
+          ) : response?.lapTransactionState == 'PENDING' ? (
+            <div>Transacción Pendiente</div>
+          ) : (
+            <div></div>
+          )
+        }
+        extra={[
+          <Button
+            key='action'
+            style={{ marginTop: '10px' }}
+            type='primary'
+            size='large'
+            onClick={() => {
+              setVisible(false);
+              window.location.href = `${window.location.origin}/landing/${props.cEvent.value._id}/evento`;
+            }}>
+            Aceptar
+          </Button>,
+        ]}
+      />
+    </Modal>
+  );
+};
 
 export default withContext(ResponsePayu);
