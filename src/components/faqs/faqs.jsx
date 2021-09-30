905ac77a0811f492dd5ba9eaa1f3d88a49c4115b
Maria Perdomo <alejandra.perdomo@mocionsoft.com>
2021-09-30 17:54:36 -0500

    Cambio de las vistas de 'Listas' con un componente unificado

--- src/components/faqs/faqs.jsx
+++ src/components/faqs/faqs.jsx
@@ -1,16 +1,7 @@
-import React, { useState, useEffect } from 'react';
 import { FaqsApi } from '../../helpers/request';
-import { handleRequestError } from '../../helpers/utils';
-import { Modal, message } from 'antd';
-import { ExclamationCircleOutlined } from '@ant-design/icons';
-import Header from '../../antdComponents/Header';
-import Table from '../../antdComponents/Table';
-
-const { confirm } = Modal;
+import CMS from '../newComponent/CMS';
 
 const Faqs = (props) => {
-  const [list, setList] = useState([]);
-  const [loading, setLoading] = useState(true);
   const columns = [
     {
       title: 'Título',
@@ -27,75 +18,22 @@ const Faqs = (props) => {
     }
   ];
 
-  useEffect(() => {
-    getFaqs();
-  }, [])
-
-  const getFaqs = async () => {
-    const data = await FaqsApi.byEvent(props.event._id);
-    setList(data);
-    setLoading(false);
-    console.log(data);
-  };
-
-  const remove = async (id) => {
-    const loading = message.open({
-      key: 'loading',
-      type: 'loading',
-      content: <> Por favor espere miestras borra la información..</>,
-    });
-    confirm({
-      title: `¿Está seguro de eliminar la información?`,
-      icon: <ExclamationCircleOutlined />,
-      content: 'Una vez eliminado, no lo podrá recuperar',
-      okText: 'Borrar',
-      okType: 'danger',
-      cancelText: 'Cancelar',
-      onOk() {
-        const onHandlerRemove = async () => {
-          try {
-            await FaqsApi.deleteOne(id, props.event._id);
-            message.destroy(loading.key);
-            message.open({
-              type: 'success',
-              content: <> Se eliminó la información correctamente!</>,
-            });
-            getFaqs();
-          } catch (e) {
-            message.destroy(loading.key);
-            message.open({
-              type: 'error',
-              content: handleRequestError(e).message,
-            });
-          }
-        }
-        onHandlerRemove();
-      }
-    });
-  };
-
   return (
-    <div>
-      <Header
-        title={'Preguntas Frecuentes'}
-        titleTooltip={'Agregue o edite las Preguntas Frecuentes que se muestran en la aplicación'}
-        addUrl={{
-          pathname: `${props.matchUrl}/faq`,
-          state: { new: true },
-        }}
-      />
-
-      <Table 
-        header={columns}
-        loading={loading}
-        list={list}
-        key='_id'
-        pagination={false}
-        actions
-        editPath={`${props.matchUrl}/faq`}
-        remove={remove}
-      />
-    </div>
+    <CMS 
+      API={FaqsApi}
+      eventId={props.event._id}
+      title={'Preguntas Frecuentes'}
+      titleTooltip={'Agregue o edite las Preguntas Frecuentes que se muestran en la aplicación'}
+      addUrl={{
+        pathname: `${props.matchUrl}/faq`,
+        state: { new: true },
+      }}
+      columns={columns}
+      key='_id'
+      editPath={`${props.matchUrl}/faq`}
+      pagination={false}
+      actions
+    />
   );
 }
 
--- src/components/news/news.jsx
+++ src/components/news/news.jsx
@@ -1,27 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { NewsFeed, Actions } from '../../helpers/request';
-import Loading from '../loaders/loading';
-import Moment from 'moment';
-import EventContent from '../events/shared/content';
-import EvenTable from '../events/shared/table';
-import TableAction from '../events/shared/tableAction';
-import { handleRequestError, sweetAlert } from '../../helpers/utils';
-import axios from 'axios/index';
-import ImageInput from '../shared/imageInput';
-import { toast } from 'react-toastify';
-import { FormattedMessage } from 'react-intl';
+import { NewsFeed } from '../../helpers/request';
 import { withRouter } from 'react-router';
-import { message, Modal } from 'antd';
-import { ExclamationCircleOutlined } from '@ant-design/icons';
-import Header from '../../antdComponents/Header';
-import Table from '../../antdComponents/Table';
 import moment from 'moment';
-
-const { confirm } = Modal;
+import CMS from '../newComponent/CMS';
 
 const News = ( props ) => {
-  const [list, setList] = useState([]);
-  const [loading, setLoading] = useState(true);
   const columns = [
     {
       title: 'Título',
@@ -39,76 +21,23 @@ const News = ( props ) => {
       }
     }
   ];
-
-  useEffect(() => {
-    getNews();
-  }, [])
-
-
-  const getNews = async () => {
-    const data = await NewsFeed.byEvent(props.event._id);    
-    setList(data);
-    setLoading(false);
-  }
-
-  const remove = async (id) => {
-    const loading = message.open({
-      key: 'loading',
-      type: 'loading',
-      content: <> Por favor espere miestras borra la información..</>,
-    });
-    confirm({
-      title: `¿Está seguro de eliminar la información?`,
-      icon: <ExclamationCircleOutlined />,
-      content: 'Una vez eliminado, no lo podrá recuperar',
-      okText: 'Borrar',
-      okType: 'danger',
-      cancelText: 'Cancelar',
-      onOk() {
-        const onHandlerRemove = async () => {
-          try {
-            await NewsFeed.deleteOne(id, props.event._id);
-            message.destroy(loading.key);
-            message.open({
-              type: 'success',
-              content: <> Se eliminó la información correctamente!</>,
-            });
-            getNews();
-          } catch (e) {
-            message.destroy(loading.key);
-            message.open({
-              type: 'error',
-              content: handleRequestError(e).message,
-            });
-          }
-        }
-        onHandlerRemove();
-      }
-    });
-  };
   
   return(
-    <div>
-      <Header
-        title={'Noticias'}
-        titleTooltip={'Agregue o edite las Noticias que se muestran en la aplicación'}
-        addUrl={{
-          pathname: `${props.match.url}/new`,
-          state: { new: true },
-        }}
-      />
-
-      <Table 
-        header={columns}
-        loading={loading}
-        list={list}
-        key='_id'
-        pagination={false}
-        actions
-        editPath={`${props.match.url}/new`}
-        remove={remove}
-      />
-    </div>
+    <CMS 
+      API={NewsFeed}
+      eventId={props.event._id}
+      title={'Noticias'}
+      titleTooltip={'Agregue o edite las Noticias que se muestran en la aplicación'}
+      addUrl={{
+        pathname: `${props.match.url}/new`,
+        state: { new: true },
+      }}
+      columns={columns}
+      key='_id'
+      editPath={`${props.match.url}/new`}
+      pagination={false}
+      actions
+    />
   )
 }
 
