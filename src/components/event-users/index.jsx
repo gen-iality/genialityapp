3a54e0d369e12fa094b987f84a7dab9e5d5a19ee
Jaime Daniel <jaimedaniel.bm91@gmail.com>
2021-10-05 18:02:23 -0500

    fix update attendess

--- src/components/event-users/index.jsx
+++ src/components/event-users/index.jsx
@@ -304,8 +304,8 @@ class ListEventUser extends Component {
              
               updatedAttendees[i][codearea[0].name]=updatedAttendees[i]['code']?"(+"+updatedAttendees[i]['code']+")"+updatedAttendees[i].user[codearea[0].name]:"(+0)"+updatedAttendees[i].user[codearea[0].name]
             }else{
-              console.log("KEY==>",updatedAttendees[i]['properties'][key])
-            updatedAttendees[i][key.name] =  updatedAttendees[i]['properties'][key.name]==true ?"SI":updatedAttendees[i]['properties'][key.name]==false?"NO":updatedAttendees[i]?.user[key.name];
+             // console.log("KEY==>",updatedAttendees[i]['properties'][key])
+            //updatedAttendees[i][key.name] =  updatedAttendees[i]['properties'][key.name]==true ?"SI":updatedAttendees[i]['properties'][key.name]==false?"NO":updatedAttendees[i]?.user[key.name];
             updatedAttendees[i]["textodeautorizacionparaimplementarenelmeetupfenalcoycolsubsidio"]= self.props.event._id=="60c8affc0b4f4b417d252b29" ? "SI" :""          
           }
           }
