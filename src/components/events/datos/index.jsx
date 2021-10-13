c44f14d74b20fbc268372fe5477c91015e7a3253
Edwin Ferney VIlla T <edwin.villa@mocionsoft.com>
2021-10-13 14:06:26 -0500

    ♻️ Refactor : se cambia el acceso al componente datos, se aloja en un componente padre llamado membersettings y se pasan callback por props para guardar, editar, eliminar y cambiar posiciones

--- src/components/events/datos/index.jsx
+++ src/components/events/datos/index.jsx
@@ -40,7 +40,7 @@ class Datos extends Component {
     this.html = document.querySelector('html');
     this.submitOrder = this.submitOrder.bind(this);
     this.handlevisibleModal = this.handlevisibleModal.bind(this);
-    this.organization = this.props?.org;
+    this.organization = this.props?.sendprops?.org;
   }
 
   async componentDidMount() {
@@ -61,7 +61,7 @@ class Datos extends Component {
       const organizationId = this?.organization?._id;
       let fields;
       if (organizationId) {
-        fields = await OrganizationApi.getUserProperties(organizationId);
+        fields = await this.props.getFields();
       } else {
         fields = await EventFieldsApi.getAll(this.eventID);
         fields = this.orderFieldsByWeight(fields);
@@ -92,8 +92,8 @@ class Datos extends Component {
       const organizationId = this?.organization?._id;
 
       if (organizationId) {
-        if (this.state.edit) await OrganizationApi.editOneUserProperties(organizationId, field._id, field);
-        else await OrganizationApi.createOneUserProperties(organizationId, field);
+        if (this.state.edit) await this.props.editField(field._id, field);
+        else await this.props.createNewField(field);
       } else {
         if (this.state.edit) await EventFieldsApi.editOne(field, field._id, this.eventID);
         else await EventFieldsApi.createOne(field, this.eventID);
@@ -130,8 +130,7 @@ class Datos extends Component {
   async submitOrder() {
     const organizationId = this?.organization?._id;
     if (organizationId) {
-      console.log('10. this.state.properties ', this.state.properties);
-      await OrganizationApi.editAllUserProperties(organizationId, this.state.properties);
+      await this.props.orderFields(this.state.properties);
     } else {
       await Actions.put(`api/events/${this.props.eventID}`, this.state.properties);
     }
@@ -153,7 +152,7 @@ class Datos extends Component {
     try {
       const organizationId = this?.organization?._id;
       if (organizationId) {
-        await OrganizationApi.deleteUserProperties(organizationId, this.state.deleteModal);
+        await this.props.deleteField(this.state.deleteModal);
         this.setState({ message: { ...this.state.message, class: 'msg_success', content: 'FIELD DELETED' } });
       } else {
         await EventFieldsApi.deleteOne(this.state.deleteModal, this.eventID);
@@ -417,9 +416,10 @@ class Datos extends Component {
             </TabPane>
           )} */}
 
-          <TabPane tab='Plantillas' key='3'>
-            {this.state.isEditTemplate.status ? (
+          <TabPane tab={this.props.type === 'configMembers' ? 'Configuración Miembros' : 'Plantillas'} key='3'>
+            {this.state.isEditTemplate.status || this.props.type === 'configMembers' ? (
               <Fragment>
+                {this.props.type !== 'configMembers' && 
                 <Button
                   danger
                   style={{ marginTop: '3%' }}
@@ -427,7 +427,7 @@ class Datos extends Component {
                     this.setState({ isEditTemplate: { ...this.state.isEditTemplate, status: false, datafields: [] } })
                   }>
                   Volver a plantillas
-                </Button>
+                </Button>}
 
                 <EventContent
                   title={'Recopilación de datos'}
@@ -438,7 +438,7 @@ class Datos extends Component {
                   addTitle={'Agregar dato'}>
                   <Table
                     columns={columns}
-                    dataSource={this.state.isEditTemplate.datafields}
+                    dataSource={this.props.type === 'configMembers' ? fields : this.state.isEditTemplate.datafields }
                     pagination={false}
                     rowKey='index'
                     components={{
