1b18145f2c5c62c09831501ceff6f3c98c27355d
Edwin Ferney VIlla T <edwin.villa@mocionsoft.com>
2021-10-22 08:20:05 -0500

    ⚒️ FIX : Se corrige nombre de ruta ./documents ya que estaba don d mayuscula

--- src/components/documents/index.jsx
+++ src/components/documents/index.jsx
@@ -1,22 +1,30 @@
-import React, {Fragment} from 'react';
-import {Route, Switch, withRouter} from "react-router-dom";
-import Documents from "./Documents";
-import Document from "./Document";
-import DocumentsEdit from "./edit";
-import DocumentsPermission from "./filePermission"
+import React, { Fragment } from 'react';
+import { Route, Switch, withRouter } from 'react-router-dom';
+import Documents from './documents';
+import Document from './Document';
+import DocumentsEdit from './edit';
+import DocumentsPermission from './filePermission';
 
-function DocumentsRoutes({...props}){
-    const {event, match} = props;
-    return (
-        <Fragment>
-            <Switch>
-                <Route exact path={`${match.url}/`} render={()=><Documents event={event} matchUrl={match.url}/>}/>
-                <Route exact path={`${match.url}/document`} render={()=><Document event={event} matchUrl={match.url} {...props}/>}/>
-                <Route exact path={`${match.url}/upload`} render={()=><DocumentsEdit event={event} matchUrl={match.url}/>}/>
-                <Route exact path={`${match.url}/permission`} render={()=><DocumentsPermission event={event} matchUrl={match.url}/>}/>
-            </Switch>
-        </Fragment>
-    );
+function DocumentsRoutes({ ...props }) {
+  const { event, match } = props;
+  return (
+    <Fragment>
+      <Switch>
+        <Route exact path={`${match.url}/`} render={() => <Documents event={event} matchUrl={match.url} />} />
+        <Route
+          exact
+          path={`${match.url}/document`}
+          render={() => <Document event={event} matchUrl={match.url} {...props} />}
+        />
+        <Route exact path={`${match.url}/upload`} render={() => <DocumentsEdit event={event} matchUrl={match.url} />} />
+        <Route
+          exact
+          path={`${match.url}/permission`}
+          render={() => <DocumentsPermission event={event} matchUrl={match.url} />}
+        />
+      </Switch>
+    </Fragment>
+  );
 }
 
 export default withRouter(DocumentsRoutes);
