1b7102e98f317a263bf568f6ffce3a2c600817a8
Edwin Ferney Villa T <edwin.villa@mocionsoft.com>
2021-10-27 14:09:08 -0500

    ➕ ADD : Se agrega tabla al controlador de la actividad, ubicado en espacio virtual, dicha tabla permite activar mas juegos

--- src/components/agenda/roomManager/controller.js
+++ src/components/agenda/roomManager/controller.js
@@ -1,8 +1,103 @@
-import React from 'react';
-import { Card, Row, Col, Switch } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Card, Row, Col, Switch, Popover, Avatar, Empty, Image } from 'antd';
+import { InfoCircleOutlined } from '@ant-design/icons';
+import Header from '../../../antdComponents/Header';
+import Table from '../../../antdComponents/Table';
+import { getColumnSearchProps } from '../../speakers/getColumnSearch';
+import { firestore } from '../../../helpers/firebase';
 
 export default function RoomController(props) {
   const { handleRoomState, handleTabsController, roomStatus, surveys, games } = props;
+  const [listOfGames, setListOfGames] = useState([]);
+  let [columnsData, setColumnsData] = useState({});
+  const [showavailableGames, setShowavailableGames] = useState(games);
+  const [isLoading, setIsLoading] = useState(true);
+
+  async function getGamesData() {
+    let gamesData = [];
+    const docRef = firestore.collection('gamesAvailable');
+    await docRef.get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        gamesData.push({ ...doc.data(), id: doc.id });
+      });
+    });
+
+    setListOfGames(gamesData);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    getGamesData();
+  }, [games]);
+
+  const columns = [
+    {
+      title: 'Logo',
+      render: (item) => (
+        <Row gutter={8}>
+          <Col>
+            <Popover
+              placement='top'
+              content={() => (
+                <>
+                  {item.picture ? (
+                    <Image key={'img' + item._id} width={200} height={200} src={item.picture} />
+                  ) : (
+                    <Empty description='Imagen no encontrada' />
+                  )}
+                </>
+              )}>
+              {item.picture ? (
+                <Avatar key={'img' + item._id} src={item.picture} />
+              ) : (
+                <Avatar icon={<InfoCircleOutlined />} />
+              )}
+            </Popover>
+          </Col>
+        </Row>
+      ),
+    },
+    {
+      title: 'Nombre',
+      dataIndex: 'name',
+      ...getColumnSearchProps('name', columnsData),
+    },
+    {
+      title: 'Url',
+      dataIndex: 'baseUrl',
+      ...getColumnSearchProps('email', columnsData),
+    },
+    {
+      title: 'Visible',
+      dataIndex: 'showGame',
+      render(val, item) {
+        const [publish, setPublish] = useState(item.showGame);
+        const update = async (checked) => {
+          const docRef = firestore.collection('gamesAvailable').doc(item.id);
+          docRef
+            .update({
+              showGame: checked,
+            })
+            .then(() => {
+              setPublish(checked);
+            })
+            .catch((error) => {
+              console.error('Error updating document: ', error);
+            });
+        };
+        return (
+          <Switch
+            checkedChildren='Sí'
+            unCheckedChildren='No'
+            onChange={update}
+            checked={publish}
+            id={`editSwitch${item.index}`}
+          />
+        );
+      },
+    },
+  ];
+
   return (
     <>
       <Card>
@@ -41,9 +136,35 @@ export default function RoomController(props) {
             Habilitar Juegos
           </Col>
           <Col xs={4} lg={2}>
-            <Switch checked={games} onChange={(checked) => handleTabsController(checked, 'games')} />
+            <Switch
+              checked={games}
+              onChange={(checked) => {
+                handleTabsController(checked, 'games'), setShowavailableGames(!showavailableGames);
+              }}
+            />
           </Col>
         </Row>
+        {showavailableGames && (
+          <>
+            {' '}
+            <Header
+              title={'Juegos disponibles'}
+              titleTooltip={'Seleccione los juego que desea mostrar en la zona social'}
+              description='Seleccione los juego que desea mostrar en la zona social'
+            />
+            <Table
+              key='index'
+              list={listOfGames}
+              header={columns}
+              setColumnsData={setColumnsData}
+              loading={isLoading}
+              search
+              // extraFn={extraField}
+              // extraFnIcon={<SendOutlined />}
+              // extraFnTitle='Enviar notificación a este usuario'
+            />
+          </>
+        )}
       </Card>
     </>
   );
