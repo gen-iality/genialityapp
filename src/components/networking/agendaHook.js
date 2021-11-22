697a416a1e4ab48813fb578978abc1518c339fb6
Mario Montero <marioupc3@gmail.com>
2021-11-22 09:23:05 -0500

    change for valid date mensaje bienvenida

--- src/components/networking/agendaHook.js
+++ src/components/networking/agendaHook.js
@@ -1,4 +1,17 @@
 import { firestoreeviuschat, app } from '../../helpers/firebase';
+var hoy = new Date();
+
+function formatAMPM(hoy) {
+  var hours = hoy.getHours();
+  var minutes = hoy.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  return strTime;
+}
+
 export const createChatRoom = (idroom) => {
   console.log('creando este chat para ver de one');
   let refchatroom = firestoreeviuschat.collection('messages' + idroom).doc('initial');
@@ -14,7 +27,7 @@ export const createChatRoom = (idroom) => {
             profilePicUrl:
               'https://storage.googleapis.com/eviusauth.appspot.com/evius/events/Re391nNxdz7DhzJLuRekOW67j9ejpDYLsJ6xOE8U.jpg',
             text: 'Bienvenidos al chat de Evius.co',
-            timestamp: app.firestore.FieldValue.serverTimestamp(),
+            timestamp: formatAMPM(new Date()),
           })
           .then(function(messageRef) {})
           .then(() => console.log('chat  iniciado'))
@@ -37,7 +50,7 @@ const addInitalMessage = (colection) => {
       name: 'Evius',
       text: 'Bienvenidos al chat de Evius',
       profilePicUrl: 'https://cutt.ly/sRxhivy',
-      timestamp: app.firestore.FieldValue.serverTimestamp(),
+      timestamp: formatAMPM(new Date()),
     })
     .then(function(messageRef) {})
     .then(() => {})
