d16ef32400f6218e805974f774e99ddf6dffef43
Mario Montero <marioupc3@gmail.com>
2021-11-23 13:09:57 -0500

    without register for event universo de creadores

--- src/components/authentication/ModalAuth.jsx
+++ src/components/authentication/ModalAuth.jsx
@@ -274,7 +274,7 @@ const ModalAuth = (props) => {
           </TabPane>
           {props.cEventUser?.value == null &&
             props.organization !== 'landing' &&
-           (
+            props.cEvent.value?._id != '61816f5a039c0f2db65384a2' && (
               <TabPane tab={intl.formatMessage({ id: 'modal.title.register', defaultMessage: 'Registrarme' })} key='2'>
                 <div
                   // className='asistente-list'
