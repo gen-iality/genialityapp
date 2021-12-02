1478a60cb757ebf09ded6e1ae1936216a6339e6f
Maria Perdomo <alejandra.perdomo@mocionsoft.com>
2021-12-01 21:41:33 -0500

    pequeño detalle en preguntas frecuentes

--- src/components/faqsLanding/index.jsx
+++ src/components/faqsLanding/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { FaqsApi } from '../../helpers/request';
-import { Collapse, Col } from 'antd';
+import { Collapse, Col, Row } from 'antd';
 import withContext from '../../Context/withContext'
 const { Panel } = Collapse;
 
@@ -41,23 +41,25 @@ class Faqs extends Component {
   render() {
     const { faqsData, styles } = this.state;
     return (
-      <Col xs={22} sm={22} md={18} lg={18} xl={18} style={center}>
-        <Collapse
-          className='collapse_question'
-          style={{
-            backgroundColor:
-              this.props.cEvent.value.styles.toolbarDefaultBg !== '#ffffff'
-                ? this.props.cEvent.value.styles.toolbarDefaultBg
-                : '#7d8485d4',
-          }}
-          defaultActiveKey={['3']}>
-          {faqsData.map((faqs, key) => (
-            <Panel key={key} header={<span style={{ color: this.props.cEvent.value.styles.textMenu }}>{faqs.title}</span>}>
-              <div dangerouslySetInnerHTML={{ __html: faqs.content }} />
-            </Panel>
-          ))}
-        </Collapse>
-      </Col>
+      <Row gutter={[8, 8]} wrap justify='center'>
+        <Col xs={22} sm={22} md={18} lg={18} xl={18}>
+          <Collapse
+            className='collapse_question'
+            /* style={{
+              backgroundColor:
+                this.props.cEvent.value.styles.toolbarDefaultBg !== '#ffffff'
+                  ? this.props.cEvent.value.styles.toolbarDefaultBg
+                  : '#7d8485d4',
+            }} */
+            defaultActiveKey={['3']}>
+            {faqsData.map((faqs, key) => (
+              <Panel key={key} header={<span style={{ color: this.props.cEvent.value.styles.textMenu }}>{faqs.title}</span>}>
+                <div dangerouslySetInnerHTML={{ __html: faqs.content }} />
+              </Panel>
+            ))}
+          </Collapse>
+        </Col>
+      </Row>
     );
   }
 }
