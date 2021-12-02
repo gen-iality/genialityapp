e26d8aaa4b38529086bc1c1586ef24bdf0646422
Maria Perdomo <alejandra.perdomo@mocionsoft.com>
2021-12-02 09:30:07 -0500

    Tenia un error en el log del zoomIframe y se quito el botón de volver y el título de la encuesta como tal

--- src/components/events/surveys/graphics.jsx
+++ src/components/events/surveys/graphics.jsx
@@ -298,7 +298,7 @@ class Graphics extends Component {
       return (
         <>
           <Card bodyStyle={{ padding: '0' }} className='survyCard'>
-            <strong style={{ fontSize: '18px' }}>{cSurveys.currentSurvey.name}</strong>
+            {/* <strong style={{ fontSize: '18px' }}>{cSurveys.currentSurvey.name}</strong>
             <div style={{ marginTop: 5 }}>
               {this.props.currentActivity === null && (
                 <Button
@@ -311,8 +311,8 @@ class Graphics extends Component {
                   <ArrowLeftOutlined /> Volver a {surveyLabel ? surveyLabel.name : 'encuestas'}
                 </Button>
               )}
-            </div>
-            <strong style={{ fontSize: '16px' }}>{titleQuestion}</strong>
+            </div> */}
+            <strong style={{ fontSize: '16px'}}>{titleQuestion}</strong>
             {/* esta validacion es para que tomo los estilos la torta */}
             {/*
              */}
