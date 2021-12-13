daf938ba39dcd3c43f8bdac39df4002d48f83149
Edwin Ferney Villa T <edwin.villa@mocionsoft.com>
2021-12-13 18:03:45 -0500

    ⚒️ FIX : Se corrige novedad en la seccion myProfile, no estaba llegando la data de los endpoint, ya que faltaba el token, se mejora la logica del mismo componente aislando cada peticion con su loader independiente, se elimina llamado a cookie inexistente

--- src/components/profile/main.jsx
+++ src/components/profile/main.jsx
@@ -24,20 +24,22 @@ const MainProfile = (props) => {
   const [eventsLimited, seteventsLimited] = useState([]);
   const [ticketsLimited, setticketsLimited] = useState([]);
   const [organizationsLimited, setorganizationsLimited] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [organizationsIsLoading, setOrganizationsIsLoading] = useState(true);
+  const [eventsIHaveCreatedIsLoading, setEventsIHaveCreatedIsLoading] = useState(true);
+  const [eventsThatIHaveParticipatedIsLoading, setEventsThatIHaveParticipatedIsLoading] = useState(true);
   const screens = useBreakpoint();
   const selectedTab = props.match.params.tab;
 
-  const fetchItem = async () => {
-    /* Eventos creados por el usuario    */
+  const eventsIHaveCreated = async () => {
     const events = await EventsApi.mine();
     const eventsDataSorted = events.sort((a, b) => moment(b.datetime_from) - moment(a.datetime_from));
     setevents(eventsDataSorted);
     seteventsLimited(events.slice(0, 3));
-    /* ----------------------------------*/
-    /* Eventos en los que esta registrado el usuario */
-    const token = Cookie.get('evius_token');
-    const ticketsall = await TicketsApi.getAll(token);
+    setEventsIHaveCreatedIsLoading(false);
+  };
+
+  const eventsThatIHaveParticipated = async () => {
+    const ticketsall = await TicketsApi.getAll();
     const ticketsDataSorted = ticketsall.sort((a, b) => moment(b.created_at) - moment(a.created_at));
     const usersInscription = [];
     ticketsDataSorted.forEach(async (element) => {
@@ -47,16 +49,28 @@ const MainProfile = (props) => {
       }
       settickets(usersInscription);
       setticketsLimited(usersInscription.slice(0, 4));
+      setEventsThatIHaveParticipatedIsLoading(false);
     });
+  };
 
-    /* ----------------------------------*/
-    /* Organizaciones del usuario */
+  const myOrganizations = async () => {
     const organizations = await OrganizationApi.mine();
     const organizationDataSorted = organizations.sort((a, b) => moment(b.created_at) - moment(a.created_at));
     setorganizations(organizationDataSorted);
     setorganizationsLimited(organizationDataSorted.slice(0, 5));
+    setOrganizationsIsLoading(false);
+  };
+
+  const fetchItem = async () => {
+    /* Eventos creados por el usuario    */
+    eventsIHaveCreated();
     /* ----------------------------------*/
-    setIsLoading(false);
+    /* Eventos en los que esta registrado el usuario */
+
+    eventsThatIHaveParticipated();
+    /* ----------------------------------*/
+    /* Organizaciones del usuario */
+    myOrganizations();
   };
 
   useEffect(() => {
@@ -90,9 +104,7 @@ const MainProfile = (props) => {
             size={5}
             direction='vertical'
             style={{ textAlign: 'center', paddingLeft: '15px', paddingRight: '15px' }}>
-            {isLoading ? (
-              <Skeleton.Avatar active={true} size={150} shape='circle' />
-            ) : (
+            {props?.cUser?.value ? (
               <>
                 {props?.cUser?.value?.picture ? (
                   <Avatar size={150} src={props?.cUser?.value?.picture} />
@@ -100,6 +112,8 @@ const MainProfile = (props) => {
                   <Avatar style={{ backgroundColor: '#50D3C9' }} size={150} icon={<UserOutlined />} />
                 )}
               </>
+            ) : (
+              <Skeleton.Avatar active={true} size={150} shape='circle' />
             )}
             <Typography.Text style={{ fontSize: '20px', width: '250px' }}>
               {props?.cUser?.value?.names || props?.cUser?.value?.displayName || props?.cUser?.value?.name}
@@ -148,95 +162,109 @@ const MainProfile = (props) => {
                   </Space>
                 }
                 key='1'>
-                {isLoading ? (
-                  <Loading />
-                ) : (
-                  <Row gutter={[16, 16]}>
-                    <Col span={24}>
-                      <Divider orientation='left'>Eventos creado</Divider>
-                      <Row gutter={[16, 16]}>
-                        <Col key={'index'} xs={24} sm={12} md={12} lg={8} xl={6}>
-                          {organizationsLimited.length > 0 ? (
-                            <NewCard entityType='event' cUser={props.cUser} org={organizationsLimited} />
-                          ) : (
-                            <NewCard entityType='event' cUser={props.cUser} />
-                          )}
-                        </Col>
-                        {/* aqui empieza el mapeo de eventCard.jsx maximo 4 */}
-                        {eventsLimited.length > 0 &&
-                          eventsLimited.map((event) => {
-                            return (
-                              <Col key={event._id} xs={24} sm={12} md={12} lg={8} xl={6}>
-                                <EventCard
-                                  isAdmin
-                                  bordered={false}
-                                  key={event._id}
-                                  event={event}
-                                  action={{ name: 'Ver', url: `landing/${event._id}` }}
-                                  right={[
-                                    <div key={'event-' + event._id}>
-                                      <Link to={`/eventadmin/${event._id}`}>
-                                        <Space>
-                                          <SettingOutlined />
-                                          <span>Administrar</span>
-                                        </Space>
-                                      </Link>
-                                    </div>,
-                                  ]}
-                                />
-                              </Col>
-                            );
-                          })}
-                        {/* aqui termina el mapeo de eventCard.jsx maximo 4  */}
-                      </Row>
-                    </Col>
-                    <Col span={24}>
-                      <Divider orientation='left'>Eventos en los que estoy registrado</Divider>
-                      <Row gutter={[16, 16]}>
-                        {ticketsLimited.length > 0 ? (
-                          ticketsLimited.map((event) => {
-                            return (
-                              <Col key={event._id} xs={24} sm={12} md={12} lg={8} xl={6}>
-                                <EventCard
-                                  bordered={false}
-                                  key={event._id}
-                                  event={event}
-                                  action={{ name: 'Ver', url: `landing/${event._id}` }}
-                                />
-                              </Col>
-                            );
-                          })
-                        ) : (
-                          <Col span={24}>
-                            <ExploreEvents />
+                <Row gutter={[16, 16]}>
+                  <Col span={24}>
+                    <Divider orientation='left'>Eventos creado</Divider>
+                    <Row gutter={[16, 16]}>
+                      {eventsIHaveCreatedIsLoading ? (
+                        <Loading />
+                      ) : (
+                        <>
+                          <Col xs={24} sm={12} md={12} lg={8} xl={6}>
+                            {organizationsLimited.length > 0 ? (
+                              <NewCard entityType='event' cUser={props.cUser} org={organizationsLimited} />
+                            ) : (
+                              <NewCard entityType='event' cUser={props.cUser} />
+                            )}
                           </Col>
-                        )}
-                      </Row>
-                    </Col>
-                    <Col span={24}>
-                      <Divider orientation='left'>Organizaciones</Divider>
-                      <Row gutter={[16, 16]}>
-                        <Col key={'index1'} xs={12} sm={8} md={8} lg={6} xl={4} xxl={4}>
-                          <NewCard entityType='organization' cUser={props.cUser} fetchItem={fetchItem} />
-                        </Col>
-                        {/* aqui empieza el mapeo maximo 6 */}
-                        {organizationsLimited.length > 0 &&
-                          organizationsLimited.map((organization) => {
-                            return (
-                              <Col key={'index'} xs={12} sm={8} md={8} lg={6} xl={4} xxl={4}>
-                                <OrganizationCard data={organization} />
-                              </Col>
-                            );
-                          })}
-                        {/* aqui termina el mapeo maximo 6 */}
-                      </Row>
-                    </Col>
-                  </Row>
-                )}
+                          {/* aqui empieza el mapeo de eventCard.jsx maximo 4 */}
+                          {eventsLimited.length > 0 &&
+                            eventsLimited.map((event, index) => {
+                              return (
+                                <Col key={index} xs={24} sm={12} md={12} lg={8} xl={6}>
+                                  <EventCard
+                                    isAdmin
+                                    bordered={false}
+                                    event={event}
+                                    action={{ name: 'Ver', url: `landing/${event._id}` }}
+                                    right={[
+                                      <div>
+                                        <Link to={`/eventadmin/${event._id}`}>
+                                          <Space>
+                                            <SettingOutlined />
+                                            <span>Administrar</span>
+                                          </Space>
+                                        </Link>
+                                      </div>,
+                                    ]}
+                                  />
+                                </Col>
+                              );
+                            })}
+                          {/* aqui termina el mapeo de eventCard.jsx maximo 4  */}
+                        </>
+                      )}
+                    </Row>
+                  </Col>
+
+                  <Col span={24}>
+                    <Divider orientation='left'>Eventos en los que estoy registrado</Divider>
+                    <Row gutter={[16, 16]}>
+                      {eventsThatIHaveParticipatedIsLoading ? (
+                        <Loading />
+                      ) : (
+                        <>
+                          {ticketsLimited.length > 0 ? (
+                            ticketsLimited.map((event, index) => {
+                              return (
+                                <Col key={index} xs={24} sm={12} md={12} lg={8} xl={6}>
+                                  <EventCard
+                                    bordered={false}
+                                    event={event}
+                                    action={{ name: 'Ver', url: `landing/${event._id}` }}
+                                  />
+                                </Col>
+                              );
+                            })
+                          ) : (
+                            <Col span={24}>
+                              <ExploreEvents />
+                            </Col>
+                          )}
+                        </>
+                      )}
+                    </Row>
+                  </Col>
+
+                  <Col span={24}>
+                    <Divider orientation='left'>Organizaciones</Divider>
+                    <Row gutter={[16, 16]}>
+                      {organizationsIsLoading ? (
+                        <Loading />
+                      ) : (
+                        <>
+                          <Col xs={12} sm={8} md={8} lg={6} xl={4} xxl={4}>
+                            <NewCard entityType='organization' cUser={props.cUser} fetchItem={fetchItem} />
+                          </Col>
+                          {/* aqui empieza el mapeo maximo 6 */}
+                          {organizationsLimited.length > 0 &&
+                            organizationsLimited.map((organization, index) => {
+                              return (
+                                <Col key={index} xs={12} sm={8} md={8} lg={6} xl={4} xxl={4}>
+                                  <OrganizationCard data={organization} />
+                                </Col>
+                              );
+                            })}
+                          {/* aqui termina el mapeo maximo 6 */}
+                        </>
+                      )}
+                    </Row>
+                  </Col>
+                </Row>
               </TabPane>
             )}
             <TabPane tab='Organizaciones' key='2'>
-              {isLoading ? (
+              {organizationsIsLoading ? (
                 <Loading />
               ) : (
                 <Row gutter={[16, 16]}>
@@ -244,9 +272,9 @@ const MainProfile = (props) => {
                     <NewCard entityType='organization' cUser={props.cUser} fetchItem={fetchItem} />
                   </Col>
                   {organizations.length > 0 &&
-                    organizations.map((organization) => {
+                    organizations.map((organization, index) => {
                       return (
-                        <Col key={'index'} xs={12} sm={8} md={8} lg={6} xl={4} xxl={4}>
+                        <Col key={index} xs={12} sm={8} md={8} lg={6} xl={4} xxl={4}>
                           <OrganizationCard data={organization} />
                         </Col>
                       );
@@ -255,7 +283,7 @@ const MainProfile = (props) => {
               )}
             </TabPane>
             <TabPane tab='Eventos creados' key='3'>
-              {isLoading ? (
+              {eventsIHaveCreatedIsLoading ? (
                 <Loading />
               ) : (
                 <Row gutter={[16, 16]}>
@@ -266,17 +294,16 @@ const MainProfile = (props) => {
                       <NewCard entityType='event' cUser={props.cUser} />
                     )}
                   </Col>
-                  {events.map((event) => {
+                  {events.map((event, index) => {
                     return (
-                      <Col key={event._id} xs={24} sm={12} md={12} lg={8} xl={6}>
+                      <Col key={index} xs={24} sm={12} md={12} lg={8} xl={6}>
                         <EventCard
                           isAdmin
                           bordered={false}
-                          key={event._id}
                           event={event}
-                          action={{ name: 'Ver', url: `landing/${event._id}` }}
+                          // action={{ name: 'Ver', url: `landing/${event._id}` }}
                           right={[
-                            <div key={'event-' + event._id}>
+                            <div>
                               <Link to={`/eventadmin/${event._id}`}>
                                 <Space>
                                   <SettingOutlined />
@@ -293,16 +320,15 @@ const MainProfile = (props) => {
               )}
             </TabPane>
             <TabPane tab='Registros a eventos' key='4'>
-              {isLoading ? (
+              {eventsThatIHaveParticipatedIsLoading ? (
                 <Loading />
               ) : (
                 <Row gutter={[16, 16]}>
-                  {tickets.map((event) => {
+                  {tickets.map((event, index) => {
                     return (
-                      <Col key={event._id} xs={24} sm={12} md={12} lg={8} xl={6}>
+                      <Col key={index} xs={24} sm={12} md={12} lg={8} xl={6}>
                         <EventCard
                           bordered={false}
-                          key={event._id}
                           event={event}
                           action={{ name: 'Ver', url: `landing/${event._id}` }}
                         />
