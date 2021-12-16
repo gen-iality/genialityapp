c50ac515ed84d83478c2ca4607b385ea321f51e2
Marlon Carrillo <marlon.carrillo@mocionsoft.com>
2021-12-16 12:35:59 -0500

    se creo la vista intermedia para el acceso con link desde el email

---
+++ src/components/AdminUsers/ResultLink.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { Spin, Result, Button, Typography, Grid } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
+
+const { useBreakpoint } = Grid;
+
+const ResultLink = ({ status, data }) => {
+  const screens = useBreakpoint();
+  // statust -> loading || error
+  status = status ? status : 'loading';
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        width: '100vw',
+        backgroundColor: '#ECF2F7',
+      }}>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#FFFFFF',
+          width: `${screens.xs ? '90%' : '60%'}`,
+          height: `${screens.xs ? '80%' : '70%'}`,
+          borderRadius: '25px',
+        }}>
+        <Result
+          icon={status === 'loading' && <LoadingOutlined />}
+          status={status === 'loading' ? null : 'error'}
+          title={
+            <Typography.Title level={1}>
+              {status === 'loading' ? 'Iniciando la sesión...' : 'Acceso denegado'}
+            </Typography.Title>
+          }
+          subTitle={
+            status === 'loading' ? null : (
+              <Typography.Paragraph
+                type='secondary'
+                style={{
+                  fontSize: `${screens.xs ? '14px' : '18px'}`,
+                  maxWidth: '550px',
+                  overflowWrap: 'anywhere',
+                }}>
+                El enlace enviado a {data} ya fue usado, ingrese al evento para solicitar uno nuevo.
+              </Typography.Paragraph>
+            )
+          }
+          extra={
+            status === 'loading'
+              ? null
+              : [
+                  <Button size='large' type='primary' key='goToEvent'>
+                    Ir al evento
+                  </Button>,
+                  <Button size='large' type='text' key='goToEvius'>
+                    Ir a Evius
+                  </Button>,
+                ]
+          }
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ResultLink;
