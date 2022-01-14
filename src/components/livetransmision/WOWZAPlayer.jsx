be25df5765ef018548c9141bae8b5565ec88bbe2
Edwin Ferney Villa T <edwin.villa@mocionsoft.com>
2022-01-14 16:27:24 -0500

    ♻️⚒️ REFACTOR, FIX : Se cambia el link del video por defecto del componente wowzaplayer, se agrega loader para cuando el estado del streaming esta en starting, se corrige error 404 al emitir por primera vez desde eviusMeet hacia wowza

--- src/components/livetransmision/WOWZAPlayer.jsx
+++ src/components/livetransmision/WOWZAPlayer.jsx
@@ -10,63 +10,70 @@ function WOWZAPlayer({ meeting_id, thereIsConnection }) {
   const [muted, setMuted] = useState(false);
   const [loopBackGround, setLoopBackGround] = useState(false);
   const defaultVideo =
-    'https://firebasestorage.googleapis.com/v0/b/eviusauth.appspot.com/o/evius%2FLoading.mp4?alt=media&token=883ec61c-157a-408b-876c-b09f70402d14';
+    'https://firebasestorage.googleapis.com/v0/b/eviusauth.appspot.com/o/evius%2FCortinillaProntoIniciamos.mp4?alt=media&token=b31c0745-4542-4b55-b79c-357db7633c53';
 
   useEffect(() => {
-    setPlatformurl(null);
     if (!meeting_id) return;
     if (thereIsConnection === 'No') {
-      setLoopBackGround(true), setPlatformurl(defaultVideo);
-    } else if (thereIsConnection === 'Yes') {
       setLoopBackGround(false);
+      setPlatformurl(defaultVideo);
+      setMuted(true);
+    } else if (thereIsConnection === 'Yes') {
       let asyncfunction = async () => {
+        setLoopBackGround(true);
+        setPlatformurl('none');
         let live_stream = await getLiveStream(meeting_id);
         let url = live_stream.player_hls_playback_url;
-        setPlatformurl(url);
-        setMuted(true);
+        /** se hace uso de un TimeOut para dar tiempo a wowza de inicializar la playList para que no devuelva error 404 la primera vez que el origen 'eviusMeets' envie data */
+        setTimeout(() => {
+          setPlatformurl(url);
+          setMuted(true);
+        }, 2000);
       };
       asyncfunction();
     }
     return () => {
       setLoopBackGround(false);
       setPlatformurl(null);
+      setMuted(false);
     };
   }, [meeting_id, thereIsConnection]);
 
   return (
     <>
       <div className='mediaplayer'>
-        {muted && thereIsConnection !== 'No' && 
-          <Button 
+        {muted && thereIsConnection !== 'No' && (
+          <Button
             onClick={() => setMuted(false)}
-            shape="circle"
+            shape='circle'
             style={{
               /* fontSize: '25px',  */
-              position:'absolute', 
+              position: 'absolute',
               top: '4%',
               /* color: 'white', */
               left: '3%',
-              zIndex: '500'
+              zIndex: '500',
             }}
             icon={<AudioMutedOutlined />}
           />
-        }
+        )}
         <ReactPlayer
           muted={muted}
           playing={true}
-          loop={loopBackGround}
+          loop={!loopBackGround}
           /* style={{ height: '100% !important', objectFit: 'cover' }} */
           height='100%'
           width='100%'
           url={platformurl}
-          controls={!loopBackGround}
-          config={{
-            file: {
-              forceHLS: loopBackGround,
-            },
-          }}
+          controls={loopBackGround}
+          // config={{
+          //   file: {
+          //     forceHLS: loopBackGround,
+          //   },
+          // }}
         />
       </div>
+
       {/* <div style={{ position: 'relative', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         {muted && thereIsConnection !== 'No' && <IconMute callback={() => setMuted(false)} />}
         <ReactPlayer
