8b69eacaca35cd75309336d720ec4dbb4382ef66
Maria Perdomo <alejandra.perdomo@mocionsoft.com>
2022-02-02 07:20:21 -0500

    fix del tamaño de select en transmisión

--- src/components/agenda/roomManager/components/conferenceConfig.jsx
+++ src/components/agenda/roomManager/components/conferenceConfig.jsx
@@ -59,7 +59,9 @@ export default function ConferenceConfig({
                 value={roomStatus}
                 onChange={(value) => {
                   setRoomStatus(value);
-                }}>
+                }}
+                style={{width: '180px'}}
+              >
                 <Option value=''>Actividad creada</Option>
                 <Option value='closed_meeting_room'>Iniciará pronto</Option>
                 <Option value='open_meeting_room'>En vivo</Option>
