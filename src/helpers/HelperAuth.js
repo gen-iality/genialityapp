a64d3ddd19aa73a51295711f24d5074b549fa7ec
Mario Montero <marioupc3@gmail.com>
2022-02-21 12:01:39 -0500

    checkin users firebase

--- src/helpers/HelperAuth.js
+++ src/helpers/HelperAuth.js
@@ -1,15 +1,30 @@
-import { app } from './firebase';
+import { useEffect } from 'react';
+import { app, firestore } from './firebase';
 
 export async function GetTokenUserFirebase() {
   return new Promise((resolve, reject) => {
     app.auth().onAuthStateChanged((user) => {
       if (user) {
-        user.getIdToken().then(async function(idToken) {
+        user.getIdToken().then(async function (idToken) {
           resolve(idToken);
         });
       } else {
         reject('unauthenticated user');
       }
     });
+  });
+}
+
+
+export const useCheckinUser = (AttendeId, eventId) => {
+  console.log("cuantos usuarios estan en el evento");
+  const userRef = firestore.collection(`${eventId}_event_attendees`).doc(AttendeId);
+  userRef.onSnapshot(function (doc) {
+    if (doc.exists) {
+      userRef.set({
+        checkin: true,
+        checkedin_at: new Date(),
+      }, { merge: true });
+    }
   });
 }
