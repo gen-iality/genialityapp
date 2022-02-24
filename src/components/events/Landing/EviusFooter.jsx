b0a0ac4ff84245db80fd59cba65f602527273a0c
Maria Perdomo <alejandra.perdomo@mocionsoft.com>
2022-02-24 10:34:24 -0500

    100% footer en landing

--- src/components/events/Landing/EviusFooter.jsx
+++ src/components/events/Landing/EviusFooter.jsx
@@ -8,7 +8,7 @@ const EviusFooter = () => {
     <>
       {cEventContext.value.styles && cEventContext.value.styles.banner_footer && (
         <div onClick={()=>cEventContext.value._id=='60cb7c70a9e4de51ac7945a2'? history.push(`/landing/${cEventContext.value._id}/tickets`):null} style={{ textAlign: 'center' }}>
-          <img alt='image-dialog' src={cEventContext.value.styles.banner_footer} />
+          <img alt='image-dialog' src={cEventContext.value.styles.banner_footer} style={{maxWidth: '100%'}}/>
         </div>
       )}
     </>
