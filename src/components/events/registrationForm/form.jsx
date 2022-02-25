867d3ecd75ac5dcef55890d33e3a045d5af57d1e
Jaime Daniel <jaimedaniel.bm91@gmail.com>
2022-02-25 18:04:30 -0500

    ajustes flujo login

--- src/components/events/registrationForm/form.jsx
+++ src/components/events/registrationForm/form.jsx
@@ -538,6 +538,9 @@ const FormRegister = ({
           : initialValues
           ? initialValues[target]
           : '';
+        //VISIBILIDAD DE CAMPOS
+        let visible = name == 'email' || name == 'names' ? true : false;
+
         /* console.log(initialValues, 'initialValues', m) */
 
         //no entiendo b esto para que funciona
@@ -552,16 +555,6 @@ const FormRegister = ({
         }
         let input = (
           <Input
-            disabled={
-              /* cEvent.value.allow_register === false && Este para el caso que se evalue tambien anonimo */
-              validateUrl() === true
-                ? m.name == 'email' && initialValues?.email
-                  ? true
-                  : cEvent.value?.visibility === 'PUBLIC' && m.name == 'names' && initialValues?.names
-                  ? true
-                  : false
-                : false
-            }
             {...props}
             addonBefore={
               labelPosition === 'izquierda' && (
@@ -865,6 +858,8 @@ const FormRegister = ({
                   <Form.Item
                     // validateStatus={type=='codearea' && mandatory && (numberareacode==null || areacodeselected==null)&& 'error'}
                     // style={eventUserId && hideFields}
+                    noStyle={visible}
+                    hidden={visible}
                     valuePropName={type === 'boolean' ? 'checked' : 'value'}
                     label={
                       (labelPosition !== 'izquierda' || !labelPosition) && type !== 'tituloseccion'
