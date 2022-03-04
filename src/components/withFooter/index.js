1659760a1b9b69d5bdd1737df9db18339311b149
Maria Perdomo <alejandra.perdomo@mocionsoft.com>
2022-03-04 07:35:02 -0500

    toast faltante

--- src/components/withFooter/index.js
+++ src/components/withFooter/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Footer from '../../containers/footer';
-import { ToastContainer } from 'react-toastify';
+/* import { ToastContainer } from 'react-toastify'; */
 
 export default function WithFooter({ children}) {  
   return (
     <>
       {children}
       <Footer />
-      <ToastContainer autoClose={2000} newestOnTop pauseOnVisibilityChange />
+      {/* <ToastContainer autoClose={2000} newestOnTop pauseOnVisibilityChange /> */}
     </>
   );
 }
