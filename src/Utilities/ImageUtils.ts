c1f5d13cb3707c8bb1f55998e4dd7c5ad20b775c
Mario Montero <marioupc3@gmail.com>
2022-03-22 11:54:25 -0500

    package json lock delete

--- src/Utilities/ImageUtils.ts
+++ src/Utilities/ImageUtils.ts
@@ -3,6 +3,7 @@ import NOTFOUND from '../Assets/img/notfound.png';
 import EnVivo from '../Assets/img/EnVivo.svg';
 import EventImage from '../Assets/img/eventimage.png';
 import LogoSvg from '../Assets/img/logo.svg';
+
 export const imageUtils = {
   NOTFOUND,
   LOGOEVIUS,
