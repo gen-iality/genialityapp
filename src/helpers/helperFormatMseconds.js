ae4051091e8bf54ea6aacf4c7d9a41b27041ad29
Marlon Carrillo <marlon.carrillo@mocionsoft.com>
2022-04-06 16:15:01 -0500

    formato de tiempo para los video mejorados visulamente, se arreglo icono de estado que no salia para actividades de rtmp y se ocultaron los botones de opciones mientras se procesa el video

--- src/helpers/helperFormatMseconds.js
+++ src/helpers/helperFormatMseconds.js
@@ -12,5 +12,12 @@ export const milisegundosTohour = (milisegundos) => {
   milisegundos -= minutos * 60 * 1000;
   const segundos = milisegundos / 1000;
   console.log('SEGUNDOS==>', segundos, hours, minutos);
-  return `${addFormatCero(hours)}:${addFormatCero(minutos)}:${addFormatCero(segundos.toFixed(1))}`;
+  if (hours > 0 && minutos > 0) {
+    return `${addFormatCero(hours)}:${addFormatCero(minutos)}:${addFormatCero(segundos.toFixed(0))}`;
+  } else if (hours === 0 && minutos > 0) {
+    return `${addFormatCero(minutos)}:${addFormatCero(segundos.toFixed(0))}`;
+  } else {
+    return `${addFormatCero(segundos.toFixed(0))} s`;
+  }
+  // return `${addFormatCero(hours)}:${addFormatCero(minutos)}:${addFormatCero(segundos.toFixed(0))}`;
 };
