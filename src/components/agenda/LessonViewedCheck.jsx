7f14b50c966bebc22b3f3868629eeb0780f28b0a
David Borja <davidborja456@gmail.com>
2022-06-07 15:37:25 -0500

    Improve LessonViewedCheck code

--- src/components/agenda/LessonViewedCheck.jsx
+++ src/components/agenda/LessonViewedCheck.jsx
@@ -1,9 +1,13 @@
 import { Badge, Card } from 'antd';
 
 function LessonViewedCheck(props) {
-  const { isTaken = 'false' } = props;
+  const { isTaken = false } = props;
 
-  return <>{isTaken && <Badge.Ribbon text='Viewed' color='green' placement='start' />}</>;
+  if (!isTaken) return null;
+
+  return (
+    <Badge.Ribbon text='Viewed' color='green' placement='start' />
+  );
 }
 
 export default LessonViewedCheck;
