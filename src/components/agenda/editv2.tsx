b423d0f2d763065a7a5555628af1904d90e9b848
Mauricio J Carrillo <mauricio.jcarrillo@gmail.com>
2022-06-22 14:21:38 -0500

    Init new Edit File Refactoring

---
+++ src/components/agenda/editv2.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import { Redirect, withRouter, Link } from 'react-router-dom';
+import Moment from 'moment';
+import {
+  Tabs,
+  Row,
+  Col,
+  Checkbox,
+  Space,
+  Typography,
+  Button,
+  Form,
+  Input,
+  Switch,
+  Empty,
+  Card,
+  Image,
+  Modal,
+  TimePicker,
+} from 'antd';
+import Header from '../../antdComponents/Header';
+import { RouterPrompt } from '../../antdComponents/RoutePrompt';
+
+
+function AgendaEdit(props) {
+  return ( <></> );
+}
+
+export default AgendaEdit;
--- src/components/agenda/index.jsx
+++ src/components/agenda/index.jsx
@@ -2,6 +2,7 @@ import { Fragment } from 'react';
 import { Route, Switch, withRouter } from 'react-router-dom';
 import Agenda from './agenda';
 import AgendaEdit from './edit';
+import AgendaEditv2 from './editv2';
 import AgendaTypeCat from './typecat';
 import AgendaTypeCatCE from './AgendaTypeCatCE';
 /* import AgendaEditLanguage from "./editLanguage_old";
@@ -13,6 +14,7 @@ function AgendaRoutes({ ...props }) {
       <Switch>
         <Route exact path={`${match.url}/`} render={() => <Agenda event={event} matchUrl={match.url} />} />
         <Route exact path={`${match.url}/actividad`} render={() => <AgendaEdit event={event} matchUrl={match.url} />} />
+        <Route exact path={`${match.url}/actividadv2`} render={() => <AgendaEditv2 event={event} matchUrl={match.url} />} />
         <Route exact path={`${match.url}/tipos`} render={() => <AgendaTypeCat event={event} matchUrl={match.url} />} />
         <Route
           exact
