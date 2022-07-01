db70f5b11f58d83c958e32fbeecf350075b9ccef
David Borja <davidborja456@gmail.com>
2022-06-30 19:40:25 -0500

    Replace the original edit.jsx

--- src/components/agenda/index.jsx
+++ src/components/agenda/index.jsx
@@ -2,7 +2,6 @@ import { Fragment } from 'react';
 import { Route, Switch, withRouter } from 'react-router-dom';
 import Agenda from './agenda';
 import AgendaEdit from './edit';
-import AgendaEditv2 from './editv2';
 import AgendaTypeCat from './typecat';
 import AgendaTypeCatCE from './AgendaTypeCatCE';
 /* import AgendaEditLanguage from "./editLanguage_old";
@@ -13,8 +12,7 @@ function AgendaRoutes({ ...props }) {
     <Fragment>
       <Switch>
         <Route exact path={`${match.url}/`} render={() => <Agenda event={event} matchUrl={match.url} />} />
-        {/* <Route exact path={`${match.url}/actividad`} render={() => <AgendaEdit event={event} matchUrl={match.url} />} /> */}
-        <Route exact path={`${match.url}/actividad`} render={() => <AgendaEditv2 event={event} matchUrl={match.url} />} />
+        <Route exact path={`${match.url}/actividad`} render={() => <AgendaEdit event={event} matchUrl={match.url} />} />
         <Route exact path={`${match.url}/tipos`} render={() => <AgendaTypeCat event={event} matchUrl={match.url} />} />
         <Route
           exact
