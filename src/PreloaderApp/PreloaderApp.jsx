e30e3f3721560d866f424146e8da3179287b5f93
Mauricio J Carrillo <mauricio.jcarrillo@gmail.com>
2022-07-13 10:34:37 -0500

    Charge images in .env files from firebase of Evius

--- src/PreloaderApp/PreloaderApp.jsx
+++ src/PreloaderApp/PreloaderApp.jsx
@@ -3,7 +3,7 @@ export const PreloaderApp = () => {
   return (
     <div className='preloaderBg'>
       <div className='preloaderIcon animate__animated animate__flip'>
-        <img className='sizeBackground' src={import.meta.env.VITE_IMAGE_FAVICON} />
+        <img className='sizeBackground' src={import.meta.env.VITE_IMAGE_FAVICON_PRELOADER} />
       </div>
       <div className='preloader2'></div>
     </div>
