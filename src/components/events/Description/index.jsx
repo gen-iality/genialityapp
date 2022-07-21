d1ce09d5916ce36999f34830c08ac257fef00059
Marlon Carrillo <marlon.carrillo@mocionsoft.com>
2022-07-21 17:42:30 -0500

    ajustes del drag para que se vea centrado

--- src/components/events/Description/index.jsx
+++ src/components/events/Description/index.jsx
@@ -1,13 +1,14 @@
-import { DeleteOutlined, EditOutlined, MenuOutlined, PlusCircleOutlined, PlusOutlined } from '@ant-design/icons';
+import { DeleteOutlined, EditOutlined, PlusOutlined } from '@ant-design/icons';
 import {
+  Affix,
   Avatar,
   Button,
   Card,
   Col,
   ConfigProvider,
   Divider,
+  Empty,
   Image,
-  Layout,
   Popover,
   Row,
   Space,
@@ -74,10 +75,10 @@ const DescriptionDynamic = () => {
     {
       title: 'Edit',
       dataIndex: 'index',
-      width: 100,
+      width: 50,
       className: 'drag-visible',
       render: (value, item) => (
-        <Space>
+        <Space direction='vertical'>
           <Button icon={<EditOutlined />} onClick={() => editItem(item)} />
           <Button icon={<DeleteOutlined />} onClick={() => deleteItem(item)} type='primary' danger />
         </Space>
@@ -120,8 +121,9 @@ const DescriptionDynamic = () => {
   const SortableItem = SortableElement((props) => <tr {...props} />);
   const SortableBody = SortableContainer((props) => <tbody {...props} />);
   const content = (
-    <Space size={20}>
+    <Space size={20} id='content-block-description'>
       <Card
+        className='animate__animated animate__bounceIn '
         hoverable={true}
         style={styleCardButton}
         onClick={() => {
@@ -132,12 +134,18 @@ const DescriptionDynamic = () => {
           <Avatar
             shape='square'
             size={80}
-            icon={<CardTextIcon style={{ fontSize: '50px' }} />}
+            icon={
+              <CardTextIcon
+                className='animate__animated animate__bounceIn animate__delay-2s '
+                style={{ fontSize: '50px' }}
+              />
+            }
             style={{ display: 'grid', placeContent: 'center', backgroundColor: '#517FD6' }}></Avatar>
           <Typography.Text>Texto</Typography.Text>
         </Space>
       </Card>
       <Card
+        className='animate__animated animate__bounceIn '
         hoverable={true}
         style={styleCardButton}
         onClick={() => {
@@ -148,17 +156,27 @@ const DescriptionDynamic = () => {
           <Avatar
             shape='square'
             size={80}
-            icon={<ImageAreaIcon style={{ fontSize: '50px' }} />}
+            icon={
+              <ImageAreaIcon
+                className='animate__animated animate__bounceIn animate__delay-2s '
+                style={{ fontSize: '50px' }}
+              />
+            }
             style={{ display: 'grid', placeContent: 'center', backgroundColor: '#51D6A8' }}></Avatar>
           <Typography.Text>Imagen</Typography.Text>
         </Space>
       </Card>
-      <Card hoverable={true} style={styleCardButton}>
+      <Card className='animate__animated animate__bounceIn ' hoverable={true} style={styleCardButton}>
         <Space size={8} style={{ textAlign: 'center' }} direction='vertical'>
           <Avatar
             shape='square'
             size={80}
-            icon={<VideoBoxtIcon style={{ fontSize: '50px' }} />}
+            icon={
+              <VideoBoxtIcon
+                className='animate__animated animate__bounceIn animate__delay-2s '
+                style={{ fontSize: '50px' }}
+              />
+            }
             style={{ display: 'grid', placeContent: 'center', backgroundColor: '#D6A851' }}></Avatar>
           <Typography.Text>Video</Typography.Text>
         </Space>
@@ -212,22 +230,24 @@ const DescriptionDynamic = () => {
     <Row gutter={[16, 16]}>
       <Col span={24}>
         <Row justify='center' align='middle'>
-          <Table
-            id='tableDescription'
-            tableLayout='auto'
-            showHeader={false}
-            style={{ userSelect: 'none', width: '100%' }}
-            pagination={false}
-            dataSource={dataSource}
-            columns={columns}
-            rowKey='index'
-            components={{
-              body: {
-                wrapper: DraggableContainer,
-                row: DraggableBodyRow,
-              },
-            }}
-          />
+          <ConfigProvider renderEmpty={() => <Empty />}>
+            <Table
+              id='tableDescription'
+              tableLayout='auto'
+              showHeader={false}
+              style={{ userSelect: 'none', width: '100%' }}
+              pagination={false}
+              dataSource={dataSource}
+              columns={columns}
+              rowKey='index'
+              components={{
+                body: {
+                  wrapper: DraggableContainer,
+                  row: DraggableBodyRow,
+                },
+              }}
+            />
+          </ConfigProvider>
         </Row>
       </Col>
       <Col span={24}>
@@ -240,8 +260,8 @@ const DescriptionDynamic = () => {
                 className={`animate__animated animate__heartBeat animate__slow ${focus ? '' : 'animate__infinite'}`}
                 type='dashed'
                 shape='default'
-                icon={<PlusOutlined style={{ fontSize: '30px' }} />}
-                style={{ height: '60px', width: '60px' }}
+                icon={<PlusOutlined style={{ fontSize: '30px', color: '#2698FA' }} />}
+                style={{ height: '60px', width: '60px', position: 'sticky', bottom: '10px' }}
               />
             </Divider>
           </Popover>
