d874c847f6950ff0c2e3e67821ad8a518151152f
David Borja <davidborja456@gmail.com>
2022-07-25 08:09:36 -0500

    Add layout/component to modal that select the activity type

---
+++ src/components/agenda/activityType/ActivityTypeModal.tsx
@@ -0,0 +1,71 @@
+import * as React from 'react';
+import { useState, useMemo, useEffect } from 'react';
+
+import { Modal } from 'antd';
+
+import ActivityTypeSelectableCards from './components/ActivityTypeSelectableCards';
+import ActivityTypeModalLayout from './components/ActivityTypeModalLayout';
+import { activityTypeData } from './schema/activityTypeFormStructure';
+// import { ActivityTypeCard } from './schema/structureInterfaces';
+
+export interface ActivityTypeModalProps {
+  visible: boolean,
+  onClose: (success?: boolean) => void,
+  onSelectionChange: (selected: string | null) => void,
+};
+
+function ActivityTypeModal(props: ActivityTypeModalProps) {
+  const [selected, setSelected] = useState<string | null>(null);
+  
+  const handleCancel = () => {
+    props.onClose(false)
+    setSelected(null); // Reset the selected item
+  };
+
+  const somethingWasSelected = useMemo(() => selected !== null, [selected]);
+
+  const handleConfirm = () => {
+    console.log('activity type saved:', selected);
+    if (somethingWasSelected) {
+      props.onSelectionChange(selected);
+      // setSelected(null);
+    }
+  }
+
+  const handleSelectChange = (newSelected: string) => {
+    console.log('selected changed to', newSelected);
+    setSelected(newSelected);
+  }
+
+  useEffect(() => {
+    if (!props.visible) {
+      setSelected(null);
+    }
+  }, [props.visible]);
+
+  return (
+    <Modal
+      centered
+      width={1200}
+      footer={null}
+      visible={props.visible}
+      onCancel={handleCancel}
+    >
+      <ActivityTypeModalLayout
+        somethingWasSelected={somethingWasSelected}
+        title={activityTypeData.MainTitle}
+        onClose={props.onClose}
+        onConfirm={handleConfirm}
+        render={() => <ActivityTypeSelectableCards
+          selected={selected}
+          widget={activityTypeData}
+          onWidgetChange={(widget) => {
+            handleSelectChange(widget.key);
+          }}
+        />}
+      />
+    </Modal>
+  );
+}
+
+export default ActivityTypeModal;
---
+++ src/components/agenda/activityType/components/ActivityTypeModalLayout.tsx
@@ -0,0 +1,63 @@
+import * as React from 'react';
+
+import { Typography, Layout, Row, Col, Button, Spin } from 'antd';
+
+import { ModalWrapperUIProps } from '../interfaces/ModalWrapperUIProps';
+
+const { Header, Content, Footer } = Layout;
+const { Title } = Typography;
+
+export interface ActivityTypeModalLayoutProps extends ModalWrapperUIProps {
+  somethingWasSelected: boolean,
+  render: () => React.ReactNode,
+};
+
+function ActivityTypeModalLayout(props: ActivityTypeModalLayoutProps) {
+  const {
+    render,
+    somethingWasSelected,
+    // Inheret
+    title,
+    onClose = () => {},
+    onConfirm = () => {},
+  } = props;
+
+  const handleSelectButton = () => {
+    console.log('somethingWasSelected', somethingWasSelected);
+    if (somethingWasSelected) {
+      onConfirm();
+      onClose(true);
+      console.log('confirm & close modal');
+    }
+  };
+
+  return (
+    <Layout>
+      <Header style={{ textAlign: 'center', padding: '20px 0px 20px 0px' }}>
+        <Title level={3}>
+          {title}
+        </Title>
+      </Header>
+
+      <Content style={{ padding: '60px 50px 60px 50px' }}>
+        {render()}
+      </Content>
+
+      <Footer style={{ backgroundColor: '#fff', padding: '20px 0px 0px 0px' }}>
+        <Row justify='end' gutter={[8, 8]}>
+          <Col>
+            <Button
+              disabled={!somethingWasSelected}
+              type='primary'
+              onClick={handleSelectButton}
+            >
+              Seleccionar
+            </Button>
+          </Col>
+        </Row>
+      </Footer>
+    </Layout>
+  );
+}
+
+export default ActivityTypeModalLayout;
