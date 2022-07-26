8ef75cb7d5b971b28e3fa9de27d241fb0da73df3
Marlon Carrillo <marloncb2012@gmail.com>
2022-07-26 15:29:39 -0500

    ajustes en en preview imagen

--- src/components/events/Description/hooks/functions.jsx
+++ src/components/events/Description/hooks/functions.jsx
@@ -1,30 +1,15 @@
-import { Button, Image } from "antd";
-import ReactPlayer from "react-player";
+import { Button, Image } from 'antd';
+import ReactPlayer from 'react-player';
 
-export const renderTypeComponent = 
-    (type, value) => {
-      switch (type) {
-        case 'image':
-          return (
-            <Image
-              preview={{
-                mask: (
-                  <Button size='large' type='primary'>
-                    Ver imagen completa
-                  </Button>
-                ),
-              }}
-              src={value}
-              style={{ objectFit: 'contain' }}
-              width='100%'
-              height='250px'
-            />
-          );
-        case 'text':
-          return <div dangerouslySetInnerHTML={{ __html: value }} />;
-        case 'video':
-          return <ReactPlayer controls width={'100%'} height={'350'} style={{}} url={value} />;
-        default:
-          return <div></div>;
-      }
-    };
+export const renderTypeComponent = (type, value) => {
+  switch (type) {
+    case 'image':
+      return <Image preview={false} src={value} width='100%' />;
+    case 'text':
+      return <div dangerouslySetInnerHTML={{ __html: value }} />;
+    case 'video':
+      return <ReactPlayer controls width={'100%'} height={'350'} style={{}} url={value} />;
+    default:
+      return <div></div>;
+  }
+};
