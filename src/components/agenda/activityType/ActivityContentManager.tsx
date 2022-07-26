88cca684de139aa984f9ee5b511138b146b4d713
David Borja <davidborja456@gmail.com>
2022-07-26 09:37:13 -0500

    Preparation to update the activity type manager

--- src/components/agenda/activityType/ActivityContentManager.tsx
+++ src/components/agenda/activityType/ActivityContentManager.tsx
@@ -1,15 +1,39 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 
 import {
   Row,
   Col,
   Affix,
 } from 'antd';
+import useActivityType from '@/context/activityType/hooks/useActivityType';
+import AgendaContext from '@/context/AgendaContext';
 
 export interface ActivityContentManagerProps {};
 
 function ActivityContentManager(props: ActivityContentManagerProps) {
+  const {
+    contentSource,
+    activityContentType,
+  } = useActivityType();
+
+  const {
+    roomStatus,
+    setRoomStatus,
+    dataLive,
+    meeting_id,
+    obtainUrl,
+    recordings,
+    startRecordTransmition,
+    stopRecordTransmition,
+    loadingRecord,
+    record,
+    viewers,
+    viewersOnline,
+    totalViews,
+    maxViewers,
+  } = useContext(AgendaContext);
+
   return (
     <>
     <Row gutter={[16, 16]}>
