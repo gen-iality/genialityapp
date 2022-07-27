03308a315064fa99506aec716d6452250bd0afc0
David Borja <davidborja456@gmail.com>
2022-07-27 15:29:50 -0500

    Raise up the activity type context provider

--- src/components/agenda/activityType/ActivityContentSelector.tsx
+++ src/components/agenda/activityType/ActivityContentSelector.tsx
@@ -1,18 +1,12 @@
 import * as React from 'react';
 import { useState } from 'react';
 
-import { TypeActivityProvider } from '@/context/typeactivity/typeActivityProvider';
 import SubActivityContentSelector from './SubActivityContentSelector';
 import { SubActivityContentSelectorProps } from './SubActivityContentSelector';
-import ActivityTypeProvider from '@/context/activityType/activityTypeProvider';
 
 function ActivityContentSelector(props: SubActivityContentSelectorProps) {
   return (
-    <TypeActivityProvider>
-      <ActivityTypeProvider>
-        <SubActivityContentSelector {...props}/>
-      </ActivityTypeProvider>
-    </TypeActivityProvider>
+    <SubActivityContentSelector {...props}/>
   );
 }
 
--- src/components/agenda/activityType/ActivityTypeSelector.tsx
+++ src/components/agenda/activityType/ActivityTypeSelector.tsx
@@ -1,16 +1,9 @@
-import { TypeActivityProvider } from '@/context/typeactivity/typeActivityProvider';
-import ActivityTypeProvider from '@/context/activityType/activityTypeProvider';
-
 import SubActivityTypeSelector from './SubActivityTypeSelector';
 import { SubActivityTypeSelectorProps } from './SubActivityTypeSelector';
 
 function ActivityTypeSelector(props: SubActivityTypeSelectorProps) {
   return (
-    <TypeActivityProvider>
-      <ActivityTypeProvider>
-        <SubActivityTypeSelector {...props} />
-      </ActivityTypeProvider>
-    </TypeActivityProvider>
+    <SubActivityTypeSelector {...props} />
   );
 }
 
