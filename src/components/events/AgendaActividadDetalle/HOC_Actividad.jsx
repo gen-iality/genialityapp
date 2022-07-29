bc97ad43d05a81a49aa7b226f82fd129e21d2e0e
David Borja <davidborja456@gmail.com>
2022-07-29 10:11:15 -0500

    Tolerate new activity type in the HOC actividad component

--- src/components/events/AgendaActividadDetalle/HOC_Actividad.jsx
+++ src/components/events/AgendaActividadDetalle/HOC_Actividad.jsx
@@ -6,17 +6,23 @@ import VideoActivity from './ActivityTypes/VideoActivity';
 import GenericActivity from './ActivityTypes/GenericActivity';
 
 function ActivityTypeSwitch({ activity }) {
+  console.debug(activity)
   let activityType = activity.type ? activity.type.name : 'generic';
+  console.debug('HOC: activityType', activityType);
   switch(activityType) {
     case 'generic':
       return (<GenericActivity />);
     case 'eviusMeet':
+    case 'vimeo':
+    case 'youTube':
       return (<StreamingActivity />);
     case 'meeting':
       return (<MeetingActivity />);
     case 'url':
+    case 'cargarvideo':
       return (<VideoActivity />);
     case 'quiz':
+    case 'quizing':
       return (<QuizActivity />);
     default: return <GenericActivity />;
   }
