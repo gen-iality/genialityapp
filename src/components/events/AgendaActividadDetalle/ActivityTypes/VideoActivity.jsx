314907e8fadc956b10c2b903c965693b1a89165d
David Borja <davidborja456@gmail.com>
2022-07-29 11:47:28 -0500

    Fix not showing player to gcore video in landing of course

--- src/components/events/AgendaActividadDetalle/ActivityTypes/VideoActivity.jsx
+++ src/components/events/AgendaActividadDetalle/ActivityTypes/VideoActivity.jsx
@@ -1,24 +1,53 @@
-import { useState } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import ReactPlayer from 'react-player';
 import { useHelper } from '../../../../context/helperContext/hooks/useHelper';
+import AgendaContext from '@/context/AgendaContext';
 import HeaderColumnswithContext from '../HeaderColumns';
 
 const VideoActivity = () => {
   let { currentActivity } = useHelper();
 
   const [activityState, setactivityState] = useState('');
+  const [processedURL, setProcessedURL] = useState(null);
+  const [isItAnFrame, setIsItAnFrame] = useState(false);
+  const { obtainUrl } = useContext(AgendaContext);
+
+  useEffect(() => {
+    try {
+      const {
+        urlVideo,
+        visibleReactPlayer, // boolean, when the video is ok
+      } = obtainUrl(currentActivity?.type.name, currentActivity?.video);
+      console.debug('obtainUrl gets', urlVideo);
+      setProcessedURL(urlVideo);
+      setIsItAnFrame(!visibleReactPlayer);
+    } catch (err) {
+      console.error(err);
+    }
+  }, [currentActivity]);
 
   return (
     <>
       <HeaderColumnswithContext isVisible={true} activityState={activityState}/>
         <div className='mediaplayer' style={{ aspectRatio: '16/9' }}>
+          {isItAnFrame ? (
+            <iframe
+            style={{ aspectRatio: '16/9' }}
+            width='100%'
+            src={processedURL + '?muted=1&autoplay=1'}
+            frameBorder='0'
+            allow='autoplay; encrypted-media'
+            allowFullScreen
+            ></iframe>
+          ) : (
           <ReactPlayer
             style={{ objectFit: 'cover' }}
             width='100%'
             height='100%'
-            url={currentActivity && currentActivity?.video}
+            url={processedURL}
             controls
           />
+          )}
         </div>
     </>
   );
