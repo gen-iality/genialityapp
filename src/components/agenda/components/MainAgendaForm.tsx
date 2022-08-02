edcaabb40b896340b0eafe488c47e196cd3171ec
David Borja <davidborja456@gmail.com>
2022-08-01 21:39:55 -0500

    Avoid array objects as null from API

--- src/components/agenda/components/MainAgendaForm.tsx
+++ src/components/agenda/components/MainAgendaForm.tsx
@@ -164,10 +164,10 @@ function MainAgendaForm(props: MainAgendaFormProps) {
       const remoteCategories = await CategoriesAgendaApi.byEvent(props.event._id);
 
       // The object struct should be like [{ label, value }] for the Select components
-      const newAllHosts = handleSelect(remoteHosts);
-      const newAllRoles = handleSelect(remoteRoles);
-      const newAllSpaces = handleSelect(remoteSpaces);
-      const newAllCategories = handleSelect(remoteCategories);
+      const newAllHosts = handleSelect(remoteHosts) || [];
+      const newAllRoles = handleSelect(remoteRoles) || [];
+      const newAllSpaces = handleSelect(remoteSpaces) || [];
+      const newAllCategories = handleSelect(remoteCategories) || [];
 
       setAllHosts(newAllHosts);
       setAllRoles(newAllRoles);
@@ -201,10 +201,10 @@ function MainAgendaForm(props: MainAgendaFormProps) {
       length: agenda.length,
       latitude: agenda.latitude,
       selectedTickets: agenda.selectedTicket ? agenda.selectedTicket : [],
-      selectedDocuments: agenda.selected_document,
-      selectedCategories: fieldsSelect(agenda.activity_categories_ids, allCategories),
-      selectedHosts: fieldsSelect(agenda.host_ids, allHosts),
-      selectedRol: fieldsSelect(agenda.access_restriction_rol_ids, allRoles),
+      selectedDocuments: agenda.selected_document || [],
+      selectedCategories: fieldsSelect(agenda.activity_categories_ids, allCategories) || [],
+      selectedHosts: fieldsSelect(agenda.host_ids, allHosts) || [],
+      selectedRol: fieldsSelect(agenda.access_restriction_rol_ids, allRoles) || [],
     }));
 
   }, [agenda, allCategories, allHosts, allRoles]);
