f0d7cdcaf8047f5a83d1068bb53dd8e9eb1cd657
David Borja <davidborja456@gmail.com>
2022-08-05 20:03:27 -0500

    Remove the attribute isPublished from type

--- src/components/agenda/types/AgendaDocumentType.ts
+++ src/components/agenda/types/AgendaDocumentType.ts
@@ -1,3 +1,7 @@
+// printed agenda keys from frontend:
+// _id,name,subtitle,bigmaker_meeting_id,datetime_start,datetime_end,space_id,image,description,registration_message,capacity,activity_categories_ids,access_restriction_type,access_restriction_rol_ids,has_date,meeting_id,vimeo_id,platform,start_url,join_url,requires_registration,host_ids,length,latitude,selected_document,event_id,date_start_zoom,date_end_zoom,updated_at,created_at,role_attendee_ids,access_restriction_types_available,activity_categories,space,hosts,type,access_restriction_roles
+// NOTE: where is real the agenda schema?
+
 export default interface AgendaDocumentType {
   _id?: string,
   name: string,
@@ -25,7 +29,6 @@ export default interface AgendaDocumentType {
   name_host: any,
   key: any,
   requires_registration: boolean,
-  isPublished: boolean,
   host_ids: any[] | null,
   length: string,
   latitude: string,
