2f53057b54fef861f8de3781c5a25fa8709ac445
David Borja <davidborja456@gmail.com>
2022-08-25 18:36:53 -0500

    New method to save the acumulative quiz points

---
+++ src/components/events/surveys/functions/saveAcumulativePoints.ts
@@ -0,0 +1,38 @@
+import { QuizStatus } from '@/components/quiz/types';
+import { firestore } from '@helpers/firebase';
+
+/**
+ * Given a user's survey adds a passed earned points.
+ * @param surveyId The current survey ID.
+ * @param userId The current user ID.
+ * @param nextPoints The acumulative points.
+ */
+export async function saveAcumulativePoints(
+  surveyId: string,
+  userId: string,
+  nextPoints: number,
+) {
+  // Get last points
+  const firebaseRef = firestore
+    .collection('votingStatusByUser')
+    .doc(userId)
+    .collection('surveyStatus')
+    .doc(surveyId);
+    
+  const result = await firebaseRef.get();
+
+  // Create the payload
+  let acumulativePoints = 0;
+
+  // Check if there is saved points
+  if (result.exists) {
+    const {
+      right = 0,
+    } = result.data() as QuizStatus;
+    acumulativePoints = acumulativePoints + right;
+    // Add points
+  }
+  // Speak pretty goodly with Firestore
+  await firebaseRef.set({ right: acumulativePoints + nextPoints } as QuizStatus, {merge: true});
+  console.debug(`save survey status /votingStatusByUser/${userId}/surveyStatus/${surveyId}`, 'value', acumulativePoints);
+}
