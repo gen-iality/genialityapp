572444d9cd256c5308b2a1c44d1548fdcba5d34e
David Borja <davidborja456@gmail.com>
2022-08-29 10:11:38 -0500

    New message for no-required status

--- src/components/quiz/quizStatus.ts
+++ src/components/quiz/quizStatus.ts
@@ -2,6 +2,7 @@ const QuizStatusMessage = {
   PASSED: 'aprobado',
   NOT_PASSED: 'reprobado',
   NO_QUESTIONS: 'sin preguntas',
+  NO_REQUIRED: 'sin requisitos',
   PROCESSING: 'procesando...',
 };
 
