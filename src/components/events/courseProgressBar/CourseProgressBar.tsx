aa682a0910d50f48db87d918a2b5347554847ddc
David Borja <davidborja456@gmail.com>
2022-09-01 11:35:10 -0500

    Import course progress bar as it comes

---
+++ src/components/events/courseProgressBar/CourseProgressBar.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+
+import Step from './Step';
+
+import './CourseProgressBar.css';
+
+export interface CourseProgressBarProps {
+  total: number,
+  count: number,
+};
+
+function CourseProgressBar(props: CourseProgressBarProps) {
+  const {
+    total,
+    count,
+  } = props;
+
+  const [progressWidth, setProgressWidth] = useState(0);
+
+  useEffect(() => {
+    if (count === total) {
+      setProgressWidth(100);
+    } else {
+      setProgressWidth((100 / (total - 1)) * count);
+    }
+  }, [count, total]);
+
+  if (total === 0) {
+    return null;
+  }
+
+  return (
+    <div className='CourseProgressBar-container'>
+      <div className='CourseProgressBar-line' style={{ width: progressWidth + "%" }}></div>
+      {Array.from(Array(total).keys()).map((i) => (
+        <Step
+          isActive={i < count}
+        >
+          {i+1}
+        </Step>
+      ))}
+    </div>
+  );
+}
+
+export default CourseProgressBar;
