013f376540d8e5370df7a089ce7643a6884032a8
David Borja <davidborja456@gmail.com>
2022-09-13 09:22:58 -0500

    Put an icon and a delay time

--- src/components/agenda/components/ActivitiesList.tsx
+++ src/components/agenda/components/ActivitiesList.tsx
@@ -2,7 +2,10 @@ import { Divider, List, Typography, Button, Spin } from 'antd';
 import { ReadFilled } from '@ant-design/icons';
 import { useState, useEffect } from 'react';
 import { AgendaApi } from '@/helpers/request';
-import AgendaType from '@/Utilities/types/AgendaType';
+import dayjs from 'dayjs';
+import { ExtendedAgendaType } from '@/Utilities/types/AgendaType';
+import { activityContentValues } from '@/context/activityType/constants/ui';
+import { ActivityType } from '@/context/activityType/types/activityType';
 
 const data = [
   <div>
@@ -17,6 +20,8 @@ const data = [
 
 type TruncatedAgenda = {
   title: string,
+  type?: ActivityType.ContentValue,
+  timeString?: string,
 };
 
 interface ActivitiesListProps {
@@ -35,15 +40,28 @@ const ActivitiesList = (props: ActivitiesListProps) => {
     if (!eventId) return;
 
     (async () => {
-      const { data: agendaList } = await AgendaApi.byEvent(eventId) as { data: AgendaType[]};
+      setIsLoading(true);
+      setTruncatedAgendaList([]);
+      const { data: agendaList } = await AgendaApi.byEvent(eventId) as { data: ExtendedAgendaType[]};
 
       setTruncatedAgendaList((previous) => ([
         ...previous,
         ...agendaList.map((agenda) => {
           // Logic here
-          return {
-            title: agenda.name
+          let diff = Math.floor(Math.random() * 60*60);
+
+          try {
+            diff = dayjs(agenda.datetime_end).diff(dayjs(agenda.datetime_start));
+          } catch (err) {
+            console.error(err);
+          }
+
+          const result: TruncatedAgenda = {
+            title: agenda.name,
+            type: agenda.type?.name as ActivityType.ContentValue,
+            timeString: dayjs(diff).format('h:mm').concat(' min'),
           };
+          return result;
         })
       ]))
 
@@ -60,7 +78,26 @@ const ActivitiesList = (props: ActivitiesListProps) => {
       bordered
       dataSource={truncatedAgendaList}
       renderItem={(item: TruncatedAgenda) => (
-        <List.Item className='shadow-box'>{item.title}</List.Item>
+        <List.Item className='shadow-box'>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              width: '100%',
+            }}
+          >
+            <div>
+              <ReadFilled className='list-icon' style={{marginRight: '1em'}} />
+              <span>{item.title}</span>
+            </div>
+            <span
+              style={{
+                fontWeight: '100',
+                fontSize: '1.2rem',
+              }}
+            >{item.timeString}</span>
+          </div>
+        </List.Item>
       )}
     />
   );
