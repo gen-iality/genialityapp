1ef6745aa84469527dd2e6a44a21c37acdfa71ca
David Borja <davidborja456@gmail.com>
2022-09-20 18:51:56 -0500

    Add sample data for testing

--- src/components/isolated/quiz/LikertScaleEditorPage.tsx
+++ src/components/isolated/quiz/LikertScaleEditorPage.tsx
@@ -1,13 +1,53 @@
 import LikertScaleEditor from '@/components/quiz/LikertScaleEditor';
 import * as React from 'react';
 
+// Taken from: https://surveyjs.io/form-library/examples/questiontype-matrix/reactjs#content-js
+const sampleData = {
+  "columns": [
+    {
+      "value": 5,
+      "text": "Strongly agree"
+    },
+    {
+      "value": 4,
+      "text": "Agree"
+    },
+    {
+      "value": 3,
+      "text": "Neutral"
+    },
+    {
+      "value": 2,
+      "text": "Disagree"
+    }, {
+      "value": 1,
+      "text": "Strongly disagree"
+    }
+  ],
+  "rows": [
+    {
+      "value": "affordable",
+      "text": "Product is affordable"
+    }, {
+      "value": "does what it claims",
+      "text": "Product does what it claims"
+    }, {
+      "value": "better than others",
+      "text": "Product is better than other products on the market"
+    }, {
+      "value": "easy to use",
+      "text": "Product is easy to use"
+    }
+  ],
+};
+
 export interface ILikertScaleEditorPageProps {
 }
 
 export function LikertScaleEditorPage (props: ILikertScaleEditorPageProps) {
   return (
     <div>
-      <LikertScaleEditor/>
+      <LikertScaleEditor source={sampleData}/>
     </div>
   );
 }
