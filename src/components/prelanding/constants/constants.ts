cfeb4a28d364fdc6191f11dd1e1fa2b6559d5758
apps <apps@mocionsoft.com>
2022-09-21 15:10:06 -0500

    ✨ feat(Add):
    Se agrega, imagen template para los speakers, y se crea funcion para validar cuando mostrar una imagen si existe o un template de la imagen.

---
+++ src/components/prelanding/constants/constants.ts
@@ -0,0 +1,2 @@
+export const speakerWithoutImage =
+  'https://img.freepik.com/free-photo/blue-user-icon-symbol-website-admin-social-login-element-concept-white-background-3d-rendering_56104-1217.jpg?w=996&t=st=1663788736~exp=1663789336~hmac=cb343b1aad9f3ecfd4eb2530c61a1a38d7c1c494003e87838f8c40533d655850';
