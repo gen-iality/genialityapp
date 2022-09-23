c205379097b2a52f6cec024a3b72fcb2e05398c3
Marlon Carrillo <marloncb2012@gmail.com>
2022-09-23 16:10:12 -0500

    cambio de la imagen cuando no tiene foto el conferencista

--- src/components/prelanding/constants/constants.ts
+++ src/components/prelanding/constants/constants.ts
@@ -1,2 +1,2 @@
 export const speakerWithoutImage =
-  'https://img.freepik.com/free-photo/blue-user-icon-symbol-website-admin-social-login-element-concept-white-background-3d-rendering_56104-1217.jpg?w=996&t=st=1663788736~exp=1663789336~hmac=cb343b1aad9f3ecfd4eb2530c61a1a38d7c1c494003e87838f8c40533d655850';
+  'https://firebasestorage.googleapis.com/v0/b/eviusauth.appspot.com/o/template%2FspeakerWithoutImage.png?alt=media&token=9b2dcec8-1f0e-4575-a842-310651cd8077';
