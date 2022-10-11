82f46257d8dd074d40d9657910b62265cc868a51
David Borja <davidborja456@gmail.com>
2022-10-11 14:23:33 -0500

    Update PooledQuestions

--- src/classes/PooledQuestions.ts
+++ src/classes/PooledQuestions.ts
@@ -1,24 +1,48 @@
+import { SurveyQuestion } from "@components/events/surveys/models/types";
 import { getQuestionsRef } from "@components/events/surveys/services/surveys";
 
 export default class PooledQuestions {
-  public pooled: any[];
+  #userId?: string;
+  #surveyId?: string;
+  public pooled: SurveyQuestion[];
 
-  constructor(partedQuestions: any[]) {
+  constructor(partedQuestions: any[], userId?: string, surveyId?: string) {
     console.debug('PooledQuestions.new');
+    this.#userId = userId;
+    this.#surveyId = surveyId;
     this.pooled = partedQuestions;
   }
 
   /**
-   * Requests the questions in the surveys document by IDs.
-   * @param surveyId The survey ID.
-   * @param userId The user ID.
-   * @returns A PooledQuestions object.
+   * Returns true if all IDs is defined.
+   * @returns true if IDS are defined.
    */
-  static async fromFirebase(surveyId: string, userId: string) {
-    console.debug('PooledQuestions.fromFirebase', `surveyId: ${surveyId}, userId: ${userId}`);
+  private checkIDs() {
+    if (!this.#userId) {
+      console.error('PooledQuestions.checkIDs:', 'userId is invalid', this.#userId);
+      return false;
+    }
+    if (!this.#surveyId) {
+      console.error('PooledQuestions.checkIDs:', 'surveyId is invalid', this.#surveyId);
+      return false;
+    }
+    return true;
+  }
+
+  setUserId(userId: string) { this.#userId = userId; return this; }
+  setSurveyId(surveyId: string) { this.#surveyId = surveyId; return this; }
+
+  /**
+   * Pull data.
+   * @returns PooledQuestions updated.
+   */
+  async pull() {
+    console.debug('PooledQuestions.pull');
+
+    if (!this.checkIDs()) return this;
 
     // Get the questions reference
-    const questionsRef = getQuestionsRef(surveyId, userId);
+    const questionsRef = getQuestionsRef(this.#surveyId!, this.#userId!);
 
     // Get data
     const result = await questionsRef.get();
@@ -30,7 +54,34 @@ export default class PooledQuestions {
       if (data) document = data;      
     }
 
-    // Now create the objet
-    return new PooledQuestions(document.pooled);
+    // Update
+    this.pooled = document.pooled as any[];
+  }
+
+  async push() {
+    console.debug('PooledQuestions.push');
+
+    if (!this.checkIDs()) return this;
+
+    // Get the questions reference
+    const questionsRef = getQuestionsRef(this.#surveyId!, this.#userId!);
+
+    // Set data
+    await questionsRef.set(this, { merge: false });
+
+    return this;
+  }
+
+  /**
+   * Requests the questions in the surveys document by IDs.
+   * @param surveyId The survey ID.
+   * @param userId The user ID.
+   * @returns A PooledQuestions object.
+   */
+  static async fromFirebase(surveyId: string, userId: string) {
+    console.debug('PooledQuestions.fromFirebase', `surveyId: ${surveyId}, userId: ${userId}`);
+    const pooledQuestions = new PooledQuestions([], userId, surveyId);
+    await pooledQuestions.pull();
+    return pooledQuestions;
   }
 }
