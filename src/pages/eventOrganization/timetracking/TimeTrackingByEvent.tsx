010868fb8e7ae0d59bd660080bf914eaa59d0804
David Borja <davidborja456@gmail.com>
2022-10-18 19:38:54 -0500

    Add new stats and improve the UI

--- src/pages/eventOrganization/timetracking/TimeTrackingByEvent.tsx
+++ src/pages/eventOrganization/timetracking/TimeTrackingByEvent.tsx
@@ -1,7 +1,8 @@
+import { SessionPayload } from '@components/presence/types';
 import { fireRealtime } from '@helpers/firebase';
 import Logger from '@Utilities/logger';
-import { Typography } from 'antd';
-import { useEffect, useState, type FunctionComponent } from 'react';
+import { Typography, Card, Space } from 'antd';
+import { useEffect, useMemo, useState, type FunctionComponent } from 'react';
 
 const { LOG, ERROR } = Logger('time-tracking-by-event');
 
@@ -12,27 +13,46 @@ export interface TimeTrackingByEventProps {
 }
 
 const TimeTrackingByEvent: FunctionComponent<TimeTrackingByEventProps> = (props) => {
-  const [loadedCount, setLoadedCount] = useState(0);
+  const [loadSessionPayloadItems, setLoadSessionPayloadItems] = useState<SessionPayload[]>([]);
 
   useEffect(() => {
+    LOG(props.eventId);
     (async () => {
       //
       const localRef = await fireRealtime.ref(`user_sessions/local/${props.userId}`);
       const result = await localRef.get();
       LOG(result.exists())
       if (result.exists()) {
-        const documents: any[] = result.val();
-        setLoadedCount(documents.length);
+        const allDocuments: {[x: string]: SessionPayload} = result.val();
+        LOG(allDocuments);
+        // Load all logs that its data.eventId does match with the eventId prop.
+        setLoadSessionPayloadItems(
+          Object.values(allDocuments).filter((document) => document.data?.eventId === props.eventId),
+        );
       }
     })();
   }, []);
 
+  const loggedSeconds = useMemo(() => {
+    return loadSessionPayloadItems.map((log) => (log.endTimestamp - log.startTimestamp) / 1000).reduce((a, b) => a+b, 0);
+  }, [loadSessionPayloadItems]);
+
+  const loggedHours = useMemo(() => loggedSeconds/3600, [loggedSeconds]);
+
   return (
-    <div>
-      <Typography.Text strong>{props.eventName}</Typography.Text>
-      {': '}
-      {loadedCount === 0 ? 'Sin datos' : `${loadedCount} registros encontrados`}
-    </div>
+    <Card title={props.eventName}>
+      <Space direction='vertical'>
+        <Typography.Text>
+          {loadSessionPayloadItems.length === 0 ? 'Sin datos' : `${loadSessionPayloadItems.length} registros encontrados`}
+        </Typography.Text>
+        {loadSessionPayloadItems.length > 0 && (
+          <Space direction='horizontal'>
+          <Card>{loggedSeconds.toPrecision(4)} segundos</Card>
+          <Card>{loggedHours.toPrecision(2)} horas</Card>
+          </Space>
+        )}
+      </Space>
+    </Card>
   );
 };
 
