81fa116953c5f6766b9ebdcf09a98543556b3a16
David Borja <davidborja456@gmail.com>
2022-10-21 13:56:44 -0500

    Update organization time tracking to show data in tables

--- src/pages/eventOrganization/timetracking/OrganizationTimeTrackingPage.tsx
+++ src/pages/eventOrganization/timetracking/OrganizationTimeTrackingPage.tsx
@@ -1,11 +1,11 @@
 import { useState, useEffect, useMemo, type FunctionComponent, useCallback } from 'react';
-import { Avatar, Card, Divider, Result, Select, Space, Typography, Table } from 'antd';
+import { Avatar, Card, Divider, Result, Select, Space, Typography, Table, Modal, Button, Tooltip } from 'antd';
 import { ColumnsType } from 'antd/es/table';
 import { fireRealtime } from '@helpers/firebase';
 import Logger from '@Utilities/logger';
 import Online from '@components/online/Online';
-import { OrganizationApi, UsersApi } from '@helpers/request';
-import { LoadingOutlined, UserOutlined } from '@ant-design/icons';
+import { AgendaApi, OrganizationApi, UsersApi } from '@helpers/request';
+import { LoadingOutlined, UnorderedListOutlined, UserOutlined } from '@ant-design/icons';
 import { type SessionPayload } from '@components/presence/types';
 
 const { LOG, ERROR, WARN } = Logger('time-tracking-page');
@@ -16,7 +16,15 @@ type TimeInfo = {
 }
 
 type RowDataByEvent = {
-  key: string,
+  key: string;
+  name: string;
+  logCount: number;
+  timeInfo: TimeInfo;
+  event: any;
+};
+
+type RowDataByActivity = {
+  key: string;
   name: string;
   logCount: number;
   timeInfo: TimeInfo;
@@ -33,27 +41,6 @@ export type OrganizationUserInfo = {
   picture?: string,
 };
 
-const columns: ColumnsType<RowDataByEvent> = [
-  {
-    title: 'Evento',
-    dataIndex: 'name',
-    key: 'name',
-  },
-  {
-    title: 'Ingresos',
-    dataIndex: 'logCount',
-    key: 'logCount',
-  },
-  {
-    title: 'Tiempo',
-    dataIndex: 'timeInfo',
-    key: 'timeInfo',
-    render: (item: TimeInfo) => {
-      return `${item.time.toFixed(3)} ${item.description}`;
-    },
-  },
-];
-
 const OrganizationTimeTrackingPage: FunctionComponent<OrganizationTimeTrackingPageProps> = (props) => {
   const {
     org: organization,
@@ -64,8 +51,83 @@ const OrganizationTimeTrackingPage: FunctionComponent<OrganizationTimeTrackingPa
   const [isLoading, setIsLoading] = useState(true);
   const [userInfo, setUserInfo] = useState({} as OrganizationUserInfo);
   const [logs, setLogs] = useState<SessionPayload[]>([]);
+  const [globalLogs, setGlobalLogs] = useState<SessionPayload[]>([]);
+  const [localLogs, setLocalLogs] = useState<SessionPayload[]>([]);
   const [timeMode, setTimeMode] = useState<'minutes'|'hours'|'days'>('minutes');
-  const [dataSource, setDataSource] = useState<RowDataByEvent[]>([]);
+  const [eventDataSource, setEventDataSource] = useState<RowDataByEvent[]>([]);
+  const [activityDataSource, setActivityDataSource] = useState<RowDataByActivity[]>([]);
+  const [isModalShown, setIsModalShown] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState<any>();
+
+  const openModal = useCallback(() => setIsModalShown(true), []);
+  const closeModal = useCallback(() => setIsModalShown(false), []);
+
+  const closeModalAndResetSelectedEvent = () => {
+    closeModal();
+    setSelectedEvent(undefined);
+  }
+
+  const eventColumns: ColumnsType<RowDataByEvent> = [
+    {
+      title: 'Evento',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Ingresos',
+      dataIndex: 'logCount',
+      key: 'logCount',
+    },
+    {
+      title: 'Tiempo',
+      dataIndex: 'timeInfo',
+      key: 'timeInfo',
+      render: (item: TimeInfo) => {
+        return `${item.time.toFixed(3)} ${item.description}`;
+      },
+    },
+    {
+      title: 'Opción',
+      dataIndex: 'event',
+      key: 'event',
+      render: (item: any) => {
+        return (
+          <Tooltip title='Abrir información de esta actividad'>
+            <Button
+              icon={<UnorderedListOutlined />}
+              type='primary'
+              size='small'
+              onClick={() => {
+                setSelectedEvent(item);
+                openModal();
+              }}
+            ></Button>
+          </Tooltip>
+        );
+      },
+    }
+  ];
+
+  const activityColumns: ColumnsType<RowDataByActivity> = [
+    {
+      title: 'Actividad',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Ingresos',
+      dataIndex: 'logCount',
+      key: 'logCount',
+    },
+    {
+      title: 'Tiempo',
+      dataIndex: 'timeInfo',
+      key: 'timeInfo',
+      render: (item: TimeInfo) => {
+        return `${item.time.toFixed(3)} ${item.description}`;
+      },
+    },
+  ];
 
   useEffect(() => {
     if (!memberId) return;
@@ -100,13 +162,8 @@ const OrganizationTimeTrackingPage: FunctionComponent<OrganizationTimeTrackingPa
         const globalRef = await fireRealtime.ref(`user_sessions/global/${member.user._id}`);
         const result = await globalRef.get();
         const logDict = (result && result.exists()) ? result.val() : {};
-        LOG(logDict);
-        const filteredLogDict = Object.values(logDict)
-          .filter((log: any) => log.status === 'offline')
-          .filter((log: any) => log.startTimestamp !== undefined)
-          .filter((log: any) => log.endTimestamp !== undefined);
-        LOG(filteredLogDict.length, 'logs loaded', filteredLogDict);
-        setLogs(filteredLogDict as SessionPayload[]);
+        LOG('logDict', logDict);
+        setGlobalLogs(Object.values(logDict));
       } catch (err) {
         ERROR('Cannot get the organization member to orgId and userId:', organization._id, memberId, 'getting:', err);
       }
@@ -127,45 +184,104 @@ const OrganizationTimeTrackingPage: FunctionComponent<OrganizationTimeTrackingPa
         LOG('no have user sessions for local');
         return;
       }
-
       const allDocuments: {[x: string]: SessionPayload} = result.val();
-
-      const tableList: typeof dataSource = [];
-
       // We need all logs that are offline, have time, are type activity
-      const filteredAllDocuments = Object.values(allDocuments)
-        .filter((document) => document.status === 'offline')
-        .filter((document) => document.startTimestamp !== undefined)
-        .filter((document) => document.endTimestamp !== undefined)
-        .filter((document) => document.data?.type === 'activity')
-        .filter((document) => document.data?.eventId !== undefined)
-        .filter((document) => document.data?.activityId !== undefined)
-
-      for (const event of events) {
-        const logsByEvent = filteredAllDocuments
-          .filter((document) => document.data?.eventId === event._id)
-        LOG(event._id, 'has', logsByEvent.length, 'logs');
-
-        // Create stats
-        const rowData: RowDataByEvent = {
-          key: event._id,
-          name: event.name,
-          logCount: logsByEvent.length,
-          timeInfo: processTime(logsByEvent, timeMode),
-        }
-        tableList.push(rowData);
-      }
-
-      const sorttedTableList = tableList.sort((a, b) => {
-        if (a.timeInfo.time > b.timeInfo.time) return 1;
-        if (a.timeInfo.time < b.timeInfo.time) return -1;
-        return 0;
-      }).reverse();
-
-      setDataSource(sorttedTableList);
+      setLocalLogs(Object.values(allDocuments));
     };
     request().catch((err) => ERROR('error to request events', err));
-  }, [events, timeMode]);
+  }, [events, userId]);
+
+  useEffect(() => {
+    LOG('update from globalLogs');
+    const filteredLogDict = globalLogs
+      .filter((log: any) => log.status === 'offline')
+      .filter((log: any) => log.startTimestamp !== undefined)
+      .filter((log: any) => log.endTimestamp !== undefined);
+    LOG(filteredLogDict.length, 'logs loaded', filteredLogDict);
+    setLogs(filteredLogDict as SessionPayload[]);
+  }, [globalLogs]);
+
+  useEffect(() => {
+    LOG('update from localLogs');
+    const tableList: typeof eventDataSource = [];
+    const filteredAllDocuments = localLogs
+      .filter((document) => document.status === 'offline')
+      .filter((document) => document.startTimestamp !== undefined)
+      .filter((document) => document.endTimestamp !== undefined)
+      .filter((document) => document.data?.type === 'activity')
+      .filter((document) => document.data?.eventId !== undefined)
+      .filter((document) => document.data?.activityId !== undefined)
+
+    for (const event of events) {
+      const logsByEvent = filteredAllDocuments
+        .filter((document) => document.data?.eventId === event._id)
+      LOG(event._id, 'has', logsByEvent.length, 'logs');
+
+      // Create stats
+      const rowData: RowDataByEvent = {
+        key: event._id,
+        name: event.name,
+        logCount: logsByEvent.length,
+        timeInfo: processTime(logsByEvent, timeMode),
+        event: event,
+      }
+      tableList.push(rowData);
+    }
+
+    const sorttedTableList = tableList.sort((a, b) => {
+      if (a.timeInfo.time > b.timeInfo.time) return 1;
+      if (a.timeInfo.time < b.timeInfo.time) return -1;
+      return 0;
+    }).reverse();
+
+    setEventDataSource(sorttedTableList);
+  }, [localLogs, timeMode]);
+
+  useEffect(() => {
+    if (!selectedEvent) return;
+
+    const filteredLocalDocuments = localLogs //
+      .filter((document) => document.status === 'offline')
+      .filter((document) => document.startTimestamp !== undefined)
+      .filter((document) => document.endTimestamp !== undefined)
+      .filter((document) => document.data?.type === 'activity')
+      .filter((document) => document.data?.eventId !== undefined)
+      .filter((document) => document.data?.activityId !== undefined)
+
+    const request = async () => {
+      const currentEvent = events.filter((event) => event._id === selectedEvent._id)[0];
+      if (!currentEvent) {
+        ERROR('cannot get the event', selectedEvent);
+        return;
+      }
+
+      // Get only the logs for the current selected event
+      const eventLogs = filteredLocalDocuments //
+        .filter((document) => document.data?.eventId === selectedEvent._id);
+      
+      // Get all the activities
+      const { data: activities } = await AgendaApi.byEvent(selectedEvent._id);
+
+      const dataRows: typeof activityDataSource = [];
+
+      for (const activity of activities) {
+        // Filter by activity ID
+        const itsLogs = eventLogs.filter((log) => log.data?.activityId === activity._id);
+        // Calc stuffs with this set of logs
+        const dataRow: RowDataByActivity = {
+          key: activity._id,
+          logCount: itsLogs.length,
+          name: activity.name,
+          timeInfo: processTime(itsLogs, timeMode),
+        };
+        dataRows.push(dataRow);
+      }
+
+      setActivityDataSource(dataRows);
+      LOG('dataRows', dataRows);
+    };
+    request().catch((err) => ERROR('error to request activities', err));
+  }, [localLogs, selectedEvent, timeMode]);
 
   /**
    * Given logs and timeMode, will return an object with information about time
@@ -255,10 +371,30 @@ const OrganizationTimeTrackingPage: FunctionComponent<OrganizationTimeTrackingPa
         {events.length === 0 ? (
           <Typography.Text italic>Sin datos.</Typography.Text>
         ) : (
-          <Table columns={columns} dataSource={dataSource} />
+          <Table columns={eventColumns} dataSource={eventDataSource} />
         )}
       </Space>
     </Card>
+    <Modal
+      visible={isModalShown}
+      title={selectedEvent?.name && `Evento: ${selectedEvent.name}` || 'Cargando...'}
+      footer={null}
+      onCancel={closeModalAndResetSelectedEvent}
+      onOk={closeModalAndResetSelectedEvent}
+    >
+      {(activityDataSource.length) ? (
+        <Table
+          columns={activityColumns}
+          dataSource={activityDataSource}
+        />
+      ) : (
+        <Result
+          title='Cargando...'
+          subTitle={'Recuperando información del evento'}
+          icon={<LoadingOutlined/>}
+        />
+      )}
+    </Modal>
     </>
   );
 };
