087a725d12744603f214286196be2626b2d25b15
Cristian Florez <cristian@fabioarias.co>
2022-10-24 09:10:49 -0500

    [ADD] pdf in base64

--- src/components/games/bingo/components/PrintCardBoard.tsx
+++ src/components/games/bingo/components/PrintCardBoard.tsx
@@ -5,13 +5,12 @@ import PrinterIcon from '@2fd/ant-design-icons/lib/Printer';
 import { useState } from 'react';
 import { jsPDF } from 'jspdf';
 import html2canvas from 'html2canvas';
-
+import html2pdf from 'html2pdf';
 export default function PrintCardBoard({ bingoCardRef, cardboardCode }: { bingoCardRef: any; cardboardCode: string }) {
   const [loading, setLoading] = useState(false);
   const handlePrintHook = useReactToPrint({
     content: () => bingoCardRef.current,
     documentTitle: `Bingo ID: ${cardboardCode}`,
-
     onAfterPrint: () => setLoading(false),
   });
 
@@ -27,28 +26,25 @@ export default function PrintCardBoard({ bingoCardRef, cardboardCode }: { bingoC
       const document = printIframe.contentDocument;
       if (document) {
         const html = document.getElementsByTagName('html')[0];
-        console.log(html);
-        html2canvas(html).then((canvas) => {
-          const imgData = canvas.toDataURL('image/png');
-          const pdf = new jsPDF();
-          {
-            /*
-          ts-ignore
-        */
-          }
-          pdf.addImage(imgData, 'JPEG', 0, 0);
-          // pdf.output('dataurlnewwindow');
-          pdf.save('download.pdf');
+        const canvas = await html2canvas(html, {
+          logging: false,
+          windowWidth: html.scrollWidth,
+          windowHeight: html.scrollHeight,
+          allowTaint: true,
+          useCORS: true,
         });
+        const data = canvas.toDataURL('image/png');
+        console.log('base64', data);
+        // pdf.output('dataurlnewwindow');
       }
     },
   });
   return (
     <>
       <Space>
-        {/*  <Button loading={loading} disabled={loading} onClick={handleShare} icon={<DownloadIcon />}>
+        <Button loading={loading} disabled={loading} onClick={handleShare} icon={<DownloadIcon />}>
           Descargar
-        </Button> */}
+        </Button>
         <Button loading={loading} disabled={loading} onClick={() => onHandlePrint()}>
           <Space split='/'>
             <DownloadIcon />
