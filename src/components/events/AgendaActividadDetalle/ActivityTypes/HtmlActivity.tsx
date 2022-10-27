6e55ccdfa915957f78f67eb419cac31274a6d76d
David Borja <davidborja456@gmail.com>
2022-10-26 19:06:45 -0500

    Update HtmlActivity to show the HTML content

--- src/components/events/AgendaActividadDetalle/ActivityTypes/HtmlActivity.tsx
+++ src/components/events/AgendaActividadDetalle/ActivityTypes/HtmlActivity.tsx
@@ -1,30 +1,32 @@
-import { useState, useEffect, useRef, type FunctionComponent } from 'react';
+import { useState, useEffect, useContext, useRef, type FunctionComponent } from 'react';
 import { useHelper } from '@context/helperContext/hooks/useHelper';
+import AgendaContext from '@context/AgendaContext';
 import HeaderColumnswithContext from '../HeaderColumns';
 
 export interface HtmlActivityProps {};
 
 const HtmlActivity: FunctionComponent<HtmlActivityProps> = () => {
-  const { currentActivity } = useHelper();
-
   const [activityState, setActivityState] = useState<any>();
   const [htmlData, setHtmlData] = useState<string>('');
 
   const ref = useRef<HTMLDivElement>(null);
+  const cAgenda = useContext(AgendaContext);
 
   useEffect(() => {
-    setActivityState(currentActivity);
-  }, [currentActivity]);
+    setActivityState(cAgenda);
+    console.log('currentActivity', cAgenda);
+  }, [cAgenda]);
   
   useEffect(() => {
     if (!activityState) return;
     setHtmlData(activityState.meeting_id || '<em>Contenido no <b>establecido</b></em>.');
-  }, [activityState]);
+  }, [activityState?.meeting_id]);
 
   useEffect(() => {
     if (!ref.current) return;
     if (!htmlData.trim()) return;
     if (htmlData !== ref.current.innerHTML) {
+      console.debug('Update innerHTML');
       ref.current.innerHTML = htmlData;
     }
   }, [ref.current, htmlData]);
