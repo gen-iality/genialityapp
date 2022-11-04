4534189a3bc1f8e9c6c7bb756dfd860166ea7050
Marlon Carrillo <marloncb2012@gmail.com>
2022-11-04 11:00:39 -0500

    Animacion al perder vidas

--- src/components/games/whereIs/components/landing/Lifes.tsx
+++ src/components/games/whereIs/components/landing/Lifes.tsx
@@ -5,32 +5,32 @@ import useWhereIs from '../../hooks/useWhereIs';
 import useWhereIsInLanding from '../../hooks/useWhereIsInLanding';
 
 export default function Lifes() {
-	const { whereIs } = useWhereIs();
-	const {
-		whereIsGame: { lifes },
-		location,
-	} = useWhereIsInLanding();
+  const { whereIs } = useWhereIs();
+  const {
+    whereIsGame: { lifes },
+    location,
+  } = useWhereIsInLanding();
 
-	if (whereIs === null) return null;
+  if (whereIs === null) return null;
 
-	if (location.activeView !== 'game') return null;
+  if (location.activeView !== 'game') return null;
 
-	return (
-		<Space>
-			{Array.from({ length: whereIs.lifes })
-				.map((_, i) => i)
-				.sort((a, b) => b - a)
-				.map(life =>
-					life < lifes ? (
-						<CardsHeartIcon
-							className='animate__animated animate__heartBeat'
-							key={`life-${life}`}
-							style={{ color: 'red', fontSize: '25px' }}
-						/>
-					) : (
-						<HeartBrokenIcon key={`life-${life}`} style={{ color: 'gray', fontSize: '25px' }} />
-					)
-				)}
-		</Space>
-	);
+  return (
+    <Space>
+      {Array.from({ length: whereIs.lifes })
+        .map((_, i) => i)
+        .sort((a, b) => b - a)
+        .map((life) =>
+          life < lifes ? (
+            <CardsHeartIcon key={`life-${life}`} style={{ color: 'red', fontSize: '25px' }} />
+          ) : (
+            <HeartBrokenIcon
+              className='animate__animated animate__heartBeat'
+              key={`life-${life}`}
+              style={{ color: 'gray', fontSize: '25px' }}
+            />
+          )
+        )}
+    </Space>
+  );
 }
