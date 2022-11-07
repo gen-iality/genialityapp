ec6e499a49545e214feda722d4fc5f7f59a824a6
Marlon Carrillo <marloncb2012@gmail.com>
2022-11-07 13:11:42 -0500

    Ajustes para las clases de estilos usadas en el ranking para dinamicas

--- src/components/games/common/Ranking/RankingList.tsx
+++ src/components/games/common/Ranking/RankingList.tsx
@@ -5,39 +5,39 @@ import { useState } from 'react';
 import { Score } from './RankingMyScore';
 
 interface Props {
-	scores: Score[];
-	type: 'time' | 'points';
+  scores: Score[];
+  type: 'time' | 'points';
 }
 
 export default function RankingList(props: Props) {
-	const { scores, type } = props;
-	const cEvent = UseEventContext();
-	const [loading, setLoading] = useState(false);
-	const styles = cEvent.value.styles;
+  const { scores, type } = props;
+  const cEvent = UseEventContext();
+  const [loading, setLoading] = useState(false);
+  const styles = cEvent.value.styles;
 
-	return (
-		<div style={{ marginTop: 16 }}>
-			<Row justify='center'>
-				<h1
-					style={{
-						fontSize: '25px',
-						fontWeight: 'bold',
-						lineHeight: '3px',
-						color: `${styles && styles.textMenu}`,
-					}}>
-					Ranking
-				</h1>
-				<Divider style={{ backgroundColor: `${styles && styles.textMenu}` }} />
-			</Row>
-			<div className='container-ranking' style={{ marginTop: 16, height: 'auto', overflowY: 'auto' }}>
-				<List
-					className='demo-loadmore-list'
-					loading={loading}
-					itemLayout='horizontal'
-					dataSource={scores}
-					renderItem={(item, key) => <UsersCard type='ranking' item={item} isTime={type === 'time'} />}
-				/>
-			</div>
-		</div>
-	);
+  return (
+    <div style={{ marginTop: 16 }}>
+      <Row justify='center'>
+        <h1
+          style={{
+            fontSize: '25px',
+            fontWeight: 'bold',
+            lineHeight: '3px',
+            color: `${styles && styles.textMenu}`,
+          }}>
+          Ranking
+        </h1>
+        <Divider style={{ backgroundColor: `${styles && styles.textMenu}` }} />
+      </Row>
+      <div className='container-dinamic-ranking' style={{ marginTop: 16, height: 'auto', overflowY: 'auto' }}>
+        <List
+          className='demo-loadmore-list'
+          loading={loading}
+          itemLayout='horizontal'
+          dataSource={scores}
+          renderItem={(item, key) => <UsersCard type='ranking' item={item} isTime={type === 'time'} />}
+        />
+      </div>
+    </div>
+  );
 }
--- src/components/games/common/Ranking/index.tsx
+++ src/components/games/common/Ranking/index.tsx
@@ -6,32 +6,32 @@ import RankingList from './RankingList';
 import RankingMyScore, { Score } from './RankingMyScore';
 
 export default function Ranking() {
-	const cUser = UseUserEvent();
-	const [scores, setScores] = useState<Score[]>([]);
-	const [myScore, setMyScore] = useState<Score>({} as Score);
-	const { getPlayer, getScores, player } = useWhereIsInLanding();
+  const cUser = UseUserEvent();
+  const [scores, setScores] = useState<Score[]>([]);
+  const [myScore, setMyScore] = useState<Score>({} as Score);
+  const { getPlayer, getScores, player } = useWhereIsInLanding();
 
-	useEffect(() => {
-		getScores().then(({ scoresFinished, scoresNotFinished }) => {
-			// const myIndexScore = scores.findIndex(score => score.uid === cUser.value._id);
-			const myScoreWin = scoresFinished.find(score => score.uid === (cUser.value._id as string));
-			const myScoreLose = scoresNotFinished.find(score => score.uid === (cUser.value._id as string));
-			if (myScoreWin) {
-				setMyScore(myScoreWin);
-			}
-			if (myScoreLose) {
-				setMyScore(myScoreLose);
-			}
-			// const scoresFinished = scoresFinished.filter(score => score.isFinish === true);
-			setScores(scoresFinished);
-		});
-	}, []);
-	return (
-		<>
-			{myScore.uid && <RankingMyScore myScore={myScore} type='time' />}
-			{/* {!myScore.isFinish && <p>Lo sentimos, perdiste</p>} */}
-			<Divider />
-			{!!scores.length && <RankingList scores={scores} type='time' />}
-		</>
-	);
+  useEffect(() => {
+    getScores().then(({ scoresFinished, scoresNotFinished }) => {
+      // const myIndexScore = scores.findIndex(score => score.uid === cUser.value._id);
+      const myScoreWin = scoresFinished.find((score) => score.uid === (cUser.value._id as string));
+      const myScoreLose = scoresNotFinished.find((score) => score.uid === (cUser.value._id as string));
+      if (myScoreWin) {
+        setMyScore(myScoreWin);
+      }
+      if (myScoreLose) {
+        setMyScore(myScoreLose);
+      }
+      // const scoresFinished = scoresFinished.filter(score => score.isFinish === true);
+      setScores(scoresFinished);
+    });
+  }, []);
+  return (
+    <div style={{ height: '80vh', overflowY: 'auto' }}>
+      {myScore.uid && <RankingMyScore myScore={myScore} type='time' />}
+      {/* {!myScore.isFinish && <p>Lo sentimos, perdiste</p>} */}
+      <Divider />
+      {!!scores.length && <RankingList scores={scores} type='time' />}
+    </div>
+  );
 }
