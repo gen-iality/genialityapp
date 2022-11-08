b787eff3d0250c60ce0623b9bb05e76f3aea3827
Juan Camayo <jd.camayo@gmail.com>
2022-11-08 17:47:55 -0500

    Fix - se corrigio vista de resultados en la dinamica

--- src/components/games/whereIs/views/landing/Results.tsx
+++ src/components/games/whereIs/views/landing/Results.tsx
@@ -6,52 +6,53 @@ import Ranking from '@/components/games/common/Ranking';
 import { parseTime } from '../../utils/parseTime';
 
 const DataTimerResult = ({ time = 0, won }: { time?: number; won: boolean }) => {
-  return (
-    <Tag
-      style={{ padding: '5px 10px', fontSize: '16px' }}
-      icon={<TimerOutlineIcon style={{ fontSize: '16px' }} />}
-      color={won ? 'success' : 'error' /* Si gano 'success', si perdio 'error' */}>
-      {parseTime(time)}
-    </Tag>
-  );
+	return (
+		<Tag
+			style={{ padding: '5px 10px', fontSize: '16px' }}
+			icon={<TimerOutlineIcon style={{ fontSize: '16px' }} />}
+			color={won ? 'success' : 'error' /* Si gano 'success', si perdio 'error' */}>
+			{parseTime(time)}
+		</Tag>
+	);
 };
 
 export default function Results() {
-  const {
-    whereIsGame: { won },
-    player,
-  } = useWhereIsInLanding();
-  const dataResult = {
-    winner: {
-      icon: ' ',
-      title: '¡Felicitaciones!',
-      msg:
-        'Has logrado el objetivo de la dinámica, esperamos te hayas divertido. Hemos registrado tu tiempo correctamente.',
-      // extra: <DataTimerResult time={player?.duration && 0} />,
-    },
-    loser: {
-      icon: <HeartBrokenIcon style={{ color: 'gray' }} />,
-      title: 'Lo sentimos',
-      msg:
-        'Te quedaste sin vidas para continuar con la búsqueda, esperamos te hayas divertido, en otra oportunidad será.',
-      // extra: <DataTimerResult time={player?.duration && 0} />,
-    },
-  };
-  return (
-    <Row justify='center' align='middle'>
-      <Col xs={24} sm={24} md={14} lg={14} xl={14} xxl={14}>
-        <Result
-          icon={won ? dataResult.winner.icon : dataResult.loser.icon}
-          title={won ? dataResult.winner.title : dataResult.loser.title}
-          subTitle={won ? dataResult.winner.msg : dataResult.loser.msg}
-          extra={<DataTimerResult time={player?.duration} won={won} />}
-        />
-      </Col>
-      <Col xs={24} sm={24} md={10} lg={10} xl={10} xxl={10}>
-        <Card bodyStyle={{ padding: '10px' }}>
-          <Ranking />
-        </Card>
-      </Col>
-    </Row>
-  );
+	const {
+		whereIsGame: { won },
+		player,
+	} = useWhereIsInLanding();
+	const dataResult = {
+		winner: {
+			icon: ' ',
+			title: '¡Felicitaciones!',
+			msg:
+				'Has logrado el objetivo de la dinámica, esperamos te hayas divertido. Hemos registrado tu tiempo correctamente.',
+			// extra: <DataTimerResult time={player?.duration && 0} />,
+		},
+		loser: {
+			icon: <HeartBrokenIcon style={{ color: 'gray' }} />,
+			title: 'Lo sentimos',
+			msg:
+				'Te quedaste sin vidas para continuar con la búsqueda, esperamos te hayas divertido, en otra oportunidad será.',
+			// extra: <DataTimerResult time={player?.duration && 0} />,
+		},
+	};
+	return (
+		<Row justify='center' align='middle'>
+			<Col xs={24} sm={24} md={14} lg={14} xl={14} xxl={14}>
+				<Result
+					icon={player?.isFinish ? dataResult.winner.icon : dataResult.loser.icon}
+					title={player?.isFinish ? dataResult.winner.title : dataResult.loser.title}
+					subTitle={player?.isFinish ? dataResult.winner.msg : dataResult.loser.msg}
+					extra={<DataTimerResult time={player?.duration} won={player?.isFinish ?? false} />}
+				/>
+			</Col>
+
+			<Col xs={24} sm={24} md={10} lg={10} xl={10} xxl={10}>
+				<Card bodyStyle={{ padding: '10px' }}>
+					<Ranking />
+				</Card>
+			</Col>
+		</Row>
+	);
 }
