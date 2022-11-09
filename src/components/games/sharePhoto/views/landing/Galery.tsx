79a8e5a93834db6b4913e418c368d304a81dfc27
Marlon Carrillo <marloncb2012@gmail.com>
2022-11-09 14:17:46 -0500

    Estilos para la grid y las imagenes en la galeria y el visualizador individual de las imagenes

--- src/components/games/sharePhoto/views/landing/Galery.tsx
+++ src/components/games/sharePhoto/views/landing/Galery.tsx
@@ -1,21 +1,5 @@
 import { HeartFilled, UserOutlined } from '@ant-design/icons';
-import {
-  Avatar,
-  Badge,
-  Button,
-  Card,
-  Col,
-  Comment,
-  Drawer,
-  Grid,
-  Image,
-  Input,
-  PageHeader,
-  Row,
-  Space,
-  Typography,
-} from 'antd';
-import moment from 'moment';
+import { Badge, Button, Card, Col, Drawer, Grid, Image, Input, PageHeader, Row, Space, Typography } from 'antd';
 import { useEffect, useState } from 'react';
 import useSharePhoto from '../../hooks/useSharePhoto';
 import useSharePhotoInLanding from '../../hooks/useSharePhotoInLanding';
@@ -40,6 +24,10 @@ export default function Galery() {
     }
   };
 
+  const handlerDoubleClick = (id: string) => {
+    addLike(id);
+  };
+
   // useEffect(() => {
   //   const postUpdated = sharePhoto?.posts.find((post) => post.id === postSelected?.id);
   //   if (postUpdated) {
@@ -58,62 +46,84 @@ export default function Galery() {
       {postSelected && (
         <Drawer
           bodyStyle={{ padding: screens.xs ? '0px' : '0' }}
-          width={screens.xs ? '100vw' : '40vw'}
+          width={screens.xs ? '100vw' : '35vw'}
           visible={postSelected ? true : false}
-          onClose={() => handleBack()}>
-          <Row gutter={[0, 0]}>
-            <Col span={24}>
-              <PageHeader
-                style={{ padding: '0px 10px' }}
-                avatar={{ icon: <UserOutlined />, src: postSelected.picture }}
-                title={
-                  <Typography.Text type='secondary' style={{ fontSize: screens.xs ? '16px' : '18px' }}>
-                    {postSelected.user_name}
-                  </Typography.Text>
-                }
-                children={<Typography.Title level={4}>{postSelected.title}</Typography.Title>}
+          onClose={() => handleBack()}
+          title={''}
+          extra={
+            <PageHeader
+              style={{ padding: '0px 0px' }}
+              avatar={{ icon: <UserOutlined />, src: postSelected.picture }}
+              title={
+                <Typography.Text type='secondary' style={{ fontSize: screens.xs ? '16px' : '18px' }}>
+                  {postSelected.user_name}
+                </Typography.Text>
+              }
+              /* children={<Typography.Title level={4}>{postSelected.title}</Typography.Title>} */
+            />
+          }
+          footerStyle={{ border: 'none' }}
+          footer={
+            <Space>
+              <Button
+                size='large'
+                style={{ width: '100%', height: '100%', padding: 2, margin: 2, border: 'none' }}
+                danger
+                ghost
+                // onClick={() => console.log('postId', postSelected?.id)}
+                onClick={() => addLike(postSelected.id)}
+                icon={<HeartFilled style={{ fontSize: '35px' }} />}
               />
-            </Col>
-
+              <Badge
+                overflowCount={99999}
+                style={{
+                  fontSize: '28px',
+                  height: 'auto',
+                  width: 'auto',
+                  color: '#000000',
+                  backgroundColor: 'transparent',
+                }}
+                count={postSelected.likes.length}></Badge>
+            </Space>
+          }>
+          <Row gutter={[0, 0]}>
             <Col span={24}>
               <Card
                 style={{ width: '100%' }}
                 bodyStyle={{ padding: '10px 10px' }}
                 bordered={false}
                 cover={
-                  <Image
-                    width={'100%'}
-                    style={{ maxHeight: '450px', minHeight: '450px', objectFit: 'cover', borderRadius: '0px' }}
-                    src={postSelected.image}
-                    preview={false}
-                  />
-                }>
-                <Space>
-                  <Button
-                    size='large'
-                    style={{ width: '100%', height: '100%', padding: 2, margin: 2, border: 'none' }}
-                    danger
-                    ghost
-                    // onClick={() => console.log('postId', postSelected?.id)}
-                    onClick={() => addLike(postSelected.id)}
-                    icon={<HeartFilled style={{ fontSize: '40px' }} />}
-                  />
-                  <Badge
-                    overflowCount={99999}
+                  <div
                     style={{
-                      fontSize: '28px',
-                      height: 'auto',
-                      width: 'auto',
-                      color: '#000000',
-                      backgroundColor: '#FFFFFF',
-                    }}
-                    count={postSelected.likes.length}></Badge>
-                </Space>
+                      backgroundImage: `url(${postSelected.image})`,
+                      backgroundRepeat: 'no-repeat',
+                      backgroundSize: 'cover',
+                      backgroundPosition: 'center',
+                    }}>
+                    <Image
+                      /*  onDoubleClick={() => handlerDoubleClick(postSelected.id)} */
+                      width={'100%'}
+                      style={{
+                        minHeight: '420px',
+                        objectFit: screens.xs ? 'contain' : 'unset',
+                        backgroundColor: '#00000033',
+                        backdropFilter: 'blur(8px)',
+                      }}
+                      src={postSelected.image}
+                      preview={false}
+                    />
+                  </div>
+                }>
+                <Typography.Title level={5}>{postSelected.title}</Typography.Title>
               </Card>
             </Col>
           </Row>
         </Drawer>
       )}
+      {/* ==================================================================================
+                                             GALLERY 
+          ==================================================================================
+      */}
       <Row align='middle' justify='center'>
         <Col>
           <Row align='middle' justify='center'>
@@ -128,7 +138,7 @@ export default function Galery() {
           </Row>
         </Col>
       </Row>
-      <Row gutter={[16, 0]}>
+      <Row gutter={[0, 0]}>
         {sharePhoto?.posts.map((post) => (
           <Col
             key={post.id}
@@ -142,15 +152,31 @@ export default function Galery() {
             xl={4}
             xxl={4}
             style={{
-              backgroundImage: `url(${post.thumb || 'https://via.placeholder.com/500/?text=Error'})`,
+              /* backgroundImage: `url(${post.thumb || 'https://via.placeholder.com/500/?text=Error'})`,
               backgroundSize: 'cover',
               backgroundRepeat: 'no-repeat',
               scrollSnapAlign: 'center',
-              backgroundPosition: 'center',
+              backgroundPosition: 'center', */
               height: '100%',
               aspectRatio: '4/4',
               padding: '0px',
             }}>
+            <Image
+              preview={false}
+              height={'100%'}
+              width={'100%'}
+              style={{ objectFit: 'cover', position: 'absolute', top: '0' }}
+              src={post.thumb}
+              fallback={'https://via.placeholder.com/500/?text=Image not found!'}
+              placeholder={
+                <Image
+                  preview={false}
+                  height={'100%'}
+                  width={'100%'}
+                  src={'https://via.placeholder.com/800/000000/FFFFFF/?text=Cargando'}
+                />
+              }
+            />
             <div
               style={{
                 width: '100%',
@@ -159,6 +185,8 @@ export default function Galery() {
                 backdropFilter: 'blur(2px)',
                 display: `${overlay === post.id ? 'block' : 'none'}`,
                 cursor: 'pointer',
+                position: 'absolute',
+                top: '0',
               }}>
               <Row justify='center' align='middle' style={{ height: '100%' }}>
                 <Space style={{ fontSize: '20px', color: '#FFFFFF' }}>
