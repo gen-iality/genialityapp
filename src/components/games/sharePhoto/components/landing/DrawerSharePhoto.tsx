34003fbfa774454ff8b6d1851f9e4eb751cb0d94
Juan Camayo <jd.camayo@gmail.com>
2022-11-10 14:40:53 -0500

    Add - se agrego ranking al landing

--- src/components/games/sharePhoto/components/landing/DrawerSharePhoto.tsx
+++ src/components/games/sharePhoto/components/landing/DrawerSharePhoto.tsx
@@ -3,66 +3,62 @@ import { useEffect, useState } from 'react';
 import useSharePhoto from '../../hooks/useSharePhoto';
 import DrawerRanking from '../../views/landing/DrawerRanking';
 import GoBack from '../GoBack';
+import RankingDrawer from './RankingDrawer';
 
 interface Props {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }
 
 const { useBreakpoint } = Grid;
 
 export default function DrawerSharePhoto(props: Props) {
-  const [open, setOpen] = useState(false);
-  const { sharePhoto } = useSharePhoto();
-  const screens = useBreakpoint();
+	const [open, setOpen] = useState(false);
+	const { sharePhoto } = useSharePhoto();
+	const screens = useBreakpoint();
 
-  const handleOpen = () => {
-    setOpen(true);
-  };
+	const handleOpen = () => {
+		setOpen(true);
+	};
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+	const handleClose = () => {
+		setOpen(false);
+	};
 
-  useEffect(() => {
-    // if (sharePhoto !== null) {
-    // 	const unSubscribe = listenSharePhoto();
-    // 	return () => unSubscribe();
-    // }
-  }, [sharePhoto]);
+	useEffect(() => {
+		// if (sharePhoto !== null) {
+		// 	const unSubscribe = listenSharePhoto();
+		// 	return () => unSubscribe();
+		// }
+	}, [sharePhoto]);
 
-  if (sharePhoto === null) return null;
+	if (sharePhoto === null) return null;
 
-  return (
-    <>
-      <Button
-        size='large'
-        type='primary'
-        onClick={handleOpen}
-        style={{ display: sharePhoto.published ? 'block' : 'none' }}>
-        ¡Comparte tu foto 📷!
-      </Button>
-      <Drawer
-        title={
-          <Space>
-            <Button type='primary'>Ver Ranking</Button>
-            <DrawerRanking />
-          </Space>
-        }
-        extra={<GoBack />}
-        visible={open}
-        bodyStyle={{
-          // backgroundImage: `url(${cEvent.value?.styles?.BackgroundImage})`,
-          backgroundRepeat: 'no-repeat',
-          backgroundAttachment: 'fixed',
-          backgroundSize: 'cover',
-          paddingLeft: screens.xs ? '5px' : '24px',
-          paddingRight: screens.xs ? '5px' : '24px',
-        }}
-        onClose={handleClose}
-        width='100vw'
-        destroyOnClose={true}>
-        {sharePhoto.active ? props.children : <p>Esta dinamica no esta activa aun</p>}
-      </Drawer>
-    </>
-  );
+	return (
+		<>
+			<Button
+				size='large'
+				type='primary'
+				onClick={handleOpen}
+				style={{ display: sharePhoto.published ? 'block' : 'none' }}>
+				¡Comparte tu foto 📷!
+			</Button>
+			<Drawer
+				title={<RankingDrawer />}
+				extra={<GoBack />}
+				visible={open}
+				bodyStyle={{
+					// backgroundImage: `url(${cEvent.value?.styles?.BackgroundImage})`,
+					backgroundRepeat: 'no-repeat',
+					backgroundAttachment: 'fixed',
+					backgroundSize: 'cover',
+					paddingLeft: screens.xs ? '5px' : '24px',
+					paddingRight: screens.xs ? '5px' : '24px',
+				}}
+				onClose={handleClose}
+				width='100vw'
+				destroyOnClose={true}>
+				{sharePhoto.active ? props.children : <p>Esta dinamica no esta activa aun</p>}
+			</Drawer>
+		</>
+	);
 }
---
+++ src/components/games/sharePhoto/components/landing/RankingDrawer.tsx
@@ -0,0 +1,63 @@
+import Ranking from '@/components/games/common/Ranking';
+import { Button, Drawer, Grid, Space } from 'antd';
+import React, { useState } from 'react';
+
+const myScore = {
+	uid: '636c0c6d96121d4a4e3995a4',
+	imageProfile:
+		'https://firebasestorage.googleapis.com/v0/b/eviusauthdev.appspot.com/o/images%2F1664828271521.png?alt=media&token=dae70e80-45ea-4daa-9523-ed9bd0ffdf67',
+	score: '100',
+	name: 'Juan Camayo',
+	index: 1,
+};
+
+const scores = [
+	{
+		uid: '636c0c6d96121d4a4e3995a4',
+		imageProfile:
+			'https://firebasestorage.googleapis.com/v0/b/eviusauthdev.appspot.com/o/images%2F1664828271521.png?alt=media&token=dae70e80-45ea-4daa-9523-ed9bd0ffdf67',
+		score: '100',
+		name: 'Juan Camayo',
+		index: 1,
+	},
+	{
+		uid: '636bb6b521b48d56a8144709',
+		imageProfile:
+			'https://firebasestorage.googleapis.com/v0/b/eviusauthdev.appspot.com/o/images%2F1664828271521.png?alt=media&token=dae70e80-45ea-4daa-9523-ed9bd0ffdf67',
+		score: '50',
+		name: 'Juan Camayo',
+		index: 2,
+	},
+];
+
+const type: 'time' | 'points' = 'points';
+
+const { useBreakpoint } = Grid;
+
+export default function RankingDrawer() {
+	const [open, setOpen] = useState(false);
+	const handleOpen = () => {
+		setOpen(true);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+	const screens = useBreakpoint();
+
+	return (
+		<Space>
+			<Button type='primary' onClick={handleOpen}>
+				Ver Ranking
+			</Button>
+			<Drawer
+				title='Ranking'
+				bodyStyle={{ padding: '0px' }}
+				width={screens.xs ? '100vw' : '35vw'}
+				visible={open}
+				onClose={handleClose}>
+				<Ranking scores={scores} myScore={myScore} withMyScore={true} type='points' />
+			</Drawer>
+		</Space>
+	);
+}
