02eb6f4608c6c41c27fb890294c45d14eec59fed
Marlon Carrillo <marloncb2012@gmail.com>
2022-11-11 10:02:41 -0500

    cambios en los estilos de fondo del ranking

--- src/components/games/common/Ranking/RankingMyScore.tsx
+++ src/components/games/common/Ranking/RankingMyScore.tsx
@@ -1,45 +1,43 @@
 import UsersCard from '@/components/shared/usersCard';
 import { UseEventContext } from '@/context/eventContext';
-import { Divider, List, Row } from 'antd';
+import { Divider, List, Row, Typography } from 'antd';
 import { useState } from 'react';
 import { Score } from './types';
 
 interface Props {
-	myScore: Score;
-	type: 'time' | 'points';
+  myScore: Score;
+  type: 'time' | 'points';
 }
 
 export default function RankingMyScore(props: Props) {
-	const { myScore, type } = props;
-	const cEvent = UseEventContext();
-	const { styles } = cEvent;
-	const [loading, setloading] = useState(false);
+  const { myScore, type } = props;
+  const cEvent = UseEventContext();
+  const { styles } = cEvent;
+  const [loading, setloading] = useState(false);
 
-	return (
-		<>
-			<div style={{ marginTop: 16 }} className='card-dinamic-ranking ranking-user'>
-				<Row justify='center'>
-					<h1
-						style={{
-							fontSize: '25px',
-							fontWeight: 'bold',
-							lineHeight: '3px',
-							color: `${styles && styles.textMenu}`,
-						}}>
-						Mi Puntaje
-					</h1>
-					<Divider style={{ backgroundColor: `${styles && styles.textMenu}` }} />
-				</Row>
-				<div className='container-dinamic-ranking' style={{ marginTop: 16, height: 'auto', overflowY: 'auto' }}>
-					<List
-						className='demo-loadmore-list'
-						loading={loading}
-						itemLayout='horizontal'
-						dataSource={[myScore]}
-						renderItem={(item, key) => <UsersCard type='ranking' item={item} isTime={type === 'time'} />}
-					/>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className='card-dinamic-ranking ranking-user'>
+        <Row justify='center'>
+          <Typography.Title
+            level={3}
+            style={{
+              color: `${styles && styles.textMenu}`,
+            }}>
+            Mi Puntaje
+          </Typography.Title>
+          <Divider style={{ backgroundColor: `${styles && styles.textMenu}` }} />
+        </Row>
+        <div className='container-dinamic-ranking' style={{ marginTop: 16, height: 'auto', overflowY: 'auto' }}>
+          <List
+            className='demo-loadmore-list'
+            loading={loading}
+            itemLayout='horizontal'
+            dataSource={[myScore]}
+            renderItem={(item, key) => <UsersCard type='ranking' item={item} isTime={type === 'time'} />}
+          />
+        </div>
+      </div>
+    </>
+  );
 }
--- src/components/games/sharePhoto/components/landing/RankingDrawer.tsx
+++ src/components/games/sharePhoto/components/landing/RankingDrawer.tsx
@@ -3,31 +3,31 @@ import { Button, Drawer, Grid, Space } from 'antd';
 import React, { useState } from 'react';
 
 const myScore = {
-	uid: '636c0c6d96121d4a4e3995a4',
-	imageProfile:
-		'https://firebasestorage.googleapis.com/v0/b/eviusauthdev.appspot.com/o/images%2F1664828271521.png?alt=media&token=dae70e80-45ea-4daa-9523-ed9bd0ffdf67',
-	score: '100',
-	name: 'Juan Camayo',
-	index: 1,
+  uid: '636c0c6d96121d4a4e3995a4',
+  imageProfile:
+    'https://firebasestorage.googleapis.com/v0/b/eviusauthdev.appspot.com/o/images%2F1664828271521.png?alt=media&token=dae70e80-45ea-4daa-9523-ed9bd0ffdf67',
+  score: '100',
+  name: 'Juan Camayo',
+  index: 1,
 };
 
 const scores = [
-	{
-		uid: '636c0c6d96121d4a4e3995a4',
-		imageProfile:
-			'https://firebasestorage.googleapis.com/v0/b/eviusauthdev.appspot.com/o/images%2F1664828271521.png?alt=media&token=dae70e80-45ea-4daa-9523-ed9bd0ffdf67',
-		score: '100',
-		name: 'Juan Camayo',
-		index: 1,
-	},
-	{
-		uid: '636bb6b521b48d56a8144709',
-		imageProfile:
-			'https://firebasestorage.googleapis.com/v0/b/eviusauthdev.appspot.com/o/images%2F1664828271521.png?alt=media&token=dae70e80-45ea-4daa-9523-ed9bd0ffdf67',
-		score: '50',
-		name: 'Juan Camayo',
-		index: 2,
-	},
+  {
+    uid: '636c0c6d96121d4a4e3995a4',
+    imageProfile:
+      'https://firebasestorage.googleapis.com/v0/b/eviusauthdev.appspot.com/o/images%2F1664828271521.png?alt=media&token=dae70e80-45ea-4daa-9523-ed9bd0ffdf67',
+    score: '100',
+    name: 'Juan Camayo',
+    index: 1,
+  },
+  {
+    uid: '636bb6b521b48d56a8144709',
+    imageProfile:
+      'https://firebasestorage.googleapis.com/v0/b/eviusauthdev.appspot.com/o/images%2F1664828271521.png?alt=media&token=dae70e80-45ea-4daa-9523-ed9bd0ffdf67',
+    score: '50',
+    name: 'Juan Camayo',
+    index: 2,
+  },
 ];
 
 const type: 'time' | 'points' = 'points';
@@ -35,29 +35,29 @@ const type: 'time' | 'points' = 'points';
 const { useBreakpoint } = Grid;
 
 export default function RankingDrawer() {
-	const [open, setOpen] = useState(false);
-	const handleOpen = () => {
-		setOpen(true);
-	};
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
 
-	const handleClose = () => {
-		setOpen(false);
-	};
-	const screens = useBreakpoint();
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const screens = useBreakpoint();
 
-	return (
-		<Space>
-			<Button type='primary' onClick={handleOpen}>
-				Ver Ranking
-			</Button>
-			<Drawer
-				title='Ranking'
-				bodyStyle={{ padding: '0px' }}
-				width={screens.xs ? '100vw' : '35vw'}
-				visible={open}
-				onClose={handleClose}>
-				<Ranking scores={scores} myScore={myScore} withMyScore={true} type='points' />
-			</Drawer>
-		</Space>
-	);
+  return (
+    <Space>
+      <Button type='primary' onClick={handleOpen}>
+        Ver Ranking
+      </Button>
+      <Drawer
+        title='Ranking'
+        bodyStyle={{ padding: screens.xs ? '10px' : '24px', backgroundColor: '#F1F1F1' }}
+        width={screens.xs ? '100vw' : '35vw'}
+        visible={open}
+        onClose={handleClose}>
+        <Ranking scores={scores} myScore={myScore} withMyScore={true} type='points' />
+      </Drawer>
+    </Space>
+  );
 }
