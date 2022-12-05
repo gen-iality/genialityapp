e62187ec29b7eb5ede130ef0f0e512e6040ab499
Marlon Carrillo <marloncb2012@gmail.com>
2022-12-05 14:28:27 -0500

    estilos drawer problema con giro de pantalla  y tamaños de fuentes

--- src/components/games/bingo/constants/styleConstants.ts
+++ src/components/games/bingo/constants/styleConstants.ts
@@ -1,3 +1,5 @@
+import { fontWeight } from 'html2canvas/dist/types/css/property-descriptors/font-weight';
+
 // estilos para los valores en el carton 5x5
 export const gridStyleText_5x5: React.CSSProperties = {
   width: '17%',
@@ -62,7 +64,7 @@ export const gridStyleImage_4x4: React.CSSProperties = {
   alignItems: 'center',
   overflow: 'hidden',
   userSelect: 'none',
-  position: 'relative',  
+  position: 'relative',
 };
 // estilos para los valores en el carton 3x3
 export const gridStyleText_3x3: React.CSSProperties = {
@@ -112,96 +114,120 @@ export const gridStyleSelected: React.CSSProperties = {
 export const gridTextBreaking_5x5: React.CSSProperties = {
   overflowWrap: 'anywhere',
   fontSize: '12px',
+  fontWeight: 'bold',
 };
 export const gridTextNotBreaking_5x5: React.CSSProperties = {
   fontSize: '12px',
+  fontWeight: 'bold',
 };
 export const gridTextOnlyNumber_5x5: React.CSSProperties = {
-  fontSize: '25px',
-  fontVariant:'tabular-nums'
-}
+  fontSize: '48px',
+  fontVariant: 'tabular-nums',
+  fontWeight: 'bold',
+};
 export const gridText_5x5: React.CSSProperties = {
   fontSize: '16px',
+  fontWeight: 'bold',
 };
 
 /* Mobile */
 export const gridTextBreakingMobile_5x5: React.CSSProperties = {
   overflowWrap: 'anywhere',
   fontSize: '10px',
+  fontWeight: 'bold',
 };
 export const gridTextNotBreakingMobile_5x5: React.CSSProperties = {
   fontSize: '10px',
+  fontWeight: 'bold',
 };
 export const gridTextOnlyNumberMobile_5x5: React.CSSProperties = {
-  fontSize: '14px',
-  fontVariant:'tabular-nums'
-}
+  fontSize: '24px',
+  fontVariant: 'tabular-nums',
+  fontWeight: 'bold',
+};
 export const gridTextMobile_5x5: React.CSSProperties = {
   fontSize: '12px',
+  fontWeight: 'bold',
 };
 // --------------------------------------------------------------------------------------------
 // tamaño de la fuente para carton 4x4 --------------------------------------------------------
 export const gridTextBreaking_4x4: React.CSSProperties = {
   overflowWrap: 'anywhere',
   fontSize: '14px',
+  fontWeight: 'bold',
 };
 export const gridTextNotBreaking_4x4: React.CSSProperties = {
   fontSize: '14px',
+  fontWeight: 'bold',
 };
 export const gridTextOnlyNumber_4x4: React.CSSProperties = {
-  fontSize: '25px',
-  fontVariant:'tabular-nums'
-}
+  fontSize: '60px',
+  fontVariant: 'tabular-nums',
+  fontWeight: 'bold',
+};
 export const gridText_4x4: React.CSSProperties = {
   fontSize: '16px',
+  fontWeight: 'bold',
 };
 
 /* Mobile */
 export const gridTextBreakingMobile_4x4: React.CSSProperties = {
   overflowWrap: 'anywhere',
   fontSize: '10px',
+  fontWeight: 'bold',
 };
 export const gridTextNotBreakingMobile_4x4: React.CSSProperties = {
   fontSize: '10px',
+  fontWeight: 'bold',
 };
 export const gridTextOnlyNumberMobile_4x4: React.CSSProperties = {
-  fontSize: '25px',
-  fontVariant:'tabular-nums'
-}
+  fontSize: '30px',
+  fontVariant: 'tabular-nums',
+  fontWeight: 'bold',
+};
 export const gridTextMobile_4x4: React.CSSProperties = {
   fontSize: '12px',
+  fontWeight: 'bold',
 };
 // --------------------------------------------------------------------------------------------
 // tamaño de la fuente para carton 3x3 --------------------------------------------------------
 export const gridTextBreaking_3x3: React.CSSProperties = {
   overflowWrap: 'anywhere',
   fontSize: '14px',
+  fontWeight: 'bold',
 };
 export const gridTextNotBreaking_3x3: React.CSSProperties = {
   fontSize: '14px',
+  fontWeight: 'bold',
 };
 export const gridTextOnlyNumber_3x3: React.CSSProperties = {
-  fontSize: '45px',
-  fontVariant:'tabular-nums'
-}
+  fontSize: '80px',
+  fontVariant: 'tabular-nums',
+  fontWeight: 'bold',
+};
 export const gridText_3x3: React.CSSProperties = {
   fontSize: '18px',
+  fontWeight: 'bold',
 };
 
 /* Mobile */
 export const gridTextBreakingMobile_3x3: React.CSSProperties = {
   overflowWrap: 'anywhere',
   fontSize: '12px',
+  fontWeight: 'bold',
 };
 export const gridTextNotBreakingMobile_3x3: React.CSSProperties = {
   fontSize: '12px',
+  fontWeight: 'bold',
 };
 export const gridTextOnlyNumberMobile_3x3: React.CSSProperties = {
-  fontSize: '30px',
-  fontVariant:'tabular-nums'
-}
+  fontSize: '40px',
+  fontVariant: 'tabular-nums',
+  fontWeight: 'bold',
+};
 export const gridTextMobile_3x3: React.CSSProperties = {
   fontSize: '14px',
+  fontWeight: 'bold',
 };
 
 // --------------------------------------------------------------------------------------------
