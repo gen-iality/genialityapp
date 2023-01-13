77b9245a4a4a98548a71f90bf740ff167c1ba118
Juan Camayo <juan.camayo@evius.com>
2023-01-13 16:39:55 -0500

    Add - added some types missed

--- src/components/events/datos/utils.tsx
+++ src/components/events/datos/utils.tsx
@@ -1,216 +1,236 @@
 import { Divider, List, Modal, Typography } from 'antd';
+import { CheckboxChangeEvent } from 'antd/lib/checkbox';
 
 const { confirm } = Modal;
 const { Text } = Typography;
 
 const checkInInstructions = [
-  'Si ya hay asistentes inscritos en el evento, se deberá actualizar la información de dicho campo.',
-  'Esta función está disponible solo para cédula de ciudadanía Colombiana.',
-  'Se requiere Lector de documentos.',
-  'Durante la inscripción se almacenará la información capturada del documento.',
+	'Si ya hay asistentes inscritos en el evento, se deberá actualizar la información de dicho campo.',
+	'Esta función está disponible solo para cédula de ciudadanía Colombiana.',
+	'Se requiere Lector de documentos.',
+	'Durante la inscripción se almacenará la información capturada del documento.',
 ];
 
 const checkInFields = [
-  {
-    id: undefined,
-    name: 'checkInField',
-    description: undefined,
-    label: 'Documento',
-    unique: false,
-    mandatory: true,
-    options: undefined,
-    order_weight: undefined,
-    type: 'checkInField',
-    visibleByAdmin: true,
-    visibleByContacts: undefined,
-  },
-  {
-    id: undefined,
-    name: 'gender',
-    description: undefined,
-    label: 'Genero',
-    unique: false,
-    mandatory: true,
-    options: undefined,
-    order_weight: undefined,
-    type: 'text',
-    visibleByAdmin: true,
-    visibleByContacts: undefined,
-  },
-  {
-    id: undefined,
-    name: 'bloodtype',
-    description: undefined,
-    label: 'RH',
-    unique: false,
-    mandatory: true,
-    options: undefined,
-    order_weight: undefined,
-    type: 'text',
-    visibleByAdmin: true,
-    visibleByContacts: undefined,
-  },
-  {
-    id: undefined,
-    name: 'birthdate',
-    description: undefined,
-    label: 'Fecha de nacimiento',
-    unique: false,
-    mandatory: true,
-    options: undefined,
-    order_weight: undefined,
-    type: 'date',
-    visibleByAdmin: true,
-    visibleByContacts: undefined,
-  },
+	{
+		id: undefined,
+		name: 'checkInField',
+		description: undefined,
+		label: 'Documento',
+		unique: false,
+		mandatory: true,
+		options: undefined,
+		order_weight: undefined,
+		type: 'checkInField',
+		visibleByAdmin: true,
+		visibleByContacts: undefined,
+	},
+	{
+		id: undefined,
+		name: 'gender',
+		description: undefined,
+		label: 'Genero',
+		unique: false,
+		mandatory: true,
+		options: undefined,
+		order_weight: undefined,
+		type: 'text',
+		visibleByAdmin: true,
+		visibleByContacts: undefined,
+	},
+	{
+		id: undefined,
+		name: 'bloodtype',
+		description: undefined,
+		label: 'RH',
+		unique: false,
+		mandatory: true,
+		options: undefined,
+		order_weight: undefined,
+		type: 'text',
+		visibleByAdmin: true,
+		visibleByContacts: undefined,
+	},
+	{
+		id: undefined,
+		name: 'birthdate',
+		description: undefined,
+		label: 'Fecha de nacimiento',
+		unique: false,
+		mandatory: true,
+		options: undefined,
+		order_weight: undefined,
+		type: 'date',
+		visibleByAdmin: true,
+		visibleByContacts: undefined,
+	},
 ];
 
 // a little function to help us with reordering the result
-export const Reorder = (list: any, startIndex: any, endIndex: any) => {
-  const result = Array.from(list);
-  const [removed] = result.splice(startIndex, 1);
-  result.splice(endIndex, 0, removed);
+export const Reorder = (list: any, startIndex: number, endIndex: number) => {
+	const result = Array.from(list);
+	const [removed] = result.splice(startIndex, 1);
+	result.splice(endIndex, 0, removed);
 
-  return result;
+	return result;
 };
 
 const grid = 6;
 export const getItemStyle = (isDragging: any, draggableStyle: any) => {
-  return {
-    // some basic styles to make the items look a bit nicer
-    userSelect: 'none',
-    padding: grid * 2,
-    margin: `0 0 ${grid}px 0`,
-    textAlign: 'left',
+	return {
+		// some basic styles to make the items look a bit nicer
+		userSelect: 'none',
+		padding: grid * 2,
+		margin: `0 0 ${grid}px 0`,
+		textAlign: 'left',
 
-    // change background colour if dragging
-    background: isDragging ? 'lightgreen' : '#1BD5DF',
+		// change background colour if dragging
+		background: isDragging ? 'lightgreen' : '#1BD5DF',
 
-    // styles we need to apply on draggables
-    ...draggableStyle,
-  };
+		// styles we need to apply on draggables
+		...draggableStyle,
+	};
 };
 
 export const getQuestionListStyle = (isDraggingOver: any) => ({
-  background: isDraggingOver ? '#CA4FC4' : '',
-  padding: 8,
-  width: 350,
+	background: isDraggingOver ? '#CA4FC4' : '',
+	padding: 8,
+	width: 350,
 });
 
-export const createFieldForCheckInPerDocument = async ({ value, checkInFieldsIds, save, remove }: any) => {
-  const saveCheckInField = async () => {
-    checkInFields.map(async (checkInField) => {
-      await save(checkInField);
-    });
-  };
+export const createFieldForCheckInPerDocument = async ({
+	value,
+	checkInFieldsIds,
+	save,
+	remove,
+}: {
+	value: CheckboxChangeEvent;
+	checkInFieldsIds: string[];
+	save: Function;
+	remove: Function;
+}) => {
+	const saveCheckInField = async () => {
+		checkInFields.map(async checkInField => {
+			await save(checkInField);
+		});
+	};
 
-  if (value.target.checked) {
-    confirm({
-      title: `Al habilitar el checkIn por documento, recuerda que:`,
-      content: (
-        <List
-          size='small'
-          dataSource={checkInInstructions}
-          renderItem={(item, index) => (
-            <List.Item>
-              <Text>
-                {index + 1}. {item}
-              </Text>
-            </List.Item>
-          )}
-        />
-      ),
-      okText: 'Habilitar',
-      okType: 'primary',
-      cancelText: 'Cancelar',
-      onOk() {
-        saveCheckInField();
-      },
-    });
-  } else {
-    confirm({
-      title: `¿Se deshabilitará el checkIn por documento estás seguro?`,
-      // content: 'Una vez eliminado, no lo podrá recuperar',
-      okText: 'Si',
-      okType: 'danger',
-      cancelText: 'Cancelar',
-      onOk() {
-        checkInFieldsIds.map(async (checkInFieldId: any) => {
-          await remove(checkInFieldId, true);
-        });
-      },
-    });
-  }
+	if (value.target.checked) {
+		confirm({
+			title: `Al habilitar el checkIn por documento, recuerda que:`,
+			content: (
+				<List
+					size='small'
+					dataSource={checkInInstructions}
+					renderItem={(item, index) => (
+						<List.Item>
+							<Text>
+								{index + 1}. {item}
+							</Text>
+						</List.Item>
+					)}
+				/>
+			),
+			okText: 'Habilitar',
+			okType: 'primary',
+			cancelText: 'Cancelar',
+			onOk() {
+				saveCheckInField();
+			},
+		});
+	} else {
+		confirm({
+			title: `¿Se deshabilitará el checkIn por documento estás seguro?`,
+			// content: 'Una vez eliminado, no lo podrá recuperar',
+			okText: 'Si',
+			okType: 'danger',
+			cancelText: 'Cancelar',
+			onOk() {
+				checkInFieldsIds.map(async (checkInFieldId: string) => {
+					await remove(checkInFieldId, true);
+				});
+			},
+		});
+	}
 };
-
 
 // Logic to create the voting coefficient
 // TODO: pending copy for this instructions
 const voteWeightInstructions = [
-  'Si ya hay asistentes inscritos en el evento, se deberá actualizar la información de dicho campo.',
-  'Esta función está disponible solo para cédula de ciudadanía Colombiana.',
-  'Se requiere Lector de documentos.',
-  'Durante la inscripción se almacenará la información capturada del documento.',
+	'Si ya hay asistentes inscritos en el evento, se deberá actualizar la información de dicho campo.',
+	'Esta función está disponible solo para cédula de ciudadanía Colombiana.',
+	'Se requiere Lector de documentos.',
+	'Durante la inscripción se almacenará la información capturada del documento.',
 ];
 
 const voteWeightFields = [
-  {
-    id: undefined,
-    name: 'voteWeight',
-    description: undefined,
-    label: 'Coeficiente',
-    unique: false,
-    mandatory: true,
-    options: undefined,
-    order_weight: undefined,
-    type: 'voteWeight',
-    visibleByAdmin: true,
-    visibleByContacts: undefined,
-  },
+	{
+		id: undefined,
+		name: 'voteWeight',
+		description: undefined,
+		label: 'Coeficiente',
+		unique: false,
+		mandatory: true,
+		options: undefined,
+		order_weight: undefined,
+		type: 'voteWeight',
+		visibleByAdmin: true,
+		visibleByContacts: undefined,
+	},
 ];
 
-export const createFieldForAssembly = async ({ value, checkInByAssemblyFields, save, remove }: any) => {
-  const saveFieldForAssembly = async () => {
-    voteWeightFields.map(async (voteWeightField) => {
-      await save(voteWeightField);
-    });
-  };
+export const createFieldForAssembly = async ({
+	value,
+	checkInByAssemblyFields,
+	save,
+	remove,
+}: {
+	value: CheckboxChangeEvent;
+	checkInByAssemblyFields: string[];
+	save: Function;
+	remove: Function;
+}) => {
+	const saveFieldForAssembly = async () => {
+		voteWeightFields.map(async voteWeightField => {
+			await save(voteWeightField);
+		});
+	};
 
-  if (value.target.checked) {
-    confirm({
-      title: `Al habilitar el checkIn por asamblea, recuerda que:`,
-      content: (
-        <List
-          size='small'
-          dataSource={voteWeightInstructions}
-          renderItem={(item, index) => (
-            <List.Item>
-              <Text>
-                {index + 1}. {item}
-              </Text>
-            </List.Item>
-          )}
-        />
-      ),
-      okText: 'Habilitar',
-      okType: 'primary',
-      cancelText: 'Cancelar',
-      onOk() {
-        saveFieldForAssembly();
-      },
-    });
-  } else {
-    confirm({
-      title: `¿Se deshabilitará el checkIn por asamblea estás seguro?`,
-      // content: 'Una vez eliminado, no lo podrá recuperar',
-      okText: 'Si',
-      okType: 'danger',
-      cancelText: 'Cancelar',
-      onOk() {
-        checkInByAssemblyFields.map(async (checkInFieldId: any) => {
-          await remove(checkInFieldId, true);
-        });
-      },
-    });
-  }
+	if (value.target.checked) {
+		confirm({
+			title: `Al habilitar el checkIn por asamblea, recuerda que:`,
+			content: (
+				<List
+					size='small'
+					dataSource={voteWeightInstructions}
+					renderItem={(item, index) => (
+						<List.Item>
+							<Text>
+								{index + 1}. {item}
+							</Text>
+						</List.Item>
+					)}
+				/>
+			),
+			okText: 'Habilitar',
+			okType: 'primary',
+			cancelText: 'Cancelar',
+			onOk() {
+				saveFieldForAssembly();
+			},
+		});
+	} else {
+		confirm({
+			title: `¿Se deshabilitará el checkIn por asamblea estás seguro?`,
+			// content: 'Una vez eliminado, no lo podrá recuperar',
+			okText: 'Si',
+			okType: 'danger',
+			cancelText: 'Cancelar',
+			onOk() {
+				checkInByAssemblyFields.map(async (checkInFieldId: string) => {
+					await remove(checkInFieldId, true);
+				});
+			},
+		});
+	}
 };
