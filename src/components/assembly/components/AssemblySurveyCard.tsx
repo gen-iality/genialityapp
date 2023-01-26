acb495b57b8cf1cee417fe4d5aff87ace6afc7ba
Marlon Carrillo <marloncb2012@gmail.com>
2023-01-26 11:50:07 -0500

    Se creo el componente para las encuestas dentro de las actividades en las estadisticas de asambleas

---
+++ src/components/assembly/components/AssemblySurveyCard.tsx
@@ -0,0 +1,50 @@
+import { Button, Card, Space, Tag } from 'antd';
+
+interface props {
+	status: Status;
+	title: React.ReactNode | string;
+	extra?: React.ReactNode | string;
+}
+
+const STATUS: Record<Status, StatusProps> = {
+	closed: {
+		label: 'Cerrado',
+		color: 'red',
+	},
+	opened: {
+		label: 'Abierto',
+		color: 'green',
+	},
+	finished: {
+		label: 'Finalizado',
+		color: 'orange',
+	},
+};
+
+export type Status = 'closed' | 'opened' | 'finished';
+
+export interface StatusProps {
+	label: string;
+	color: string;
+}
+
+const AssemblySurveyCard = (props: props) => {
+	const { status, title, extra } = props;
+
+	return (
+		<Card
+			title={
+				<Space size={0} style={{ fontWeight: '500' }}>
+					<Tag color={STATUS[status].color}>{STATUS[status].label}</Tag>
+				</Space>
+			}
+			headStyle={{ border: 'none', fontSize: '14px' }}
+			bodyStyle={{ paddingTop: '0px' }}
+			extra={extra}
+			actions={[]}>
+			<Card.Meta title={title} description={'aqui se supone van las fechas'} />
+		</Card>
+	);
+};
+
+export default AssemblySurveyCard;
--- src/components/assembly/views/AssemblyInCMS.tsx
+++ src/components/assembly/views/AssemblyInCMS.tsx
@@ -1,17 +1,24 @@
-import { Card, Col, Collapse, Row, Space, Typography } from 'antd';
+import { Button, Card, Col, Collapse, Row, Space, Statistic, Switch, Tag, Timeline, Typography } from 'antd';
 import AccountEyeIcon from '@2fd/ant-design-icons/lib/AccountEye';
-import React from 'react';
 import AccountGroupIcon from '@2fd/ant-design-icons/lib/AccountGroup';
+import ChartBarIcon from '@2fd/ant-design-icons/lib/ChartBar';
+import AssemblySurveyCard from '../components/AssemblySurveyCard';
 
 export default function AssemblyInCMS() {
+
+	const arraySurveyExample = Array.from({ length: 4 })
 	return (
 		<div style={{ padding: '40px' }}>
 			<Row gutter={[16, 16]}>
 				<Col span={12}>
-					<Card>Inscritos / Registrados</Card>
+					<Card>
+						<Statistic title={'Inscritos / Registrados'} value={15} />
+					</Card>
 				</Col>
 				<Col span={12}>
-					<Card>Asistencia / Checkeados</Card>
+					<Card>
+						<Statistic title={'Asistencia / Checkeados'} value={15} />
+					</Card>
 				</Col>
 				<Col span={24}>
 					<Card>
@@ -26,16 +33,28 @@ export default function AssemblyInCMS() {
 											<AccountGroupIcon />
 											<Typography.Text>5</Typography.Text>
 										</Space>
-                    <Space>
+										<Space>
 											<AccountEyeIcon />
 											<Typography.Text>15</Typography.Text>
 										</Space>
-                    <Space>
-											Quorum
+										<Space>
+											Quórum
 											<Typography.Text>45%</Typography.Text>
 										</Space>
 									</Space>
-								}></Collapse.Panel>
+								}>
+								<Row gutter={[16, 16]}>
+									{arraySurveyExample.map((survey , index)=>(
+										<Col span={8}>
+										<AssemblySurveyCard
+											status={index === 0 ? 'closed': index === 1 ? 'opened': 'finished'}
+											title={`Encuesta ${index + 1}`}
+											extra={<Button type='primary' icon={<ChartBarIcon />}></Button>}
+										/>
+									</Col>
+									))}
+								</Row>
+							</Collapse.Panel>
 						</Collapse>
 					</Card>
 				</Col>
