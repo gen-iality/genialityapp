af223a68295059c3e0820473f0c715d07b686e8c
Marlon Carrillo <marloncb2012@gmail.com>
2023-01-27 18:32:45 -0500

    Render de cards con el conteo de votaciones

--- src/components/assembly/components/assemblyGraphicsSections/PercentageSection.tsx
+++ src/components/assembly/components/assemblyGraphicsSections/PercentageSection.tsx
@@ -19,23 +19,23 @@ export default function PercentageSection(props: Props) {
 	return (
 		<Card style={{ height: '100%', overflowY: 'auto' }}>
 			<Row gutter={[16, 16]}>
-				{arrayExample.map((item, index) => (
-					<Col key={index} xs={24} sm={24} md={12} lg={12} xl={12} xxl={12}>
+				{graphicsData.labels.map(({color, letter, question, percentage, quantity}) => (
+					<Col key={question} xs={24} sm={24} md={12} lg={12} xl={12} xxl={12}>
 						<Card bodyStyle={{ padding: '0px' }}>
 							<Space align='start'>
-								<Avatar size={80} shape='square' style={{ backgroundColor: `${'#c4c4c4'}` }}>
-									{'A' + index}
+								<Avatar size={80} shape='square' style={{ backgroundColor: `${color}` }}>
+									{letter}
 								</Avatar>
 								<Space style={{ padding: '5px' }} size={0} align='start' direction='vertical'>
-									<span style={{ fontWeight: '600' }}>{singularOrPluralString(randomNumber(), 'Voto', 'Votos')}</span>
+									<span style={{ fontWeight: '600' }}>{singularOrPluralString(quantity, 'Voto', 'Votos')}</span>
 									<Typography.Paragraph
 										style={{ color: '#808080', lineHeight: '1.25' }}
 										ellipsis={{ rows: 2, expandable: false, tooltip: 'Respuesta' }}>
-										{'Respuesta' + index}
+										{question}
 									</Typography.Paragraph>
 								</Space>
 							</Space>
-							<span style={{ position: 'absolute', top: '5px', right: '10px', fontWeight: '600' }}>{40}%</span>
+							<span style={{ position: 'absolute', top: '5px', right: '10px', fontWeight: '600' }}>{percentage}%</span>
 						</Card>
 					</Col>
 				))}
