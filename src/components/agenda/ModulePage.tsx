20414da59a00cff9477c70dbdd0b63e65c2c8d51
Mauricio J Carrillo <mauricio.jcarrillo@gmail.com>
2023-01-30 07:48:35 -0500

    Formatted file with prettier

--- src/components/agenda/ModulePage.tsx
+++ src/components/agenda/ModulePage.tsx
@@ -1,10 +1,5 @@
 import { useState, useEffect } from 'react';
-import {
-  Modal,
-  Form,
-  Input,
-  Button,
-} from 'antd';
+import { Modal, Form, Input, Button } from 'antd';
 import CMS from '../newComponent/CMS';
 import { ModulesApi } from '@helpers/request';
 
@@ -18,28 +13,28 @@ function ModulePage(props: any) {
 
   const openModal = () => {
     setIsOpened(true);
-  }
+  };
 
   const closeModal = () => {
     setIsOpened(false);
-  }
+  };
 
   const cancelModel = () => {
     form.resetFields();
     setCurrentEditingItem(null);
     closeModal();
-  }
+  };
 
   useEffect(() => {
     if (currentEditingItem) {
-      form.setFields([{name: 'moduleName', value: currentEditingItem.module_name}])
+      form.setFields([{ name: 'moduleName', value: currentEditingItem.module_name }]);
       openModal();
     }
-  }, [currentEditingItem])
+  }, [currentEditingItem]);
 
   useEffect(() => {
-    console.log(columnsData)
-  }, [columnsData])
+    console.log(columnsData);
+  }, [columnsData]);
 
   const columns = [
     {
@@ -50,71 +45,67 @@ function ModulePage(props: any) {
 
   return (
     <>
-    <p style={{color: 'red'}}>TODO: falta recargar el CMS cuando se agregue/edite cosas</p>
-    <CMS
-      API={ModulesApi}
-      eventId={props.event._id}
-      title={'Modules'}
-      titleTooltip={'Agregue o edite los modules disponibles en este curso'}
-      addFn={() => {
-        openModal();
-        console.log('agregar:', ...arguments);
-      }}
-      columns={columns}
-      // key='_id'
-      editFn={(item: any) => {
-        setCurrentEditingItem(item);
-        console.log('edit', item);
-      }}
-      pagination={false}
-      actions
-      search
-      setColumnsData={setColumnsData}
-      scroll={{ x: 'auto' }}
-    />
-
-    <Modal
-      visible={isOpened}
-      title={currentEditingItem === null ? 'Agregar nuevo modulo' : 'Editar módulo'}
-      okButtonProps={{ style: { display: 'none' } }}
-      onCancel={cancelModel}
-    >
-      <Form
-        form={form}
-        onFinish={(values) => {
-          values.event_id = props.event._id
-          console.log('finish', values);
-
-          if (currentEditingItem === null) {
-            ModulesApi.create(values.moduleName, props.event._id)
-            .finally(() => {
-              closeModal();
-              form.resetFields();
-            })
-          } else {
-            ModulesApi.update(currentEditingItem._id, values.moduleName)
-            .finally(() => {
-              closeModal();
-              setCurrentEditingItem(null);
-              form.resetFields();
-            })
-          }
+      <p style={{ color: 'red' }}>TODO: falta recargar el CMS cuando se agregue/edite cosas</p>
+      <CMS
+        API={ModulesApi}
+        eventId={props.event._id}
+        title={'Modules'}
+        titleTooltip={'Agregue o edite los modules disponibles en este curso'}
+        addFn={() => {
+          openModal();
+          console.log('agregar:', ...arguments);
         }}
+        columns={columns}
+        // key='_id'
+        editFn={(item: any) => {
+          setCurrentEditingItem(item);
+          console.log('edit', item);
+        }}
+        pagination={false}
+        actions
+        search
+        setColumnsData={setColumnsData}
+        scroll={{ x: 'auto' }}
+      />
+
+      <Modal
+        visible={isOpened}
+        title={currentEditingItem === null ? 'Agregar nuevo modulo' : 'Editar módulo'}
+        okButtonProps={{ style: { display: 'none' } }}
+        onCancel={cancelModel}
       >
-        <Form.Item
-          name="moduleName"
-          label="Nombre del módulo"
-          rules={[{ required: true, message: 'Es necesario el nombre de módulo' }]}
+        <Form
+          form={form}
+          onFinish={(values) => {
+            values.event_id = props.event._id;
+            console.log('finish', values);
+
+            if (currentEditingItem === null) {
+              ModulesApi.create(values.moduleName, props.event._id).finally(() => {
+                closeModal();
+                form.resetFields();
+              });
+            } else {
+              ModulesApi.update(currentEditingItem._id, values.moduleName).finally(() => {
+                closeModal();
+                setCurrentEditingItem(null);
+                form.resetFields();
+              });
+            }
+          }}
         >
-          <Input />
-        </Form.Item>
-        <Form.Item>
-          <Button htmlType='submit'>
-            {currentEditingItem === null ? 'Agregar' : 'Editar'}
-          </Button>
-        </Form.Item>
-      </Form>
-    </Modal>
+          <Form.Item
+            name='moduleName'
+            label='Nombre del módulo'
+            rules={[{ required: true, message: 'Es necesario el nombre de módulo' }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item>
+            <Button htmlType='submit'>{currentEditingItem === null ? 'Agregar' : 'Editar'}</Button>
+          </Form.Item>
+        </Form>
+      </Modal>
     </>
   );
 }
