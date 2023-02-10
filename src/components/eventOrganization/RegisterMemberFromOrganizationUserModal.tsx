c03f2827fe7b9827090586bbf9e81c8d1eca4deb
Mauricio J Carrillo <mauricio.jcarrillo@gmail.com>
2023-02-10 12:03:50 -0500

    Formatted file with prettier

--- src/components/eventOrganization/RegisterMemberFromOrganizationUserModal.tsx
+++ src/components/eventOrganization/RegisterMemberFromOrganizationUserModal.tsx
@@ -1,44 +1,31 @@
-import {
-  FunctionComponent, useState,
-} from 'react'
+import { FunctionComponent, useState } from 'react';
 
-import {
-  Modal,
-  Form,
-  Input,
-  Button,
-  Card,
-  Alert,
-} from 'antd'
+import { Modal, Form, Input, Button, Card, Alert } from 'antd';
 
-import { WarningOutlined } from '@ant-design/icons'
-import { OrganizationApi } from '@helpers/request'
+import { WarningOutlined } from '@ant-design/icons';
+import { OrganizationApi } from '@helpers/request';
 
 type FormOrganizationUser = {
-  name: string,
-  email: string,
-}
+  name: string;
+  email: string;
+};
 
 export interface RegisterMemberFromOrganizationUserModalProps {
-  orgMember?: any,
-  user?: any,
-  visible?: boolean,
-  organization: any,
-  onRegister?: (orgUserData: any) => void,
+  orgMember?: any;
+  user?: any;
+  visible?: boolean;
+  organization: any;
+  onRegister?: (orgUserData: any) => void;
 }
 
-const RegisterMemberFromOrganizationUserModal: FunctionComponent<RegisterMemberFromOrganizationUserModalProps> = (props) => {
-  const {
-    organization,
-    orgMember,
-    user,
-    visible,
-    onRegister,
-  } = props
+const RegisterMemberFromOrganizationUserModal: FunctionComponent<RegisterMemberFromOrganizationUserModalProps> = (
+  props,
+) => {
+  const { organization, orgMember, user, visible, onRegister } = props;
 
   const [isModalOpened, setIsModalOpened] = useState(visible);
 
-  const [form] = Form.useForm<FormOrganizationUser>()
+  const [form] = Form.useForm<FormOrganizationUser>();
 
   const onFormSubmit = (values: FormOrganizationUser) => {
     if (!organization?._id) {
@@ -47,8 +34,8 @@ const RegisterMemberFromOrganizationUserModal: FunctionComponent<RegisterMemberF
         content: 'No se ha cargado la información de la organización aún',
         icon: <WarningOutlined />,
         onOk: () => setIsModalOpened(false),
-      })
-      return
+      });
+      return;
     }
 
     let data: any = {};
@@ -58,75 +45,63 @@ const RegisterMemberFromOrganizationUserModal: FunctionComponent<RegisterMemberF
       // some data are: user.names, user.email
       data = {
         // ...
-      } // TODO: fill that data for the organization user
-      console.log('Register Organization User from current user')
+      }; // TODO: fill that data for the organization user
+      console.log('Register Organization User from current user');
     } else {
       // Take data from the form
-      const { name, email } = values
+      const { name, email } = values;
       // TODO: do the register
-      data = {
-      } // TODO: fill that data for the organization user
-      console.log('Register Organization User', data)
+      data = {}; // TODO: fill that data for the organization user
+      console.log('Register Organization User', data);
     }
 
     if (onRegister) {
-      onRegister(data)
+      onRegister(data);
     }
-  
+
     // OrganizationApi.saveUser(organization._id, data)
     //   .finally(() => {
     //     setIsModalOpened(false)
     //   })
-  }
+  };
 
   if (orgMember) {
     return (
       <Modal
         visible={isModalOpened}
-        title="Usuario ya inscrito"
+        title='Usuario ya inscrito'
         onOk={() => setIsModalOpened(false)}
         onCancel={() => setIsModalOpened(false)}
       >
         El usuario ya está inscrito como miembro
       </Modal>
-    )
+    );
   }
 
   return (
     <Modal
       visible={isModalOpened}
-      title="Registrarse como miembro de esta organización"
-      okText="Inscribirse"
+      title='Registrarse como miembro de esta organización'
+      okText='Inscribirse'
       onOk={() => form.submit()}
     >
-      <Form
-        form={form}
-        onFinish={onFormSubmit}
-      >
+      <Form form={form} onFinish={onFormSubmit}>
         {user ? (
-          <Alert message="No se requieren más datos" />
-        ): (
+          <Alert message='No se requieren más datos' />
+        ) : (
           <>
-            <Form.Item
-              label="Nombre"
-              name="name"
-              rules={[{ required: true, message: 'Falta el nombre' }]}
-            >
+            <Form.Item label='Nombre' name='name' rules={[{ required: true, message: 'Falta el nombre' }]}>
               <Input />
             </Form.Item>
 
-            <Form.Item
-              label="Correo"
-              name="email"
-              rules={[{ required: true, message: 'Falta el correo' }]}
-            >
+            <Form.Item label='Correo' name='email' rules={[{ required: true, message: 'Falta el correo' }]}>
               <Input />
             </Form.Item>
           </>
         )}
       </Form>
     </Modal>
-  )
-}
+  );
+};
 
-export default RegisterMemberFromOrganizationUserModal
+export default RegisterMemberFromOrganizationUserModal;
