ba2fef42b73a7b45e76ad79209fc0de8454ddc4f
David Borja <davidborja456@gmail.com>
2023-02-14 19:12:28 -0500

    Update room controller that only controls games ._.

--- src/components/agenda/roomManager/controller.jsx
+++ src/components/agenda/roomManager/controller.jsx
@@ -1,7 +1,7 @@
-import { useContext, useEffect, useState, lazy } from 'react';
+import { useContext, useEffect, useState, lazy, useCallback } from 'react';
 import { Card, Row, Col, Switch, Popover, Avatar, Empty, Image, Alert, Select, Form } from 'antd';
 import GamepadVariantOutline from '@2fd/ant-design-icons/lib/GamepadVariantOutline';
-import { getColumnSearchProps } from '../../speakers/getColumnSearch';
+import { getColumnSearchProps } from '@components/speakers/getColumnSearch';
 import { firestore } from '@helpers/firebase';
 import AgendaContext from '@context/AgendaContext';
 import { Suspense } from 'react';
@@ -9,154 +9,170 @@ import { Suspense } from 'react';
 const Header = lazy(() => import('../../../antdComponents/Header'));
 const Table = lazy(() => import('../../../antdComponents/Table'));
 
-export default function RoomController(props) {
-  const { games, avalibleGames } = useContext(AgendaContext);
-  const { handleTabsController, handleGamesSelected } = props;
-
-  const [listOfGames, setListOfGames] = useState([]);
-  const [updateMensaje, setUpdatedMensaje] = useState(false);
+/**
+ * TODO: check that switch component can change the current game status
+ */
+export default function RoomController() {
+  // const [loadedGameList, setLoadedGameList] = useState([]);
+  const [isAvailableChanges, setIsAvailableChanges] = useState(false);
   const [columnsData, setColumnsData] = useState({});
-  const [showavailableGames, setShowavailableGames] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    setShowavailableGames(games);
-  }, [games]);
+  const [columns, setColumns] = useState([])
 
-  async function getGamesData() {
-    const gamesData = [];
+  const agendaContext = useContext(AgendaContext);
+  const {
+    games: isGamesActived,
+    avalibleGames: availableGames,
+  } = agendaContext;
+
+  const requestGames = async() => {
+    const newAvailableGames = [];
     const docRef = firestore.collection('gamesAvailable');
-    await docRef.get().then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        gamesData.push({ ...doc.data(), id: doc.id });
-      });
-    });
+    const querySnapshot = await docRef.get() // async does not bite)))
 
-    if (avalibleGames?.length === 0) {
-      setListOfGames(gamesData);
-      handleGamesSelected('newOrUpdate', '', gamesData);
-    } else if (avalibleGames?.length !== gamesData?.length) {
-      setListOfGames(gamesData);
-      handleGamesSelected('newOrUpdate', '', gamesData);
-      setUpdatedMensaje(true);
-    } else {
-      setListOfGames(avalibleGames);
-    }
+    querySnapshot.forEach((doc) => {
+      newAvailableGames.push({ ...doc.data(), id: doc.id })
+    })
+
+    // Load the available games
+    agendaContext.setAvailableGames(newAvailableGames)
+    setIsAvailableChanges(true)
+
     setIsLoading(false);
   }
 
-  useEffect(() => {
-    (listOfGames != avalibleGames) && getGamesData();
-  }, [avalibleGames]);
+  const updateStatusOfGame = (status, gameId, allGames) => {
+    console.debug('allGames', allGames)
+    const newData = allGames.map((game) => {
+      if (game.id === gameId) return { ...game, showGame: status };
+      else return { ...game };
+    });
+    console.debug('new allGames', newData)
+    agendaContext.setAvailableGames(newData);
+    setIsAvailableChanges(false);
+  }
 
-  const columns = [
-    {
-      title: 'Logo',
-      render: (item) => (
-        <Row gutter={8}>
-          <Col>
-            <Popover
-              placement='top'
-              content={() => (
-                <>
-                  {item.picture ? (
-                    <Image key={'img' + item._id} width={200} height={200} src={item.picture} />
-                  ) : (
-                    <Empty description='Imagen no encontrada' />
-                  )}
-                </>
-              )}>
-              {item.picture ? (
-                <Avatar key={'img' + item._id} src={item.picture} />
-              ) : (
-                <Avatar icon={<GamepadVariantOutline />} />
-              )}
-            </Popover>
-          </Col>
-        </Row>
-      ),
-    },
-    {
-      title: 'Nombre',
-      dataIndex: 'name',
-      ...getColumnSearchProps('name', columnsData),
-    },
-    {
-      title: 'Visible',
-      dataIndex: 'showGame',
-      render(val, item) {
-        return (
-          <Switch
-            checkedChildren='Sí'
-            unCheckedChildren='No'
-            onChange={(status) => {
-              handleGamesSelected(status, item.id, listOfGames);
-              setUpdatedMensaje(false);
-            }}
-            checked={item.showGame}
-            id={`editSwitch${item.index}`}
-          />
-        );
+  // We need to load the game list
+  useEffect(() => requestGames(), [])
+
+  // If the games status changes, we have to update the config
+  useEffect(() => agendaContext.saveConfig(), [isGamesActived])
+
+  // If the game availabled list changes, we have to update the config
+  useEffect(() => {
+    if (isLoading) return
+    agendaContext.saveConfig()
+  }, [availableGames])
+
+  useEffect(() => {
+    const newColumns = [
+      {
+        key: 'logo',
+        title: 'Logo',
+        render: (item) => (
+          <Row gutter={8}>
+            <Col>
+              <Popover
+                placement="top"
+                content={() => (
+                  <>
+                    {item.picture ? (
+                      <Image key={`img ${item._id}`} width={200} height={200} src={item.picture} />
+                    ) : (
+                      <Empty description="Imagen no encontrada" />
+                    )}
+                  </>
+                )}>
+                {item.picture ? (
+                  <Avatar key={`img ${item._id}`} src={item.picture} />
+                ) : (
+                  <Avatar icon={<GamepadVariantOutline />} />
+                )}
+              </Popover>
+            </Col>
+          </Row>
+        ),
       },
-    },
-  ];
+      {
+        key: 'name',
+        title: 'Nombre',
+        dataIndex: 'name',
+        ...getColumnSearchProps('name', columnsData),
+      },
+      {
+        key: 'visibility',
+        title: 'Visible',
+        dataIndex: 'showGame',
+        render(_, item) {
+          return (
+            <Switch
+              checkedChildren="Sí"
+              unCheckedChildren="No"
+              onChange={(status) => updateStatusOfGame(status, item.id, availableGames)}
+              checked={item.showGame}
+              id={`editSwitch ${item.index}`}
+            />
+          );
+        },
+      },
+    ];
+
+    setColumns(newColumns)
+  }, [availableGames])
 
   return (
-    <>
-      <Card>
-        <Row style={{ padding: '8px 0px' }}>
-          <Col span={24}>
-            <Form.Item label={'Habilitar juegos'}>
-              <Switch
-                checked={games}
-                onChange={(checked) => {
-                  handleTabsController(checked, 'games'), setShowavailableGames(!showavailableGames);
-                }}
-              />
-            </Form.Item>
-          </Col>
-        </Row>
+    <Card>
+      <Row style={{ padding: '8px 0px' }}>
+        <Col span={24}>
+          <Form.Item label="Habilitar juegos">
+            <Switch
+              checked={isGamesActived}
+              onChange={(isChecked) => agendaContext.setGames(isChecked)}
+            />
+          </Form.Item>
+        </Col>
+      </Row>
 
-        {showavailableGames && (
-          <>
-            {updateMensaje && (
-              <Alert
-                showIcon
-                type='warning'
-                message={'El listado de juegos ha sido actualizado.'}
-                description='Por favor seleccione los juegos que desee visualizar en la zona social de su lección'
-                className='animate__animated animate__bounceIn animate__faster'
-                style={{
-                  boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-                  backgroundColor: '#FFFFFF',
-                  color: '#000000',
-                  borderLeft: '5px solid #FAAD14',
-                  fontSize: '14px',
-                  textAlign: 'start',
-                  borderRadius: '5px',
-                  margin: '15px',
-                }}
-              />
-            )}
+      {isGamesActived && (
+        <>
+          {isAvailableChanges && (
+            <Alert
+              showIcon
+              type="warning"
+              message="El listado de juegos ha sido actualizado."
+              description="Por favor seleccione los juegos que desee visualizar en la zona social de su lección"
+              className="animate__animated animate__bounceIn animate__faster"
+              style={{
+                boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+                backgroundColor: '#FFFFFF',
+                color: '#000000',
+                borderLeft: '5px solid #FAAD14',
+                fontSize: '14px',
+                textAlign: 'start',
+                borderRadius: '5px',
+                margin: '15px',
+              }}
+            />
+          )}
 
-            <Suspense fallback={<div>Cargando...</div>}>
-              <Header
-                title={'Juegos disponibles'}
-                titleTooltip={'Seleccione los juegos que desea mostrar en la zona social'}
-                description='Seleccione los juegos que desea mostrar en la zona social'
-              />
-              <Table
-                key='index'
-                list={listOfGames}
-                header={columns}
-                setColumnsData={setColumnsData}
-                loading={isLoading}
-                search
-              />
-            </Suspense>
-          </>
-        )}
-      </Card>
-    </>
+          <Suspense fallback={<div>Cargando...</div>}>
+            <Header
+              title="Juegos disponibles"
+              titleTooltip="Seleccione los juegos que desea mostrar en la zona social"
+              description="Seleccione los juegos que desea mostrar en la zona social"
+            />
+            <Table
+              key="index"
+              list={availableGames}
+              header={columns}
+              setColumnsData={setColumnsData}
+              loading={isLoading}
+              search
+            />
+          </Suspense>
+        </>
+      )}
+    </Card>
   );
 }
