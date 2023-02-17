01f51197a50bcc77306f2403ddbdd14eb14749ca
David Borja <davidborja456@gmail.com>
2023-02-16 21:52:45 -0500

    Refactoring BackTop

--- src/antdComponents/BackTop.tsx
+++ src/antdComponents/BackTop.tsx
@@ -1,22 +1,18 @@
-import { BackTop as BackTopAnt, Button, Tooltip } from 'antd';
-import { UpOutlined } from '@ant-design/icons';
+import { BackTop as BackTopAnt, Button, Tooltip } from 'antd'
+import { UpOutlined } from '@ant-design/icons'
+import { FunctionComponent } from 'react'
 
-const BackTop = () => {
+const BackTop: FunctionComponent = () => (
+  <BackTopAnt style={{ marginRight: '-90px', marginBottom: '-30px' }}>
+    <Tooltip title="Subir para guardar cambios">
+      <Button 
+        type="primary" 
+        shape="circle"
+        icon={<UpOutlined />}
+        size="large"
+      />
+    </Tooltip>
+  </BackTopAnt>
+)
 
-  return (
-    <>
-      <BackTopAnt style={{marginRight: '-90px', marginBottom: '-30px'}}>
-        <Tooltip title='Subir para guardar cambios'>
-          <Button 
-            type='primary' 
-            shape={'circle'} 
-            icon={<UpOutlined />} 
-            size='large' 
-          />
-        </Tooltip>
-      </BackTopAnt>
-    </>
-  )
-}
-
-export default BackTop;
+export default BackTop
