42d0a414e265e4532f7520f82d4b9bee3f866160
David Borja <davidborja456@gmail.com>
2023-02-20 11:08:34 -0500

    Pull apart the types for the dynamic field stuffs

---
+++ src/components/organization/forms/dynamic-fields/types.ts
@@ -0,0 +1,33 @@
+// This type should be used in the options written in helpers/constants.jsx
+export type FieldType = 'text'
+  | 'country'
+  | 'region'
+  | 'city'
+  | 'longtext'
+  | 'email'
+  | 'number'
+  | 'list'
+  | 'multiplelist'
+  | 'date'
+  | 'boolean'
+  | 'file'
+  | 'complex'
+  | 'tituloseccion'
+  | 'password'
+  | 'multiplelisttable'
+  | 'codearea'
+  | 'onlyCodearea'
+  | 'avatar' // Is it new?
+
+
+export interface IDynamicFieldData {
+  type?: FieldType,
+  props?: any,
+  name: string,
+  label: string,
+  mandatory: boolean,
+  description?: string,
+  labelPosition: any, // NOTE: Check this
+  visibleByAdmin?: boolean,
+  options?: { label: string, value: string }[]
+}
