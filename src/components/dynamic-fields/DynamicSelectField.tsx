8f5a763a4db5b653876364c8e51a5259f0fe43a2
David Borja <davidborja456@gmail.com>
2023-02-20 20:22:25 -0500

    Update the component DynamicSelectField

--- src/components/dynamic-fields/DynamicSelectField.tsx
+++ src/components/dynamic-fields/DynamicSelectField.tsx
@@ -1,5 +1,6 @@
 import { Select } from 'antd';
 import * as React from 'react';
+import { useMemo } from 'react';
 import DynamicFormItem from './DynamicFormItem';
 import useMandatoryRule from './hooks/useMandatoryRule';
 import useSearchInSelectOptions from './hooks/useSearchInSelectComponent';
@@ -12,11 +13,12 @@ type OptionLike = {
 }
 
 interface IDynamicSelectFieldProps extends IDynamicFieldProps {
-  items: any[],
+  items?: any[],
   isLoading?: boolean,
   placeholder?: string,
   onChange?: (value: string, option: any) => void,
-  transformOption: (option: any) => OptionLike,
+  transformOption?: (option: any) => OptionLike,
+  afterTransformOptions?: (options: any[]) => OptionLike[],
 }
 
 const DynamicSelectField: React.FunctionComponent<IDynamicSelectFieldProps> = (props) => {
@@ -26,15 +28,25 @@ const DynamicSelectField: React.FunctionComponent<IDynamicSelectFieldProps> = (p
     fieldData,
     allInitialValues,
     placeholder = '',
-    transformOption,
     onChange = () => {},
+    transformOption = (option) => option,
+    afterTransformOptions = (options) => options,
   } = props
 
-  const { name } = fieldData
+  const {
+    name,
+    options: importedOptions,
+  } = fieldData
 
   const searchInSelectOptions = useSearchInSelectOptions()
 
   const { basicRule } = useMandatoryRule(fieldData)
+
+  const options = useMemo(() => {
+    if (items && items.length > 0) return items
+    if (importedOptions && importedOptions.length > 0 ) return importedOptions
+    return []
+  }, [items, importedOptions])
 
   return (
     <DynamicFormItem
@@ -47,10 +59,10 @@ const DynamicSelectField: React.FunctionComponent<IDynamicSelectFieldProps> = (p
         optionFilterProp="children"
         filterOption={searchInSelectOptions}
         style={{ width: '100%' }}
-        disabled={isLoading || items.length === 0}
+        disabled={isLoading || options.length === 0}
         loading={isLoading}
         placeholder={placeholder}
-        options={items.map((item) => transformOption(item))}
+        options={afterTransformOptions(options.map((option) => transformOption(option)))}
         onChange={onChange}
       />
     </DynamicFormItem>
