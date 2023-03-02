9ea44ff44bb2517590240c5d56528bb0651791d3
Mauricio J Carrillo <mauricio.jcarrillo@gmail.com>
2023-03-02 18:22:54 -0500

    WIP: Enroll user (eventUser) to event since Event Landing

--- src/components/authentication/ModalPermission.jsx
+++ src/components/authentication/ModalPermission.jsx
@@ -5,6 +5,11 @@ import withContext from '@context/withContext';
 import { useHelper } from '@context/helperContext/hooks/useHelper';
 
 import { useIntl } from 'react-intl';
+import { DispatchMessageService } from '@context/MessageService';
+import { useEventContext } from '@context/eventContext';
+import { useUserEvent } from '@context/eventUserContext';
+import { useState } from 'react';
+import { UsersApi } from '@helpers/request';
 const { useBreakpoint } = Grid;
 
 const stylePaddingDesktop = {
@@ -20,6 +25,35 @@ const ModalPermission = (props) => {
   const { handleChangeTypeModal, typeModal } = useHelper();
   const screens = useBreakpoint();
   const intl = useIntl();
+  const cEvent = useEventContext();
+  const cEventUser = useUserEvent();
+
+  const [openModal, setOpenModal] = useState(false);
+
+  async function saveEventUser(values) {
+    const eventUserBody = {
+      properties: { ...values },
+    };
+
+    //const resp = await UsersApi.editEventUser(eventUserBody, cEvent.value?._id, cEventUser.value._id);
+    const resp = await UsersApi.createOne(eventUserBody, cEvent.value?._id);
+
+    if (resp._id) {
+      DispatchMessageService({
+        type: 'success',
+        msj: `Usuario editado correctamente`,
+        action: 'show',
+      });
+      cEventUser.setUpdateUser(true);
+      setOpenModal(false);
+    } else {
+      DispatchMessageService({
+        type: 'error',
+        msj: `No fue posible editar el Usuario`,
+        action: 'show',
+      });
+    }
+  }
 
   return (
     <Modal
@@ -41,7 +75,7 @@ const ModalPermission = (props) => {
           paddingTop: '8px',
           paddingBottom: '8px',
         }}>
-        <FormComponent />
+        <FormComponent callback={saveEventUser}/>
       </div>
     </Modal>
   );
