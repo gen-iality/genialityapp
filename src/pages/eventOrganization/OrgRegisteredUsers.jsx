9fb65ba55f85c8a6b70c7264a7488ae9d07a7695
David Borja <davidborja456@gmail.com>
2023-03-09 14:25:17 -0500

    Show dynamic field in registered organization member too

--- src/pages/eventOrganization/OrgRegisteredUsers.jsx
+++ src/pages/eventOrganization/OrgRegisteredUsers.jsx
@@ -41,20 +41,23 @@ function OrgRegisteredUsers(props) {
 
   const getRegisteredUsers = async () => {
     const { data: orgEvents } = await OrganizationApi.events(organizationId);
-    console.log('orgEvents', orgEvents);
+    // con//sole.log('orgEvents', orgEvents);
+    const { data: orgUsers } = await OrganizationApi.getUsers(organizationId);
+    // con//sole.log('orgUsers', orgUsers)
 
     const totalRegistered = (
       await Promise.all(
         orgEvents.map(async (orgEvent) => {
           const asistentes = firestore.collection(`${orgEvent._id}_event_attendees`);
           const querySnapshot = await asistentes.get();
-          console.log('querySnapshot', querySnapshot);
+          // con//sole.log('querySnapshot', querySnapshot);
 
           const certificationsByEvent = await CerticationsApi.getByUserAndEvent(null, orgEvent._id);
-          console.log('1. certificationsByEvent', certificationsByEvent);
+          // con//sole.log('1. certificationsByEvent', certificationsByEvent);
 
           const registeredByEvent = querySnapshot.docs.map((doc) => {
             const infoEventUser = doc.data();
+            // con//sole.log('infoEventUser', infoEventUser)
             const properties = {
               checkedin_at: infoEventUser?.checkedin_at
                 ? formattedRealDate(infoEventUser?.checkedin_at)
@@ -64,12 +67,23 @@ function OrgRegisteredUsers(props) {
               validity_date: null,
               event_name: orgEvent.name,
               created_at: orgEvent.created_at,
+              // Why "organiser"? anyway...
             };
+
+            const userId = infoEventUser.account_id
+            const orgMember = orgUsers.find((member) => member.account_id === userId)
+
+            // Inject the dynamic field
+            const filteredDynamicField = (orgEvent.organiser.user_properties || []).filter((data) => !['email', 'names'].includes(data.name))
+            if (orgMember) {
+              filteredDynamicField.forEach((field) => {
+                properties[field.name] = orgMember.properties[field.name]
+              })
+            }
 
             const userCertification = certificationsByEvent.find(
               (certificationByEvent) => certificationByEvent.user_id === infoEventUser?.account_id,
             );
-            console.log('1. certification', userCertification);
             if (userCertification?.approved_until_date) {
               properties.validity_date = dayjs(userCertification.approved_until_date);
             }
