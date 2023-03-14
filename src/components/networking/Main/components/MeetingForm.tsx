9c582f020f1cb0657d1a1dabc7fad94bdb76d708
lortiz24 <luis.ortiz@evius.co>
2023-03-14 14:45:05 -0500

    Se devuelve a tsx el componente MeetingForm

--- src/components/networking/Main/components/MeetingForm.tsx
+++ src/components/networking/Main/components/MeetingForm.tsx
@@ -1,39 +1,46 @@
-import { useState, Fragment, createRef } from 'react';
-import { Form, Input, Button, Row, Transfer, DatePicker } from 'antd';
-import { UsuariosArray } from '../interfaces/MeetingForm.interface';
+import React, { useState, Fragment, createRef } from 'react';
+import { Form, Input,  Button, Row, Transfer, DatePicker } from 'antd';
+import type { TransferDirection } from 'antd/es/transfer';
+import { PropsMeetingForm, TransferType, UsuariosArray } from '../interfaces/MeetingForm.interface';
 const formLayout = {
   labelCol: { span: 24 },
   wrapperCol: { span: 24 },
 };
 
-const filterOption = (inputValue, option) => {
-  return option.title.toLowerCase().indexOf(inputValue.toLowerCase()) > -1;
-};
 
-const mockData = Array.from({ length: 20 }).map((_, i) => ({
+
+const filterOption=(inputValue:string, option:TransferType)=> {
+  return option.title.toLowerCase().indexOf(inputValue.toLowerCase()) > -1;
+}
+
+
+const mockData: TransferType[] = Array.from({ length: 20 }).map((_, i) => ({
   key: i.toString(),
   title: `content${i + 1}`,
   description: `description of content${i + 1}`,
 }));
 
-const initialTargetKeys = [];
+const initialTargetKeys:string[] =[] ;
 
-export default function MeetingForm({ cancel }) {
+
+export default function MeetingForm({ cancel }: PropsMeetingForm) {
   const [form] = Form.useForm();
-  const formRef = createRef();
+  const formRef = createRef<any>();
   const [targetKeys, setTargetKeys] = useState(initialTargetKeys);
-  const [selectedKeys, setSelectedKeys] = useState([]);
+  const [selectedKeys, setSelectedKeys] = useState<string[]>([]);
 
-  const onChange = (nextTargetKeys, direction, moveKeys) => {
+  const onChange = (nextTargetKeys: string[], direction: TransferDirection, moveKeys: string[]) => {
     setTargetKeys(nextTargetKeys);
   };
 
-  const onSelectChange = (sourceSelectedKeys, targetSelectedKeys) => {
-    console.log({ sourceSelectedKeys, targetSelectedKeys });
+  const onSelectChange = (sourceSelectedKeys: string[], targetSelectedKeys: string[]) => {
+   console.log({sourceSelectedKeys,targetSelectedKeys})
     setSelectedKeys([...sourceSelectedKeys, ...targetSelectedKeys]);
   };
 
-  const onScroll = (direction, e) => {};
+  const onScroll = (direction: TransferDirection, e: React.SyntheticEvent<HTMLUListElement>) => {
+  };
+
 
   return (
     <Fragment>
@@ -66,41 +73,33 @@ export default function MeetingForm({ cancel }) {
             onChange={onChange}
             onSelectChange={onSelectChange}
             onScroll={onScroll}
-            render={(item) => item.title}
+            render={item => item.title}
           />
+          
         </Form.Item>
-
+       
         <Form.Item
           label={'Fecha reunion'}
           name='fecha'
           rules={[{ required: true, message: 'Es necesario seleccionar una fecha' }]}>
-          <DatePicker
-            inputReadOnly={true}
-            style={{ width: '100%' }}
-            allowClear={false}
-            format={'DD/MM/YYYY'}
-          />
+            {/* @ts-ignore */} 
+          <DatePicker inputReadOnly={true} style={{ width: '100%' }} allowClear={false} format={'DD/MM/YYYY'} />
         </Form.Item>
         <Form.Item
-          label={'Fecha reunion'}
-          name='fecha'
-          rules={[{ required: true, message: 'Es necesario seleccionar una fecha' }]}>
-          <DatePicker
-            inputReadOnly={true}
-            style={{ width: '100%' }}
-            allowClear={false}
-            format={'DD/MM/YYYY'}
-          />
+          label={'Lugar'}
+          name='lugar'
+          rules={[{ required: true, message: 'Es necesario seleccionar el lugar de la reunion' }]}>
+          <Input ref={formRef} name={'lugar'} type='text' placeholder={'Ej: Salon principal'} />
         </Form.Item>
 
         <Row style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-          <Button type='primary' style={{ marginRight: 10 }} htmlType='submit'>
-            Guardar
-          </Button>
-          <Button onClick={cancel} type='default' style={{ marginRight: 10 }}>
-            Cancelar
-          </Button>
-        </Row>
+            <Button type='primary' style={{ marginRight: 10 }} htmlType='submit'>
+              Guardar
+            </Button>
+            <Button onClick={cancel} type='default' style={{ marginRight: 10 }}>
+              Cancelar
+            </Button>
+          </Row>
       </Form>
     </Fragment>
   );
