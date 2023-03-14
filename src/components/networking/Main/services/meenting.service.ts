e54640b639a992207ce61d366afde1498239fb0c
Juan Camayo <juan.camayo@evius.com>
2023-03-14 15:13:11 -0500

    Add - added rest of services

--- src/components/networking/Main/services/meenting.service.ts
+++ src/components/networking/Main/services/meenting.service.ts
@@ -1,14 +1,24 @@
 import { firestore } from '@/helpers/firebase';
 
-export const getMeentings = () => {};
-
-export const getAttendees = (eventId: string, setAttendees: any) => {
-	return firestore.collection(`${eventId}_event_attendees`).onSnapshot(snaphot => {
-		if (!snaphot.empty) {
-			const data = snaphot.docs.map(doc => ({ id: doc.id, ...doc.data() })) as {};
+export const listenAttendees = (eventId: string, setAttendees: any) => {
+	return firestore.collection(`${eventId}_event_attendees`).onSnapshot(snapshot => {
+		if (!snapshot.empty) {
+			const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })) as {};
 			setAttendees(data);
 		}
 	});
+};
+
+export const listenMeetings = (eventId: string, setMeetings: any) => {
+	return firestore
+		.collection(`networkingByEventId`)
+		.doc(eventId)
+		.collection('meetings').onSnapshot(snapshot => {
+      if (!snapshot.empty) {
+        const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })) as {};
+        setMeetings(data);
+      }
+    })
 };
 
 export const createMeeting = async (eventId: string, createMeetingDto: any) => {
@@ -18,7 +28,33 @@ export const createMeeting = async (eventId: string, createMeetingDto: any) => {
 			.doc(eventId)
 			.collection('meetings')
 			.doc()
-			.set(createMeeting);
+			.set(createMeetingDto);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export const updateMeeting = async (eventId: string, meetingId: string, updateMeetingDto: any) => {
+	try {
+		await firestore
+			.collection(`networkingByEventId`)
+			.doc(eventId)
+			.collection('meetings')
+			.doc(meetingId)
+			.update(updateMeetingDto);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export const deleteMeeting = async (eventId: string, meetingId: string) => {
+	try {
+		await firestore
+			.collection(`networkingByEventId`)
+			.doc(eventId)
+			.collection('meetings')
+			.doc(meetingId)
+			.delete();
 	} catch (error) {
 		console.log(error);
 	}
