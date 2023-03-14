b149647a8a9b4301f69abc58923af1f1ab832469
Juan Camayo <juan.camayo@evius.com>
2023-03-14 15:06:13 -0500

    Add - added some services for attendees and create meeting

--- src/components/networking/Main/services/meenting.service.ts
+++ src/components/networking/Main/services/meenting.service.ts
@@ -1,6 +1,25 @@
-import { fireStorage } from '@/helpers/firebase';
+import { firestore } from '@/helpers/firebase';
 
-export const getMeentings = () => {
+export const getMeentings = () => {};
 
-}
+export const getAttendees = (eventId: string, setAttendees: any) => {
+	return firestore.collection(`${eventId}_event_attendees`).onSnapshot(snaphot => {
+		if (!snaphot.empty) {
+			const data = snaphot.docs.map(doc => ({ id: doc.id, ...doc.data() })) as {};
+			setAttendees(data);
+		}
+	});
+};
 
+export const createMeeting = async (eventId: string, createMeetingDto: any) => {
+	try {
+		await firestore
+			.collection(`networkingByEventId`)
+			.doc(eventId)
+			.collection('meetings')
+			.doc()
+			.set(createMeeting);
+	} catch (error) {
+		console.log(error);
+	}
+};
