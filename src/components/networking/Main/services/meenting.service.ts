32df4bbdeb39c823caee2d05b7ab9f5d8fd1d161
carlos rubio <86839953+gitrubio@users.noreply.github.com>
2023-03-15 14:47:20 -0500

    fix : asincronismo y firebase

--- src/components/networking/Main/services/meenting.service.ts
+++ src/components/networking/Main/services/meenting.service.ts
@@ -6,6 +6,8 @@ export const listenAttendees = (eventId: string, setAttendees: any) => {
 		if (!snapshot.empty) {
 			const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })) as {};
 			setAttendees(data);
+		} else {
+			setAttendees([])
 		}
 	});
 };
@@ -19,7 +21,9 @@ export const listenMeetings = (eventId: string, setMeetings: any) => {
       if (!snapshot.empty) {
         const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })) as {};
         setMeetings(data);
-      }
+      } else {
+		setMeetings([])
+	  }
     })
 };
 
