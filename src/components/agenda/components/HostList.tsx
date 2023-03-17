929b3c9a035675ea12af6d85e6aa0d8034df9e34
Mauricio J Carrillo <mauricio.jcarrillo@gmail.com>
2023-03-17 11:51:50 -0500

    Formatted file with prettier and commenting imports

--- src/components/agenda/components/HostList.tsx
+++ src/components/agenda/components/HostList.tsx
@@ -1,33 +1,40 @@
-import { List, Avatar } from 'antd'
-import { useEffect, useState } from 'react'
-import { useEventContext } from '@context/eventContext'
-import { SpeakersApi, ToolsApi } from '@helpers/request'
+/** React's libraries */
+import { useEffect, useState } from 'react';
+
+/** Antd imports */
+import { List, Avatar } from 'antd';
+
+/** Helpers and utils */
+import { SpeakersApi, ToolsApi } from '@helpers/request';
+
+/** Context */
+import { useEventContext } from '@context/eventContext';
 
 const dataDuration = [
   {
     title: '5 hora de contenido',
-  }
-]
+  },
+];
 
 const HostList = () => {
-  const cEvent = useEventContext()
-  const [speakers, setSpeakers] = useState<any[]>([])
-  const [tools, setTools] = useState<any[]>([])
+  const cEvent = useEventContext();
+  const [speakers, setSpeakers] = useState<any[]>([]);
+  const [tools, setTools] = useState<any[]>([]);
 
   useEffect(() => {
     (async () => {
-      const speakersApi = await SpeakersApi.byEvent(cEvent.value._id)
-      setSpeakers(speakersApi)
-    })()
-  }, [cEvent.value])
+      const speakersApi = await SpeakersApi.byEvent(cEvent.value._id);
+      setSpeakers(speakersApi);
+    })();
+  }, [cEvent.value]);
 
   useEffect(() => {
     (async () => {
-      const toolsApi = await ToolsApi.byEvent(cEvent.value._id)
+      const toolsApi = await ToolsApi.byEvent(cEvent.value._id);
       console.log('toolsApi', toolsApi);
-      setTools(toolsApi)
-    })()
-  }, [cEvent.value])
+      setTools(toolsApi);
+    })();
+  }, [cEvent.value]);
 
   return (
     <>
@@ -47,7 +54,13 @@ const HostList = () => {
         dataSource={tools}
         renderItem={(item) => (
           <List.Item>
-            {item.link ? (<a href={item.link} target="_blank">{item.name}</a>) : (<p style={{ margin: 0, padding: 0, lineHeight: 1 }}>{item.name}</p>)}
+            {item.link ? (
+              <a href={item.link} target="_blank">
+                {item.name}
+              </a>
+            ) : (
+              <p style={{ margin: 0, padding: 0, lineHeight: 1 }}>{item.name}</p>
+            )}
           </List.Item>
         )}
       />
@@ -70,6 +83,6 @@ const HostList = () => {
         )}
       />
     </>
-  )
-}
-export default HostList
+  );
+};
+export default HostList;
