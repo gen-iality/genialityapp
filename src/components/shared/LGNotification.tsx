42cf1ec587e688e248ad585ad2d7fe5945af750f
Juan Camayo <juan.camayo@evius.com>
2023-03-17 11:37:18 -0500

    Add - added some config in LGNotification and production lg event id

--- src/components/shared/LGNotification.tsx
+++ src/components/shared/LGNotification.tsx
@@ -18,11 +18,20 @@ export default function LGNotification() {
 	const history = useHistory();
 
 	const eventId = (eventContext.value?._id as string) || null;
+	const isLanding = history.location.pathname === `/landing/${eventId}/evento`;
+	const isPreLading = history.location.pathname === `/${eventId}`;
+	const isLGEvent = !!eventId ? LG_EVENT_IDS.includes(eventId) : false;
+	const isInitial = state === State.INITIAL;
+	const isRegistered = !!eventUserContext.value?._id;
+	const hasRegistered = state === State.REGISTERED;
 
 	const openNotification = () => {
 		notification.open({
 			message: '¡YA TE HAS REGISTRADO!',
-			description: `Verifica tu correo electrónico, llegará un código QR con el cuál podrás acceder de manera presencial al evento. Recuerda que tener el código QR no asegura el ingreso al lugar del evento, tendremos aforo limitado para esta gran experiencia.`,
+			duration: 0,
+			description: isLGEvent
+				? `Verifica tu correo electrónico, llegará un código QR con el cuál podrás acceder de manera presencial al evento. Recuerda que tener el código QR no asegura el ingreso al lugar del evento, tendremos aforo limitado para esta gran experiencia.`
+				: `Verifica tu correo electrónico, llegará un email con toda la información del evento.`,
 			onClick: () => {
 				console.log('Notification Clicked!');
 			},
@@ -31,13 +40,6 @@ export default function LGNotification() {
 			},
 		});
 	};
-
-	const isLanding = history.location.pathname === `/landing/${eventId}/evento`;
-	const isPreLading = history.location.pathname === `/${eventId}`;
-	const isLGEvent = !!eventId ? LG_EVENT_IDS.includes(eventId) : false;
-	const isInitial = state === State.INITIAL;
-	const isRegistered = !!eventUserContext.value?._id;
-	const hasRegistered = state === State.REGISTERED;
 
 	useEffect(() => {
 		if ((isLanding || isPreLading) && isLGEvent && isRegistered && (isInitial || hasRegistered)) {
