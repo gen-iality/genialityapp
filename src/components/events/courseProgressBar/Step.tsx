e83c1ba5927dc61ed6d9f9cfae4c8063d09c0fd5
Mauricio J Carrillo <mauricio.jcarrillo@gmail.com>
2023-03-22 11:22:49 -0500

    Active the next activity step when clickig the activity next button

--- src/components/events/courseProgressBar/Step.tsx
+++ src/components/events/courseProgressBar/Step.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
 import { useMemo, memo, useEffect, useState } from 'react';
+import { useLocation, useParams, useRouteMatch } from 'react-router';
 import './Step.css';
 
 export interface StepProps {
@@ -17,12 +18,25 @@ export interface StepProps {
 function Step(props: StepProps) {
   const { children, isActive, isSurvey, key, currentId, setCurrentId, id, ...rest } = props;
 
+  const location = useLocation();
+  console.log('location', location);
+
+  const params = useParams();
+  console.log('params', params);
+
   const className = useMemo(() => {
     if (isActive) {
       return 'Step active';
     }
     return 'Step';
   }, [isActive]);
+
+  useEffect(() => {
+    const urlCompleta = location.pathname;
+    const urlSplited = urlCompleta.split('activity/');
+    const currentActivityId = urlSplited[1];
+    setCurrentId(currentActivityId);
+  }, [location]);
 
   return (
     <div
