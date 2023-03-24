68db1985ba5329fbe391f1bd1fd852e16985e7cc
Mauricio J Carrillo <mauricio.jcarrillo@gmail.com>
2023-03-24 07:46:19 -0500

    Formatted file with prettier and commenting imports

--- src/components/dynamic-fields/DynamicTermsAndCondictionsField.tsx
+++ src/components/dynamic-fields/DynamicTermsAndCondictionsField.tsx
@@ -1,66 +1,65 @@
-import { Checkbox, Typography } from 'antd';
-import { Rule } from 'antd/lib/form';
+/** React's libraries */
 import * as React from 'react';
 import { useEffect, useMemo, useState } from 'react';
 import { useIntl } from 'react-intl';
-import DynamicFormItem from './DynamicFormItem';
-import useMandatoryRule from './hooks/useMandatoryRule';
-import { IDynamicFieldProps } from './types';
 
-interface IDynamicTermsAndCondictionsFieldProps extends IDynamicFieldProps {
-}
+/** Antd imports */
+import { Checkbox, Typography } from 'antd';
+import { Rule } from 'antd/lib/form';
+
+/** Hooks, helpers and utils */
+import { IDynamicFieldProps } from './types';
+import useMandatoryRule from './hooks/useMandatoryRule';
+
+/** Components */
+import DynamicFormItem from './DynamicFormItem';
+
+interface IDynamicTermsAndCondictionsFieldProps extends IDynamicFieldProps {}
 
 const DynamicTermsAndCondictionsField: React.FunctionComponent<IDynamicTermsAndCondictionsFieldProps> = (props) => {
-  const {
-    fieldData,
-    allInitialValues,
-  } = props
+  const { fieldData, allInitialValues } = props;
 
-  const {
-    name,
-    mandatory,
-    label,
-    link,
-    props: secondProps,
-  } = fieldData
-  
-  const [rules, setRules] = useState<Rule[]>([])
+  const { name, mandatory, label, link, props: secondProps } = fieldData;
 
-  const intl = useIntl()
-  
-  const requiredFieldErrorMessage = intl.formatMessage({ id: 'form.field.required' })
-  
-  const {basicRule, setCondiction} = useMandatoryRule(fieldData, requiredFieldErrorMessage)
+  const [rules, setRules] = useState<Rule[]>([]);
+
+  const intl = useIntl();
+
+  const requiredFieldErrorMessage = intl.formatMessage({ id: 'form.field.required' });
+
+  const { basicRule, setCondiction } = useMandatoryRule(fieldData, requiredFieldErrorMessage);
 
   // Clone the basic rule and inject the type for email type
   useEffect(() => {
-    const newRule: Rule = { ...basicRule }
-    newRule.validator = (rule, value) => {
+    const newRule: Rule = { ...basicRule };
+    (newRule.validator = (rule, value) => {
       if (mandatory) {
-        return value
-          ? Promise.resolve()
-          : Promise.reject(requiredFieldErrorMessage)
+        return value ? Promise.resolve() : Promise.reject(requiredFieldErrorMessage);
       } else {
         return value || !value || value == '' || value == undefined
           ? Promise.resolve()
-          : Promise.reject(requiredFieldErrorMessage)
+          : Promise.reject(requiredFieldErrorMessage);
       }
-    },
-    setRules([newRule])
-  }, [basicRule])
+    }),
+      setRules([newRule]);
+  }, [basicRule]);
 
   // Create a copy of the fieldData object without the label property
   const fieldDataWithoutLabel = useMemo(() => {
-    const newFieldData = { ...fieldData }
-    newFieldData.label = ''
-    return newFieldData
-  }, [fieldData])
+    const newFieldData = { ...fieldData };
+    newFieldData.label = '';
+    return newFieldData;
+  }, [fieldData]);
 
   const TTCC = () => (
     <Typography.Text>
-      Acepta que he leído y entendido los <Typography.Link target="_blank" href={link || '#'}>términos y condiciones</Typography.Link>.
+      Acepta que he leído y entendido los{' '}
+      <Typography.Link target="_blank" href={link || '#'}>
+        términos y condiciones
+      </Typography.Link>
+      .
     </Typography.Text>
-  )
+  );
 
   return (
     <DynamicFormItem
@@ -76,16 +75,17 @@ const DynamicTermsAndCondictionsField: React.FunctionComponent<IDynamicTermsAndC
       >
         {mandatory ? (
           <span>
-            <span style={{ color: 'red' }}>*</span>
-            {' '}
-            <strong><TTCC /></strong>
+            <span style={{ color: 'red' }}>*</span>{' '}
+            <strong>
+              <TTCC />
+            </strong>
           </span>
         ) : (
           <TTCC />
         )}
       </Checkbox>
     </DynamicFormItem>
-  )
+  );
 };
 
 export default DynamicTermsAndCondictionsField;
