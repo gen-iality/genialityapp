caafdf7c919dd299efe7e95edd993e5505d6049e
carlos rubio <86839953+gitrubio@users.noreply.github.com>
2023-03-30 08:12:40 -0500

    add : retirando funcionalidad

--- src/components/networking/components/MeetingList.tsx
+++ src/components/networking/components/MeetingList.tsx
@@ -1,17 +1,17 @@
 import { Card, Col, Input, Row } from 'antd';
-import React from 'react';
+import React, { useState } from 'react';
 import MeetingItem from './MeetingItem';
-import { IMeetingList } from '../interfaces/Meetings.interfaces';
+import { IMeetingList, IMeeting } from '../interfaces/Meetings.interfaces';
 const { Search } = Input
 export default function MeetingList({ meentings }: IMeetingList) {
 
   return (
     <Card headStyle={{ border: 'none' }} bodyStyle={{ paddingTop: '0px' }}>
       <Col span={12} style={{ margin: 10}}>
-      <Search  placeholder="input search text" onSearch={()=>{}} enterButton />
+      <Search  placeholder="input search text" onSearch={(filter)=>{}} enterButton />
       </Col>
       <Row gutter={[0, 8]}>
-        {meentings?.map((meenting, key) => (
+        {meentings.map((meenting, key) => (
           <Col key={key} span={24} >
             <MeetingItem meenting={meenting} />
           </Col>
