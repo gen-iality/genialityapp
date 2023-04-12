bbc10e6043545444f37f2c13d403c2276eabdc94
David Borja <davidborja456@gmail.com>
2023-04-12 14:08:15 -0500

    Check it out: improvement of createNewUser

--- src/components/authentication/ModalsFunctions/createNewUser.tsx
+++ src/components/authentication/ModalsFunctions/createNewUser.tsx
@@ -3,62 +3,93 @@ import { UsersApi } from '@helpers/request';
 import { message } from 'antd';
 import { DispatchMessageService } from '@context/MessageService';
 
-const createNewUser = async (props) => {
-  const { picture, email, names, password, resetFields, setModalInfo, setOpenOrCloseTheModalFeedback, cEvent } = props;
-  const pictureDefault = 'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y';
-  function sendDataFinished() {
-    DispatchMessageService({
-      key: 'loading',
-      action: 'destroy',
-    });
-    resetFields();
-  }
-  const uploadLogo = async () => {
-    const selectedLogo = picture && picture !== '' ? picture[0].thumbUrl : null;
+export const CREATE_NEW_USER_FAIL = 'CREATE_NEW_USER_FAIL'
+export const CREATE_NEW_USER_SUCCESS = 'CREATE_NEW_USER_SUCCESS'
+export const CREATE_NEW_USER_FAIL_BECAUSE_EMAIL = 'CREATE_NEW_USER_FAIL_BECAUSE_EMAIL'
 
-    if (selectedLogo) {
-      const urlOfTheUploadedImage = await saveImageStorage(selectedLogo);
+export type UserDataType = {
+  picture: any,
+  email: string,
+  names: string,
+  password: string,
+}
 
+export type CreateNewUserResultType = {
+  user: any | null,
+  status: string,
+  message: string,
+}
+
+/**
+ * Upload the picture to FireStorage and return the picture URL.
+ * 
+ * @param picture A picture object that has to have a thumbUrl attrib.
+ * @param defaultPictureURL A default picture URL.
+ * @returns Final picture URL.
+ */
+const uploadLogo = async (picture: any, defaultPictureURL: string): Promise<string> => {
+  const selectedLogo = picture?.[0]?.thumbUrl ?? null;
+
+  if (selectedLogo) {
+    const urlOfTheUploadedImage = await saveImageStorage(selectedLogo);
+
+    if (urlOfTheUploadedImage !== null)
       return urlOfTheUploadedImage;
-    }
-    return pictureDefault;
+  }
+
+  return defaultPictureURL;
+};
+
+/**
+ * Create a new user and return an user registration status.
+ * @param userData Info abot the user registration process.
+ * @returns An object that contains: user, status and message
+ */
+const createNewUser = async (userData: UserDataType, onFinish?: () => void): Promise<CreateNewUserResultType> => {
+  const { picture, email, names, password } = userData;
+
+  const defaultPicture = 'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y';
+
+  const pictureURL = await uploadLogo(picture, defaultPicture);
+
+  const body = {
+    picture: pictureURL,
+    email,
+    names,
+    password: password || undefined,
   };
 
-  const sendData = async () => {
-    const imageUrl = await uploadLogo();
+  let isErrorByExistentEmail = false
+  let errorMessage = 'Error inesperado al registrar el usuario'
 
-    const body = {
-      picture: imageUrl,
-      email: email,
-      names: names,
-      password: password !== '' ? password : undefined,
-    };
-    try {
-      const response = await UsersApi.createUser(body);
-      if (response._id) {
-        /* setModalInfo({
-          status: 'success',
-          title: `Bienvenido ${response.names}`,
-          description: 'Tu registro ha sido exitoso',
-        });
-      setOpenOrCloseTheModalFeedback(true);
-      sendDataFinished();*/
-        return response; // HAHA! I remember when Juan change 1 for `response`, now some bugs happen
-      }
-    } catch (e) {
-      // Permite validar cuando el email es incorrecto
-      if (e.response.status == 422 && e.response.data.errors.email[0] !== 'email ya ha sido registrado.') {
-        return 2;
-      }
-      const registeredEmail = e.response.data.errors.email[0];
-      if (registeredEmail === 'email ya ha sido registrado.') {
-        return 0;
-      }
+  try {
+    const response = await UsersApi.createUser(body);
+    if (response._id) {
+      onFinish && onFinish()
+      // HAHA! I remember when Juan change 1 for `response`, now some bugs happen
+      return {
+        user: response,
+        status: CREATE_NEW_USER_SUCCESS,
+        message: 'Usuario registrado',
+      };
     }
-  };
+  } catch (e: any) {
+    // Permite validar cuando el email es incorrecto
+    const registeredEmail = e.response.data.errors.email[0];
+    errorMessage = registeredEmail
 
-  const resp = await sendData();
-  return resp;
+    if (e.response.status == 422 && registeredEmail !== 'email ya ha sido registrado.') {
+      isErrorByExistentEmail = false;
+    } else if (registeredEmail === 'email ya ha sido registrado.') {
+      isErrorByExistentEmail = true;
+    }
+  }
+
+  return {
+    user: null,
+    status: isErrorByExistentEmail ? CREATE_NEW_USER_FAIL_BECAUSE_EMAIL : CREATE_NEW_USER_FAIL,
+    message: errorMessage,
+  };
 };
 
 export default createNewUser;
