54fba7a89e4bd3ba2484c81767f9bfb727a74a7d
carlos rubio <86839953+gitrubio@users.noreply.github.com>
2023-04-13 09:29:58 -0500

    add :  cambiando un poco los estilos

--- src/components/networking/components/Configurations/ConfigMeet.tsx
+++ src/components/networking/components/Configurations/ConfigMeet.tsx
@@ -6,30 +6,30 @@ import Toolbar from '../../../agenda/activityType/components/manager/eviusMeet/T
 import { generalItems } from '../../../agenda/activityType/components/manager/eviusMeet/generalItems';
 import { MeetConfig } from '../../interfaces/Index.interfaces';
 import { INITIAL_MEET_CONFIG } from '../../utils/utils';
+import { PlusCircleOutlined } from '@ant-design/icons';
 
 const { useBreakpoint } = Grid;
 
-
 export interface ElementProps {
-	meetConfig: MeetConfig;
-	setMeetConfig: React.Dispatch<React.SetStateAction<MeetConfig>>;
+  meetConfig: MeetConfig;
+  setMeetConfig: React.Dispatch<React.SetStateAction<MeetConfig>>;
 }
 
 export interface ShareMeetLinkCardProps {
-	activityId: string;
+  activityId: string;
 }
 
 export default function ConfigMeet() {
-	const eventContext = UseEventContext();
-	const activityId = '1';
-	const eventId = eventContext?.idEvent;
-	const [meetConfig, setMeetConfig] = useState<MeetConfig>(INITIAL_MEET_CONFIG);
-	const [loading, setLoading] = useState(false);
-	const [open, setOpen] = useState(false);
-	const screens = useBreakpoint();
+  const eventContext = UseEventContext();
+  const activityId = '1';
+  const eventId = eventContext?.idEvent;
+  const [meetConfig, setMeetConfig] = useState<MeetConfig>(INITIAL_MEET_CONFIG);
+  const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+  const screens = useBreakpoint();
 
-	const updateMeeting = async (eventId: string, activityId: string, status: boolean) => {
-/* 		try {
+  const updateMeeting = async (eventId: string, activityId: string, status: boolean) => {
+    /* 		try {
 			console.log(`events/${eventId}/activities/${activityId}`);
 			const newMeetConfig = status ? { ...meetConfig, openMeet: status } : INITIAL_MEET_CONFIG;
 
@@ -45,68 +45,67 @@ export default function ConfigMeet() {
 		} finally {
 			setLoading(false);
 		} */
-	};
+  };
 
-	const handleOpenModal = () => setOpen(true);
+  const handleOpenModal = () => setOpen(true);
 
-	const handleCloseModal = () => setOpen(false);
+  const handleCloseModal = () => setOpen(false);
 
-	const handleOpenMeeting = async () => {
-/* 		await updateMeeting(eventId, activityId, true);
+  const handleOpenMeeting = async () => {
+    /* 		await updateMeeting(eventId, activityId, true);
 		handleCloseModal(); */
-	};
+  };
 
-	const handleCloseMeeting = async () => {
-	/* 	await updateMeeting(eventId, activityId, false); */
-	};
+  const handleCloseMeeting = async () => {
+    /* 	await updateMeeting(eventId, activityId, false); */
+  };
 
-	return (
-		<>
-<Row>
-						<Col xs={24}>
-							<Tabs>
-								<Tabs.TabPane
-									className={!screens.xs ? 'desplazar' : ''}
-									style={{ height: '60vh', overflowY: 'auto' }}
-									tab='General'
-									key='item-general'>
-									<Form layout='vertical'>
-										<Card bordered={false}>
-											<List
-												size='small'
-												dataSource={generalItems}
-												renderItem={(option: any) => (
-													<List.Item
-														style={{ padding: '0px' }}
-														key={option.key}
-														extra={
-															<Form.Item style={{ margin: '10px' }}>
-																{option.element({ meetConfig, setMeetConfig })}
-															</Form.Item>
-														}>
-														<List.Item.Meta title={option.label} />
-													</List.Item>
-												)}
-											/>
-										</Card>
-									</Form>
-								</Tabs.TabPane>
-								<Tabs.TabPane
-									className={!screens.xs ? 'desplazar' : ''}
-									style={{ overflowY: 'auto', height: 380 }}
-									tab='Toolbar'
-									key='item-toolbar'>
-								
-									<Toolbar
-										values={meetConfig.config.toolbarButtons}
-										onChange={(list : any) =>
-											setMeetConfig(prev => ({ ...prev, config: { ...prev.config, toolbarButtons: list } }))
-										}
-									/>
-								</Tabs.TabPane>
-							</Tabs>
-						</Col>
-					</Row>
-		</>
-	);
+  return (
+    <>
+      <Row>
+        <Col xs={24} >
+          <Tabs>
+            <Tabs.TabPane
+              className={!screens.xs ? 'desplazar' : ''}
+              style={{ height: '60vh', overflowY: 'auto' }}
+              tab='General'
+              key='item-general'>
+              <Form layout='vertical'>
+                <Card bordered={false}>
+                  <List
+                    size='small'
+                    dataSource={generalItems}
+                    renderItem={(option: any) => (
+                      <List.Item
+                        style={{ padding: '0px' }}
+                        key={option.key}
+                        extra={
+                          <Form.Item style={{ margin: '10px' }}>
+                            {option.element({ meetConfig, setMeetConfig })}
+                          </Form.Item>
+                        }>
+                        <List.Item.Meta title={option.label} />
+                      </List.Item>
+                    )}
+                  />
+                </Card>
+              </Form>
+            </Tabs.TabPane>
+            <Tabs.TabPane
+              className={!screens.xs ? 'desplazar' : ''}
+              style={{ overflowY: 'auto', height: 380 }}
+              tab='Toolbar'
+              key='item-toolbar'>
+              <Toolbar
+                values={meetConfig.config.toolbarButtons}
+                onChange={(list: any) =>
+                  setMeetConfig((prev) => ({ ...prev, config: { ...prev.config, toolbarButtons: list } }))
+                }
+              />
+            </Tabs.TabPane>
+          </Tabs>
+        </Col>
+      </Row>
+    </>
+  );
 }
--- src/components/networking/views/Configuration.view.tsx
+++ src/components/networking/views/Configuration.view.tsx
@@ -8,33 +8,33 @@ import ConfigMeet from '../components/Configurations/ConfigMeet';
 export default function Configuration() {
   return (
     <>
-      <Row justify='center' gutter={[32, 32]} style={{ marginBottom: 10 }} wrap >
-        <Col xs={24} sm={12} md={12} lg={6} xl={6} xxl={6}>
-          <Card hoverable style={{height: '100%', backgroundColor: '#FDFEFE'}}>
+      <Row justify='start' gutter={[32, 32]} style={{ marginBottom: 10 }} wrap >
+        <Col xs={22} sm={16} md={16} lg={12} xl={8} xxl={6}>
+          <Card hoverable style={{ height: 550, maxWidth: 600, backgroundColor: '#FDFEFE'}}>
             <Divider orientation='left'>
               <Typography.Title level={5}>Configuración de Meet</Typography.Title>
             </Divider>
             <ConfigMeet />
           </Card>
         </Col>
-        <Col xs={24} sm={12} md={12} lg={6} xl={6} xxl={6}>
-          <Card hoverable style={{height: '100%', backgroundColor: '#FDFEFE'}}>
+        <Col xs={22} sm={16} md={16} lg={12} xl={8} xxl={6}>
+          <Card hoverable style={{ height: 550, maxWidth: 600, backgroundColor: '#FDFEFE'}}>
             <Divider orientation='left'>
               <Typography.Title level={5}>Configuración de observadores</Typography.Title>
             </Divider>
             <ConfigObservers />
           </Card>
         </Col>
-        <Col xs={24} sm={12} md={12} lg={6} xl={6} xxl={6}>
-          <Card hoverable style={{height: '100%', backgroundColor: '#FDFEFE'}}>
+        <Col xs={22} sm={16} md={16} lg={12} xl={8} xxl={6}>
+          <Card hoverable style={{ height: 550, maxWidth: 600, backgroundColor: '#FDFEFE'}}>
             <Divider orientation='left'>
               <Typography.Title level={5}>Configuración de tipos</Typography.Title>
             </Divider>
             <TypeMeenting />
           </Card>
         </Col>
-        <Col xs={24} sm={12} md={12} lg={6} xl={6} xxl={6}>
-          <Card hoverable style={{height: '100%', backgroundColor: '#FDFEFE'}}>
+        <Col xs={22} sm={16} md={16} lg={12} xl={8} xxl={6}>
+          <Card hoverable style={{ height: 550,  maxWidth: 600, backgroundColor: '#FDFEFE'}}>
             <Divider orientation='left'>
               <Typography.Title level={5}>Configuración de espacios</Typography.Title>
             </Divider>
