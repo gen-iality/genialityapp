040f4a08fb999840f175454f3a692915091ccdb3
Maria Alejandra Perdomo <alejandrav3115@gmail.com>
2023-04-14 15:53:04 -0500

    cambios de estilos en las configuraciones del CMS

--- src/components/networking/views/Configuration.view.tsx
+++ src/components/networking/views/Configuration.view.tsx
@@ -1,46 +1,25 @@
-import { Card, Col, Divider, Row, Typography } from 'antd';
+import { Col, Row } from 'antd';
 import React from 'react';
 import ConfigObservers from '../components/Configurations/ConfigObservers';
 import TypeMeenting from '../components/Configurations/TypeMeenting';
 import ConfigSpaces from '../components/Configurations/ConfigSpaces';
 import ConfigMeet from '../components/Configurations/ConfigMeet';
 
-
 export default function Configuration() {
   return (
     <>
-      <Row justify='start' gutter={[32, 32]} style={{ marginBottom: 10 }} wrap >
-        <Col xs={22} sm={16} md={16} lg={12} xl={8} xxl={6}>
-          <Card  hoverable style={{ height: 550, maxWidth: 600, backgroundColor: '#FDFEFE'}}>
-            <Divider orientation='left'>
-              <Typography.Title level={5}>Configuración de Meet</Typography.Title>
-            </Divider>
-            <ConfigMeet />
-          </Card>
+      <Row gutter={[32, 32]} wrap >
+        <Col xs={24} sm={12} md={12} lg={8} xl={8} xxl={6}>
+          <ConfigMeet />
         </Col>
-        <Col xs={22} sm={16} md={16} lg={12} xl={8} xxl={6}>
-          <Card hoverable style={{ height: 550, maxWidth: 600, backgroundColor: '#FDFEFE'}}>
-            <Divider orientation='left'>
-              <Typography.Title level={5}>Configuración de observadores</Typography.Title>
-            </Divider>
-            <ConfigObservers />
-          </Card>
+        <Col xs={24} sm={12} md={12} lg={8} xl={8} xxl={6}>
+          <ConfigObservers />
         </Col>
-        <Col xs={22} sm={16} md={16} lg={12} xl={8} xxl={6}>
-          <Card hoverable style={{ height: 550, maxWidth: 600, backgroundColor: '#FDFEFE'}}>
-            <Divider orientation='left'>
-              <Typography.Title level={5}>Configuración de tipos</Typography.Title>
-            </Divider>
-            <TypeMeenting />
-          </Card>
+        <Col xs={24} sm={12} md={12} lg={8} xl={8} xxl={6}>
+          <TypeMeenting />
         </Col>
-        <Col xs={22} sm={16} md={16} lg={12} xl={8} xxl={6}>
-          <Card hoverable style={{ height: 550,  maxWidth: 600, backgroundColor: '#FDFEFE'}}>
-            <Divider orientation='left'>
-              <Typography.Title level={5}>Configuración de espacios</Typography.Title>
-            </Divider>
-            <ConfigSpaces />
-          </Card>
+        <Col xs={24} sm={12} md={12} lg={8} xl={8} xxl={6}>
+          <ConfigSpaces />
         </Col>
       </Row>
     </>
