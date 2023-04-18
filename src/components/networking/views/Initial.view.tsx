7e2d0cf664d1670d39142578c4ce710ecd28f436
Maria Alejandra Perdomo <alejandrav3115@gmail.com>
2023-04-18 16:59:50 -0500

    modificación en la configuración del tab principal de networking, drawer reubicación de contenido y anexado de componentes para la vizualización

--- src/components/networking/views/Initial.view.tsx
+++ src/components/networking/views/Initial.view.tsx
@@ -1,22 +1,22 @@
 import {
   CheckCircleOutlined,
+  CloseCircleOutlined,
   DeleteOutlined,
   ExclamationCircleOutlined,
   FlagOutlined,
   InfoCircleOutlined,
   SaveOutlined,
   StarOutlined,
-  WarningOutlined,
 } from '@ant-design/icons';
 import {
   Button,
   Card,
   Col,
-  Divider,
   Form,
   Input,
   InputNumber,
   Modal,
+  Result,
   Row,
   Space,
   Steps,
@@ -27,11 +27,13 @@ import React, { useState } from 'react';
 import { UseEventContext } from '@/context/eventContext';
 import { deleteNetworking,createConfgi, updateConfig } from '../services/configuration.service';
 import { networkingGlobalConfig } from '../interfaces/Index.interfaces';
-
-const { Step } = Steps;
+import CalendarStarIcon from '@2fd/ant-design-icons/lib/CalendarStar';
+import CalendarCheckIcon from '@2fd/ant-design-icons/lib/CalendarCheck';
+import useDateFormat from '../hooks/useDateFormat';
 
 export default function Initial({ active,ConfigTime }: networkingGlobalConfig) {
   const { value: Event } = UseEventContext();
+  const { dateFormat } = useDateFormat();
   const [loading, setloading] = useState(false);
   const [modal, setModal] = useState(false);
   const [permit, setPermit] = useState(true);
@@ -55,7 +57,7 @@ export default function Initial({ active,ConfigTime }: networkingGlobalConfig) {
 
     notification[response ? 'success' : 'error']({
       icon: response ? <CheckCircleOutlined /> : <ExclamationCircleOutlined />,
-      message: response ? 'Networking Configurado' : 'Problemas al Configurar Networking',
+      message: response ? 'Networking configurado' : 'Problemas al configurar networking',
       style: { color: response ? 'green' : 'orange' },
     });
     setloading(false);
@@ -65,42 +67,35 @@ export default function Initial({ active,ConfigTime }: networkingGlobalConfig) {
     const response = await deleteNetworking(Event._id);
     notification[response ? 'success' : 'error']({
       icon: response ? <CheckCircleOutlined /> : <ExclamationCircleOutlined />,
-      message: response ? 'Configuracion Eliminada' : 'No se logro eliminar la configuracion',
+      message: response ? 'Configuración eliminada' : 'No se logro eliminar la configuración',
       style: { color: response ? 'green' : 'orange' },
     });
   };
   return (
     <>
       <Row justify='center' gutter={[16, 16]}>
-        <Col span={16} style={{ margin: 30 }}>
-          
-            <Modal
-              visible={modal}
+        <Col style={{ paddingTop: 70 }}>
+          <Modal
+            visible={modal}
+            onCancel={() => setModal(false)}
+            destroyOnClose={true}
+            footer={[
+              <Button type='primary' onClick={() => setModal(false)} icon={<CloseCircleOutlined />}>
+                Cancelar
+              </Button>,
+              <Button type="default" danger onClick={onDelete} disabled={permit} icon={<DeleteOutlined />}>
+                Eliminar
+              </Button>
+            ]}
+          >
+            <Result
+              status={'warning'}
               title={
-                <Space align='center' direction='horizontal' style={{ color: '#C62828' }}>
-                  <WarningOutlined />
-                  <Typography.Title level={5} style={{ color: '#C62828', margin: 0 }}>
-                    ¿Quieres eliminar networking?
-                  </Typography.Title>
-                </Space>
+                <Typography.Text strong type='warning' style={{ fontSize: 22 }} >
+                  ¿Quieres eliminar networking?
+                </Typography.Text>
               }
-              onCancel={() => setModal(false)}
-              onOk={onDelete}
-              okText={'Eliminar'}
-              okButtonProps={{ danger: true, disabled: permit }}
-              destroyOnClose={true}>
-              <Form>
-                <Space style={{ color: '#C62828' }} direction='vertical'>
-                  <Typography.Paragraph style={{ color: '#C62828', margin: 0 }}>
-                    Esta acción borrará permanentemente los datos de la configuracion de networking asi como las
-                    reuniones previamente establecidas.
-                  </Typography.Paragraph>
-                
-                <Typography.Paragraph>
-                  Para confirmar que deseas borrar toda la configuracion de networking, escribe la siguiente palabra:
-                  <strong>Networking</strong>
-                </Typography.Paragraph>
-                </Space>
+              extra={
                 <Input placeholder="Networking" onChange={(e)=>{
                   if(e.target.value === "Networking" ){
                     setPermit(false)
@@ -108,45 +103,68 @@ export default function Initial({ active,ConfigTime }: networkingGlobalConfig) {
                     setPermit(true)
                   }
                 }}/>
-              </Form>
-            </Modal>
-          
+              }
+              subTitle={
+                <Space style={{ textAlign: 'justify' }} direction='vertical'>
+                  <Typography.Paragraph >
+                    Esta acción borrará permanentemente los datos de la configuración de networking así como las
+                    reuniones previamente establecidas.
+                  </Typography.Paragraph>
+                
+                  <Typography.Paragraph>
+                    Para confirmar que deseas borrar toda la configuración de networking, escribe la siguiente palabra:
+                    <Typography.Text strong type='danger'> Networking</Typography.Text>
+                  </Typography.Paragraph>
+                </Space>
+              }
+            />
+          </Modal>
           <Card
             hoverable
-            style={{ cursor: 'auto', height: '100%', width: '100%', backgroundColor: '#FDFEFE', borderRadius: 8 }}>
-            <Divider orientation='left'>
-              <Typography.Title level={5}> Configuración de Networking</Typography.Title>
-            </Divider>
-            <Steps direction='horizontal'>
-              <Step title={'Fecha Inicio'} description={Event.datetime_from} icon={<StarOutlined />} status='finish' />
-              <Step title={'Fecha Fin'} description={Event.datetime_to} icon={<FlagOutlined />} status='finish' />
-            </Steps>
-            <Form onFinish={onSave} layout='vertical' style={{ margin: 10 }}>
+            style={{ cursor: 'auto', height: '100%', backgroundColor: 'rgba(196, 196, 196, 0.1)'}}
+            title={
+              <Typography.Text strong> Configuración de Networking</Typography.Text>
+            }
+            headStyle={{border: 'none'}}>
+            
+            <Row justify='center' align='middle' gutter={[16, 16]}>
+              <Col span={20}>
+                <Steps labelPlacement="vertical" >
+                  <Steps.Step title={'Fecha Inicio'} description={dateFormat(Event.datetime_from, 'MM/DD/YYYY hh:mm A')} icon={<CalendarStarIcon />} status='finish' />
+                  <Steps.Step title={'Fecha Fin'} description={dateFormat(Event.datetime_to, 'MM/DD/YYYY hh:mm A')} icon={<CalendarCheckIcon />} status='finish' />
+                </Steps>
+              </Col>
+            </Row>
+            
+            <Form onFinish={onSave} layout='vertical' style={{ paddingTop: 15 }}> 
               <Form.Item
                 name={'meetingDuration'}
                 label={'Tiempo entre reuniones'}
-                rules={[{ required: true, message: 'Debe configurar un minimo de 5 minutos' }]}
+                rules={[{ required: true, message: 'Debe configurar un mínimo de 5 minutos' }]}
                 help={
-                  <Typography.Text type='secondary'>
+                  <Typography.Link type='secondary'>
                     <InfoCircleOutlined /> Este tiempo es en minutos y no puede ser mayor a 60 ni menor a 5
-                  </Typography.Text>
+                  </Typography.Link>
                 }
                 initialValue={ConfigTime ? ConfigTime.meetingDuration : 5}>
                 <InputNumber disabled={loading} max={60} min={5} style={{ width: '100%' }} />
               </Form.Item>
-              <Row justify='center' style={{ marginTop: 10 }}>
-                <Space align='center'>
+
+              <Row justify='end' gutter={[8, 8]} style={{paddingTop: 15}}>
+                <Col>
                   <Button loading={loading} type='primary' icon={<SaveOutlined />} htmlType='submit'>
                     Guardar
                   </Button>
-                  {active ? (
+                </Col>
+                {active ? (
+                  <Col>
                     <Button icon={<DeleteOutlined />} danger type='primary' onClick={() => setModal(true)}>
                       Eliminar
                     </Button>
-                  ) : (
-                    <></>
-                  )}
-                </Space>
+                  </Col>
+                ) : (
+                  <></>
+                )}
               </Row>
             </Form>
           </Card>
