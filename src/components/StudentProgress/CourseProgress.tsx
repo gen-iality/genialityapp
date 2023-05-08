b0079ba15976997cbe17a3689b2a1e6cbdefa196
David Borja <davidborja456@gmail.com>
2023-05-07 22:05:54 -0500

    Fix logic for render the course progress

--- src/components/StudentProgress/CourseProgress.tsx
+++ src/components/StudentProgress/CourseProgress.tsx
@@ -24,27 +24,22 @@ function CourseProgress(props: CourseProgressProps | SteppedCourseProgressProps)
         <p style={{ color: 'black', fontWeight: 'bold' }}>{title || 'Mi progreso'}:</p>
       )}
 
-      {type === 'circle' && (
+      {type === 'circle' ? (
         <Progress type="circle" percent={percentValue} format={() => stats} />
-      )}
-
-      {type === 'steps' && (
+      ) : type === 'steps' ? (
         <Progress percent={percentValue} steps={props.steps || 0} format={() => stats} />
-      )}
-
-      {type === 'block' ||
-        (type === undefined && (
-          <Progress
-            strokeColor={{
-              from: '#f7981d', //'#108ee9',
-              to: '#FFB453', //'#87d068',
-            }}
-            trailColor="#E6E6E6"
-            percent={percentValue}
-            status="active"
-            format={() => stats}
-          />
-        ))}
+      ) : type === 'block' || type === undefined ? (
+        <Progress
+          strokeColor={{
+            from: '#f7981d', //'#108ee9',
+            to: '#FFB453', //'#87d068',
+          }}
+          trailColor="#E6E6E6"
+          percent={percentValue}
+          status="active"
+          format={() => stats}
+        />
+      ) : undefined}
     </div>
   )
 }
