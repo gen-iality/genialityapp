c2613c452049dc3309ea5bf7604e013b9a6c49d9
David Borja <davidborja456@gmail.com>
2023-05-09 20:16:38 -0500

    WIP: creating the firestore request file

---
+++ src/helpers/firestore-request.ts
@@ -0,0 +1,8 @@
+/**
+ * This module defines some function to do request to firestore, instead to use
+ * firestore directly.
+ */
+
+import { firestore } from '@helpers/firebase'
+
+export const Attendee = {}
