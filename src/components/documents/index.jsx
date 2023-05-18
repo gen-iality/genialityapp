682d7dfe1bf65226c7a29524c1d6fa1ebf24f783
David Borja <davidborja456@gmail.com>
2023-05-18 14:27:56 -0500

    change: remove unused things

--- src/components/documents/index.jsx
+++ src/components/documents/index.jsx
@@ -2,10 +2,8 @@ import { Fragment } from 'react'
 import { Route, Switch, withRouter } from 'react-router-dom'
 import Documents from './documents'
 import Document from './Document'
-/* import DocumentsEdit from './edit_old'; */
-/* import DocumentsPermission from './filePermission_old'; */
 
-function DocumentsRoutes({ ...props }) {
+function DocumentsRoutes(props) {
   const { event, matchUrl } = props
   return (
     <Fragment>
@@ -18,7 +16,7 @@ function DocumentsRoutes({ ...props }) {
         <Route
           exact
           path={`${matchUrl}/document`}
-          render={() => <Document event={event} parentUrl={matchUrl} {...props} />}
+          render={() => <Document event={event} parentUrl={matchUrl} />}
         />
       </Switch>
     </Fragment>
