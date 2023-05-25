f6cc2d7f8536a4d6da1bbe6a58ea752ead856537
David Borja <davidborja456@gmail.com>
2023-05-25 14:19:00 -0500

    change: move LissonsInfoModal to external file

--- src/components/event-users/ListEventUserPage.tsx
+++ src/components/event-users/ListEventUserPage.tsx
@@ -27,6 +27,7 @@ import {
   Space,
   Table,
   Tag,
+  Tooltip,
   Typography,
 } from 'antd'
 
@@ -42,6 +43,7 @@ import { utils, writeFileXLSX } from 'xlsx'
 import {
   CheckOutlined,
   DownloadOutlined,
+  EditOutlined,
   LoadingOutlined,
   PlusCircleOutlined,
   SafetyCertificateOutlined,
@@ -53,137 +55,8 @@ import { checkinAttendeeInActivity } from '@helpers/HelperAuth'
 import UserModal from '../modal/modalUser'
 import { Link } from 'react-router-dom'
 import { StateMessage } from '@context/MessageService'
-
-interface ILessonsInfoModalProps {
-  show: boolean
-  onHidden: () => void
-  allActivities: any[]
-  user: any
-  event: any
-}
-
-const LessonsInfoModal: FunctionComponent<ILessonsInfoModalProps> = (props) => {
-  const { show, onHidden, allActivities, user, event } = props
-
-  const [dataLoaded, setDataLoaded] = useState(false)
-  const [viewedActivities, setViewedActivities] = useState<any[]>([])
-  const [isSending, setIsSending] = useState(false)
-
-  const requestAllData = async () => {
-    const existentActivities = await allActivities.map(async (activity) => {
-      const activity_attendee = await firestore
-        .collection(`${activity._id}_event_attendees`)
-        .doc(user._id)
-        .get()
-      if (activity_attendee.exists) {
-        return activity
-      }
-      return null
-    })
-    // Filter non-null result that means that the user attendees them
-    const viewedActivities = (await Promise.all(existentActivities)).filter(
-      (item) => item !== null,
-    )
-    setViewedActivities(viewedActivities.map((activity) => activity.name))
-  }
-
-  const handleSendCertificate = async () => {
-    setIsSending(true)
-    const emailEncoded = encodeURIComponent(user.email)
-    const redirect = `${window.location.origin}/landing/${event._id}/certificate`
-    const url = `${window.location.origin}/direct-login?email=${emailEncoded}&redirect=${redirect}`
-
-    try {
-      await EventsApi.generalMagicLink(
-        user.email,
-        url,
-        'Entra al ver el certificado en el siguiente link',
-      )
-      StateMessage.show(null, 'success', `Se ha enviado el mensaje a ${user.email}`)
-    } catch (err) {
-      console.error(err)
-      Modal.error({
-        title: 'Error en el envío',
-        content: 'No se ha podido enviar el certificado por problemas de fondo',
-      })
-    }
-    setIsSending(false)
-  }
-
-  const isDone = useMemo(
-    () => allActivities.every((activity) => viewedActivities.includes(activity.name)),
-    [allActivities, viewedActivities],
-  )
-
-  useEffect(() => {
-    if (!user) return
-    if (allActivities.length == 0) return
-
-    requestAllData().finally(() => setDataLoaded(true))
-
-    // if (!show) setLoaded(false)
-    return () => setDataLoaded(false)
-  }, [allActivities, user, show])
-
-  if (!user) return null
-
-  return (
-    <Modal centered footer={null} visible={show} closable onCancel={onHidden}>
-      <Space direction="vertical" style={{ width: '100%' }}>
-        {dataLoaded ? (
-          <List
-            size="small"
-            header={
-              <Row justify="space-between">
-                <Typography.Text strong>
-                  Lecciones vistas por {user.names}
-                </Typography.Text>
-                <Button
-                  type="primary"
-                  title={
-                    isDone
-                      ? 'Envía un correo con un enlace mágico'
-                      : 'Se necesita pasar todos los cursos'
-                  }
-                  disabled={!isDone || isSending}
-                  onClick={() => handleSendCertificate()}
-                  icon={isSending ? <LoadingOutlined /> : <SafetyCertificateOutlined />}
-                >
-                  Enviar certificado
-                </Button>
-              </Row>
-            }
-            // bordered
-            dataSource={allActivities}
-            renderItem={(item) => (
-              <List.Item>
-                {viewedActivities.filter((activityName) => activityName == item.name)
-                  .length ? (
-                  <CheckOutlined />
-                ) : (
-                  <Checkbox
-                    onChange={async () => {
-                      await checkinAttendeeInActivity(user, item._id)
-                      requestAllData()
-                    }}
-                  />
-                )}{' '}
-                {item.name}
-              </List.Item>
-            )}
-          />
-        ) : (
-          <Result
-            icon={<LoadingOutlined />}
-            title="Cargando"
-            status="info"
-            subTitle={`Cargando datos de ${user.names}...`}
-          />
-        )}
-      </Space>
-    </Modal>
-  )
-}
+import { useIntl } from 'react-intl'
+import LessonsInfoModal from './LessonsInfoModal'
 
 interface ITimeTrackingStatsProps {
   user: any
@@ -309,6 +182,8 @@ const ListEventUserPage: FunctionComponent<IListEventUserPageProps> = (props) =>
   const [allActivities, setAllActivities] = useState<any[]>([])
 
   const [eventUsersRef] = useState(firestore.collection(`${event._id}_event_attendees`))
+
+  const intl = useIntl()
 
   const getNameFromURL = (fileUrl: string) => {
     if (typeof fileUrl == 'string') {
@@ -763,16 +638,16 @@ const ListEventUserPage: FunctionComponent<IListEventUserPageProps> = (props) =>
       {isRegistrationModalOpened && (
         <UserModal
           handleModal={() => setIsRegistrationModalOpened(false)}
-          modal={null}
+          modal={!!watchedUserInProgressingModal}
           ticket={null}
           tickets={[]}
           rolesList={rolesList}
           value={watchedUserInProgressingModal}
-          checkIn={(...args) => console.log(args)}
+          checkIn={checkIn}
           badgeEvent={badgeEvent}
           extraFields={extraFields}
           spacesEvent={[]}
-          edit={null}
+          edit={watchedUserInProgressingModal}
           substractSyncQuantity={(...args) => console.log(args)}
           activityId={activityId}
         />
