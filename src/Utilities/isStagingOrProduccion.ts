9b3829ba498cb682875f08e1266b634f9255331b
Marlon Carrillo <marloncb2012@gmail.com>
2023-05-26 16:49:42 -0500

    Added developers to domains that use staging

--- src/Utilities/isStagingOrProduccion.ts
+++ src/Utilities/isStagingOrProduccion.ts
@@ -3,7 +3,8 @@ export const isStagingOrProduccion = (): string => {
     window.location.href.includes('staging') ||
     window.location.href.includes('localhost') ||
     window.location.href.includes('dinamicas') ||
-    window.location.href.includes('eviusbeta')
+    window.location.href.includes('eviusbeta') ||
+    window.location.href.includes('development')
   ) {
     return 'staging';
   } else {
