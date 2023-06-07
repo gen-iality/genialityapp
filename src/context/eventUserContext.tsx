132bcf6109af7ad69658205d062807af472041a0
David Borja <davidborja456@gmail.com>
2023-06-07 09:07:30 -0500

    improve: eventUserContext's logic

--- src/context/eventUserContext.tsx
+++ src/context/eventUserContext.tsx
@@ -1,16 +1,71 @@
-import { useState, useEffect, createContext, useContext } from 'react'
+import { useState, useEffect, createContext, useContext, FunctionComponent } from 'react'
 import { EventsApi } from '@helpers/request'
 import { useEventContext } from './eventContext'
 import { app } from '@helpers/firebase'
 import { useCurrentUser } from './userContext'
-export const CurrentEventUserContext = createContext()
-const initialContextState = { status: 'LOADING', value: null }
 
-export function CurrentUserEventProvider({ children }) {
+interface EventUserContextState {
+  status: 'LOADING' | 'LOADED'
+  value: any
+  error?: any
+  resetEventUser: () => void
+  setuserEvent?: (eu: EventUserContextState) => void
+  setUpdateUser?: (value: boolean) => void
+}
+
+const initialContextState: EventUserContextState = {
+  status: 'LOADING',
+  value: null,
+  resetEventUser: () => {},
+}
+
+const EventUserContext = createContext<EventUserContextState>(initialContextState)
+
+export default EventUserContext
+
+export const CurrentEventUserContext = EventUserContext
+
+const UserEventProvider: FunctionComponent = (props) => {
+  const { children } = props
+
   const cEvent = useEventContext()
   const cUser = useCurrentUser()
-  const [userEvent, setUserEvent] = useState(initialContextState)
+
+  const [eventUserContext, setEventUserContext] =
+    useState<EventUserContextState>(initialContextState)
   const [updateUser, setUpdateUser] = useState(true)
+
+  const requestEventUserData = async (eventId: string) => {
+    console.log('request event user for event:', eventId)
+    try {
+      EventsApi.getStatusRegister(eventId, cUser.value.email).then((responseStatus) => {
+        console.log('responseStatus ', responseStatus, 'upadateUser ', updateUser)
+        if (responseStatus.data.length > 0) {
+          setEventUserContext({
+            ...eventUserContext,
+            status: 'LOADED',
+            value: responseStatus.data[0],
+          })
+        } else {
+          setEventUserContext({ ...eventUserContext, status: 'LOADED', value: null })
+        }
+        setUpdateUser(false)
+      })
+    } catch (e) {
+      setEventUserContext({
+        ...eventUserContext,
+        status: 'LOADED',
+        value: null,
+        error: e,
+      })
+      setUpdateUser(false)
+    }
+  }
+
+  const resetEventUser = () => {
+    console.log('call reset event user')
+    setEventUserContext({ ...eventUserContext, status: 'LOADING', value: null })
+  }
 
   useEffect(() => {
     const unsubscribe = app.auth().onAuthStateChanged((user) => {
@@ -24,43 +79,32 @@ export function CurrentUserEventProvider({ children }) {
   }, [])
 
   useEffect(() => {
-    const event_id = cEvent.value?._id
-    if (cUser.value == null || cUser.value == undefined || updateUser == false) return
-    async function asyncdata() {
-      try {
-        EventsApi.getStatusRegister(event_id, cUser.value.email).then(
-          (responseStatus) => {
-            console.log('responseStatus ', responseStatus, 'upadateUser ', updateUser)
-            if (responseStatus.data.length > 0) {
-              setUserEvent({ status: 'LOADED', value: responseStatus.data[0] })
-            } else {
-              setUserEvent({ status: 'LOADED', value: null })
-            }
-            setUpdateUser(false)
-          },
-        )
-      } catch (e) {
-        setUserEvent({ status: 'LOADED', value: null })
-        setUpdateUser(false)
-      }
+    const eventId = cEvent.value?._id
+
+    if (!eventId) return
+
+    if (cUser.value == null || cUser.value == undefined || updateUser == false) {
+      return
     }
 
-    if (!event_id) return
-    asyncdata()
+    requestEventUserData(eventId)
   }, [cEvent.value, cUser.value, updateUser])
 
   return (
     <CurrentEventUserContext.Provider
       value={{
-        ...userEvent,
-        setuserEvent: setUserEvent,
+        ...eventUserContext,
+        setuserEvent: setEventUserContext,
         setUpdateUser: setUpdateUser,
+        resetEventUser,
       }}
     >
       {children}
     </CurrentEventUserContext.Provider>
   )
 }
+
+export const CurrentUserEventProvider = UserEventProvider
 
 export function useUserEvent() {
   const contextuser = useContext(CurrentEventUserContext)
