33dee45646db5470b117992933b3ad70324ef406
David Borja <davidborja456@gmail.com>
2023-06-08 16:30:19 -0500

    update: CertificateType has new properties

--- src/Utilities/types/CertificateType.ts
+++ src/Utilities/types/CertificateType.ts
@@ -3,7 +3,13 @@ export type CertificateData = {
   content: string
   background: string
   event_id: string
+  event: any
   rol?: { _id: string; [key: string]: any }
   cert_width?: number
   cert_height?: number
+  requirement_config?: {
+    enable: boolean
+    completion: number
+    ignore_event_type: string[]
+  }
 }
