41913bb4c4919a26b5f1c9c73c6264479a2ac452
David Borja <davidborja456@gmail.com>
2023-06-19 15:13:11 -0500

    update: video displayer mask as viewed when video progress is full

--- src/components/events/activities/displayers/VideoActivityDisplayer.tsx
+++ src/components/events/activities/displayers/VideoActivityDisplayer.tsx
@@ -1,8 +1,14 @@
-import { useState, useEffect, FunctionComponent } from 'react'
+import { useState, useEffect, FunctionComponent, useRef } from 'react'
 import ReactPlayer from 'react-player'
 import HeaderColumnswithContext from '../HeaderColumns'
-
 import { IBasicActivityProps } from './basicTypes'
+import { FB } from '@helpers/firestore-request'
+
+import { useUserEvent } from '@context/eventUserContext'
+import {
+  updateAttendeeInActivityRealTime,
+  checkinAttendeeInActivity,
+} from '@helpers/HelperAuth'
 
 const VideoActivityDisplayer: FunctionComponent<IBasicActivityProps> = (props) => {
   const { activity } = props
@@ -10,19 +16,74 @@ const VideoActivityDisplayer: FunctionComponent<IBasicActivityProps> = (props) =
 
   const [activityState] = useState('')
   const [isItAnFrame, setIsItAnFrame] = useState(false)
+  const [viewedVideoProgress, setViewedVideoProgress] = useState(0)
+  const [attendeeRealTime, setAttendeeRealTime] = useState<any>({})
+  const [realtimeUnsubscribe, setRealtimeUnsubscribe] = useState<(() => void) | null>(
+    null,
+  )
+
+  const ref = useRef<ReactPlayer>()
+
+  const cEventUser = useUserEvent()
+
+  // Unsubscribe the realtime mechanism when the componet gets be unmounted
+  useEffect(
+    () => () => {
+      if (typeof realtimeUnsubscribe === 'function') {
+        realtimeUnsubscribe()
+      }
+    },
+    [realtimeUnsubscribe],
+  )
 
   useEffect(() => {
-    if (activity.type.name === 'cargarvideo') {
-      setIsItAnFrame(true)
-    } else {
-      setIsItAnFrame(false)
+    if (!ref.current) return
+    if (!activity?._id) return
+
+    try {
+      const unsubscrubeCallback = FB.Attendees.ref(
+        activity._id,
+        cEventUser.value._id,
+      ).onSnapshot((doc) => {
+        const data = doc.data()
+        if (!data) return
+
+        setAttendeeRealTime(data)
+        console.log('vimeo asistente ', data)
+        if (parseFloat(data.viewProgress) && data.viewProgess > viewedVideoProgress) {
+          setViewedVideoProgress(data.viewProgess)
+          console.log('vimeo timeupdate in database', data)
+          ref.current!.seekTo(data.viewProgess)
+        }
+      })
+      setRealtimeUnsubscribe(unsubscrubeCallback)
+    } catch (e) {
+      console.log('vimeo error', { e })
     }
-  }, [activity])
+  }, [activity, ref.current])
+
+  useEffect(() => {
+    if (!viewedVideoProgress) return
+    if (Math.round(viewedVideoProgress / 1) !== viewedVideoProgress) return
+    console.log('vimeo timeupdate', activity, viewedVideoProgress)
+
+    updateAttendeeInActivityRealTime(cEventUser.value, activity._id, {
+      viewProgess: viewedVideoProgress,
+    })
+
+    // Save the progress when it is over 99
+    if (viewedVideoProgress >= 99)
+      checkinAttendeeInActivity(cEventUser.value, activity._id)
+  }, [viewedVideoProgress])
 
   return (
     <>
       <HeaderColumnswithContext isVisible activityState={activityState} />
       <div className="mediaplayer" style={{ aspectRatio: '16/9' }}>
+        <p>
+          Progreso: {viewedVideoProgress}% &nbsp; Completo:{' '}
+          {attendeeRealTime?.checked_in === true ? 'COMPLETO' : 'AÚN NO'}
+        </p>
         {isItAnFrame ? (
           <iframe
             style={{ aspectRatio: '16/9' }}
@@ -35,9 +96,11 @@ const VideoActivityDisplayer: FunctionComponent<IBasicActivityProps> = (props) =
         ) : (
           <ReactPlayer
             style={{ objectFit: 'cover' }}
+            ref={ref}
             width="100%"
             height="100%"
             url={urlVideo}
+            onProgress={(state) => setViewedVideoProgress(state.played)}
             controls
           />
         )}
