04b8ab05959d9a27144f6af0b6712d003eb46605
Maria Alejandra Perdomo <alejandrav3115@gmail.com>
2023-06-20 16:43:52 -0500

    cambio en las card de fechas en el CMS en datos a recolectar

--- src/components/events/multiples-fechas/CustomDateEvent.tsx
+++ src/components/events/multiples-fechas/CustomDateEvent.tsx
@@ -1,9 +1,9 @@
-import { Alert, Button, Card, Col, Row, Space, Typography } from 'antd';
+import { Alert, Button, Card, Col, List, Row, Space, Typography } from 'antd';
 import { DateRangeEvius, useCustomDateEvent } from '../hooks/useCustomDateEvent';
 import Loading from '@/components/loaders/loading';
 import { TimeItem } from './TimeItem';
 import { DateEventItem } from './DateEventItem';
-import { PlusOutlined } from '@ant-design/icons';
+import { PlusCircleOutlined, PlusOutlined, SaveOutlined } from '@ant-design/icons';
 import { useState } from 'react';
 import { DateModal } from './DateModal';
 
@@ -47,53 +47,82 @@ export default function CustomDateEvent(props: Props) {
   };
 
   return (
-    <Row gutter={[16, 24]}>
-      <Col xs={24} lg={24}>
-        {openModal && (
-          <DateModal
-            closeModal={closeModal}
-            handledEdit={handledEdit}
-            footer={false}
-            disabledDate={disabledDate}
-            setOpenModal={setOpenModal}
-            handledInterceptor={handleInterceptor}
-            date={selectedDate}
-            visible={openModal}
-            onCancel={closeModal}
-            handleUpdateTime={handleUpdateTime}
-          />
-        )}
-        {mustUpdateDate && (
-          <Alert
-            message='Formato de fecha incorrecto'
-            description={`Fecha inicio ${datesOld?.startDateOld} y fecha final ${datesOld?.endDateOld} Se han corregido las fechas al nuevo formato, confirme`}
-            type='error'
-            style={{ marginTop: '2rem' }}
-          />
-        )}
-      </Col>
-      <Col xs={24} lg={24}>
-        <Typography.Title level={5}>Fechas seleccionadas</Typography.Title>
-        <Space wrap>
-          {!!dates.length &&
-            dates.map((date) => (
-              <DateEventItem
-                key={date.id}
-                date={date}
-                onClick={() => openEditDate(date)}
-                handledDelete={handledDelete}
-              />
-            ))}
-          <Card onClick={openCreateNewDate} hoverable>
-            <PlusOutlined />
-          </Card>
-        </Space>
-      </Col>
-      <Col span={24}>
-        <Button style={{ marginTop: 16 }} type='primary' onClick={handleSubmit} loading={isSaving}>
-          Guardar
-        </Button>
-      </Col>
-    </Row>
+    <Card style={{borderRadius: 20}} hoverable>
+      <Row gutter={[16, 16]}>
+        <Col xs={24} lg={24}>
+          {openModal && (
+            <DateModal
+              closeModal={closeModal}
+              handledEdit={handledEdit}
+              footer={false}
+              disabledDate={disabledDate}
+              setOpenModal={setOpenModal}
+              handledInterceptor={handleInterceptor}
+              date={selectedDate}
+              visible={openModal}
+              onCancel={closeModal}
+              handleUpdateTime={handleUpdateTime}
+            />
+          )}
+          {mustUpdateDate && (
+            <Alert
+              message='Formato de fecha incorrecto'
+              description={`Fecha inicio ${datesOld?.startDateOld} y fecha final ${datesOld?.endDateOld} Se han corregido las fechas al nuevo formato, confirme`}
+              type='error'
+              style={{ marginTop: '2rem' }}
+            />
+          )}
+        </Col>
+        <Col xs={24} lg={24}>
+          <Typography.Text strong>Fechas seleccionadas</Typography.Text>
+          <Space wrap>
+            <List 
+              grid={{gutter: 8, column: 2}}
+              dataSource={dates}
+              renderItem={date => (
+                <List.Item style={{border: 'none'}}>
+                  <DateEventItem
+                    key={date.id}
+                    date={date}
+                    onClick={() => openEditDate(date)}
+                    handledDelete={handledDelete}
+                  />
+                </List.Item>
+              )}
+            >
+            </List>
+            {/* {!!dates.length &&
+              dates.map((date) => (
+                <DateEventItem
+                  key={date.id}
+                  date={date}
+                  onClick={() => openEditDate(date)}
+                  handledDelete={handledDelete}
+                />
+              ))} */}
+            {/* <Card onClick={openCreateNewDate} hoverable>
+              <PlusOutlined />
+            </Card> */}
+          </Space>
+        </Col>
+        <Col span={24}>
+          <Row justify='end' gutter={[8, 8]} wrap>
+            <Col>
+              <Button icon={<PlusCircleOutlined />} type='default' onClick={openCreateNewDate}>
+                Agregar
+              </Button>
+            </Col>
+            <Col>
+              <Button icon={<SaveOutlined />} type='primary' onClick={handleSubmit} loading={isSaving}>
+                Guardar fechas
+              </Button>
+            </Col>
+          </Row>
+          {/* <Button style={{ marginTop: 16 }} type='primary' onClick={handleSubmit} loading={isSaving}>
+            Guardar
+          </Button> */}
+        </Col>
+      </Row>
+    </Card>
   );
 }
--- src/components/events/multiples-fechas/DateEventItem.tsx
+++ src/components/events/multiples-fechas/DateEventItem.tsx
@@ -2,6 +2,8 @@ import { Button, Card, CardProps, Col, Row, Space, TimePicker, Typography } from
 import { DateRangeEvius } from '../hooks/useCustomDateEvent';
 import moment from 'moment';
 import * as IconsAntDesing from '@ant-design/icons';
+import useDateFormat from '@/components/networking/hooks/useDateFormat';
+import ClockTimeFourOutlineIcon from '@2fd/ant-design-icons/lib/ClockTimeFourOutline';
 
 interface Props extends CardProps {
   date: DateRangeEvius;
@@ -11,16 +13,47 @@ interface Props extends CardProps {
 const format = 'hh:mm A';
 
 export const DateEventItem = ({ date, onClick, handledDelete, ...cardProps }: Props) => {
+  const { dateFormat } = useDateFormat();
   return (
     <Card
       hoverable
+      style={{borderRadius: 10/* , border: 'none' */}}
       {...cardProps}
-      actions={[
+      /* actions={[
         <Button type='text' icon={<IconsAntDesing.EditOutlined />} onClick={onClick} />,
         <Button type='text' icon={<IconsAntDesing.DeleteOutlined />} onClick={() => handledDelete(date.id)} />,
-      ]}>
-      <Row gutter={[10, 2]}>
-        <Col
+      ]} */
+    >
+      <Row gutter={[8, 8]}>
+        <Card.Meta 
+          avatar={
+            <Space 
+              size={0} 
+              direction='vertical' 
+              align='center'
+            >
+              <Typography.Text strong style={{textTransform: 'capitalize'}}>
+                {dateFormat(date.start, 'MMM')}
+              </Typography.Text>
+              <Typography.Text strong style={{fontSize: 25}}>
+                {dateFormat(date.start, 'DD')}
+              </Typography.Text>
+            </Space>
+          }
+          title={
+            <Space size={4} wrap style={{paddingTop: 16}}>
+              <ClockTimeFourOutlineIcon />
+              <Typography.Text>{dateFormat(date.start, format)}</Typography.Text>
+              <Typography.Text>-</Typography.Text>
+              <Typography.Text>{dateFormat(date.end, format)}</Typography.Text>
+            </Space>
+          }
+        />
+        <Space wrap style={{paddingLeft: 45}}>
+          <Button type='default' icon={<IconsAntDesing.EditOutlined />} onClick={onClick} />
+          <Button danger type='default' icon={<IconsAntDesing.DeleteOutlined />} onClick={() => handledDelete(date.id)} />
+        </Space>
+        {/* <Col
           span={6}
           style={{
             display: 'flex',
@@ -55,7 +88,7 @@ export const DateEventItem = ({ date, onClick, handledDelete, ...cardProps }: Pr
               {moment(date.end, format).format(format)}
             </Typography>
           </Space>
-        </Col>
+        </Col> */}
       </Row>
     </Card>
   );
