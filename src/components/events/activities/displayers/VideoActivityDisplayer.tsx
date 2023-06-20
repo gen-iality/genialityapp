8c1c83eebeea317126bf59018bf5241f1fbc9043
David Borja <davidborja456@gmail.com>
2023-06-20 10:41:28 -0500

    add: feature to checking the progress and mark as viewed by required percent

--- src/components/events/activities/displayers/VideoActivityDisplayer.tsx
+++ src/components/events/activities/displayers/VideoActivityDisplayer.tsx
@@ -12,7 +12,7 @@ import {
 import { Badge } from 'antd'
 
 const VideoActivityDisplayer: FunctionComponent<IBasicActivityProps> = (props) => {
-  const { activity } = props
+  const { activity, onActivityProgress } = props
   const urlVideo = activity?.video
 
   const [activityState] = useState('')
@@ -71,7 +71,7 @@ const VideoActivityDisplayer: FunctionComponent<IBasicActivityProps> = (props) =
 
   useEffect(() => {
     if (!viewedVideoProgress) return
-    if (Math.round(viewedVideoProgress / 1) !== viewedVideoProgress) return
+    // if (Math.round(viewedVideoProgress / 1) !== viewedVideoProgress) return
     console.log('vimeo timeupdate', activity, viewedVideoProgress)
 
     updateAttendeeInActivityRealTime(cEventUser.value, activity._id, {
@@ -79,8 +79,10 @@ const VideoActivityDisplayer: FunctionComponent<IBasicActivityProps> = (props) =
     })
 
     // Save the progress when it is over 99
-    if (viewedVideoProgress >= 99)
-      checkinAttendeeInActivity(cEventUser.value, activity._id)
+    // if (viewedVideoProgress >= 99)
+    //   checkinAttendeeInActivity(cEventUser.value, activity._id)
+    if (typeof onActivityProgress === 'function')
+      onActivityProgress(viewedVideoProgress * 100)
   }, [viewedVideoProgress])
 
   return (
@@ -89,7 +91,7 @@ const VideoActivityDisplayer: FunctionComponent<IBasicActivityProps> = (props) =
       <div className="mediaplayer" style={{ aspectRatio: '16/9' }}>
         <div>
           <Badge
-            count={`Progreso: ${viewedVideoProgress}%`}
+            count={`Progreso: ${Math.round(viewedVideoProgress * 100)}%`}
             style={{ backgroundColor: '#0E594A', color: 'while' }}
           />
           <Badge
