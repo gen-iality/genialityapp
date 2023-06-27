46f1fc5e67a87d3bb34a2c9bbf785168e16c6bcb
davib <davidborja456@gmail.com>
2023-06-27 08:51:07 -0500

    add: filter & searcher for columns - its awesome

--- src/components/event-users/EnrollEventUserFromOrganizationMember.tsx
+++ src/components/event-users/EnrollEventUserFromOrganizationMember.tsx
@@ -6,15 +6,35 @@
  * NOTE: The name can change in a future...
  */
 
-import { Avatar, Button, Comment, List, Modal, Result, Table, Tabs } from 'antd'
-import { FunctionComponent, useEffect, useState } from 'react'
+import { FunctionComponent, useEffect, useRef, useState } from 'react'
+import {
+  Avatar,
+  Button,
+  Comment,
+  Input,
+  InputRef,
+  List,
+  Modal,
+  Result,
+  Space,
+  Table,
+  Tabs,
+} from 'antd'
+import Highlighter from 'react-highlight-words'
 import RegisterUserAndEventUser from '@components/authentication/RegisterUserAndEventUser'
 import { AttendeeApi, OrganizationApi } from '@helpers/request'
-import { CheckOutlined, LoadingOutlined, PlusOutlined } from '@ant-design/icons'
+import {
+  CheckOutlined,
+  LoadingOutlined,
+  PlusOutlined,
+  SearchOutlined,
+} from '@ant-design/icons'
 import { UsersApi } from '@helpers/request'
 import { StateMessage } from '@context/MessageService'
 import { useIntl } from 'react-intl'
 import { ColumnsType } from 'antd/lib/table'
+import { FilterConfirmProps } from 'antd/lib/table/interface'
+import { ColumnType } from 'antd/es/table'
 
 const stylePaddingDesktop = {
   paddingLeft: '30px',
@@ -49,7 +69,108 @@ const EnrollEventUserFromOrganizationMember: FunctionComponent<
   const [orgMembers, setOrgMembers] = useState<any[]>([])
   const [eventUsers, setEventUsers] = useState<any[]>([])
   const [canEnrollFromOrganization, setCanEnrollFromOrganization] = useState(false)
-  console.log(eventUsers)
+  const [searchText, setSearchText] = useState('')
+  const [searchedColumn, setSearchedColumn] = useState('')
+
+  const inputRef = useRef<InputRef>(null)
+
+  const handleSearch = (
+    selectedKeys: string[],
+    confirm: (param?: FilterConfirmProps) => void,
+    dataIndex: string,
+  ) => {
+    confirm()
+    setSearchText(selectedKeys[0])
+    setSearchedColumn(dataIndex)
+  }
+
+  const handleReset = (clearFilters: () => void) => {
+    clearFilters()
+    setSearchText('')
+  }
+
+  const getColumnSearchProps = (
+    alias: string,
+    howToRender: (value: any) => string,
+  ): ColumnType<any> => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+      <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
+        <Input
+          ref={inputRef}
+          placeholder={`Search ${alias}`}
+          value={selectedKeys[0]}
+          onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => handleSearch(selectedKeys as string[], confirm, alias)}
+          style={{ marginBottom: 8, display: 'block' }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys as string[], confirm, alias)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({ closeDropdown: false })
+              setSearchText((selectedKeys as string[])[0])
+              setSearchedColumn(alias)
+            }}
+          >
+            Filter
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              close()
+            }}
+          >
+            close
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered: boolean) => (
+      <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />
+    ),
+    onFilter: (value, record) => {
+      return howToRender(record)
+        .toString()
+        .toLowerCase()
+        .includes((value as string).toLowerCase())
+    },
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => inputRef.current?.select(), 100)
+      }
+    },
+    render: (item) => {
+      const renderedText = howToRender(item)
+      return searchedColumn === alias ? (
+        <Highlighter
+          highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={renderedText ? renderedText.toString() : ''}
+        />
+      ) : (
+        renderedText
+      )
+    },
+  })
 
   const [columns] = useState<ColumnsType>([
     {
@@ -58,22 +179,22 @@ const EnrollEventUserFromOrganizationMember: FunctionComponent<
       render: (element) => <Avatar src={element} />,
     },
     {
-      title: '',
-      dataIndex: 'position_name',
-      render: (element) => <>{element || 'Sin cargo'}</>,
+      title: 'Cargo',
+      render: (item) => <>{item.position_name || 'Sin cargo'}</>,
       sorter: (a: any, b: any) => a.position_name?.length - b.position_name?.length,
+      ...getColumnSearchProps('cargo', (item) => item.position_name || 'Sin cargo'),
     },
     {
       title: 'Nombre',
-      dataIndex: 'user',
-      render: (user) => <>{user.names}</>,
+      render: (item) => <>{item.user.names}</>,
       sorter: (a: any, b: any) => a.user.names.length - b.user.names.length,
+      ...getColumnSearchProps('nombre', (item) => item.user.names),
     },
     {
       title: 'Email',
-      dataIndex: 'user',
-      render: (user) => <>{user.email}</>,
+      render: (item) => <>{item.user.email}</>,
       sorter: (a: any, b: any) => a.user.email.length - b.user.email.length,
+      ...getColumnSearchProps('correo', (item) => item.user.email),
     },
     {
       title: 'Opciones',
@@ -98,9 +219,7 @@ const EnrollEventUserFromOrganizationMember: FunctionComponent<
               <PlusOutlined />
             )
           }
-        >
-          Inscribir
-        </Button>
+        ></Button>
       ),
     },
   ])
@@ -209,6 +328,7 @@ const EnrollEventUserFromOrganizationMember: FunctionComponent<
             />
           ) : (
             <Table
+              size="small"
               loading={isLoadingEventUsers || isLoadingOrgMembers}
               dataSource={orgMembers}
               columns={columns}
