038aaf465e32086af44641939283e12fd124d907
Mauricio J Carrillo <mauricio.jcarrillo@gmail.com>
2023-06-29 12:56:49 -0500

    HOT FIX: Delete the 'Eliminar respuestas' button in prod

--- src/components/agenda/components/ActivitiesList.tsx
+++ src/components/agenda/components/ActivitiesList.tsx
@@ -135,14 +135,14 @@ const ActivitiesList: FunctionComponent<ActivitiesListProps> = (props) => {
                 (agenda.type?.name as any) ||
                   !(import.meta.env.MODE || '').includes('staging'),
               ) ? (
-                <></>
-              ) : (
                 <ButtonToDeleteSurveyAnswers
                   userId={currentUser.value._id}
                   eventId={eventId}
                   activityId={agenda._id!}
                   onDelete={() => setAnswersIsDeleted(true)}
                 />
+              ) : (
+                <></>
               ),
           ],
           ItemWrapper: ({ children }) => (
