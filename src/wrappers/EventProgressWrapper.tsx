83909941aa003e10ecf574584ddc5c5be440f6e6
davib <davidborja456@gmail.com>
2023-07-25 14:17:17 -0500

    change: removed not used functions for event progress wrapper

--- src/wrappers/EventProgressWrapper.tsx
+++ src/wrappers/EventProgressWrapper.tsx
@@ -1,6 +1,6 @@
 import { ExtendedAgendaType } from '@Utilities/types/AgendaType'
-import { FB } from '@helpers/firestore-request'
-import { AgendaApi } from '@helpers/request'
+// import { FB } from '@helpers/firestore-request'
+// import { AgendaApi } from '@helpers/request'
 import { useEffect } from 'react'
 import { useState } from 'react'
 import { FunctionComponent } from 'react'
@@ -34,60 +34,10 @@ const EventProgressWrapper: FunctionComponent<IEventProgressWrapperProps> = (pro
   const [activities, setActivities] = useState<ExtendedAgendaType[]>([])
   const [checkedInActivities, setCheckedInActivities] = useState<any[]>([])
 
-  const updateActivities = async () => {
-    // Request for all the event activities
-    const { data }: { data: ExtendedAgendaType[] } = await AgendaApi.byEvent(event._id)
-    console.log(`Update activities. Got ${data.length} activities`)
-
-    // TODO: This filter by whether the activity is info, should be after
-    const filteredData = data.filter((activity) => !activity.is_info_only)
-    setActivities(filteredData)
-
-    return filteredData
-  }
-
-  const updateAttendees = async (theseActivities?: ExtendedAgendaType[]) => {
-    const filteredData = theseActivities || activities
-    console.log(`Update attendees for ${filteredData.length} activities`)
-
-    // Request for the attendee data in Firebase for all the activities
-    const allAttendees = await FB.Attendees.getEventUserActivities(
-      filteredData.map((activity) => activity._id as string),
-      eventUser._id,
-      true,
-    )
-
-    const checkedInOnes = allAttendees.filter((attendee) => {
-      // NOTE: Can be implemented of checking of .checkIn, but for now, we
-      // only check if exists
-      return attendee !== undefined
-    })
-
-    setCheckedInActivities(checkedInOnes)
-    console.log(`Got ${checkedInOnes.length} attendees`)
-  }
-
-  const reload = async (forceRefresh?: boolean) => {
-    const _forceRefresh = typeof forceRefresh === 'undefined' ? true : forceRefresh
-
+  const reload = async () => {
     const { activities, checked_in_activities } = eventUser.activity_progresses ?? {}
     setActivities(activities || [])
     setCheckedInActivities(checked_in_activities || [])
-    return
-
-    if (
-      !_forceRefresh &&
-      (Array.isArray(eventUser.activity_progresses?.activities) ||
-        Array.isArray(eventUser.activity_progresses?.checked_in_activities))
-    ) {
-      const { activities, checked_in_activities } = eventUser.activity_progresses
-      setActivities(activities)
-      setCheckedInActivities(checked_in_activities)
-      return
-    }
-
-    // const activities = await updateActivities()
-    // await updateAttendees(activities)
   }
 
   useEffect(() => {
@@ -95,7 +45,7 @@ const EventProgressWrapper: FunctionComponent<IEventProgressWrapperProps> = (pro
     if (!eventUser) return
 
     setIsLoading(true)
-    reload(false).finally(() => setIsLoading(false))
+    reload().finally(() => setIsLoading(false))
   }, [event, eventUser])
 
   return render({
