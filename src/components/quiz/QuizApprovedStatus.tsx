07d0eb3723db101e0b65a17be56db8948c2fe9e3
davib <davidborja456@gmail.com>
2023-07-25 19:51:58 -0500

    change: filter the surveys by activities of type quizing

--- src/components/quiz/QuizApprovedStatus.tsx
+++ src/components/quiz/QuizApprovedStatus.tsx
@@ -3,11 +3,14 @@ import { Link } from 'react-router-dom'
 
 import { Badge, Button } from 'antd'
 import { DownloadOutlined } from '@ant-design/icons'
-import { SurveysApi } from '@helpers/request'
+import { AgendaApi, SurveysApi } from '@helpers/request'
 import { SurveyData } from '@components/events/surveys/types'
 
 import { useCurrentUser } from '@context/userContext'
 import useAsyncPrepareQuizStats from './useAsyncPrepareQuizStats'
+import { ExtendedAgendaType } from '@Utilities/types/AgendaType'
+import { activityContentValues } from '@context/activityType/constants/ui'
+import { useEventProgress } from '@context/eventProgressContext'
 
 export interface QuizApprovedStatusProps {
   eventId: string
@@ -21,14 +24,42 @@ function QuizApprovedStatus(props: QuizApprovedStatusProps) {
   const [backgroundColor, setBackgroundColor] = useState('#9C835F')
   const [isApproved, setIsApproved] = useState(false)
 
+  const [passedOnes, setPassedOnes] = useState(0)
+  const [totalOnes, setTotalOnes] = useState(0)
+
   const cUser = useCurrentUser()
+  const cEventProgress = useEventProgress()
 
   // NOTE: if you want to add colors, create a state and check the logic that says if the quiz was passed or not completed.
 
   useEffect(() => {
     if (!cUser?.value?._id) return
     ;(async () => {
-      const surveys: SurveyData[] = await SurveysApi.byEvent(props.eventId)
+      const _surveys: SurveyData[] = await SurveysApi.byEvent(props.eventId)
+
+      const { data: activities }: { data: ExtendedAgendaType[] } =
+        await AgendaApi.byEvent(props.eventId)
+
+      const quizingActivities = activities
+        .filter((activity) =>
+          [activityContentValues.quizing].includes(activity.type?.name as any),
+        )
+        .filter(
+          (activity) => !cEventProgress.nonPublishedActivityIDs.includes(activity._id!),
+        )
+
+      const surveys = _surveys.filter((survey) => {
+        const matchedActivity = quizingActivities.find(
+          (activity) => survey.activity_id && survey.activity_id === activity._id,
+        )
+
+        if (!matchedActivity) return false
+
+        return (
+          !cEventProgress.nonPublishedActivityIDs.includes(matchedActivity._id!) &&
+          [activityContentValues.quizing].includes(matchedActivity.type?.name as any)
+        )
+      })
 
       let passed = 0
       let notPassed = 0
@@ -50,6 +81,9 @@ function QuizApprovedStatus(props: QuizApprovedStatusProps) {
           }
         }
       }
+
+      setPassedOnes(passed)
+      setTotalOnes(surveys.length)
 
       if (surveys.length > 0) {
         props.thereAreExam && props.thereAreExam(true)
@@ -73,7 +107,13 @@ function QuizApprovedStatus(props: QuizApprovedStatusProps) {
 
   return (
     <>
-      {isLoaded && <Badge count={status} style={{ backgroundColor }} />}
+      {isLoaded && (
+        <Badge
+          count={status}
+          style={{ backgroundColor }}
+          title={`${passedOnes} de ${totalOnes}`}
+        />
+      )}
       {isLoaded && isApproved && props.approvedLink && (
         <Link to={props.approvedLink}>
           <Button
