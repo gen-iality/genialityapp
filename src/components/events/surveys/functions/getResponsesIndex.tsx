b9b0c591c6e1ebc38f566b8b997efe75d74b15a3
davib <davidborja456@gmail.com>
2023-08-02 11:34:35 -0500

    change: rename getResponseIndex and add types

--- src/components/events/surveys/functions/getResponsesIndex.tsx
+++ src/components/events/surveys/functions/getResponsesIndex.tsx
@@ -1,25 +1,25 @@
-function singleAnswerType(question) {
+function singleAnswerType(question: any) {
   const optionIndex = (question.choices ?? []).findIndex(
-    (item) =>
+    (item: any) =>
       item.propertyHash.value === question.value || item.itemValue === question.value,
   )
   return optionIndex
 }
 
-function multipleAnswerType(question) {
-  let optionIndex = []
-  question.value.forEach((value) => {
+function multipleAnswerType(question: any) {
+  let optionIndex: any[] = []
+  question.value.forEach((value: any) => {
     optionIndex = [
       ...optionIndex,
       (question.choices ?? []).findIndex(
-        (item) => item.propertyHash.value === value || item.itemValue === value,
+        (item: any) => item.propertyHash.value === value || item.itemValue === value,
       ),
     ]
   })
   return optionIndex
 }
 
-function getResponsesIndex(question) {
+function getResponsesIndex(question: any) {
   if (typeof question.value === 'object') {
     // Busca el index de la opcion escogida
     return multipleAnswerType(question)
