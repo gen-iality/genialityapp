bcdc97f78ce3b7376c3a9de2e889637c1ab49a69
davib <davidborja456@gmail.com>
2023-08-15 09:34:07 -0500

    fix: when equal questions they got be overwritten

--- src/components/trivia/TriviaAnswerMatrixPage/hooks/useExportAsXLSX.ts
+++ src/components/trivia/TriviaAnswerMatrixPage/hooks/useExportAsXLSX.ts
@@ -2,34 +2,46 @@ import { utils, writeFileXLSX } from 'xlsx'
 
 type SurveyType = any
 type QuestionMap<QuestionId extends string> = { [key in QuestionId]: string }
+type QuestionLike = { id: string; title: string; [key: string]: any }
 
 export default function useExportAsXLSX(
   dataSource: any[],
   survey: SurveyType,
-  questions: any[],
+  questions: QuestionLike[],
 ) {
+  const questionInfoIdList = questions.map((question) => [question.id, question.title])
+
   // In this map we have { [key as QuestionId]: QuestionTitle }
   const questionMap: QuestionMap<string> = {
-    ...Object.fromEntries(questions.map((question) => [question.id, question.title])),
+    ...Object.fromEntries(questionInfoIdList),
   }
   return () => {
-    const ws = utils.json_to_sheet(
-      dataSource.map((data) => {
-        const entries = Object.entries(data) // [key, value][]
-        const newEntries = entries.map((entry) => {
-          const [key, value] = entry
+    const rows: string[][] = []
 
-          if (key in questionMap) {
-            return [questionMap[key], value]
-          }
-
-          // Some keys are taken as they are
-          return [key, value]
-        })
-
-        return Object.fromEntries(newEntries)
-      }),
+    // The right order of the question IDs
+    const orderedQuestionId = questionInfoIdList.map(
+      (questionInfoId) => questionInfoId[0],
     )
+    // Add the header
+    const headerTableAsArray = questionInfoIdList.map(
+      (questionInfoId) => questionInfoId[1],
+    )
+
+    // Add the body
+    const bodyTableAsArray = dataSource.map((data) => {
+      const normalValues = Object.entries(data)
+        .filter(([key, value]) => !orderedQuestionId.includes(key))
+        .map(([key, value]) => value)
+      return [...normalValues, ...orderedQuestionId.map((questionId) => data[questionId])]
+    })
+
+    // This lines has burnt header: "names"
+    const tableAsArray = [
+      ['Nombre de usuario', ...headerTableAsArray],
+      ...bodyTableAsArray,
+    ]
+
+    const ws = utils.aoa_to_sheet(tableAsArray)
     const wb = utils.book_new()
     utils.book_append_sheet(wb, ws, 'Evaluaciones')
     writeFileXLSX(wb, `survey_${survey?.survey ?? 'cuestionario'}.xls`)
