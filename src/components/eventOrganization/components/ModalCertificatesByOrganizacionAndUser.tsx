a135f776471d0d4aa754c5a086141060b0d33eb3
lortiz24 <luis.ortiz@evius.co>
2023-08-15 16:50:41 -0500

    Se optimiza flujo de obtencion de certificados y se cambia a agrupados por eventos

--- src/components/eventOrganization/components/ModalCertificatesByOrganizacionAndUser.tsx
+++ src/components/eventOrganization/components/ModalCertificatesByOrganizacionAndUser.tsx
@@ -1,4 +1,4 @@
-import { Button, Drawer, DrawerProps, Space, Tooltip, Typography } from 'antd';
+import { Button, Collapse, Drawer, DrawerProps, Space, Tooltip, Typography } from 'antd';
 import { useGetCertificatesByEvents } from '../hooks/useGetCertificatesByEvents';
 import CertificatesByEventsAndUserList from './CertificatesByEventsAndUserList';
 import { isMobile } from 'react-device-detect';
@@ -11,12 +11,18 @@ interface Props extends DrawerProps {
   onCloseDrawer: () => void;
 }
 
-export const ModalCertificatesByOrganizacionAndUser = ({ eventUserId, organizationId, onCloseDrawer, ...modalProps }: Props) => {
+export const ModalCertificatesByOrganizacionAndUser = ({
+  eventUserId,
+  organizationId,
+  onCloseDrawer,
+  ...modalProps
+}: Props) => {
   const { certificatesByEvents, eventsWithEventUser, eventUsers, isLoading } = useGetCertificatesByEvents(
     organizationId,
     eventUserId
   );
   //toDo: Validar el mobile para el width del drawer
+
   return (
     <Drawer
       title={
@@ -34,22 +40,29 @@ export const ModalCertificatesByOrganizacionAndUser = ({ eventUserId, organizati
       headerStyle={{ border: 'none', padding: 10 }}
       extra={
         <Tooltip placement='bottomLeft' title='Cerrar'>
-          <Button
-            icon={<CloseOutlined style={{ fontSize: 20 }} />}
-             onClick={onCloseDrawer} type='text'
-          />
+          <Button icon={<CloseOutlined style={{ fontSize: 20 }} />} onClick={onCloseDrawer} type='text' />
         </Tooltip>
       }
       {...modalProps}>
-      <CertificatesByEventsAndUserList
-        eventUsers={eventUsers}
-        itemLayout='vertical'
-        eventsWithEventUser={eventsWithEventUser}
-        loading={isLoading}
-        dataSource={certificatesByEvents}
-        style={{ overflowY: 'auto', height: '90%', padding: 0 }}
-        className='desplazar'
-      />
+      {isLoading && <>Cargando...</>}
+      {!isLoading && (
+        <Collapse defaultActiveKey={['0']}>
+          {certificatesByEvents.map((certificateByEvent, index) => (
+            <Collapse.Panel header={certificateByEvent?.event?.name ?? 'Evento sin nombre'} key={index}>
+              <CertificatesByEventsAndUserList
+                eventUser={certificateByEvent.eventUser}
+                itemLayout='vertical'
+                eventsWithEventUser={eventsWithEventUser}
+                loading={isLoading}
+                dataSource={certificateByEvent.certificatesByEvents}
+                style={{ overflowY: 'auto', height: '90%', padding: 0 }}
+                className='desplazar'
+              />
+            </Collapse.Panel>
+          ))}
+        </Collapse>
+      )}
+      {/* */}
     </Drawer>
   );
 };
--- src/components/eventOrganization/hooks/useGetCertificatesByEvents.ts
+++ src/components/eventOrganization/hooks/useGetCertificatesByEvents.ts
@@ -3,7 +3,11 @@ import { Certificates } from '@/components/agenda/types'
 import { CertsApi } from '@/helpers/request';
 import { useGetEventsWithUser } from './useGetEventsWithUser';
 
-
+export interface CertificatesByEvent {
+  event:{ [key:string] :any };
+  certificatesByEvents: Certificates[],
+  eventUser: any
+}
 
 export const useGetCertificatesByEvents = (organizationId:string, eventUserId:string) => {
 
@@ -12,48 +16,67 @@ export const useGetCertificatesByEvents = (organizationId:string, eventUserId:st
     eventUserId,
     true
   );
-  const [certificatesByEvents, setCertificatesByEvents] = useState<Certificates[]>([])
+  const [certificatesByEvents, setCertificatesByEvents] = useState<CertificatesByEvent[]>([])
   const [isLoading, setIsLoading] = useState(true)
 
+  const getCertificateByEventId = async (event: { [key:string] :any }):Promise<CertificatesByEvent> => {
+    try {
+      let certs: Certificates[] = await CertsApi.byEvent(event._id);
+      /* console.log(eventUser.event_id,{
+        id:eventUser,
+        certs
+      } ) */
+      if(certs &&  certs.length > 0) {
+        const eventUserCurrent = eventUsers.find((eventUser)=>eventUser.event_id === event._id)
 
-  const getCertificateByEventId = async (eventUser: { [key:string] :any }) => {
-    let certs: Certificates[] = await CertsApi.byEvent(eventUser.event_id);
-   
-     if(certs &&  certs.length > 0) {
-      const newCertificates: Certificates[] = certs.filter( cert=>{
+        const newCertificates: Certificates[] = certs.filter( cert=>{
+          if(!cert.userTypes || cert.userTypes.length === 0){
+            return true
+          }
+          return cert.userTypes?.includes(eventUserCurrent.properties.list_type_user)
+  
+        })
 
-        if(!cert.userTypes || cert.userTypes.length === 0){
-          return true
+        return {
+          event,
+          certificatesByEvents: newCertificates,
+          eventUser:eventUserCurrent
         }
-        return cert.userTypes?.includes(eventUser.properties.list_type_user)
-
-      })
-      return newCertificates
+      }
+      return {
+        event,
+        certificatesByEvents: [],
+        eventUser:undefined
+      }
+      } catch (error) {
+      return {
+        event,
+        certificatesByEvents: [],
+        eventUser:undefined
+      }
     }
-
-      return []
    
   };
 
   const getData= async ()=>{
-    const newCertificates:Promise<Certificates[]>[]=[]
+    const newCertificates:Promise<CertificatesByEvent>[]=[]
 
-    eventUsers.forEach( (eventUser, index) => {
-      newCertificates.push(getCertificateByEventId(eventUser));
+    eventsWithEventUser.forEach( (event) => {
+      newCertificates.push(getCertificateByEventId(event));
     });
     try {
       const certificatesByEvent = await Promise.all(newCertificates)
 
-      const resultado = ([] as Certificates[]).concat.apply([], certificatesByEvent);
-  
-      setCertificatesByEvents(resultado)
+      const newCertificatesByEvent = certificatesByEvent.filter(certificateByEvent=>certificateByEvent.certificatesByEvents.length > 0)
+
+      setCertificatesByEvents(newCertificatesByEvent)
     
       setIsLoading(false)
     } catch (error) {
 
       setIsLoading(false)
       setCertificatesByEvents([])
-      
+
     }
    
   }
