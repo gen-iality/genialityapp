70d41a262322f2126be291dddaea37ee87fc42ce
davib <davidborja456@gmail.com>
2023-08-24 09:35:55 -0500

    add: TextQuestionDisplayer component

---
+++ src/components/platform/SurveyDisplayer/components/TextQuestionDisplayer.tsx
@@ -0,0 +1,48 @@
+import { FunctionComponent, useState } from 'react'
+import { IQuestionDisplayer } from '../types'
+import { Form, Input } from 'antd'
+
+interface ITextQuestionDisplayerProps extends IQuestionDisplayer {}
+
+const TextQuestionDisplayer: FunctionComponent<ITextQuestionDisplayerProps> = (props) => {
+  const { onAnswer, question } = props
+  const [isCorrect, setIsCorrect] = useState<boolean | undefined>()
+
+  const onChange = (text: string) => {
+    let correctAnswer: undefined | string
+
+    if (typeof question.correctAnswer === 'string') {
+      correctAnswer = question.correctAnswer
+    } else if (typeof question.correctAnswerIndex === 'number') {
+      correctAnswer = (question.choices as string[])[question.correctAnswerIndex]
+    } else {
+      console.error(`the question ${question} has no correct value`)
+    }
+
+    const _isCorrect = text === correctAnswer
+    setIsCorrect(_isCorrect)
+
+    if (typeof onAnswer === 'function') {
+      onAnswer(text, _isCorrect)
+    }
+  }
+
+  return (
+    <Form>
+      <Form.Item name="answer">
+        <Input.TextArea
+          rows={4}
+          placeholder="Escriba un texto"
+          onChange={(event) => {
+            onChange(event.target.value)
+          }}
+          style={{
+            borderColor: isCorrect === undefined ? '' : isCorrect ? '#71cf57' : '#db4264',
+          }}
+        />
+      </Form.Item>
+    </Form>
+  )
+}
+
+export default TextQuestionDisplayer
