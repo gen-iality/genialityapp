abde8b7138a7b02d338261d365d9f913e9ddf602
balderban <balderban@gmail.com>
2023-08-25 10:35:25 -0500

    cambiando mensaje de pago de curso

--- src/payments/OrganizationPaymentConfirmationModal.tsx
+++ src/payments/OrganizationPaymentConfirmationModal.tsx
@@ -27,9 +27,12 @@ const OrganizationPaymentConfirmationModal: FunctionComponent<
       onOk={() => dispatch({ type: 'DISPLAY_PAYMENT' })}
       onCancel={() => dispatch({ type: 'ABORT' })}
     >
-      <p>Para ingresar a este contenido debes tener una cuenta con un plan pago</p>
       <p>
-        El costo del plan es: <b>${money}</b>
+        Para acceder al contenido seleccionado debe suscribirse a ENDOCAMPUS ACE, esta
+        suscripción tiene un costo de 50.000 COP o 15 USD y con ella tiene derecho a
+        material académico desarrollado por la ACE (Congresos y simposios). Esta
+        suscripción tiene vigencia de 1 año a partir de la fecha en que se realice el
+        pago.
       </p>
     </Modal>
   )
