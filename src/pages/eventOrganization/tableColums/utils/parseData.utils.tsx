300f2cf9724cd9768858015ba6738666a50c492f
lortiz24 <luis.ortiz@evius.co>
2023-08-31 14:30:45 -0500

    Se crea hook para paginacion de componentes ant desing y se corrige bug de dates

--- src/pages/eventOrganization/tableColums/utils/parseData.utils.tsx
+++ src/pages/eventOrganization/tableColums/utils/parseData.utils.tsx
@@ -2,7 +2,7 @@ import { DataEvent } from '../interfaces/ExcelEvent.interface';
 
 export const parseDataToExcel = (events: any[]): DataEvent[] => {
   return events.map((event) => {
-    const startDate = event.dates[0]?.start ?? event.datetime_from;
+    const startDate = Array.isArray(event.dates) ? event.dates[0]?.start : event.datetime_from;
     return {
       _id: event._id,
       name: event.name,
