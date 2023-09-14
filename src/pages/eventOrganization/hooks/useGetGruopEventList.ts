e6c9c2fb49cb607c6bec34694caa3ef6c46a07df
lortiz24 <luis.ortiz@evius.co>
2023-09-14 08:04:27 -0500

    se quita data quemada de hook de grupos

--- src/pages/eventOrganization/hooks/useGetGruopEventList.ts
+++ src/pages/eventOrganization/hooks/useGetGruopEventList.ts
@@ -3,18 +3,6 @@ import { useEffect, useState } from 'react';
 import { GroupEvent } from '../interface/group.interfaces';
 import { GroupsApi } from '@/helpers/request';
 
-const data: GroupEvent[] = [
-  {
-    name: 'Finanzas',
-    organizationId: 'f1e4fe4fef',
-    _id: 'fd125e1f5e1f12e6f5e1f51efef',
-  },
-  {
-    name: 'Deportes',
-    organizationId: 'fefefe6+f264fe69',
-    _id: 'fd125e1f5e1f1efef',
-  },
-];
 
 export const useGetGruopEventList = (organizationId: string) => {
   const [groupEvent, setGroupEvent] = useState<GroupEvent[]>([]);
@@ -23,6 +11,7 @@ export const useGetGruopEventList = (organizationId: string) => {
   const fetchData = async () => {
     try {
       const response = await GroupsApi.getAll(organizationId);
+      console.log('response',response)
       setGroupEvent(response);
       setIsLoading(false);
     } catch (error) {
