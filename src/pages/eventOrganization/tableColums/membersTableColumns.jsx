38d8ebd73586891db7c5d275acd1b86c74082c1a
davib <davidborja456@gmail.com>
2023-11-03 04:40:24 -0500

    add: new columns to show more payment plan info

--- src/pages/eventOrganization/tableColums/membersTableColumns.jsx
+++ src/pages/eventOrganization/tableColums/membersTableColumns.jsx
@@ -25,6 +25,7 @@ import {
 
 /** Helpers and utils */
 import { membersGetColumnSearchProps } from '../searchFunctions/membersGetColumnSearchProps'
+import dayjs from 'dayjs'
 
 export const columns = (
   columnsData,
@@ -106,11 +107,66 @@ export const columns = (
     key: 'payment_plan',
     width: '140px',
     ellipsis: true,
-    sorter: (a, b) => (a.payment_plan?.price ?? 0) - (b.payment_plan?.price ?? 0),
-    ...membersGetColumnSearchProps('payment_plan'),
+    sorter: (a, b) =>
+      (a.payment_plan?.date_until ?? 0) - (b.payment_plan?.date_until ?? 0),
+    // ...membersGetColumnSearchProps('payment_plan'),
     render(payment_plan) {
-      // console.log('payment_plan', payment_plan)
-      return payment_plan?.price ? 'Premium' : 'Free'
+      if (!payment_plan.date_until) return 'Indefinido'
+      const date = dayjs(payment_plan.date_until)
+      if (date.isValid()) {
+        return 'Hasta: ' + date.format('YYYY/MM/DD')
+      }
+      return 'Fecha inválida'
+    },
+  }
+
+  const payment_plan_days_count = {
+    title: 'Días faltantes',
+    dataIndex: 'payment_plan',
+    key: 'payment_plan_days_count',
+    width: '140px',
+    ellipsis: true,
+    sorter: (a, b) =>
+      (a.payment_plan?.date_until ?? 0) - (b.payment_plan?.date_until ?? 0),
+    // ...membersGetColumnSearchProps('payment_plan'),
+    render(payment_plan) {
+      if (!payment_plan.date_until) return '~'
+      const date = dayjs(payment_plan.date_until)
+      if (date.isValid()) {
+        const days = date.diff(dayjs(Date.now()), 'days')
+        return days !== 1 ? `${days} días` : `${days} día`
+      }
+      return '*'
+    },
+  }
+
+  const payment_plan_price = {
+    title: 'Plan pagado',
+    dataIndex: 'payment_plan',
+    key: 'payment_plan_price',
+    width: '140px',
+    ellipsis: true,
+    sorter: (a, b) => (a.payment_plan?.price ?? 0) - (b.payment_plan?.price ?? 0),
+    // ...membersGetColumnSearchProps('payment_plan'),
+    render(payment_plan) {
+      if (!payment_plan.price) return 'Gratis'
+      if (payment_plan.price === 0) return 'Gratis'
+      return `$${payment_plan.price}`
+    },
+  }
+
+  const payment_plan_days = {
+    title: 'Días pagados',
+    dataIndex: 'payment_plan',
+    key: 'payment_plan_days',
+    width: '140px',
+    ellipsis: true,
+    sorter: (a, b) => (a.payment_plan?.days ?? 0) - (b.payment_plan?.days ?? 0),
+    // ...membersGetColumnSearchProps('payment_plan'),
+    render(payment_plan) {
+      if (!payment_plan) return 'Sin datos'
+      if (payment_plan.days > 365 * 10) return 'Siempre'
+      return payment_plan.days
     },
   }
 
@@ -200,7 +256,12 @@ export const columns = (
   useEffect(() => {
     const newColumns = [picture, ...dynamicColumns]
 
-    organization.access_settings?.type === 'payment' && newColumns.push(payment_plan)
+    if (organization.access_settings?.type === 'payment') {
+      newColumns.push(payment_plan)
+      newColumns.push(payment_plan_price)
+      newColumns.push(payment_plan_days)
+      newColumns.push(payment_plan_days_count)
+    }
     newColumns.push(created_at)
     newColumns.push(updated_at)
     newColumns.push(editOption)
