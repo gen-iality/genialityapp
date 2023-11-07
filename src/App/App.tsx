fe5cbf77266e046f0e5d31211ad3576985faecee
davib <davidborja456@gmail.com>
2023-11-07 15:17:39 -0500

    change: move some exporting stuffs to main.tsx

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,14 +1,11 @@
 import './App.less'
-import dayjs from 'dayjs'
 import { useCurrentUser } from './../context/userContext'
-import { firestore, app } from '@helpers/firebase'
 
 import ContentContainer from '@containers/content'
 
 import { PreloaderApp } from '@/PreloaderApp/PreloaderApp'
 import { Layout } from 'antd'
 import InternetConnectionAlert from '@components/InternetConnectionAlert/InternetConnectionAlert'
-import { FB } from '@helpers/firestore-request'
 
 const App = () => {
   const cUser = useCurrentUser()
@@ -23,12 +20,3 @@ const App = () => {
 }
 
 export default App
-
-// @ts-expect-error
-window.dayjs = dayjs
-// @ts-expect-error
-window.firestore = firestore
-// @ts-expect-error
-window.FB = FB
-// @ts-expect-error
-window.appFB = app
