3bfd91d33ff0814e8b4630ff7eab97b19b5ef37a
Marlon Carrillo <marloncb2012@gmail.com>
2023-11-23 15:27:46 -0500

    Se agregan colores a la parte visual del juego de quien quiere ser millonario

--- src/components/games/WhoWantsToBeAMillonaire/components/GameAnnoucement.tsx
+++ src/components/games/WhoWantsToBeAMillonaire/components/GameAnnoucement.tsx
@@ -6,92 +6,97 @@ import ProgressClockIcon from '@2fd/ant-design-icons/lib/ProgressClock';
 import ProgressStarIcon from '@2fd/ant-design-icons/lib/ProgressStar';
 import ExitRunIcon from '@2fd/ant-design-icons/lib/ExitRun';
 import PartyPopperIcon from '@2fd/ant-design-icons/lib/PartyPopper';
+import { getCorrectColor } from '@/helpers/utils';
 const { useBreakpoint } = Grid;
 
-const CASES_ANNOUNCEMENT = {
-  TIME_OVER: {
-    icon: <ProgressClockIcon />,
-    title: '¡Se agotó el tiempo!',
-    subTitle:
-      'Lo sentimos, pero no contestaste la pregunta antes de que se agotara el tiempo, en otra ocasión será, sin embargo, no olvides revisar el ranking para saber cuál es tu posición.',
-  },
-  WRONG_ANSWER: {
-    icon: <CloseCircleOutlined />,
-    title: '¡Respuesta Incorrecta!',
-    subTitle:
-      'Lo sentimos, pero no contestaste correctamente la pregunta, en otra ocasión será, sin embargo, no olvides revisar el ranking para saber cuál es tu posición.',
-  },
-  RETIRED: {
-    icon: <ExitRunIcon />,
-    title: '¡Te retiraste!',
-    subTitle:
-      'Hemos almacenado tu puntaje acumulado hasta la etapa anterior. No olvides revisar el ranking para saber cuál es tu posición.',
-  },
-  WIN: {
-    icon: <PartyPopperIcon />,
-    title: '¡Felicitaciones!',
-    subTitle:
-      'Lograste superar todas las etapas, obtuviste el máximo puntaje. No olvides revisar el ranking para saber cuál es tu posición.',
-  },
-};
+
 
 export default function GameAnnoucement() {
-  // lo iba crear para hacer la parte este de que mostrar que gano o perdio pero no me dio tiempo :(
-  const { onChangeStatusGame, statusGame, scoreUser } = useMillonaireLanding();
+	// lo iba crear para hacer la parte este de que mostrar que gano o perdio pero no me dio tiempo :(
+	const { onChangeStatusGame, statusGame, scoreUser, millonaire } = useMillonaireLanding();
 
-  const screens = useBreakpoint();
-  return (
-    <Row
-      align='middle'
-      justify='center'
-      style={{
-        height: '100%',
-      }}>
-      <Card
-        style={{
-          border: 'none',
-          backgroundColor: '#FFFFFFCC',
-          backdropFilter: 'blur(8px)',
-          width: screens.xs ? '95vw' : '45vw',
-        }}>
-        <Result
-          icon={CASES_ANNOUNCEMENT[statusGame as keyof typeof CASES_ANNOUNCEMENT].icon}
-          title={
-            <Typography.Title level={2}>
-              {CASES_ANNOUNCEMENT[statusGame as keyof typeof CASES_ANNOUNCEMENT].title}
-            </Typography.Title>
-          }
-          subTitle={
-            <>
-              <Typography.Paragraph>
-                {CASES_ANNOUNCEMENT[statusGame as keyof typeof CASES_ANNOUNCEMENT].subTitle}
-              </Typography.Paragraph>
+	const screens = useBreakpoint();
+	const backgroundMillonaire = millonaire.appearance.background_color || '#120754';
+	const primaryMillonaire = millonaire.appearance.primary_color || '#FFB500';
 
-              <Typography.Title level={4}>
-                <Space>
-                  Puntaje:
-                  <Tag
-                    icon={<ProgressStarIcon style={{ fontSize: '20px' }} />}
-                    color='#FFB500'
-                    style={{ fontSize: '20px', padding: '10px 10px' }}>
-                    {scoreUser.score}
-                  </Tag>
-                </Space>
-              </Typography.Title>
-            </>
-          }
-          extra={
-            <Space>
-              <Button size='large' type='primary' onClick={() => onChangeStatusGame('GAME_OVER')}>
-                Ir al Ranking
-              </Button>
-              <Button size='large' onClick={() => onChangeStatusGame('NOT_STARTED')}>
-                Ir al Menu
-              </Button>
-            </Space>
-          }
-        />
-      </Card>
-    </Row>
-  );
+  const CASES_ANNOUNCEMENT = {
+    TIME_OVER: {
+      icon: <ProgressClockIcon style={{color: getCorrectColor(primaryMillonaire)}} />,
+      title: '¡Se agotó el tiempo!',
+      subTitle:
+        'Lo sentimos, pero no contestaste la pregunta antes de que se agotara el tiempo, en otra ocasión será, sin embargo, no olvides revisar el ranking para saber cuál es tu posición.',
+    },
+    WRONG_ANSWER: {
+      icon: <CloseCircleOutlined style={{color: getCorrectColor(primaryMillonaire)}} />,
+      title: '¡Respuesta Incorrecta!',
+      subTitle:
+        'Lo sentimos, pero no contestaste correctamente la pregunta, en otra ocasión será, sin embargo, no olvides revisar el ranking para saber cuál es tu posición.',
+    },
+    RETIRED: {
+      icon: <ExitRunIcon style={{color: getCorrectColor(primaryMillonaire)}} />,
+      title: '¡Te retiraste!',
+      subTitle:
+        'Hemos almacenado tu puntaje acumulado hasta la etapa anterior. No olvides revisar el ranking para saber cuál es tu posición.',
+    },
+    WIN: {
+      icon: <PartyPopperIcon style={{color: getCorrectColor(primaryMillonaire)}} />,
+      title: '¡Felicitaciones!',
+      subTitle:
+        'Lograste superar todas las etapas, obtuviste el máximo puntaje. No olvides revisar el ranking para saber cuál es tu posición.',
+    },
+  };
+	return (
+		<Row
+			align='middle'
+			justify='center'
+			style={{
+				height: '100%',
+			}}>
+			<Card
+				style={{
+					border: 'none',
+					backgroundColor: primaryMillonaire,
+					backdropFilter: 'blur(8px)',
+					width: screens.xs ? '95vw' : '45vw',
+				}}>
+				<Result
+					icon={CASES_ANNOUNCEMENT[statusGame as keyof typeof CASES_ANNOUNCEMENT].icon}
+					title={
+						<Typography.Title style={{ color: getCorrectColor(primaryMillonaire) }} level={2}>
+							{CASES_ANNOUNCEMENT[statusGame as keyof typeof CASES_ANNOUNCEMENT].title}
+						</Typography.Title>
+					}
+					subTitle={
+						<>
+							<Typography.Paragraph style={{ color: getCorrectColor(primaryMillonaire) }}>
+								{CASES_ANNOUNCEMENT[statusGame as keyof typeof CASES_ANNOUNCEMENT].subTitle}
+							</Typography.Paragraph>
+
+							<Typography.Title level={4}>
+								<Space style={{ color: getCorrectColor(primaryMillonaire) }}>
+									Puntaje:
+									<Tag
+										icon={<ProgressStarIcon style={{ fontSize: '20px' }} />}
+										color={backgroundMillonaire}
+										style={{ fontSize: '20px', padding: '10px 10px', color: getCorrectColor(backgroundMillonaire) }}>
+										{scoreUser.score}
+									</Tag>
+								</Space>
+							</Typography.Title>
+						</>
+					}
+					extra={
+						<Space>
+							<Button style={{backgroundColor:backgroundMillonaire, color:getCorrectColor(backgroundMillonaire), border:'none'}} size='large' type='primary' onClick={() => onChangeStatusGame('GAME_OVER')}>
+								Ir al Ranking
+							</Button>
+							<Button style={{backgroundColor:backgroundMillonaire, color:getCorrectColor(backgroundMillonaire), border:'none'}} size='large' onClick={() => onChangeStatusGame('NOT_STARTED')}>
+								Ir al Menu
+							</Button>
+						</Space>
+					}
+				/>
+			</Card>
+		</Row>
+	);
 }
--- src/components/games/common/Ranking/RankingList.tsx
+++ src/components/games/common/Ranking/RankingList.tsx
@@ -3,40 +3,42 @@ import { UseEventContext } from '@/context/eventContext';
 import { ConfigProvider, Divider, Empty, List, Row, Typography } from 'antd';
 import { useState } from 'react';
 import { Score } from './types';
+import { getCorrectColor } from '@/helpers/utils';
 
 interface Props {
-  scores: Score[];
-  type: 'time' | 'points';
+	scores: Score[];
+	type: 'time' | 'points';
+	currentColor?: string;
 }
 
 export default function RankingList(props: Props) {
-  const { scores, type } = props;
-  const cEvent = UseEventContext();
-  const [loading, setLoading] = useState(false);
-  const styles = cEvent.value.styles;
+	const { scores, type, currentColor } = props;
+	const cEvent = UseEventContext();
+	const [loading, setLoading] = useState(false);
+	const styles = cEvent.value.styles;
 
-  return (
-    <div>
-      <Row justify='center'>
-        <Typography.Title
-          level={3}
-          style={{
-            color: `${styles && styles.textMenu}`,
-          }}>
-          Ranking
-        </Typography.Title>
-      </Row>
-      <div className='container-dinamic-ranking' style={{ marginTop: 16, height: 'auto', overflowY: 'auto' }}>
-        <ConfigProvider renderEmpty={() => <Empty description={'No hay participantes'} />}>
-          <List
-            className='demo-loadmore-list'
-            loading={loading}
-            itemLayout='horizontal'
-            dataSource={scores}
-            renderItem={(item, key) => <UsersCard type='ranking' item={item} isTime={type === 'time'} />}
-          />
-        </ConfigProvider>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<Row justify='center'>
+				<Typography.Title
+					level={3}
+					style={{
+						color: `${currentColor ? getCorrectColor(currentColor) : styles?.textMenu}`,
+					}}>
+					Ranking
+				</Typography.Title>
+			</Row>
+			<div className='container-dinamic-ranking' style={{ marginTop: 16, height: 'auto', overflowY: 'auto' }}>
+				<ConfigProvider renderEmpty={() => <Empty description={'No hay participantes'} />}>
+					<List
+						className='demo-loadmore-list'
+						loading={loading}
+						itemLayout='horizontal'
+						dataSource={scores}
+						renderItem={(item, key) => <UsersCard type='ranking' item={item} isTime={type === 'time'} />}
+					/>
+				</ConfigProvider>
+			</div>
+		</div>
+	);
 }
