177efc578eada2e3718533426a50584fa88785ee
Marlon Carrillo <marloncb2012@gmail.com>
2023-12-05 16:01:32 -0500

    estilos de tag para el estado y se agrega componente de ant para los valores

--- src/components/events/datos/types/conditional-form.types.ts
+++ src/components/events/datos/types/conditional-form.types.ts
@@ -1,16 +1,27 @@
 export interface IConditionalField {
-  _id?: string;
-  fieldToValidate: string;
-  value: string | boolean;
-  fields: string[];
-  state: any;
+	_id?: string;
+	fieldToValidate: string;
+	value: string | boolean;
+	fields: string[];
+	state: TValueState;
 }
 
 export interface IConditionalFieldTable extends IConditionalField {
-  fieldToValidateLabel?: string;
-  fieldLabels: string[];
+	fieldToValidateLabel?: string;
+	fieldLabels: string[];
 }
 
 export interface IConditionalFieldForm extends Pick<IConditionalField, 'fields' | 'fieldToValidate' | 'value'> {}
 
 export type TTypeFieldConditional = 'list' | 'boolean';
+
+export type TValueState = 'enabled' | 'disabled';
+export interface IConditionalStatus {
+	enabled: IState;
+	disabled: IState;
+}
+
+export interface IState {
+	label: string;
+	status: 'green' | 'red';
+}
