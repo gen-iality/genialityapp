202961c6f12eb2e94d7d08044a2b79e5f6b8ca7f
Marlon Carrillo <marloncb2012@gmail.com>
2023-12-11 16:04:05 -0500

    estilos basicos para el resul de dinamicas de comparte tu foto y buscando el elemento cuando estan cerradas

--- src/components/games/whereIs/components/DrawerWhereIs.tsx
+++ src/components/games/whereIs/components/DrawerWhereIs.tsx
@@ -9,79 +9,89 @@ import { UseUserEvent } from '@/context/eventUserContext';
 import { Player } from '../types';
 
 interface Props {
-  children: ReactNode;
+	children: ReactNode;
 }
 
 const { useBreakpoint } = Grid;
 
 export default function DrawerWhereIs(props: Props) {
-  const screens = useBreakpoint();
-  const cUser = UseUserEvent();
-  const [open, setOpen] = useState(false);
-  const { whereIs } = useWhereIs();
-  const { goTo, location, ListenerPlayer, getStatePlayerAndGameAfterRestore, whereIsGame } = useWhereIsInLanding();
-  const [playerRealTime, setPlayerRealTime] = useState<Player>();
-  console.log('whereIs', whereIs);
+	const screens = useBreakpoint();
+	const cUser = UseUserEvent();
+	const [open, setOpen] = useState(false);
+	const { whereIs } = useWhereIs();
+	const { goTo, location, ListenerPlayer, getStatePlayerAndGameAfterRestore, whereIsGame } = useWhereIsInLanding();
+	const [playerRealTime, setPlayerRealTime] = useState<Player>();
+	console.log('whereIs', whereIs);
 
-  const handleOpen = () => {
-    setOpen(true);
-    goTo('introduction');
-  };
+	const handleOpen = () => {
+		setOpen(true);
+		goTo('introduction');
+	};
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+	const handleClose = () => {
+		setOpen(false);
+	};
 
-  useEffect(() => {
-    const unsubscribe = ListenerPlayer(cUser.value._id, setPlayerRealTime);
-    return () => {
-      unsubscribe();
-    };
-  }, []);
+	useEffect(() => {
+		const unsubscribe = ListenerPlayer(cUser.value._id, setPlayerRealTime);
+		return () => {
+			unsubscribe();
+		};
+	}, []);
 
-  useEffect(() => {
-    if (playerRealTime === null && whereIsGame.isFinish) {
-      getStatePlayerAndGameAfterRestore();
-    }
-  }, [playerRealTime]);
+	useEffect(() => {
+		if (playerRealTime === null && whereIsGame.isFinish) {
+			getStatePlayerAndGameAfterRestore();
+		}
+	}, [playerRealTime]);
 
-  useEffect(() => {
-    if (whereIs && !whereIs?.published) {
-      setOpen(false);
-    }
-  }, [whereIs]);
+	useEffect(() => {
+		if (whereIs && !whereIs?.published) {
+			setOpen(false);
+		}
+	}, [whereIs]);
 
-  return (
-    <>
-      <Button
-        size='large'
-        type='primary'
-        onClick={handleOpen}
-        style={{ display: !!whereIs && whereIs?.published ? 'block' : 'none' }}>
-        Buscando el elemento
-      </Button>
-      <Drawer
-        title={
-          <Row align='middle' justify='space-between'>
-            <Timer />
-            {!screens.xs && location.activeView === 'game' ? <FooterWithHints /> : undefined}
-            <Lifes />
-          </Row>
-        }
-        visible={open}
-        bodyStyle={{
-          padding: location.activeView === 'game' ? '0px' : screens.xs ? '5px' : '24px',
-        }}
-        headerStyle={{
-          padding: 12,
-        }}
-        footer={screens.xs ? location.activeView === 'game' ? <FooterWithHints /> : undefined : undefined}
-        onClose={handleClose}
-        width='100vw'
-        destroyOnClose={true}>
-          {whereIs?.active ? props.children: (<>
-          <Result title={'No disponible para jugar aun'}/></>)}
-      </Drawer>
-    </>
-  );
+	return (
+		<>
+			<Button
+				size='large'
+				type='primary'
+				onClick={handleOpen}
+				style={{ display: !!whereIs && whereIs?.published ? 'block' : 'none' }}>
+				Buscando el elemento
+			</Button>
+			<Drawer
+				title={
+					<Row align='middle' justify='space-between'>
+						<Timer />
+						{!screens.xs && location.activeView === 'game' ? <FooterWithHints /> : undefined}
+						<Lifes />
+					</Row>
+				}
+				visible={open}
+				bodyStyle={{
+					padding: location.activeView === 'game' ? '0px' : screens.xs ? '5px' : '24px',
+				}}
+				headerStyle={{
+					padding: 12,
+				}}
+				footer={screens.xs ? location.activeView === 'game' ? <FooterWithHints /> : undefined : undefined}
+				onClose={handleClose}
+				width='100vw'
+				destroyOnClose={true}>
+				{whereIs?.active ? (
+					props.children
+				) : (
+					<>
+						<Result
+							title={'Acceso cerrado a la dinámica'}
+							subTitle={
+								'La búsqueda del elemento se encuentra cerrada actualmente, ya sea porque aún no ha comenzado o porque ha llegado a su fin.'
+							}
+						/>
+					</>
+				)}
+			</Drawer>
+		</>
+	);
 }
