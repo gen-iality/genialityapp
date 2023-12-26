b02cc29f4ac37d5f8b813ab96c2c69796f6c2813
Marlon Carrillo <marloncb2012@gmail.com>
2023-12-26 15:35:52 -0500

    correccion conflicto de estilos

--- src/components/policies/TermsAndConditions/TermsAndConditions.tsx
+++ src/components/policies/TermsAndConditions/TermsAndConditions.tsx
@@ -6,34 +6,41 @@ const { useBreakpoint } = Grid;
 
 const TermsAndConditions = () => {
 	const [targetOffset, setTargetOffset] = useState<number | undefined>(undefined);
-
-  useEffect(() => {
-    setTargetOffset(window.innerHeight / 3);
-  }, []);
 	const screens = useBreakpoint();
+
+	useEffect(() => {
+		setTargetOffset(window.innerHeight / 3);
+	}, []);
+
 	return (
-		<Row gutter={[0, 16]} >
-			<Col style={{ padding: screens.xs ? '30px' : '60px' }} xs={0} sm={0} md={6} lg={6} xl={6} xxl={6}>
-				<Anchor targetOffset={targetOffset}>
-					{termsAnchor.map((item) => (
-						<Anchor.Link key={`anchor-${item.anchor}`} href={`#${item.anchor}`} title={item.title} />
-					))}
-				</Anchor>
+		<Row gutter={[0, 16]}>
+			<Col xs={0} sm={0} md={6} lg={6} xl={6} xxl={6}>
+				<Row style={{ padding: '60px 30px' }}>
+					<Anchor targetOffset={targetOffset}>
+						{termsAnchor.map((item) => (
+							<Anchor.Link key={`anchor-${item.anchor}`} href={`#${item.anchor}`} title={item.title} />
+						))}
+					</Anchor>
+				</Row>
 			</Col>
-			<Col style={{ padding: screens.xs ? '30px' : '60px 80px 60px 80px' }} xs={24} sm={24} md={18} lg={18} xl={18} xxl={18}>
-				<Col span={24}>
-					<Typography.Title>{termsTitle}</Typography.Title>
-					<Typography.Paragraph>{termsParagraph}</Typography.Paragraph>
-				</Col>
-				<List
-					split={false}
-					dataSource={termsContent}
-					renderItem={(item) => (
-						<List.Item id={item.anchor}>
-							<List.Item.Meta title={item.title} description={item.content} />
-						</List.Item>
-					)}
-				/>
+			<Col xs={24} sm={24} md={18} lg={18} xl={18} xxl={18}>
+				<Row style={{ padding: screens.xs ? '30px' : '60px 80px 60px 80px' }}>
+					<Col span={24}>
+						<Typography.Title>{termsTitle}</Typography.Title>
+						<Typography.Paragraph>{termsParagraph}</Typography.Paragraph>
+					</Col>
+					<Col span={24}>
+						<List
+							split={false}
+							dataSource={termsContent}
+							renderItem={(item) => (
+								<List.Item id={item.anchor}>
+									<List.Item.Meta title={item.title} description={item.content} />
+								</List.Item>
+							)}
+						/>
+					</Col>
+				</Row>
 			</Col>
 		</Row>
 	);
